04266b81ad90480653110d4df32a85bd1802f5f3
llj <lingjun.li1@gmail.com>
2012-03-27 17:42:08 +0800

    improved delete comfirmation

---
+++ media/js/utils.js
@@ -0,0 +1,13 @@
+//add delete confirm dialog
+var Del_url = '';
+function addConfirmTo(ele) {
+    ele.each(function() {
+        $(this).click(function() {
+            $('#dialog-delete-confirm').modal({appendTo:'#main'});
+            Del_url = $(this).attr('data');
+        });
+    });
+}
+$('#yes-btn').click(function() {
+    location.href = Del_url;
+});
