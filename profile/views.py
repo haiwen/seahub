cfd96c763de1083a104ac6146b3be43b98fd5813
xiez <xiez1989@gmail.com>
2012-04-18 21:53:37 +0800

    Modify profile page, and remove ccnet conf related fucntion

--- profile/views.py
+++ profile/views.py
@@ -13,35 +13,6 @@ from models import UserCcnetConf
 from seaserv import ccnet_rpc, translate_time_usec, get_binding_userids
 
 @login_required
-def show_profile(request):
-    userid_list = get_binding_userids(request.user.username)
-
-    profile_dict = {}
-    
-    for user_id in userid_list:
-        try:
-            profile_timestamp = ccnet_rpc.get_user_profile_timestamp(user_id)
-            profile_timestamp = translate_time_usec(profile_timestamp)
-        except:
-            profile_timestamp = None
-
-        try:
-            peernames = ccnet_rpc.get_peernames_by_userid(user_id)
-            for peername in peernames.split('\n'):
-                if not peername:
-                    continue
-                profile_dict[peername] = profile_timestamp
-        except:
-            pass
-        
-    return render_to_response('profile/profile.html', {
-                                'userid_list': userid_list,
-                                'profile_dict': profile_dict,
-                                },
-                              context_instance=RequestContext(request))
-
-
-@login_required
 def get_ccnet_profile(request):
     try:
         ccnet_conf = UserCcnetConf.objects.get(user=request.user)
