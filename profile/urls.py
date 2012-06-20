5f021a5624bd5ebcf552b2e6089a8ddfeafcf475
llj <lingjun.li1@gmail.com>
2012-06-20 19:14:18 +0800

    add 'user-profile'

--- profile/urls.py
+++ profile/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('profile.views',
     url(r'^list_user/$', 'list_userids', name="list_userids"),
     url(r'^$', 'edit_profile', name="edit_profile"),
+    url(r'^user/$', 'user_profile', name="user_profile"),
     url(r'^logout/$', 'logout_relay', name="logout_relay"),
 )
--- profile/views.py
+++ profile/views.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
@@ -56,5 +57,37 @@ def edit_profile(request):
     return render_to_response('profile/set_profile.html', {
                                 'nickname':profile.nickname,
                                 'intro':profile.intro,
+                                  },
+                              context_instance=RequestContext(request))
+
+def user_profile(request):
+    user = request.GET.get('user', '')
+    
+    user_nickname = ''
+    user_intro = ''
+    err_msg = ''
+
+    if user:
+        try:
+            user_check = ccnet_rpc.get_emailuser(user)
+        except:
+            user_check = None
+        
+        if user_check:
+            profile = Profile.objects.filter(user=user)
+            if profile:
+                profile = profile[0]
+                user_nickname = profile.nickname
+                user_intro = profile.intro
+        else:
+            err_msg = '该用户不存在'
+    else:
+        err_msg = '该用户不存在'
+    
+    return render_to_response('profile/user_profile.html', {
+                                'email': user,
+                                'nickname':user_nickname,
+                                'intro':user_intro,
+                                'err_msg':err_msg,
                                   },
                               context_instance=RequestContext(request))
