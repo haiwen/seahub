fec1ea716fefba8087493935d52d1dc3484ef523
xiez <xiez1989@gmail.com>
2012-07-05 21:40:42 +0800

    Modify po

--- po.py
+++ po.py
@@ -6,6 +6,9 @@ TRANSLATION_MAP = {
     'Deleted' : u'删除了',
     'Modified' : u'修改了',
     'Renamed' : u'重命名或移动了',
+    'Moved' : u'移动了',
     'Added directory' : u'新建了目录',
-    'Removed directory' : u'删除了目录',    
+    'Removed directory' : u'删除了目录',
+    'Renamed directory' : u'重命名了目录',
+    'Moved directory' : u'移动了目录',
 }
