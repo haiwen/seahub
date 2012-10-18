c6cbb8fc676ff848b104e6f81451b3776b796646
zhengxie <xiez1989@gmail.com>
2012-10-18 20:20:21 +0800

    Make url match more precise

--- share/urls.py
+++ share/urls.py
@@ -4,6 +4,6 @@ from views import *
 
 urlpatterns = patterns('',
     url(r'^add/$', share_repo, name='share_repo'),
-    url('^remove/(?P<token>.+)/$', remove_anonymous_share, name='remove_anonymous_share'),
-    url('^(?P<token>.+)/$', anonymous_share_confirm, name='anonymous_share_confirm'),
+    url('^remove/(?P<token>[^/]{24,24})/$', remove_anonymous_share, name='remove_anonymous_share'),
+    url('^(?P<token>[^/]{24,24})/$', anonymous_share_confirm, name='anonymous_share_confirm'),
 )
