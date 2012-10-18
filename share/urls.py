fc2011c856cc96a41b8b5bff4f824ea5acc8db3c
llj <lingjun.li1@gmail.com>
2012-10-18 21:28:15 +0800

    [shareadmin]added permission-set

--- share/urls.py
+++ share/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^add/$', share_repo, name='share_repo'),
     url('^remove/(?P<token>[^/]{24,24})/$', remove_anonymous_share, name='remove_anonymous_share'),
     url('^(?P<token>[^/]{24,24})/$', anonymous_share_confirm, name='anonymous_share_confirm'),
+    url(r'^permission_admin/$', share_permission_admin, name='share_permission_admin'),
 )
