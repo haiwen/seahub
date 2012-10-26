2c18ff7b1706c99757f633883f9e98d92f6bc68d
zhengxie <xiez1989@gmail.com>
2012-10-26 19:54:45 +0800

    Changed repo to library

--- po.js
+++ po.js
@@ -1,4 +1,16 @@
 var msg = '';
 
+// registration_form.html
+msg = gettext('Email can\'t be blank');
+msg = gettext('Password can\'t be blank');
+msg = gettext('Confirm password can\'t be blank');
+msg = gettext('The two password fields did\'t match');
+
+// login.html
+msg = gettext('Email can\'t be blank');
+msg = gettext('Password can\'t be blank');
+msg = gettext('Incorrect email or password');
+
+
 
 
