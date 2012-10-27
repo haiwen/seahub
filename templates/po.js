6ce8589a1b5d624c58cff50ce5a980cee287417a
zhengxie <xiez1989@gmail.com>
2012-10-27 20:32:45 +0800

    i18n for repo history and snapshot

--- templates/po.js
+++ templates/po.js
@@ -24,3 +24,6 @@ msg = gettext('File is not Synchronized with server, please retry later.');
 msg = gettext('An error occur during opening local file.');
 msg = gettext('Seafile client is not start or out of date');
 msg = gettext('Your Seafile client is out of date, please <a href="http://www.seafile.com/download/">upgrade</a> to latest.');
+
+// repo_view_snapshot.html
+msg = gettext('Confirm to restore this library?')
