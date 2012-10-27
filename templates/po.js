a90d4e989d3cc3bfded94071a5e650fb368a064d
zhengxie <xiez1989@gmail.com>
2012-10-27 19:41:00 +0800

    i18n for repo_view_file page

--- templates/po.js
+++ templates/po.js
@@ -14,3 +14,13 @@ msg = gettext('Incorrect email or password');
 // myhome_extra_script.html
 msg = gettext('Input can\'t be empty');
 msg = gettext('Confirm to remove shared library?');
+
+// repo_view_file.html
+msg = gettext(' <button id="comment">Comment</button>');
+msg = gettext('Please type a message');
+msg = gettext('Can\'t find a application to open file. Seafile will open the folder.');
+msg = gettext('File is not Synchronized with server, please synchronize manually.');
+msg = gettext('File is not Synchronized with server, please retry later.');
+msg = gettext('An error occur during opening local file.');
+msg = gettext('Seafile client is not start or out of date');
+msg = gettext('Your Seafile client is out of date, please <a href="http://www.seafile.com/download/">upgrade</a> to latest.');
