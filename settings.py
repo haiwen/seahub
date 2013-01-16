387884b933e7c3f2401035e691fc9e46b7b50a9a
Phillip Thelen <thelen@mpib-berlin.mpg.de>
2013-01-16 18:14:50 +0100

    [new file] added file for german translation

--- settings.py
+++ settings.py
@@ -99,6 +99,7 @@ LANGUAGES = (
     ('en', gettext_noop('English')),
     ('zh-cn', gettext_noop(u'简体中文')),
     ('ru', gettext_noop(u'Русский')),    
+    ('de', gettext_noop(u'Deutsch')),
 )
 LOCALE_PATHS = (
     os.path.join(os.path.dirname(__file__), 'locale'),
