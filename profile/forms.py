41dca2eccb8b97797bfd05d3a7e25ef3e5354f06
zhengxie <xiez1989@gmail.com>
2013-01-17 11:01:00 +0800

    Translate nickname error message

--- profile/forms.py
+++ profile/forms.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from seahub.utils import validate_group_name
 
@@ -12,4 +13,4 @@ class ProfileForm(forms.Form):
         if validate_group_name(nickname):
             return nickname
         else:
-            raise forms.ValidationError(u'昵称只能包含中英文字符、数字及下划线')
+            raise forms.ValidationError(_(u'Nickname can only contain characters, numbers or underscore.'))
