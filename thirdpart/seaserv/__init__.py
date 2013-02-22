7f887be89f637da80c90f417d4e06945e99896cb
zhengxie <xiez1989@gmail.com>
2013-02-22 14:52:33 +0800

    Added repos/groups/users count

--- thirdpart/seaserv/__init__.py
+++ thirdpart/seaserv/__init__.py
@@ -3,7 +3,7 @@ import service
 from service import ccnet_rpc, monitor_rpc, seafserv_rpc, \
     seafserv_threaded_rpc, ccnet_threaded_rpc
 from service import send_command, check_quota, web_get_access_token, unset_repo_passwd
-from service import get_emailusers, get_session_info
+from service import get_emailusers, count_emailusers, get_session_info
 from service import get_org_groups, get_personal_groups_by_user, \
     get_group_repoids, get_personal_groups, \
     check_group_staff, remove_group_user, get_group, get_org_id_by_group, \
@@ -16,7 +16,7 @@ from service import get_repos, get_repo, get_commits, get_branches, remove_repo,
     list_personal_shared_repos, is_personal_repo, list_inner_pub_repos, \
     is_org_repo_owner, get_org_repo_owner, is_org_repo, get_file_size,\
     list_personal_repos_by_owner, get_repo_token_nonnull, get_repo_owner, \
-    server_repo_size, get_file_id_by_path, get_commit, \
+    server_repo_size, get_file_id_by_path, get_commit, count_inner_pub_repos,\
     get_repo_history_limit, set_repo_history_limit
 
 from service import get_binding_peerids, is_valid_filename, check_permission,\
