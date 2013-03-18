330c00a3acd58d133b5ee3b65cf79d60f67a3a94
zhengxie <xiez1989@gmail.com>
2013-03-18 10:28:33 +0800

    Remove nickname checking, issue#20

--- profile/forms.py
+++ profile/forms.py
@@ -2,15 +2,7 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
-from seahub.utils import validate_group_name
-
 class ProfileForm(forms.Form):
     nickname = forms.CharField(max_length=64, required=False)
     intro = forms.CharField(max_length=256, required=False)
 
-    def clean_nickname(self):
-        nickname = self.cleaned_data['nickname']
-        if validate_group_name(nickname):
-            return nickname
-        else:
-            raise forms.ValidationError(_(u'Nickname can only contain characters, numbers or underscore.'))
