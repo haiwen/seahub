213708504447c8858ed3ba86324acaff98dbefdf
zhengxie <xiez1989@gmail.com>
2013-05-14 11:45:10 +0800

    Update email_id cache when user has registered

--- seahub/profile/models.py
+++ seahub/profile/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.core.cache import cache
+from django.dispatch import receiver
+
+from settings import EMAIL_ID_CACHE_PREFIX, EMAIL_ID_CACHE_TIMEOUT
+from registration.signals import user_registered
 
 class Profile(models.Model):
     user = models.EmailField(unique=True)
     nickname = models.CharField(max_length=64, blank=True)
     intro = models.TextField(max_length=256, blank=True)
 
+
+@receiver(user_registered)
+def clean_email_id_cache(sender, **kwargs):
+    user = kwargs['user']
+    cache.set(EMAIL_ID_CACHE_PREFIX+user.email, user.id, EMAIL_ID_CACHE_TIMEOUT)
