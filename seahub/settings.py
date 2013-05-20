5281beb011af7a118e1e1fd2f88964afa4f9be42
zhengxie <xiez1989@gmail.com>
2013-05-20 10:33:39 +0800

    Added allowed_hosts to settings

--- seahub/settings.py
+++ seahub/settings.py
@@ -277,6 +277,10 @@ SERVE_STATIC = True
 # Enalbe or disalbe registration on web.
 ENABLE_SIGNUP = False
 
+# For security consideration, please set to match the host/domain of your site, e.g., ALLOWED_HOSTS = ['.example.com'].
+# Please refer https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts for details.
+ALLOWED_HOSTS = ['*']
+
 # Logging
 LOGGING = {
     'version': 1,
