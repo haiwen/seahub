08f7063e3261eae113cc2e751aa3dd2dc23495ef
llj <lingjun.li1@gmail.com>
2013-07-29 20:25:24 +0800

    [file upload] improved ui/interaction, rm unused files

--- seahub/base/templatetags/upload_tags.py
+++ seahub/base/templatetags/upload_tags.py
@@ -9,50 +9,48 @@ def upload_js():
 <script id="template-upload" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-upload fade">
-        <td class="name"><span>{%=file.name%}</span></td>
-        <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
         {% if (file.error) { %}
-            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+            <td class="name">{%=file.name%}</td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+            <td class="error"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
         {% } else if (o.files.valid && !i) { %}
-            <td>
+            <td class="name">
+                {%=file.name%}
                 <div class="progress progress-success progress-striped active"><div class="bar" style="width:0%;"></div></div>
             </td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
             <td class="start">{% if (!o.options.autoUpload) { %}
                 <button class="btn btn-success">
-                    <i class="icon-upload icon-white"></i>
+                    <span class="icon-upload"></span>
                     <span>{%=locale.fileupload.start%}</span>
                 </button>
             {% } %}</td>
         {% } else { %}
-            <td colspan="2"></td>
+            <td class="name">{%=file.name%}</td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+            <td></td>
         {% } %}
         <td class="cancel">{% if (!i) { %}
             <button class="btn btn-warning">
-                <i class="icon-ban-circle icon-white"></i>
+                <span class="icon-ban-circle"></span>
                 <span>{%=locale.fileupload.cancel%}</span>
             </button>
         {% } %}</td>
     </tr>
 {% } %}
 </script>
-<!-- The template to display files available for download -->
+<!-- The template to display files after upload -->
 <script id="template-download" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-download fade">
         {% if (file.error) { %}
+            <td class="name">{%=file.name%}</td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
+            <td class="error"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
             <td></td>
-            <td class="name"><span>{%=file.name%}</span></td>
-            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
-            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
         {% } else { %}
-            <td class="preview">{% if (file.thumbnail_url) { %}
-                <a href="{%=file.url%}" title="{%=file.name%}" rel="gallery" download="{%=file.name%}"><img src="{%=file.thumbnail_url%}"></a>
-            {% } %}</td>
-            <td class="name">
-                <a href="{%=file.url%}" title="{%=file.name%}" rel="{%=file.thumbnail_url&&'gallery'%}" download="{%=file.name%}">{%=file.name%}</a>
-            </td>
-            <td class="size"><span>{%=o.formatFileSize(file.size)%}</span></td>
-            <td colspan="2"></td>
+            <td class="name">{%=file.name%}</td>
+            <td colspan="3"></td>
         {% } %}
     </tr>
 {% } %}
