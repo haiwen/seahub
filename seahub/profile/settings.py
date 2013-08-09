a874c03fee1f64fe207042a4adf27d2ea3584e50
zhengxie <xiez1989@gmail.com>
2013-08-09 13:48:42 +0800

    Change default cache timeout

--- seahub/profile/settings.py
+++ seahub/profile/settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-NICKNAME_CACHE_TIMEOUT = getattr(settings, 'NICKNAME_CACHE_TIMEOUT', 24 * 60 * 60)
+NICKNAME_CACHE_TIMEOUT = getattr(settings, 'NICKNAME_CACHE_TIMEOUT', 14 * 24 * 60 * 60)
 NICKNAME_CACHE_PREFIX = getattr(settings, 'NICKNAME_CACHE_PREFIX', 'NICKNAME_')
 
-EMAIL_ID_CACHE_TIMEOUT = getattr(settings, 'EMAIL_ID_CACHE_TIMEOUT', 24 * 60 * 60)
+EMAIL_ID_CACHE_TIMEOUT = getattr(settings, 'EMAIL_ID_CACHE_TIMEOUT', 14 * 24 * 60 * 60)
 EMAIL_ID_CACHE_PREFIX = getattr(settings, 'EMAIL_ID_CACHE_PREFIX', 'EMAIL_ID_')
