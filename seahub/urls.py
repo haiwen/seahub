35ec6785310a6098f347a9c59b9d43d69badee5a
imwhatiam <imwhatiam123@gmail.com>
2014-04-15 16:38:41 +0800

    new add admin

--- seahub/urls.py
+++ seahub/urls.py
@@ -20,7 +20,8 @@ from seahub.views.sysadmin import sys_repo_admin, sys_user_admin, user_search,\
     sys_group_admin, user_info, user_add, user_remove, user_make_admin, \
     user_remove_admin, user_reset, user_activate, sys_publink_admin, \
     sys_repo_search, sys_repo_transfer, sys_list_orphan, user_deactivate, \
-    user_toggle_status, user_set_quota, sys_ldap_user_admin, sys_list_system
+    user_toggle_status, user_set_quota, sys_ldap_user_admin, sys_list_system, \
+    sys_db_user_admin, batch_user_make_admin
 from seahub.views.ajax import *
 
 # Uncomment the next two lines to enable the admin:
@@ -179,6 +180,7 @@ urlpatterns = patterns('',
     url(r'^sys/seafadmin/transfer/$', sys_repo_transfer, name='sys_repo_transfer'),
     url(r'^sys/useradmin/$', sys_user_admin, name='sys_useradmin'),
     url(r'^sys/ldapuseradmin/$', sys_ldap_user_admin, name='sys_ldap_useradmin'),
+    url(r'^sys/dbuseradmin/$', sys_db_user_admin, name='sys_db_useradmin'),
     url(r'^sys/groupadmin/$', sys_group_admin, name='sys_group_admin'),
     url(r'^sys/publinkadmin/$', sys_publink_admin, name='sys_publink_admin'),
     url(r'^sys/notificationadmin/', notification_list, name='notification_list'),
@@ -186,6 +188,7 @@ urlpatterns = patterns('',
     (r'^useradmin/remove/(?P<user_id>[^/]+)/$', user_remove),
     url(r'^useradmin/search/$', user_search, name="user_search"),
     url(r'^useradmin/makeadmin/(?P<user_id>[^/]+)/$', user_make_admin, name='user_make_admin'),
+    url(r'^useradmin/batchmakeadmin/$', batch_user_make_admin, name='batch_user_make_admin'),
     url(r'^useradmin/removeadmin/(?P<user_id>[^/]+)/$', user_remove_admin, name='user_remove_admin'),
     url(r'^useradmin/info/(?P<email>[^/]+)/$', user_info, name='user_info'),
     url(r'^useradmin/activate/(?P<user_id>[^/]+)/$', user_activate, name='user_activate'),
