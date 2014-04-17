5ea2bc2213c1351ba1eef6750cde3da03a7b8744
lins05 <linshuai2012@gmail.com>
2014-04-17 11:09:25 +0800

    add function get_platform_name

---
+++ seahub/utils/sysinfo.py
@@ -0,0 +1,20 @@
+#coding: UTF-8
+
+import platform
+import os
+
+def get_platform_name():
+    '''Returns current platform the seafile server is running on. Possible return
+    values are:
+
+    - 'linux'
+    - 'windows'
+    - 'raspberry-pi'
+
+    '''
+    if os.name == 'nt':
+        return 'windows'
+    elif 'arm' in platform.machine():
+        return 'raspberry-pi'
+    else:
+        return 'linux'
