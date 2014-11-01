eb396fe8fe4ab57807a2ca1bc24054111f585abb
zhengxie <xiez1989@gmail.com>
2014-11-01 11:58:42 +0800

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,21 +7,23 @@ from fabric.colors import red, green
 def i18n_upload():
     """Update source language, and upload to Transifex.
     """
-    _debug('django-admin.py makemessages -l en -e py,html -i "thirdpart*"')
     local('django-admin.py makemessages -l en -e py,html -i "thirdpart*"')
 
     # some version of makemessages will produce "%%" in the string, replace that
     # to "%".
     _inplace_change('locale/en/LC_MESSAGES/django.po', '%%', '%')
 
-    _debug('tx push -s')
     local('tx push -s')
 
 def i18n_pull():
     """Update local po files with Transifex.
     """
-    _debug('tx pull')
     local('tx pull')
+
+def i18n_compile():
+    """Compile po files.
+    """
+    local('django-admin.py compilemessages')
 
 ########## utility functions
 def _inplace_change(filename, old_string, new_string):
