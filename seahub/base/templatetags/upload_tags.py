c754f19f34e608d415b30df893f60bb7341f1817
llj <lingjun.li1@gmail.com>
2014-11-14 11:23:58 +0800

    [fileupload] redesigned it.

--- seahub/base/templatetags/upload_tags.py
+++ seahub/base/templatetags/upload_tags.py
@@ -10,29 +10,27 @@ def upload_js():
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-upload fade">
         {% if (file.error) { %}
-            <td class="name">{%=file.name%}</td>
+            <td class="name ellipsis" title="{%=file.name%}">{%=file.name%}</td>
             <td class="size">{%=o.formatFileSize(file.size)%}</td>
             <td class="error"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
         {% } else if (o.files.valid && !i) { %}
-            <td class="name">
+            <td class="name ellipsis" title="{%=file.name%}">
                 {%=file.name%}
                 <div class="progress progress-success progress-striped active"><div class="bar" style="width:0%;"></div></div>
             </td>
             <td class="size">{%=o.formatFileSize(file.size)%}</td>
             <td class="start">{% if (!o.options.autoUpload) { %}
                 <button class="btn btn-success">
-                    <span class="icon-upload"></span>
                     <span>{%=locale.fileupload.start%}</span>
                 </button>
             {% } %}</td>
         {% } else { %}
-            <td class="name">{%=file.name%}</td>
+            <td class="name ellipsis" title="{%=file.name%}">{%=file.name%}</td>
             <td class="size">{%=o.formatFileSize(file.size)%}</td>
             <td></td>
         {% } %}
         <td class="cancel">{% if (!i) { %}
             <button class="btn btn-warning">
-                <span class="icon-ban-circle"></span>
                 <span>{%=locale.fileupload.cancel%}</span>
             </button>
         {% } %}</td>
@@ -43,14 +41,14 @@ def upload_js():
 <script id="template-download" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-download fade">
+            <td class="name ellipsis" title="{%=file.name%}">{%=file.name%}</td>
+            <td class="size">{%=o.formatFileSize(file.size)%}</td>
         {% if (file.error) { %}
-            <td class="name">{%=file.name%}</td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
             <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}:</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
+        {% } else if (file.canceled) { %}
+            <td colspan="2" class="tip">{%=locale.fileupload.canceled%}</td>
         {% } else { %}
-            <td class="name">{%=file.name%}</td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
-            <td colspan="2"></td>
+            <td colspan="2" class="tip">{%=locale.fileupload.uploaded%}</td>
         {% } %}
     </tr>
 {% } %}
