b22f92fe1d04e62d6971baa04794d9c7acb60b1e
llj <lingjun.li1@gmail.com>
2014-12-15 18:13:26 +0800

    [fileupload] updated plugin files, enabled 'folder drag & drop', added improvement

--- seahub/base/templatetags/upload_tags.py
+++ seahub/base/templatetags/upload_tags.py
@@ -9,33 +9,26 @@ def upload_js():
 <script id="template-upload" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-upload fade">
-        {% if (file.error) { %}
-            <td class="name ellipsis" title="{%=file.webkitRelativePath || file.name%}">
-                {%=file.webkitRelativePath || file.name%}
-            </td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
-            <td class="error"><span class="label label-important">{%=locale.fileupload.error%}</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
-        {% } else if (o.files.valid && !i) { %}
-            <td class="name ellipsis" title="{%=file.webkitRelativePath || file.name%}">
-                {%=file.webkitRelativePath || file.name%}
-                <div class="progress progress-success progress-striped active"><div class="bar" style="width:0%;"></div></div>
-            </td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
-            <td class="start">{% if (!o.options.autoUpload) { %}
-                <button class="btn btn-success">
-                    <span>{%=locale.fileupload.start%}</span>
-                </button>
-            {% } %}</td>
-        {% } else { %}
-            <td class="name ellipsis" title="{%=file.name%}">{%=file.name%}</td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
-            <td></td>
-        {% } %}
-        <td class="cancel">{% if (!i) { %}
-            <button class="btn btn-warning">
+        <td width="50%" class="name ellipsis" title="{%=file.relative_path || file.name%}">
+            {%=file.relative_path || file.name%}
+        </td>
+        <td width="30%">
+            <span class="size"></span>
+            <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="bar" style="width:0%;"></div></div>
+        </td>
+        <td width="20%">
+            {% if (!o.options.autoUpload) { %}
+            <button class="btn btn-success start" disabled>
+                <span>{%=locale.fileupload.start%}</span>
+            </button>
+            {% } %}
+            {% if (!i) { %}
+            <button class="btn btn-warning cancel">
                 <span>{%=locale.fileupload.cancel%}</span>
             </button>
-        {% } %}</td>
+            {% } %}
+            <p class="error"></p>
+        </td>
     </tr>
 {% } %}
 </script>
@@ -43,17 +36,23 @@ def upload_js():
 <script id="template-download" type="text/x-tmpl">
 {% for (var i=0, file; file=o.files[i]; i++) { %}
     <tr class="template-download fade">
-            <td class="name ellipsis" title="{%=file.webkitRelativePath || file.name%}">
-                {%=file.webkitRelativePath || file.name%}
-            </td>
-            <td class="size">{%=o.formatFileSize(file.size)%}</td>
-        {% if (file.error) { %}
-            <td class="error" colspan="2"><span class="label label-important">{%=locale.fileupload.error%}:</span> {%=locale.fileupload.errors[file.error] || file.error%}</td>
-        {% } else if (file.canceled) { %}
-            <td colspan="2" class="tip">{%=locale.fileupload.canceled%}</td>
-        {% } else { %}
-            <td colspan="2" class="tip">{%=locale.fileupload.uploaded%}</td>
-        {% } %}
+        <td width="50%" class="name ellipsis" title="{%=file.relative_path || file.name%}">
+            {%=file.relative_path || file.name%}
+        </td>
+        <td width="30%">
+            <span class="size">{%=o.formatFileSize(file.size)%}</span>
+        </td>
+        <td width="20%">
+            <span class="tip">
+            {% if (file.canceled) { %}
+                {%=locale.fileupload.canceled%}
+            {% } %}
+            {% if (file.uploaded) { %}
+                {%=locale.fileupload.uploaded%}
+            {% } %}
+            </span>
+            <span class="error">{% if (file.error) { %}{%=file.error%}{% } %}</span>
+        </td>
     </tr>
 {% } %}
 </script>
