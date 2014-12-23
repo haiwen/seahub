06dd348f664535e2b96426f266939a080c6b06b6
zhengxie <xiez1989@gmail.com>
2014-12-23 11:41:07 +0800

    Use backbone.js

---
+++ media/js/templates.js
@@ -0,0 +1,21 @@
+(function(app){
+    "use strict";
+    app.templates = {
+        repo: '' +
+            '<td>' +
+                '<% if (encrypted) { %>' +
+                '<img src="<%= app.utils.getMediaUrl() %>img/sync-folder-encrypt-20.png" title=Read-Write" alt="directory icon" />' +
+                '<% } else { %>' +
+                '<img src="<%= app.utils.getMediaUrl() %>img/sync-folder-20.png" title=Read-Write" alt="directory icon" />' +
+                '<% } %>' +
+            '</td>' +
+            '<td><a href="/repo/<%= id %>/"><%- name %></a></td>' +
+            '<td><%- desc %></td>' +
+            '<td><%- mtime %></td>' +
+            '<td><div>' +
+                '<img src="<%= app.utils.getMediaUrl() %>img/share_20.png" alt="" class="repo-share-btn op-icon vh" title="Share" />' +
+                '<img src="<%= app.utils.getMediaUrl() %>img/rm.png" class="repo-delete-btn op-icon vh" title="Delete" />' +
+            '</div></td>'
+    };
+
+}(app));
