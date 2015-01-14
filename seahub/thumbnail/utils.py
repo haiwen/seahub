6be3900d26a25495101de2a14a9f62b59d9b776a
lian <imwhatiam123@gmail.com>
2015-01-14 13:42:40 +0800

    [thumbnail] use posixpath to generate thumbnail-src

--- seahub/thumbnail/utils.py
+++ seahub/thumbnail/utils.py
@@ -1,5 +1,5 @@
-import os
+import posixpath
 from seahub.utils import get_service_url
 def get_thumbnail_src(repo_id, obj_id, size):
-    return os.path.join(get_service_url(), "thumbnail", repo_id,
-                        obj_id, size, '')
+    return posixpath.join(get_service_url(), "thumbnail", repo_id,
+                          obj_id, size)
