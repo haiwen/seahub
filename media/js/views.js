0a494cd0d3c4fad2c823afc43bf32fbbc227df04
zhengxie <xiez1989@gmail.com>
2015-01-15 11:54:01 +0800

    Show repo dirents on myhome page using router

--- media/js/views.js
+++ media/js/views.js
@@ -8,18 +8,14 @@
         template: _.template(app.templates.repo),
 
         events: {
-            'hover' : 'showAction'
+            'mouseenter' : 'showAction',
+            'mouseleave' : 'hideAction'
         },
 
         initialize: function() {
             console.log('init RepoView');
             Backbone.View.prototype.initialize.apply(this, arguments);
 
-            // _.bindAll(this, 'showAction');
-
-            // this.model.on({
-            //     'hover': this.showAction
-            // }, this);
         },
 
         render: function() {
@@ -27,9 +23,14 @@
             return this;
         },
 
-        showAction: function(){
+        showAction: function() {
             this.$el.addClass('hl');
             this.$el.find('.op-icon').removeClass('vh');
+        },
+
+        hideAction: function() {
+            this.$el.removeClass('hl');
+            this.$el.find('.op-icon').addClass('vh');
         }
 
     });
@@ -52,6 +53,45 @@
 
             this.repoList.each(function(repo) {
                 $(this.el).append(new app.RepoView({model: repo}).render().el);
+            }, this);
+
+            return this;
+        }
+    });
+
+    app.DirentView = Backbone.View.extend({
+        tagName: 'tr',
+        template: null,
+
+        initialize: function() {
+            this.template = _.template($('#dirent-template').html());
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    app.DirentListView = Backbone.View.extend({
+        tagName: 'tbody',
+
+        initialize: function(data) {
+            console.log('init DirentListView');
+
+            if (_.isUndefined(data))
+                data = {};      // TODO: show error ?
+
+            this.direntList = new app.DirentList(data);
+            this.direntList.bind('sync', this.render, this);
+            this.direntList.fetch();
+        },
+
+        render: function(eventName){
+            console.log("direntListView.render()");
+
+            this.direntList.each(function(dirent){
+                $(this.el).append(new app.DirentView({model: dirent}).render().el);
             }, this);
 
             return this;
