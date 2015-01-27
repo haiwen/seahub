352f84b7148e6964e04986bf1ca206bbef3fbfe7
Daniel Pan <freeplant@gmail.com>
2015-01-27 22:41:16 +0800

    [backbone] Add model, template and view for group navigation popup

---
+++ media/scripts/app/views/group-nav.js
@@ -0,0 +1,31 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'common',
+    'app/collections/groups',
+    'text!' + app.config._tmplRoot + 'group-nav.html',
+], function($, _, Backbone, Common, GroupCollection, GroupNavTemplate) {
+    'use strict';
+
+    var GroupNavView = Backbone.View.extend({
+
+        template: _.template(GroupNavTemplate),
+
+        initialize: function(options) {
+            this.groups = new GroupCollection();
+            this.listenTo(this.groups, 'reset', this.reset);
+        },
+
+        reset: function() {
+            console.log(this.template({groups: this.groups.models}));
+        },
+
+        fetch: function() {
+            this.groups.fetch({reset: true});
+        },
+
+    });
+
+    return GroupNavView;
+});
