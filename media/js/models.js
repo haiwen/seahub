f8c169e520abd354c27d0020451f9d7b37c4eec5
Daniel Pan <freeplant@gmail.com>
2015-02-01 10:30:31 +0800

    [backbone] Remove unused files

--- media/js/models.js
+++
@@ -1,9 +0,0 @@
-(function(app, Backbone){
-    "use strict";
-
-    app.models = {};
-
-    app.models.Repo = Backbone.Model.extend({});
-    app.models.Dirent = Backbone.Model.extend({});
-
-})(app, Backbone);
