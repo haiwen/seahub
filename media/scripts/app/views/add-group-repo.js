a8a0ad2cf4f34be8f8e0d5b3066893002ccef158
Daniel Pan <freeplant@gmail.com>
2015-03-01 20:28:25 +0800

    Enable nav to library contents in group view, remove recent changes, remove group tab in myhome

--- media/scripts/app/views/add-group-repo.js
+++ media/scripts/app/views/add-group-repo.js
@@ -1,36 +1,31 @@
 define([
     'jquery',
+    'simplemodal',
     'underscore',
     'backbone',
     'common',
     'app/collections/group-repos',
-], function($, _, Backbone, Common, GroupRepos) {
+    'text!' + app.config._tmplRoot + 'create-repo.html',
+], function($, simplemodal, _, Backbone, Common, GroupRepos, CreateRepoTemplate) {
     'use strict';
 
     var AddGroupRepoView = Backbone.View.extend({
-        el: '#repo-create-form',
+
+        tagName: 'div',
+
+        template: _.template(CreateRepoTemplate),
 
         events: {
             "submit": "addGroupRepo"
         },
 
-        initialize: function() {
-            this.render();
+        initialize: function(repos) {
+            this.repos = repos;
         },
 
         render: function() {
-            $('#encrypt-switch').click(function () {
-                var pwd_input = $('input[type="password"]', this.$createForm);
-
-                if ($(this).attr('checked')) {
-                    pwd_input.attr('disabled', false).removeClass('input-disabled');
-                } else {
-                    pwd_input.attr('disabled', true).addClass('input-disabled');
-                }
-            });
-            
-            this.$el.modal({appendTo: '#main', autoResize: true});
-            
+            this.$el.html(this.template({}));
+            this.$el.modal();
         },
 
         // Generate the attributes for a new GroupRepo item.
@@ -51,7 +46,7 @@ define([
             e.preventDefault();
 
             Common.feedback('Loading...', 'info', Common.INFO_TIMEOUT);
-            GroupRepos.create(this.newAttributes(), {
+            this.repos.create(this.newAttributes(), {
                 wait: true,
                 prepend: true,  // show newly created repo at first line
                 success: function() {
