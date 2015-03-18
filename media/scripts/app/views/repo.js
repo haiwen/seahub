afe2b83be805f4120064e3e5c63760d7c1bccae7
llj <lingjun.li1@gmail.com>
2015-03-18 10:45:13 +0800

    [share] added lib/dir/file share

--- media/scripts/app/views/repo.js
+++ media/scripts/app/views/repo.js
@@ -3,9 +3,9 @@ define([
     'underscore',
     'backbone',
     'common',
-    'app/views/share-popup',
+    'app/views/share',
     'text!' + app.config._tmplRoot + 'repo.html'
-], function($, _, Backbone, Common, SharePopupView, reposTemplate) {
+], function($, _, Backbone, Common, ShareView, reposTemplate) {
     'use strict';
 
     var RepoView = Backbone.View.extend({
@@ -22,7 +22,6 @@ define([
 
         initialize: function() {
             console.log('init RepoView');
-            this.sharePopupView = new SharePopupView();
 
             this.listenTo(this.model, 'destroy', this.remove);
         },
@@ -97,15 +96,15 @@ define([
 
         share: function() {
             var options = {
-                    'is_repo_owner': true,
-                    'is_virtual': this.model.get('virtual'),
-                    'user_perm': this.model.get('permission'),
-                    'repo_id': this.model.get('id'),
-                    'is_dir': true,
-                    'dirent_path': '/',
-                    'obj_name': this.model.get('name')
-                };
-            this.sharePopupView.showPopup(options);
+                'is_repo_owner': true,
+                'is_virtual': this.model.get('virtual'),
+                'user_perm': this.model.get('permission'),
+                'repo_id': this.model.get('id'),
+                'is_dir': true,
+                'dirent_path': '/',
+                'obj_name': this.model.get('name')
+            };
+            new ShareView(options);
         }
 
     });
