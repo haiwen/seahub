2397cf2b914a49ab2e0c923dc21b99df5a4b00d1
Daniel Pan <freeplant@gmail.com>
2015-04-11 15:14:56 +0800

    [backbone] Merge into one library app

--- static/scripts/app/collections/group-repos.js
+++ static/scripts/app/collections/group-repos.js
@@ -1,21 +1,26 @@
 define([
     'underscore',
     'backbone',
+    'common',
     'app/models/group-repo'
-], function(_, Backbone, GroupRepo) {
+], function(_, Backbone, Common, GroupRepo) {
     'use strict';
 
     var GroupRepoCollection = Backbone.Collection.extend({
         model: GroupRepo,
-        url: app.pageOptions.groupReposUrl,
-
         comparator: -'mtime',
+
+        url: function() {
+            return Common.getUrl({name: 'group_repos', group_id: this.group_id});
+        },
+
+        setGroupID: function(group_id) {
+            this.group_id = group_id;
+        }
 
         // initialize: function( ) {
 
         // },
-
-
     });
 
     return GroupRepoCollection;
