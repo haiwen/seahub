df92a895f6407d8be4e99c7a58d69b42350c1042
llj <lingjun.li1@gmail.com>
2015-05-12 16:22:49 +0800

    bug fix

--- static/scripts/app/collections/repos.js
+++ static/scripts/app/collections/repos.js
@@ -7,15 +7,23 @@ define([
 
     var RepoCollection = Backbone.Collection.extend({
         model: Repo,
-        url: function () {
-            return app.pageOptions.reposUrl + '?type=' + this.type;
-        },
+        url: app.pageOptions.reposUrl,
         type: 'mine',
 
         initialize: function(options) {
+            //console.log('init RepoCollection');
             if (options) {
                 this.type = options.type ? options.type : 'mine';
             }
+        },
+
+        fetch: function(options) {
+            // override default fetch url
+            options = options ? _.clone(options) : {};
+            options.url = this.url + '?type=' + this.type;
+
+            //call Backbone's fetch
+            return Backbone.Collection.prototype.fetch.call(this, options);
         },
 
         create: function(model, options) {
