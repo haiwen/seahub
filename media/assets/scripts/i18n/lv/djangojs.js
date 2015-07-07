6a5290866abd0cf4c42f3f933252d0de6c8c2196
zhengxie <xiez1989@gmail.com>
2015-07-07 14:08:43 +0800

    [dist] Update locales and dist

--- media/assets/scripts/i18n/lv/djangojs.js
+++ media/assets/scripts/i18n/lv/djangojs.js
@@ -5,7 +5,14 @@
   var django = globals.django || (globals.django = {});
 
   
-  django.pluralidx = function (count) { return (count == 1) ? 0 : 1; };
+  django.pluralidx = function (n) {
+    var v=(n%10==1 && n%100!=11 ? 0 : n != 0 ? 1 : 2);
+    if (typeof(v) == 'boolean') {
+      return v ? 1 : 0;
+    } else {
+      return v;
+    }
+  };
   
 
   
