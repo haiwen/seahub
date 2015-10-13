d1bd4bd7cf1ad057c39e60644c2f8d8909417123
zhengxie <xiez1989@gmail.com>
2015-10-13 11:58:47 +0800

    Remove constance from thirdpart

--- thirdpart/constance/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class ConstanceConfig(AppConfig):
-    name = 'constance'
-    verbose_name = _('Constance')
