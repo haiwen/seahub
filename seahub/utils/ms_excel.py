0e0aa5fb2577d596710960799682bf1dc0380091
lian <imwhatiam123@gmail.com>
2015-11-19 15:36:26 +0800

    update export excel

--- seahub/utils/ms_excel.py
+++ seahub/utils/ms_excel.py
@@ -7,27 +7,27 @@ def write_xls(sheet_name, head, data_list):
     """
 
     try:
-        import xlwt
+        import openpyxl
     except ImportError as e:
         logger.error(e)
         return None
 
-    wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet(sheet_name)
+    wb = openpyxl.Workbook()
+    ws = wb.get_active_sheet()
+    ws.title = sheet_name
 
-    # prepare table head
-    head_style = xlwt.XFStyle()
-    head_style.font.bold = True
     row_num = 0
 
     # write table head
     for col_num in xrange(len(head)):
-        ws.write(row_num, col_num, head[col_num], head_style)
+        c = ws.cell(row = row_num + 1, column = col_num + 1)
+        c.value = head[col_num]
 
     # write table data
     for row in data_list:
         row_num += 1
         for col_num in xrange(len(row)):
-            ws.write(row_num, col_num, row[col_num])
+            c = ws.cell(row = row_num + 1, column = col_num + 1)
+            c.value = row[col_num]
 
     return wb
