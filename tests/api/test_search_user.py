ed7b904a4f6eb5e370817cda6853b8ea4fc08e11
Enno Gröper <groepeen@cms.hu-berlin.de>
2015-12-22 12:52:06 +0100

    Make user search case insensitive
    If you want to find "Carl Smith", typing "carl" should list him, too.
    People are lazy and expect case insensitive searches.
    They don't think "Oh. I have to type 'Carl' instead.". They think
    "Search doesn't work.".

--- tests/api/test_search_user.py
+++ tests/api/test_search_user.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from seahub.profile.models import Profile
 from seahub.test_utils import BaseTestCase
 
+
 class SearchUserTest(BaseTestCase):
     def setUp(self):
         self.login_as(self.user)
@@ -24,4 +25,36 @@ class SearchUserTest(BaseTestCase):
         assert json_resp['users'][0]['avatar'] is not None
         assert json_resp['users'][0]['avatar_url'] is not None
         assert json_resp['users'][0]['name'] == 'test'
+        assert json_resp['users'][0]['contact_email'] == 'new_mail@test.com'
+
+    def test_can_search_by_nickname(self):
+        p = Profile.objects.add_or_update(self.user.email, nickname="Test")
+        p.contact_email = 'new_mail@test.com'
+        p.save()
+
+        resp = self.client.get(self.endpoint + '?q=' + "Test")
+        json_resp = json.loads(resp.content)
+
+        self.assertEqual(200, resp.status_code)
+        assert json_resp['users'] is not None
+        assert json_resp['users'][0]['email'] == self.user.email
+        assert json_resp['users'][0]['avatar'] is not None
+        assert json_resp['users'][0]['avatar_url'] is not None
+        assert json_resp['users'][0]['name'] == 'Test'
+        assert json_resp['users'][0]['contact_email'] == 'new_mail@test.com'
+
+    def test_can_search_by_nickname_insensitive(self):
+        p = Profile.objects.add_or_update(self.user.email, nickname="Test")
+        p.contact_email = 'new_mail@test.com'
+        p.save()
+
+        resp = self.client.get(self.endpoint + '?q=' + "test")
+        json_resp = json.loads(resp.content)
+
+        self.assertEqual(200, resp.status_code)
+        assert json_resp['users'] is not None
+        assert json_resp['users'][0]['email'] == self.user.email
+        assert json_resp['users'][0]['avatar'] is not None
+        assert json_resp['users'][0]['avatar_url'] is not None
+        assert json_resp['users'][0]['name'] == 'Test'
         assert json_resp['users'][0]['contact_email'] == 'new_mail@test.com'
