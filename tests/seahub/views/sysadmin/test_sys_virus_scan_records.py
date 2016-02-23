ebcd7ed44d835dbb5a58a93c8b709ee91be6c7ce
zhengxie <xiez1989@gmail.com>
2016-02-23 10:45:43 +0800

    [virus] Fix url error when records number more than 9

---
+++ tests/seahub/views/sysadmin/test_sys_virus_scan_records.py
@@ -0,0 +1,48 @@
+from mock import patch
+
+from django.core.urlresolvers import reverse
+
+from seahub.test_utils import BaseTestCase
+
+
+class VirusScanRecord(object):
+    def __init__(self, repo_id):
+        self.repo_id = repo_id
+
+
+class SysVirusScanRecordsTest(BaseTestCase):
+
+    # @patch('seahub.utils.EVENTS_ENABLED', True)
+    # @patch('seahub.utils.get_virus_record')
+    # def test_can_list_empty(self, mock_get_virus_record):
+    #     mock_get_virus_record.return_value = []
+
+    #     self.login_as(self.admin)
+
+    #     resp = self.client.get(reverse('sys_virus_scan_records'))
+    #     self.assertEqual(200, resp.status_code)
+    #     self.assertTemplateUsed(resp, 'sysadmin/sys_virus_scan_records.html')
+
+    def _get_virus_record(self, start, limit):
+        records = []
+        for i in range(11):
+            record = VirusScanRecord(self.repo.id)
+            record.vid = i + 1
+            record.has_handle = False
+            records.append(record)
+
+        return records
+
+    @patch('seahub.utils.EVENTS_ENABLED')
+    @patch('seahub.utils.get_virus_record')
+    def test_can_list_records_num_more_than_10(self, mock_get_virus_record,
+                                               mock_events_enabled):
+        mock_events_enabled = True
+        mock_get_virus_record.side_effect = self._get_virus_record
+
+        self.login_as(self.admin)
+
+        resp = self.client.get(reverse('sys_virus_scan_records'))
+        self.assertEqual(200, resp.status_code)
+        self.assertTemplateUsed(resp, 'sysadmin/sys_virus_scan_records.html')
+        assert len(resp.context['records']) >= 10
