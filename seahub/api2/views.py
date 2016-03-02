c257b6deb8f8989c0879c0af830ea247fc7c4790
lian <imwhatiam123@gmail.com>
2016-03-02 13:39:25 +0800

    [api2] update err msg of owa api

--- seahub/api2/views.py
+++ seahub/api2/views.py
@@ -1846,7 +1846,7 @@ class OwaFileView(APIView):
         filename = os.path.basename(path)
         filetype, fileext = get_file_type_and_ext(filename)
         if fileext not in OFFICE_WEB_APP_FILE_EXTENSION:
-            error_msg = 'file extension invalid.'
+            error_msg = 'path invalid.'
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
         # get wopi dict
