e844735cc257b55de3a72af53585fc575d7b1c80
zhengxie <xiez1989@gmail.com>
2016-03-08 10:59:46 +0800

    [tests] Update tests

--- tests/seahub/options/test_models.py
+++ tests/seahub/options/test_models.py
@@ -1,6 +1,7 @@
 from seahub.test_utils import BaseTestCase
 from seahub.options.models import (UserOptions, KEY_USER_GUIDE,
-                                   VAL_USER_GUIDE_ON, VAL_USER_GUIDE_OFF)
+                                   VAL_USER_GUIDE_ON, VAL_USER_GUIDE_OFF,
+                                   KEY_DEFAULT_REPO)
 
 class UserOptionsManagerTest(BaseTestCase):
     def test_is_user_guide_enabled(self):
@@ -25,3 +26,28 @@ class UserOptionsManagerTest(BaseTestCase):
         assert UserOptions.objects.is_user_guide_enabled(self.user.email) is True
         assert len(UserOptions.objects.filter(email=self.user.email,
                                               option_key=KEY_USER_GUIDE)) == 1
+
+    def test_get_default_repo(self):
+        assert len(UserOptions.objects.filter(email=self.user.email, option_key=KEY_DEFAULT_REPO)) == 0
+
+        UserOptions.objects.create(email=self.user.email,
+                                   option_key=KEY_DEFAULT_REPO,
+                                   option_val=self.repo.id)
+
+        assert len(UserOptions.objects.filter(email=self.user.email, option_key=KEY_DEFAULT_REPO)) == 1
+        assert UserOptions.objects.get_default_repo(self.user.email) is not None
+
+    def test_get_default_repo_with_multiple_records(self):
+        assert len(UserOptions.objects.filter(email=self.user.email, option_key=KEY_DEFAULT_REPO)) == 0
+
+        UserOptions.objects.create(email=self.user.email,
+                                   option_key=KEY_DEFAULT_REPO,
+                                   option_val=self.repo.id)
+
+        UserOptions.objects.create(email=self.user.email,
+                                   option_key=KEY_DEFAULT_REPO,
+                                   option_val=self.repo.id)
+
+        assert len(UserOptions.objects.filter(email=self.user.email, option_key=KEY_DEFAULT_REPO)) == 2
+        assert UserOptions.objects.get_default_repo(self.user.email) is not None
+        assert len(UserOptions.objects.filter(email=self.user.email, option_key=KEY_DEFAULT_REPO)) == 1
