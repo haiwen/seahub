855318e00307f128a076f24d24005413765acd14
zhengxie <xiez1989@gmail.com>
2016-03-21 18:06:33 +0800

    [tests] Use static file storage instead of cached

--- seahub/test_settings.py
+++ seahub/test_settings.py
@@ -15,3 +15,7 @@ REST_FRAMEWORK = {
         'user': '300/minute',
     },
 }
+
+# Use static file storage instead of cached, since the cached need to run collect
+# command first.
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
