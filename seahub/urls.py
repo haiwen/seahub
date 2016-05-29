90fb7d575edccf147a0aafa546206b6ff12c7234
Daniel Pan <freeplant@gmail.com>
2016-05-29 11:49:29 +0800

    Remove sub_repo AJAX api

--- seahub/urls.py
+++ seahub/urls.py
@@ -133,7 +133,6 @@ urlpatterns = patterns(
     url(r'^ajax/repo/(?P<repo_id>[-0-9a-f]{36})/dir/delete/$', delete_dirent, name='delete_dir'),
     url(r'^ajax/repo/(?P<repo_id>[-0-9a-f]{36})/dir/mv/$', mv_dir, name='mv_dir'),
     url(r'^ajax/repo/(?P<repo_id>[-0-9a-f]{36})/dir/cp/$', cp_dir, name='cp_dir'),
-    url(r'^ajax/repo/(?P<repo_id>[-0-9a-f]{36})/dir/sub_repo/$', sub_repo, name='sub_repo'),
     url(r'^ajax/cp_progress/$', get_cp_progress, name='get_cp_progress'),
     url(r'^ajax/cancel_cp/$', cancel_cp, name='cancel_cp'),
     url(r'^ajax/repo/(?P<repo_id>[-0-9a-f]{36})/file/rename/$', rename_dirent, name='rename_file'),
--- static/scripts/common.js
+++ static/scripts/common.js
@@ -103,7 +103,6 @@ define([
                 case 'repos': return siteRoot + 'api2/repos/';
                 case 'pub_repos': return siteRoot + 'api2/repos/public/';
                 case 'repo_del': return siteRoot + 'ajax/repo/' + options.repo_id + '/remove/';
-                case 'sub_repo': return siteRoot + 'ajax/repo/' + options.repo_id + '/dir/sub_repo/';
                 case 'get_my_unenc_repos': return siteRoot + 'ajax/my-unenc-repos/';
                 case 'unenc_rw_repos': return siteRoot + 'ajax/unenc-rw-repos/';
                 case 'api_v2.1_repo_set_password': return siteRoot + 'api/v2.1/repos/' + options.repo_id + '/set-password/';
