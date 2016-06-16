8c0707c647f7eda8ec8e24a728dfd4b4a520a031
llj <lingjun.li1@gmail.com>
2016-06-16 10:29:25 +0800

    [sysadmin] devices: fix & improvement

--- static/scripts/sysadmin-app/views/desktop-devices.js
+++ static/scripts/sysadmin-app/views/desktop-devices.js
@@ -49,10 +49,10 @@ define([
         getNextPage: function() {
             this.initPage();
             var current_page = this.deviceCollection.state.current_page;
-            if (this.deviceCollection.state.hasNextPage) {
+            if (this.deviceCollection.state.has_next_page) {
                 this.deviceCollection.getPage(current_page + 1, {
                     reset: true,
-                    data: {'platform': 'desktop'},
+                    data: {'platform': 'desktop'}
                 });
             }
             return false;
@@ -64,7 +64,7 @@ define([
             if (current_page > 1) {
                 this.deviceCollection.getPage(current_page - 1, {
                     reset: true,
-                    data: {'platform': 'desktop'},
+                    data: {'platform': 'desktop'}
                 });
             }
             return false;
@@ -132,7 +132,7 @@ define([
         },
 
         renderPaginator: function() {
-            if (this.deviceCollection.state.hasNextPage) {
+            if (this.deviceCollection.state.has_next_page) {
                 this.$jsNext.show();
             } else {
                 this.$jsNext.hide();
