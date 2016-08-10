67f376002e0129065b32942332a50ffbc48e9766
lian <lian@lian.com>
2016-08-10 14:50:29 +0800

    repo created callback
    allow user define repo created callback func at ../conf/seahub_settings.py

--- seahub/__init__.py
+++ seahub/__init__.py
@@ -4,3 +4,10 @@ from handlers import repo_created_cb, repo_deleted_cb
 
 repo_created.connect(repo_created_cb)
 repo_deleted.connect(repo_deleted_cb)
+
+try:
+    # ../conf/seahub_settings.py
+    from seahub_settings import repo_created_callback
+    repo_created.connect(repo_created_callback)
+except ImportError:
+    pass
