6bf9609dedf134b9f78a5dcfa39c1d6b33ff5149
zhengxie <xiez1989@gmail.com>
2017-01-11 13:57:55 +0800

    Update time zone to UTC

--- seahub/settings.py
+++ seahub/settings.py
@@ -35,7 +35,7 @@ DATABASES = {
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Asia/Shanghai'
+TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
