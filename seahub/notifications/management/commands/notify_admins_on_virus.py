2cb9d4c753a8d1157348522d61d577c1a5a5c925
zhengxie <xiez1989@gmail.com>
2017-02-13 11:37:53 +0800

    [notification] Update virus scan notice

--- seahub/notifications/management/commands/notify_admins_on_virus.py
+++ seahub/notifications/management/commands/notify_admins_on_virus.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 # encoding: utf-8
+import os
 import logging
 
+from django.conf import settings as dj_settings
 from django.core.management.base import BaseCommand
+from django.core.urlresolvers import reverse
 from django.utils import translation
 from django.utils.translation import ugettext as _
 import seaserv
+from seaserv import seafile_api
 
 from seahub.profile.models import Profile
 import seahub.settings as settings
@@ -18,10 +22,20 @@ class Command(BaseCommand):
     help = 'Send Email notifications to admins if there are virus files detected .'
     label = "notifications_notify_admins_on_virus"
 
+    def add_arguments(self, parser):
+        parser.add_argument('repo_file', nargs='+', type=str)
+
     def get_user_language(self, username):
         return Profile.objects.get_user_language(username)
 
     def handle(self, *args, **options):
+        self.email_admins()
+        self.email_mail_list()
+
+        for repo_file in options['repo_file']:
+            self.email_repo_owner(repo_file)
+
+    def email_admins(self):
         db_users = seaserv.get_emailusers('DB', -1, -1)
         ldpa_imported_users = seaserv.get_emailusers('LDAPImport', -1, -1)
 
@@ -46,3 +60,44 @@ class Command(BaseCommand):
 
             # restore current language
             translation.activate(cur_language)
+
+    def email_mail_list(self):
+        try:
+            notify_list = dj_settings.VIRUS_SCAN_NOTIFY_LIST
+        except AttributeError:
+            return
+
+        for mail in notify_list:
+            send_html_email_with_dj_template(
+                mail, dj_template='notifications/notify_virus.html',
+                subject=_('Virus detected on %s') % settings.SITE_NAME,
+                priority=MAIL_PRIORITY.now
+            )
+
+    def email_repo_owner(self, repo_file):
+        repo_id, file_path = repo_file.split(':')
+        owner = seafile_api.get_repo_owner(repo_id)
+        if not owner:
+            return
+
+        # save current language
+        cur_language = translation.get_language()
+
+        # get and active user language
+        user_language = self.get_user_language(owner)
+        translation.activate(user_language)
+
+        contact_email = Profile.objects.get_contact_email_by_user(owner)
+        send_html_email_with_dj_template(
+            contact_email, dj_template='notifications/notify_virus.html',
+            context={'owner': owner,
+                     'file_url': reverse('view_lib_file',
+                                         args=[repo_id, file_path]),
+                     'file_name': os.path.basename(file_path),
+                 },
+            subject=_('Virus detected on %s') % settings.SITE_NAME,
+            priority=MAIL_PRIORITY.now
+        )
+
+        # restore current language
+        translation.activate(cur_language)
