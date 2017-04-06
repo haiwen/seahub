e72cde9d0a86ea8a09adbd3c2ba295a1b09f3bb9
zhengxie <xiez1989@gmail.com>
2017-04-06 14:13:32 +0800

    Update Django to 1.8.18

--- seahub/utils/http.py
+++ seahub/utils/http.py
@@ -1,8 +1,6 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 from __future__ import unicode_literals
 
-import unicodedata
-import urlparse
 import json
 
 from functools import wraps
@@ -46,39 +44,3 @@ def int_param(request, key):
         return int(v)
     except ValueError:
         raise BadRequestException()
-
-def is_safe_url(url, host=None):
-    """
-    https://github.com/django/django/blob/fc6d147a63f89795dbcdecb0559256470fff4380/django/utils/http.py
-
-    Return ``True`` if the url is a safe redirection (i.e. it doesn't point to
-    a different host and uses a safe scheme).
-    Always returns ``False`` on an empty url.
-    """
-    if url is not None:
-        url = url.strip()
-    if not url:
-        return False
-    # Chrome treats \ completely as / in paths but it could be part of some
-    # basic auth credentials so we need to check both URLs.
-    return _is_safe_url(url, host) and _is_safe_url(url.replace('\\', '/'), host)
-
-def _is_safe_url(url, host):
-    # Chrome considers any URL with more than two slashes to be absolute, but
-    # urlparse is not so flexible. Treat any url with three slashes as unsafe.
-    if url.startswith('///'):
-        return False
-    url_info = urlparse.urlparse(url)
-    # Forbid URLs like http:///example.com - with a scheme, but without a hostname.
-    # In that URL, example.com is not the hostname but, a path component. However,
-    # Chrome will still consider example.com to be the hostname, so we must not
-    # allow this syntax.
-    if not url_info.netloc and url_info.scheme:
-        return False
-    # Forbid URLs that start with control characters. Some browsers (like
-    # Chrome) ignore quite a few control characters at the start of a
-    # URL and might consider the URL as scheme relative.
-    if unicodedata.category(url[0])[0] == 'C':
-        return False
-    return ((not url_info.netloc or url_info.netloc == host) and
-            (not url_info.scheme or url_info.scheme in ['http', 'https']))
