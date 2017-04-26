764a63e705c952793897bbbd3f4bc2192292e27e
zhengxie <xiez1989@gmail.com>
2017-04-26 10:28:33 +0800

    Update sso login

--- seahub/views/sso.py
+++ seahub/views/sso.py
@@ -2,13 +2,24 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.utils.http import is_safe_url
+
+from seahub.auth import REDIRECT_FIELD_NAME
 
 def sso(request):
+    # Ensure the user-originating redirection url is safe.
+    if REDIRECT_FIELD_NAME in request.REQUEST:
+        next_page = request.REQUEST[REDIRECT_FIELD_NAME]
+        if not is_safe_url(url=next_page, host=request.get_host()):
+            next_page = settings.LOGIN_REDIRECT_URL
+    else:
+        next_page = reverse('libraries')
+
     if getattr(settings, 'ENABLE_SHIB_LOGIN', False):
-        return HttpResponseRedirect(request.GET.get("next", reverse('libraries')))
+        return HttpResponseRedirect(next_page)
 
     if getattr(settings, 'ENABLE_KRB5_LOGIN', False):
-        return HttpResponseRedirect(request.GET.get("next", reverse('libraries')))
+        return HttpResponseRedirect(next_page)
 
     if getattr(settings, 'ENABLE_ADFS_LOGIN', False):
         return HttpResponseRedirect(reverse('saml2_login'))
---
+++ tests/seahub/views/test_sso.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.test import override_settings
+from django.utils.http import urlquote
+
+from seahub.test_utils import BaseTestCase
+from seahub.views.sso import sso
+
+
+class SSOTest(BaseTestCase):
+    def setUp(self):
+        self.url = reverse(sso)
+
+    @override_settings(ENABLE_SHIB_LOGIN=True)
+    def test_sso(self):
+        resp = self.client.get(self.url)
+        self.assertEqual(302, resp.status_code)
+        assert resp.get('location') == 'http://testserver/'
+
+        resp = self.client.get(self.url + '?next=/foo')
+        assert resp.get('location') == 'http://testserver/foo'
+
+        resp = self.client.get(self.url + '?next=' + urlquote('http://testserver\@example.com'))
+        self.assertRegexpMatches(resp['Location'], r'http://testserver%s' % settings.LOGIN_REDIRECT_URL)
