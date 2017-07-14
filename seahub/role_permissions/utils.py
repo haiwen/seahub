1bcef1e7fbfc7094e5555503af8bc310203983f4
zhengxie <xiez1989@gmail.com>
2017-07-14 16:52:35 +0800

    [role perm] Use default role if not set in settings

--- seahub/role_permissions/utils.py
+++ seahub/role_permissions/utils.py
@@ -1,6 +1,10 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
+import logging
+
 from .settings import ENABLED_ROLE_PERMISSIONS
 from seahub.constants import DEFAULT_USER
+
+logger = logging.getLogger(__name__)
 
 def get_available_roles():
     """Get available roles defined in `ENABLED_ROLE_PERMISSIONS`.
@@ -14,6 +18,7 @@ def get_enabled_role_permissions_by_role(role):
         role = DEFAULT_USER
 
     if role not in ENABLED_ROLE_PERMISSIONS.keys():
-        assert False, '%s is not a valid role' % role
+        logger.warn('%s is not a valid role, use default role.' % role)
+        role = DEFAULT_USER
 
     return ENABLED_ROLE_PERMISSIONS[role]
