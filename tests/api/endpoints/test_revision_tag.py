133ad3c8e3b700d8b335b59a0d8fb9d7a12666e9
zming <517046497@qq.com>
2017-07-29 15:48:06 +0800

    add put method, test

--- tests/api/endpoints/test_revision_tag.py
+++ tests/api/endpoints/test_revision_tag.py
@@ -11,7 +11,6 @@ from seaserv import seafile_api
 class RevisionTagsTest(BaseTestCase):
     def setUp(self):
         self.login_as(self.user)
-        self.tag_name = "test_tag_name"
         self.repo = seafile_api.get_repo(self.create_repo(
             name="test_repo",
             desc="",
@@ -22,21 +21,36 @@ class RevisionTagsTest(BaseTestCase):
         self.url_get = reverse("api-v2.1-revision-tags-tag-names") 
 
     @patch('seahub.api2.endpoints.revision_tag.check_folder_permission')
-    def test_revision_tags(self, mock_permission):
+    def test_post_revision_tags(self, mock_permission):
         mock_permission.return_value = 'rw'
         c_resp = self.client.post(self.url, {
-            "tag_names": self.tag_name,
+            "tag_names": 'test_tag_name,test_tag',
             "repo_id": self.repo.repo_id,
         })
-        assert c_resp.status_code in [200, 201]
+        self.assertIn(c_resp.status_code, [200, 201])
         g_resp = self.client.get(self.url_get+"?name_only=true")
-        assert g_resp.status_code == 200
-        assert self.tag_name in g_resp.data
+        self.assertEqual(200, g_resp.status_code)
+        self.assertIn('test_tag_name', g_resp.data)
+        self.assertIn('test_tag', g_resp.data)
 
-        #d_resp = self.client.delete(self.url+"?repo_id="+str(self.repo.repo_id)+
-        #                            "&tag_name="+str(self.tag_name))
-        #assert d_resp.status_code in [200, 202]
+    @patch('seahub.api2.endpoints.revision_tag.check_folder_permission')
+    def test_put_revision_tags(self, mock_permission):
+        mock_permission.return_value = 'rw'
+        data = 'tag_names=test_tag,test_tag_one&repo_id=%s' % self.repo.repo_id
+        resp = self.client.put(self.url, data, 'application/x-www-form-urlencoded')
+        self.assertEqual(200, resp.status_code)
 
-        #g_resp = self.client.get(self.url_get+"?name_only=true")
-        #assert g_resp.status_code == 200
-        #assert self.tag_name not in g_resp.data
+        g_resp = self.client.get(self.url_get+"?name_only=true")
+        self.assertEqual(200, g_resp.status_code)
+        self.assertIn('test_tag', g_resp.data)
+        self.assertIn('test_tag_one', g_resp.data)
+
+        data = 'tag_names=test_del_tag&repo_id=%s' % self.repo.repo_id
+        resp = self.client.put(self.url, data, 'application/x-www-form-urlencoded')
+        self.assertEqual(200, resp.status_code)
+
+        g_resp = self.client.get(self.url_get+"?name_only=true")
+        self.assertEqual(200, g_resp.status_code)
+        self.assertNotIn('test_tag', g_resp.data)
+        self.assertNotIn('test_tag_one', g_resp.data)
+        self.assertIn('test_del_tag', g_resp.data)
