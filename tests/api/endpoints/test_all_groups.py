4133d3385480cc0d3deb6dbbeb8920c3bc4bb9f8
zming <517046497@qq.com>
2017-08-16 18:05:43 +0800

    [api] fix setting don't work

--- tests/api/endpoints/test_all_groups.py
+++ tests/api/endpoints/test_all_groups.py
@@ -20,13 +20,16 @@ class GroupsShareTest(BaseTestCase):
         resp = [group["name"] for group in 
                 json.loads(self.client.get(reverse('api-v2.1-all-groups')).content)]
         self.assertIn('test_group', resp)
+        self.clear_cache()
 
     def test_can_get_with_disable_config(self):
+        config.ENABLE_SHARE_TO_ALL_GROUPS = 0
         self.login_as(self.admin)
         self.admin_group = self.create_group(group_name='test_disable_config', 
                                   username=self.admin.username)
         resp = json.loads(self.client.get(reverse('api-v2.1-all-groups')).content)
         self.assertEqual([], resp)
+        self.clear_cache()
 
     def tearDown(self):
         self.remove_group(self.admin_group.id)
