b460d89f5a7025fad3abfebd6f1444c1407eff29
zming <517046497@qq.com>
2017-08-23 15:31:57 +0800

    [test] pro -> master

--- tests/api/endpoints/admin/test_statistics.py
+++ tests/api/endpoints/admin/test_statistics.py
@@ -13,9 +13,12 @@ class FileOperationsInfoText(BaseTestCase):
     def setUp(self):
         self.login_as(self.admin)
 
+    @patch("seahub.api2.endpoints.admin.statistics.EVENTS_ENABLED")
+    @patch("seahub.api2.endpoints.admin.statistics.is_pro_version")
     @patch("seahub.api2.endpoints.admin.statistics.get_file_ops_stats")
     @patch("seahub.api2.endpoints.admin.statistics.get_file_ops_stats_by_day")
-    def test_can_get_file_audit_stats(self, mock_get_file_audit_stats_by_day, mock_get_file_audit_stats):
+    def test_can_get_file_audit_stats(self, mock_get_file_audit_stats_by_day, mock_get_file_audit_stats, 
+                                      mock_is_pro, mock_events_enabled):
         mock_get_file_audit_stats.return_value = [
             (datetime.datetime(2017, 6, 2, 7, 0), u'Added', 2L),
             (datetime.datetime(2017, 6, 2, 7, 0), u'Deleted', 2L),
@@ -28,6 +31,8 @@ class FileOperationsInfoText(BaseTestCase):
             (datetime.datetime(2017, 6, 2, 4, 2), u'Deleted', 2L),
             (datetime.datetime(2017, 6, 2, 4, 2), u'Visited', 2L),
         ]
+        mock_is_pro.return_value = True
+        mock_events_enabled = True
         url = reverse('api-v2.1-admin-statistics-file-operations')
         url += "?start=2017-06-01 07:00:00&end=2017-06-03 07:00:00&group_by=hour"
         resp = self.client.get(url)
@@ -47,12 +52,17 @@ class FileOperationsInfoText(BaseTestCase):
                 'added': 2, 'deleted': 2, 'visited': 2}
         assert data in json_resp
 
+    @patch("seahub.api2.endpoints.admin.statistics.EVENTS_ENABLED")
+    @patch("seahub.api2.endpoints.admin.statistics.is_pro_version")
     @patch("seahub.api2.endpoints.admin.statistics.get_user_activity_stats")
     @patch("seahub.api2.endpoints.admin.statistics.get_user_activity_stats_by_day")
-    def test_can_user_activity_stats(self, mock_stats_by_day, mock_stats):
+    def test_can_user_activity_stats(self, mock_stats_by_day, mock_stats, 
+                                     mock_is_pro, mock_events_enabled):
         mock_stats.return_value = [(datetime.datetime(2017, 6, 2, 7, 0), 2L),
                              (datetime.datetime(2017, 6, 2, 8, 0), 5L)]
         mock_stats_by_day.return_value = [(datetime.datetime(2017, 6, 2, 4, 0), 3L)]
+        mock_is_pro.return_value = True
+        mock_events_enabled = True
         url = reverse('api-v2.1-admin-statistics-active-users')
         url += "?start=2017-06-01 07:00:00&end=2017-06-03 07:00:00&group_by=hour"
         resp = self.client.get(url)
@@ -70,12 +80,17 @@ class FileOperationsInfoText(BaseTestCase):
         data = {'datetime': datetime_to_isoformat_timestr(datetime.datetime(2017, 6, 2, 4, 0)), 'count': 3}
         assert data in json_resp
 
+    @patch("seahub.api2.endpoints.admin.statistics.EVENTS_ENABLED")
+    @patch("seahub.api2.endpoints.admin.statistics.is_pro_version")
     @patch("seahub.api2.endpoints.admin.statistics.get_total_storage_stats")
     @patch("seahub.api2.endpoints.admin.statistics.get_total_storage_stats_by_day")
-    def test_can_get_total_storage_stats(self, mock_stats_by_day, mock_stats):
+    def test_can_get_total_storage_stats(self, mock_stats_by_day, mock_stats, 
+                                         mock_is_pro, mock_events_enabled):
         mock_stats.return_value = [(datetime.datetime(2017, 6, 2, 7, 0), 2L),
                              (datetime.datetime(2017, 6, 2, 8, 0), 5L)]
         mock_stats_by_day.return_value = [(datetime.datetime(2017, 6, 2, 3, 0), 13L)]
+        mock_is_pro.return_value = True
+        mock_events_enabled = True
         url = reverse('api-v2.1-admin-statistics-total-storage')
         url += "?start=2017-06-01 07:00:00&end=2017-06-03 07:00:00&group_by=hour"
         resp = self.client.get(url)
