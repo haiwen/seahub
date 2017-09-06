fd413b1a4e6f4a8478daac73214824f97cb60102
zming <517046497@qq.com>
2017-09-06 18:20:39 +0800

    performance, funciton name

--- tests/seahub/share/models/test_extra_groups_share_permission.py
+++ tests/seahub/share/models/test_extra_groups_share_permission.py
@@ -3,7 +3,6 @@ from seahub.test_utils import BaseTestCase
 
 from seaserv import seafile_api
 
-
 class ExtraGroupsSharePermissionTest(BaseTestCase):
     def test_can_add(self):
         ExtraGroupsSharePermission.objects.create_share_permission(self.repo.id, 
@@ -11,13 +10,13 @@ class ExtraGroupsSharePermissionTest(BaseTestCase):
                                                                    'admin')
         self.assertEqual(1, len(ExtraGroupsSharePermission.objects.all()))
 
-    def test_can_get_admin_groups(self):
-        res = ExtraGroupsSharePermission.objects.get_admin_groups(self.repo.id)
+    def test_can_get_admin_groups_by_repo(self):
+        res = ExtraGroupsSharePermission.objects.get_admin_groups_by_repo(self.repo.id)
         assert len(res) == 0
         ExtraGroupsSharePermission.objects.create_share_permission(self.repo.id, 
                                                                    self.group.id, 
                                                                    'admin')
-        res = ExtraGroupsSharePermission.objects.get_admin_groups(self.repo.id)
+        res = ExtraGroupsSharePermission.objects.get_admin_groups_by_repo(self.repo.id)
         assert res[0] == str(self.group.id)
 
     def test_can_get_repos_with_admin_permission(self):
@@ -45,3 +44,18 @@ class ExtraGroupsSharePermissionTest(BaseTestCase):
                                                                    self.group.id,
                                                                    'admin')
         self.assertEqual(1, len(ExtraGroupsSharePermission.objects.all()))
+
+    def test_batch_is_admin_group(self):
+        r = seafile_api.get_repo(self.create_repo(name='repo2',
+            desc='', username=self.user.email, passwd=None))
+        new_group = self.create_group(group_name='test_group',
+                                       username=self.user.username)
+        data = [(self.repo.id, self.group.id), (r.id, self.group.id), (self.repo.id, new_group.id)]
+        self.assertEqual([], ExtraGroupsSharePermission.objects.batch_is_admin_group(data))
+        ExtraGroupsSharePermission.objects.create_share_permission(self.repo.id, 
+                                                                   self.group.id, 
+                                                                   'admin')
+        ExtraGroupsSharePermission.objects.create_share_permission(self.repo.id, 
+                                                                   new_group.id, 
+                                                                   'admin')
+        self.assertEqual([(self.repo.id, str(self.group.id)), (self.repo.id, str(new_group.id))], ExtraGroupsSharePermission.objects.batch_is_admin_group(data))
