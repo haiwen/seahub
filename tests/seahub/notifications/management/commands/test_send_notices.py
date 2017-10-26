c1d7b588e396df354e280db950a893a7b0721018
zming <517046497@qq.com>
2017-10-26 10:03:56 +0800

    update notification

--- tests/seahub/notifications/management/commands/test_send_notices.py
+++ tests/seahub/notifications/management/commands/test_send_notices.py
@@ -12,7 +12,7 @@ class CommandTest(BaseTestCase):
     def test_can_send(self):
         self.assertEqual(len(mail.outbox), 0)
         UserNotification.objects.add_repo_share_msg(
-            self.user.username, repo_share_msg_to_json('bar@bar.com', self.repo.id))
+            self.user.username, repo_share_msg_to_json('bar@bar.com', self.repo.id, '/', None))
         Profile.objects.add_or_update(self.user.username, 'nickname')
 
         call_command('send_notices')
@@ -22,7 +22,7 @@ class CommandTest(BaseTestCase):
     def test_can_send_to_contact_email(self):
         self.assertEqual(len(mail.outbox), 0)
         UserNotification.objects.add_repo_share_msg(
-            self.user.username, repo_share_msg_to_json('bar@bar.com', self.repo.id))
+            self.user.username, repo_share_msg_to_json('bar@bar.com', self.repo.id, '/', None))
         p = Profile.objects.add_or_update(self.user.username, 'nickname')
         p.contact_email = 'contact@foo.com'
         p.save()
