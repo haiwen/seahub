05caf621f192e05c4a19b3fbf50a381ab40f1802
zMingGit <517046497@qq.com>
2017-11-14 14:12:27 +0800

    Userinfo contactemail (#1894)
    * Show contact_email on user profile
    
    I think it would be good to have the edit functionality for that
    attribute, too, but I did not implement that right now.
    It's marked with TODO though.
    
    * update api and tests
    
    * review

--- tests/seahub/base/templatetags/test_seahub_tags.py
+++ tests/seahub/base/templatetags/test_seahub_tags.py
@@ -21,14 +21,14 @@ class Email2nicknameTest(BaseTestCase):
     def test_nickname_is_space(self):
         Profile.objects.add_or_update(self.user.username, ' ')
         assert len(Profile.objects.all()) == 1
-        assert Profile.objects.all()[0].nickname == ' '
+        assert Profile.objects.all()[0].nickname == ''
 
         assert email2nickname(self.user.username) == self.user.username.split('@')[0]
 
     def test_nickname_contains_space(self):
         Profile.objects.add_or_update(self.user.username, ' foo bar ')
         assert len(Profile.objects.all()) == 1
-        assert Profile.objects.all()[0].nickname == ' foo bar '
+        assert Profile.objects.all()[0].nickname == 'foo bar'
 
         assert email2nickname(self.user.username) == 'foo bar'
 
