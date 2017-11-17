2be35db8dff7e4631113419248fc2db3547e7a09
zming <517046497@qq.com>
2017-11-17 16:13:55 +0800

    del statistics by hour(user activity, total storage)

--- seahub/api2/endpoints/admin/statistics.py
+++ seahub/api2/endpoints/admin/statistics.py
@@ -10,8 +10,7 @@ from rest_framework import status
 from django.utils import timezone
 
 from seahub.utils import get_file_ops_stats, get_file_ops_stats_by_day, \
-        get_total_storage_stats, get_total_storage_stats_by_day, \
-        get_user_activity_stats, get_user_activity_stats_by_day, \
+        get_total_storage_stats_by_day, get_user_activity_stats_by_day, \
         is_pro_version, EVENTS_ENABLED
 from seahub.utils.timeutils import datetime_to_isoformat_timestr
 from seahub.settings import TIME_ZONE
@@ -21,22 +20,17 @@ from seahub.api2.throttling import UserRateThrottle
 from seahub.api2.utils import api_error
 
 
-
 def check_parameter(func):
     def _decorated(view, request, *args, **kwargs):
         if not is_pro_version() or not EVENTS_ENABLED:
             return api_error(status.HTTP_404_NOT_FOUND, 'Events not enabled.')
         start_time = request.GET.get("start", "")
         end_time = request.GET.get("end", "")
-        group_by = request.GET.get("group_by", "hour")
         if not start_time:
             error_msg = "Start time can not be empty"
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
         if not end_time:
             error_msg = "End time can not be empty"
-            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
-        if group_by.lower() not in ["hour", "day"]:
-            error_msg = "group_by can only be day or hour."
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
         try:
             start_time = datetime.datetime.strptime(start_time,
@@ -51,7 +45,7 @@ def check_parameter(func):
             error_msg = "End time %s invalid" % end_time
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
-        return func(view, request, start_time, end_time, group_by)
+        return func(view, request, start_time, end_time)
     return _decorated
 
 
@@ -66,7 +60,7 @@ class FileOperationsView(APIView):
     permission_classes = (IsAdminUser,)
 
     @check_parameter
-    def get(self, request, start_time, end_time, group_by):
+    def get(self, request, start_time, end_time):
         """
         Get records of the specified time range.
             param:
@@ -76,6 +70,11 @@ class FileOperationsView(APIView):
             return:
                 the list of file operations record.
         """
+        group_by = request.GET.get("group_by", "hour")
+        if group_by.lower() not in ["hour", "day"]:
+            error_msg = "group_by can only be day or hour."
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+
         data = get_data_by_hour_or_day(group_by, start_time, end_time, get_file_ops_stats, get_file_ops_stats_by_day)
         ops_added_dict = get_init_data(start_time, end_time, group_by)
         ops_visited_dict = get_init_data(start_time, end_time, group_by)
@@ -104,11 +103,11 @@ class TotalStorageView(APIView):
     permission_classes = (IsAdminUser,)
 
     @check_parameter
-    def get(self, request, start_time, end_time, group_by):
-        data = get_data_by_hour_or_day(group_by, start_time, end_time, get_total_storage_stats, get_total_storage_stats_by_day)
+    def get(self, request, start_time, end_time):
+        data = get_total_storage_stats_by_day(start_time, end_time, get_time_offset())
 
         res_data = []
-        init_data = get_init_data(start_time, end_time, group_by)
+        init_data = get_init_data(start_time, end_time, 'day')
         for e in data:
             init_data[e[0]] = e[1]
         for k, v in init_data.items():
@@ -123,11 +122,11 @@ class ActiveUsersView(APIView):
     permission_classes = (IsAdminUser,)
 
     @check_parameter
-    def get(self, request, start_time, end_time, group_by):
-        data = get_data_by_hour_or_day(group_by, start_time, end_time, get_user_activity_stats, get_user_activity_stats_by_day)
+    def get(self, request, start_time, end_time):
+        data = get_user_activity_stats_by_day(start_time, end_time, get_time_offset())
 
         res_data = []
-        init_data = get_init_data(start_time, end_time, group_by)
+        init_data = get_init_data(start_time, end_time, 'day')
         for e in data:
             init_data[e[0]] = e[1]
         for k, v in init_data.items():
@@ -154,11 +153,14 @@ def get_init_data(start_time, end_time, group_by):
         res[dt] = 0
     return res
 
-
-def get_data_by_hour_or_day(parameter, start_time, end_time, func, func_by_day):
+def get_time_offset():
     timezone_name = timezone.get_current_timezone_name()
     offset = pytz.timezone(timezone_name).localize(datetime.datetime.now()).strftime('%z')
-    offset = offset[:3] + ':' + offset[3:]
+    return offset[:3] + ':' + offset[3:]
+
+
+def get_data_by_hour_or_day(parameter, start_time, end_time, func, func_by_day):
+    offset = get_time_offset()
     if parameter == "hour":
         data = func(start_time, end_time, offset)
     elif parameter == "day":
