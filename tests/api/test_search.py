16af598e16ce3ffacf8c9c9f3872476ea7ba6e78
zming <517046497@qq.com>
2018-02-09 14:24:03 +0800

    Reduce redundant operations

--- tests/api/test_search.py
+++ tests/api/test_search.py
@@ -1,14 +1,15 @@
 import json
+import pytest
 from mock import patch
 
 from django.core.urlresolvers import reverse
 
-from seahub.test_utils import BaseTestCase
-from seahub.api2.views import Search
+from seahub.test_utils import BaseTestCase, TRAVIS
+
+from seaserv import seafile_api
 
 
 class SearchTest(BaseTestCase):
-
     def setUp(self):
         self.file_path = self.file
         self.repo_id = self.repo.id
@@ -27,15 +28,16 @@ class SearchTest(BaseTestCase):
                 "fullpath": self.file_path,
                 "repo_name": "seafile-design",
                 "is_dir": False,
-                "size": 142758
+                "size": 142758,
+                "repo": seafile_api.get_repo(self.repo_id)
             },
         ]
 
     @patch('seahub.api2.views.HAS_FILE_SEARCH', True)
-    @patch.object(Search, '_search_in_repos')
-    def test_can_search_file(self, mock_search_in_repos):
-
-        mock_search_in_repos.return_value = self.mock_results, \
+    @patch('seahub.api2.views.search_files')
+    @pytest.mark.skipif(TRAVIS, reason="")
+    def test_can_search_file(self, mock_search_files):
+        mock_search_files.return_value = self.mock_results, \
                 self.mock_total
 
         self.login_as(self.user)
@@ -48,12 +50,7 @@ class SearchTest(BaseTestCase):
         assert json_resp['results'][0]['repo_id'] == self.mock_results[0]['repo_id']
 
     @patch('seahub.api2.views.HAS_FILE_SEARCH', True)
-    @patch.object(Search, '_search_in_a_single_repo')
-    def test_can_not_search_with_invalid_repo_permission(self, mock_search_in_a_single_repo):
-
-        mock_search_in_a_single_repo.return_value = self.mock_results, \
-                self.mock_total
-
+    def test_can_not_search_with_invalid_repo_permission(self):
         self.login_as(self.admin)
         resp = self.client.get(self.url + '?q=lian&search_repo=%s' %
                 self.repo_id)
