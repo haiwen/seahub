9a78a6ceefbcd72f5e335b866a123881827c3a2d
lian <imwhatiam123@gmail.com>
2018-02-10 17:27:41 +0800

    [sysadmin] use web api to transfer repo

--- seahub/urls.py
+++ seahub/urls.py
@@ -400,7 +400,6 @@ urlpatterns = patterns(
     url(r'^sysadmin/#search-libs/$', fake_view, name='sys_repo_search'),
     url(r'^sysadmin/#search-trash-libs/$', fake_view, name='sys_trash_repo_search'),
     url(r'^sysadmin/#search-groups/$', fake_view, name='sys_group_search'),
-    url(r'^sys/seafadmin/transfer/$', sys_repo_transfer, name='sys_repo_transfer'),
     url(r'^sys/seafadmin/delete/(?P<repo_id>[-0-9a-f]{36})/$', sys_repo_delete, name='sys_repo_delete'),
     url(r'^sys/useradmin/$', sys_user_admin, name='sys_useradmin'),
     url(r'^sys/useradmin/export-excel/$', sys_useradmin_export_excel, name='sys_useradmin_export_excel'),
