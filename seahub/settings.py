6e07b9a7785e181a3dc1617fbba978ed3d160eb0
zhengxie <xiez1989@gmail.com>
2018-03-21 16:24:39 +0800

    [Django 1.11] Add auth app, otherwise we will not able to import django.contrib.auth.models

--- seahub/settings.py
+++ seahub/settings.py
@@ -198,6 +198,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.auth',
 
     'registration',
     'captcha',
