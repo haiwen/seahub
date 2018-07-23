b33d4d223722323979e52293da61214799da00ce
杨顺强 <shuntian@yangshunqiangdeMacBook-Pro.local>
2018-07-23 23:01:21 +0800

    update the mobile menu style

--- static/scripts/app/views/dirent.js
+++ static/scripts/app/views/dirent.js
@@ -79,7 +79,7 @@ define([
                 el: this.$('.sf-dropdown'),
                 right: '0'
             });
-
+            this.mobileMenu = this.$(".mobile-menu-container");
             // for image files
             this.$('.img-name-link').magnificPopup(this.dirView.magnificPopupOptions);
 
@@ -113,7 +113,10 @@ define([
             'click .unlock-file': 'unlockFile',
             'click .view-details': 'viewDetails',
             'click .file-comment': 'viewFileComments',
-            'click .open-via-client': 'open_via_client'
+            'click .open-via-client': 'open_via_client',
+            'click .mobile-menu-control': 'showMobileMenu',
+            'click .mobile-menu-mask': 'closeMobileMenu',
+            'click .download-close-menu': 'hideMobileMenu'
         },
 
         _hideMenu: function() {
@@ -361,6 +364,7 @@ define([
         },
 
         share: function() {
+            this.hideMobileMenu();
             var dir = this.dir,
                 obj_name = this.model.get('obj_name'),
                 dirent_path = Common.pathJoin([dir.path, obj_name]);
@@ -386,6 +390,7 @@ define([
         },
 
         del: function() {
+            this.hideMobileMenu();
             var _this = this;
             if (this.model.get('is_img')) {
                 var index = $('.img-name-link', this.dirView.$table).index(this.$('.img-name-link'));
@@ -412,6 +417,7 @@ define([
         },
 
         rename: function() {
+            this.hideMobileMenu();
             var _this = this;
             var dirent_name = this.model.get('obj_name');
 
@@ -523,6 +529,7 @@ define([
         },
 
         mvcp: function(e) {
+            this.hideMobileMenu();
             var op_type = $(e.currentTarget).hasClass('mv') ? 'mv' : 'cp';
             var options = {
                 'dir': this.dir,
@@ -543,6 +550,7 @@ define([
         },
 
         setFolderPerm: function() {
+            this.hideMobileMenu();
             var options = {
                 'obj_name': this.model.get('obj_name'),
                 'dir_path': this.dir.path,
@@ -558,6 +566,7 @@ define([
         },
 
         lockFile: function() {
+            this.hideMobileMenu();
             var _this = this;
             this._hideMenu();
             this.model.lockFile({
@@ -572,6 +581,7 @@ define([
         },
 
         unlockFile: function() {
+            this.hideMobileMenu();
             var _this = this;
             this._hideMenu();
             this.model.unlockFile({
@@ -586,6 +596,7 @@ define([
         },
 
         viewDetails: function() {
+            this.hideMobileMenu();
             if (this.dirView.fileCommentsView.$el.is(':visible')) {
                 this.dirView.fileCommentsView.hide();
             }
@@ -690,6 +701,7 @@ define([
         },
 
         viewFileComments: function() {
+            this.hideMobileMenu();
             if (this.dirView.direntDetailsView.$el.is(':visible')) {
                 this.dirView.direntDetailsView.hide();
             }
@@ -708,8 +720,29 @@ define([
         },
 
         open_via_client: function() {
+            this.hideMobileMenu();
             this._hideMenu();
             return true;
+        },
+
+        showMobileMenu : function(event) {
+            var mobileMenu = this.mobileMenu.length ? this.mobileMenu : null;
+            if(mobileMenu){
+                mobileMenu.slideDown('fast');
+            }
+            return false;
+        },
+
+        hideMobileMenu: function() {
+            var mobileMenu = this.mobileMenu.length ? this.mobileMenu : null;
+            if(mobileMenu){
+                mobileMenu.slideUp('fast');
+            }
+        },
+
+        closeMobileMenu: function(){
+            this.hideMobileMenu();
+            return false;
         }
 
     });
