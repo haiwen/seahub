9f10eff92c4fd95383065e798d9cc11e7bafe1f1
cainiao222 <1325442034@qq.com>
2018-07-26 17:40:49 +0800

    use latest seafile-js and add a translate to back to parent direction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import SeafileEditor from '@seafile/seafile-editor';
 import 'whatwg-fetch';
-import { SeafileAPI } from './seafile-js'
-
-
+import { SeafileAPI } from 'seafile-js';
+import cookie from 'react-cookies'
 let repoID = window.app.pageOptions.repoID;
 let filePath = window.app.pageOptions.filePath;
 let fileName = window.app.pageOptions.fileName;
@@ -12,27 +11,14 @@ let domain = window.app.pageOptions.domain;
 let protocol = window.app.pageOptions.protocol;
 
 let dirPath = '/';
-console.log(window.app);
+
 const serviceUrl = window.app.config.serviceUrl;
 const seafileCollabServer = window.app.config.seafileCollabServer;
 const userInfo = window.app.userInfo;
-
-const updateUrl = `${siteRoot}api2/repos/${repoID}/update-link/?p=${dirPath}`;
-const uploadUrl = `${siteRoot}api2/repos/${repoID}/upload-link/?p=${dirPath}&from=web`;
+// init seafileAPI
 let seafileAPI = new SeafileAPI();
-function updateFile(uploadLink, filePath, fileName, content) {
-  var formData = new FormData();
-  formData.append("target_file", filePath);
-  formData.append("filename", fileName);
-  var blob = new Blob([content], { type: "text/plain"});
-  formData.append("file", blob);
-  return fetch(uploadLink, {
-    method: "POST",
-    body: formData,
-    mode: 'no-cors',
-  });
-}
-
+let xcsrfHeaders = cookie.load('csrftoken');
+seafileAPI.initForSeahubUsage({ xcsrfHeaders });
 function getImageFileNameWithTimestamp() {
   var d = Date.now();
   return "image-" + d.toString() + ".png";
@@ -47,33 +33,24 @@ class EditorUtilities {
   }
   
   saveContent(content) {
-    console.log(updateUrl);
-    return (fetch(updateUrl, {credentials: 'same-origin'})
-      .then(res => res.json())
-      .then(res => {
-        return updateFile(res, filePath, fileName, content)
+    return (
+      seafileAPI.getUpdateLink(repoID, dirPath).then((res) => {
+        const uploadLink = res.data;
+        return seafileAPI.updateFile(uploadLink, filePath, fileName, content)
       })
-    );
+    )
   }
 
   unStarFile () {
-
-    return seafileAPI.unStarFile(this.repoID,this.filePath);
+    return (
+      seafileAPI.unStarFile(repoID, this.filePath)
+    )
   }
 
   starFile() {
-    console.log('starFile');
-    let form = new FormData();
-    form.append('repo_id', repoID);
-    form.append('p', filePath);
     return (
-      fetch('/api2/starredfiles/',{
-        method: "POST",
-        body: form,
-        credentials: 'same-origin',
-      })
+      seafileAPI.starFile(this.repoID, this.filePath)
     )
-    // return seafileAPI.starFile(this.repoID,this.filePath);
   }
 
   getParentDectionaryUrl() {
@@ -86,11 +63,10 @@ class EditorUtilities {
   }
 
   uploadImage = (imageFile) => {
-    return fetch(uploadUrl, {credentials: 'same-origin'})
-      .then(res => res.json())
-      .then(res => {
-        const uploadLink = res + "?ret-json=1";
-        // change image file name
+    return (
+      seafileAPI.getUploadLink(repoID, dirPath).then((res) => {
+        let uploadLinkComponent = res.data;
+        const uploadLink = uploadLinkComponent + "?ret-json=1";
         const name = getImageFileNameWithTimestamp();
         const blob = imageFile.slice(0, -1, 'image/png');
         const newFile = new File([blob], name, {type: 'image/png'});
@@ -98,19 +74,16 @@ class EditorUtilities {
         formData.append("parent_dir", "/");
         formData.append("relative_path", "images");
         formData.append("file", newFile);
-        // upload the image
-        return fetch(uploadLink, {
-          method: "POST",
-          body: formData,
-        })
-      }).then(resp => {
-        return resp.json();
-      }).then(json => {
-      // The returned json is a list of uploaded files, need to get the first one
-        var filename = json[0].name;
+        return {uploadLink, formData}
+      }).then(({ uploadLink, formData}) => {
+        return seafileAPI.uploadImage(uploadLink, formData)
+      }).then ((res) => {
+        let resArr = res.data[0];
+        let filename = resArr.name;
         return this._getImageURL(filename);
-      });
-    }
+      })
+    )
+  }
 
   getFileURL(fileNode) {
     var url;
@@ -128,20 +101,16 @@ class EditorUtilities {
   }
 
   getFiles() {
-    const dirUrl = `${siteRoot}api2/repos/${repoID}/dir/?p=${dirPath}&recursive=1`
-    return fetch(dirUrl, {credentials: 'same-origin'})
-      .then(res => res.json())
-      .then(items => {
-        const files = items.map(item => {
-          return {
-            name: item.name,
-            type: item.type === 'dir' ? 'dir' : 'file',
-            isExpanded: item.type === 'dir' ? true : false,
-            parent_path: item.parent_dir,
-          }
-        })
-        return files;
+    return seafileAPI.listDir(repoID, dirPath, { recursive: true} ).then((response) => {
+      var files = response.data.map((item) => {
+        return {
+          name: item.name,
+          type: item.type === 'dir' ? 'dir' : 'file',
+          parent_path: item.parent_dir
+        }
       })
+      return files;
+    })
   }
 }
 
@@ -169,35 +138,27 @@ class App extends React.Component {
     }
 
   componentDidMount() {
-    const path = encodeURIComponent(filePath)
-    const url = `${siteRoot}api2/repos/${repoID}/file/?p=${path}&reuse=1`;
-    const infoPath =`${siteRoot}api2/repos/${repoID}/file/detail/?p=${path}`;
 
-    fetch(infoPath, {credentials:'same-origin'})
-      .then((response) => response.json())
-      .then(res => {
-        let { mtime, size, starred } = res;
-        this.setState((prevState, props) => ({
-          fileInfo: {
-            ...prevState.fileInfo,
-            mtime,
-            size,
-            starred
-          }
-        }));
+    seafileAPI.getFileInfo(repoID, filePath).then((res) => {
+      let { mtime, size, starred } = res.data;
+      this.setState((prevState, props) => ({
+        fileInfo: {
+          ...prevState.fileInfo,
+          mtime,
+          size,
+          starred
+        }
+      }));
 
-      fetch(url, {credentials: 'same-origin'})
-        .then(res => res.json())
-        .then(res => {
-          fetch(res)
-            .then(response => response.text())
-            .then(body => {
-              this.setState({
-                markdownContent: body,
-                loading: false
-            });
+      seafileAPI.getFileDownloadLink(repoID, filePath).then((res) => {
+        const downLoadUrl = res.data;
+        seafileAPI.getFileContent(downLoadUrl).then((res) => {
+          this.setState({
+            markdownContent: res.data,
+            loading: false
+          })
         })
-      })
+      });
     })
   }
 
