127e7f949a918807e22ea8f96f623a69ef31aa14
zhengxie <xiez1989@gmail.com>
2018-08-10 15:29:49 +0800

    [travis] Update

--- .travis/rebuild-branches.sh
+++ .travis/rebuild-branches.sh
@@ -9,6 +9,6 @@ curl -s -X POST \
    -H "Content-Type: application/json" \
    -H "Accept: application/json" \
    -H "Travis-API-Version: 3" \
-   -H "Authorization: ${TRAVIS_API_ACCESS_TOKEN}" \
+   -H "Authorization: token ${TRAVIS_API_ACCESS_TOKEN}" \
    -d "$body" \
    https://api.travis-ci.org/repo/haiwen%2Fseahub/requests
