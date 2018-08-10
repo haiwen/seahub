fbbec5ae54ccec55cd88b32011838bbac0e559d8
shanshuirenjia <978987373@qq.com>
2018-08-10 14:20:49 +0800

    change the implement method of wikiloading

--- frontend/src/components/MainPanel.js
+++ frontend/src/components/MainPanel.js
@@ -4,20 +4,8 @@ import MarkdownViewer from './markdown-viewer';
 import Account from './account';
 import { repoID, serviceUrl, slug, siteRoot } from './constance';
 import { processorGetAST } from '@seafile/seafile-editor/src/lib/seafile-markdown2html';
-import WikiOutline from './wiki-outline';
-
-
 
 class MainPanel extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      navItems: [],
-      activeId: '',
-      isLoading: true,
-    }
-  }
 
   onMenuClick = () => {
     this.props.onMenuClick();
@@ -27,78 +15,6 @@ class MainPanel extends Component {
     // const w=window.open('about:blank')
     e.preventDefault();
     window.location.href= serviceUrl + '/lib/' + repoID + '/file' + this.props.filePath + '?mode=edit';
-  }
-
-  scrollHandler = (event) => {
-    var target = event.target || event.srcElement;
-    var markdownContainer = this.refs.markdownContainer;
-    var headingList = markdownContainer.querySelectorAll('[id^="user-content"]');
-    var top = target.scrollTop;
-    var defaultOffset = markdownContainer.offsetTop;
-    var currentId = '';
-    for (let i = 0; i < headingList.length; i++) {
-      let heading = headingList[i];
-      if (heading.tagName === 'H1') {
-        continue;
-      }
-      if (top > heading.offsetTop - defaultOffset) {
-        currentId = '#' + heading.getAttribute('id');
-      } else {
-        break;
-      }
-    }
-
-    if (currentId !== this.state.activeId) {
-      this.setState({
-        activeId: currentId
-      })
-    }
-  }
-
-  formatNodeTree(nodeTree) {
-    var navItems = [];
-    var headingList = nodeTree.children.filter(node => {
-      return (node.type === 'heading' && (node.depth === 2 || node.depth === 3));
-    });
-    for (let i = 0; i < headingList.length; i++) {
-      navItems[i] = {};
-      navItems[i].id = '#user-content-' + headingList[i].data.id
-      navItems[i].key = i;
-      navItems[i].clazz = '';
-      navItems[i].depth = headingList[i].depth;
-      navItems[i].text = this.getHeadigTextValue(headingList[i]);
-    }
-    return navItems;
-  }
-
-  getHeadigTextValue(heading) {
-    while (heading.children) {
-      heading = heading.children[0];
-    }
-    return heading.value;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    var _this = this;
-    var content = nextProps.content;
-    processorGetAST.run(processorGetAST.parse(content)).then((nodeTree) => {
-      if (nodeTree && nodeTree.children && nodeTree.children.length) {
-        var navItems = _this.formatNodeTree(nodeTree);
-        var currentId = navItems.length > 0 ? navItems[0].id : 0;
-        _this.setState({
-          navItems: navItems,
-          activeId: currentId
-        })
-      }
-    });
-  }
-
-  onLoadComplete = (flag) => {
-    if (flag) {
-      this.setState({
-        isLoading: false
-      })
-    }
   }
 
   render() {
@@ -114,7 +30,7 @@ class MainPanel extends Component {
     });
 
     return (
-      <div className="main-panel o-hidden">
+      <div className="wiki-main-panel o-hidden">
         <div className="main-panel-top panel-top">
           <span className="sf2-icon-menu side-nav-toggle hidden-md-up d-md-none" title="Side Nav Menu" onClick={this.onMenuClick}></span>
            <div className={`wiki-page-ops ${this.props.permission === 'rw' ? '' : 'hide'}`}>
@@ -134,24 +50,12 @@ class MainPanel extends Component {
               {pathElem}
             </div>
           </div>
-          <div className="cur-view-content-container" onScroll={this.scrollHandler}>
-            <span className={`loading-icon loading-tip ${this.state.isLoading ? "" : "wiki-hide"}`}></span>
-            <div className={`cur-view-content ${this.state.isLoading ? "hide" : ""}`} ref="markdownContainer">
-                <MarkdownViewer
-                  markdownContent={this.props.content}
-                  onLinkClick={this.props.onLinkClick}
-                  isLoading = {this.state.isLoading}
-                  onLoadComplete={this.onLoadComplete}
-                />
-                <p id="wiki-page-last-modified">Last modified by {this.props.latestContributor}, <span>{this.props.lastModified}</span></p>
-            </div>
-            <div className={`cur-view-content-outline ${this.state.isLoading ? "hide" : ""}`}>
-                <WikiOutline 
-                  navItems={this.state.navItems} 
-                  handleNavItemClick={this.handleNavItemClick}
-                  activeId={this.state.activeId}
-                />
-            </div>
+          <div className="cur-view-container">
+            <MarkdownViewer
+              markdownContent={this.props.content}
+              latestContributor={this.props.latestContributor}
+              onLinkClick={this.props.onLinkClick}
+            />
           </div>
         </div>
     </div>
