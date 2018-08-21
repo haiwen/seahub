201a86b3dd2fbf66c336c5ea2f4adcea4a6def60
gpd <369318816@qq.com>
2018-08-21 11:05:55 +0800

    token_add_time

--- seahub/settings.py
+++ seahub/settings.py
@@ -137,7 +137,6 @@ WSGI_APPLICATION = 'seahub.wsgi.application'
 # api token expiration time.
 #API_TOKEN_AGE = -1
 API_TOKEN_AGE = 7 * 24 * 60 * 60
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
