ac6453a6be5fa2b7d7a6373ca734c387cf0391fe
shanshuirenjia <978987373@qq.com>
2018-08-30 11:07:52 +0800

    optimized code

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -7,12 +7,9 @@ class Notification extends React.Component {
       showNotice: false,
       notice_html: ''
     }
-  
-    this.onClick = this.onClick.bind(this);
-    this.loadNotices = this.loadNotices.bind(this);
   }
 
-  onClick() {
+  onClick = () => {
     this.setState({
       showNotice: !this.state.showNotice
     })
@@ -21,34 +18,32 @@ class Notification extends React.Component {
     }
   }
 
-  loadNotices() {
-    this.props.seafileAPI.getPopupNotices().then(
-      res => {
-        this.setState({
-          notice_html: res.data.notice_html
-        })
-      }) 
+  loadNotices = () => {
+    this.props.seafileAPI.getPopupNotices().then(res => {
+      this.setState({
+        notice_html: res.data.notice_html
+      })
+    }) 
   }
 
   render() {
     return (
       <div id="notifications">
-          <a href="#" onClick={this.onClick} className="no-deco" id="notice-icon" title="Notifications" aria-label="Notifications">
-              <span className="sf2-icon-bell"></span>
-              <span className="num hide">0</span>
-          </a>
-          <div id="notice-popover" className={`sf-popover ${this.state.showNotice ? '': 'hide'}`}>
-            <div className="outer-caret up-outer-caret"><div className="inner-caret"></div></div>
-            <div className="sf-popover-hd ovhd">
-                <h3 className="sf-popover-title">通知</h3>
-                <a href="#" onClick={this.onClick} title="关闭" aria-label="关闭" className="sf-popover-close js-close sf2-icon-x1 op-icon float-right"></a>
-            </div>
-            <div className="sf-popover-con">
-                <ul className="notice-list" dangerouslySetInnerHTML={{__html: this.state.notice_html}}>
-                </ul>
-                <a href="/notification/list/" className="view-all">查看所有提醒。</a>
-            </div>
+        <a href="#" onClick={this.onClick} className="no-deco" id="notice-icon" title="Notifications" aria-label="Notifications">
+          <span className="sf2-icon-bell"></span>
+          <span className="num hide">0</span>
+        </a>
+        <div id="notice-popover" className={`sf-popover ${this.state.showNotice ? '': 'hide'}`}>
+          <div className="outer-caret up-outer-caret"><div className="inner-caret"></div></div>
+          <div className="sf-popover-hd ovhd">
+            <h3 className="sf-popover-title">通知</h3>
+            <a href="#" onClick={this.onClick} title="关闭" aria-label="关闭" className="sf-popover-close js-close sf2-icon-x1 op-icon float-right"></a>
           </div>
+          <div className="sf-popover-con">
+            <ul className="notice-list" dangerouslySetInnerHTML={{__html: this.state.notice_html}}></ul>
+            <a href="/notification/list/" className="view-all">查看所有提醒。</a>
+          </div>
+        </div>
       </div>
     )
   }
--- frontend/src/components/main-side-nav.js
+++ frontend/src/components/main-side-nav.js
@@ -1,11 +1,6 @@
 import React from 'react';
 const siteRoot = window.app.config.siteRoot;
 const serverRoot = window.app.config.serverRoot;
-const logoPath =  window.app.config.logoPath;
-const mediaUrl = window.app.config.mediaUrl;
-const siteTitle = window.app.config.siteTitle;
-const logoWidth = window.app.config.logoWidth;
-const logoHeight = window.app.config.logoHeight;
 
 class  MainSideNav extends React.Component {
   constructor(props) {
@@ -14,11 +9,11 @@ class  MainSideNav extends React.Component {
       groupsExtended: false,
       sharedExtended: false,
       closeSideBar:false,
-      groupItems: [],
-      tabHeight: 0
+      groupItems: []
     };
 
-    this.listHeight = 24;
+    this.listHeight = 24; //for caculate tabheight
+    this.groupsHeight = 0;
     this.adminHeight = 0;
   }
 
@@ -39,12 +34,9 @@ class  MainSideNav extends React.Component {
     let _this = this;
     this.props.seafileAPI.getGroups().then(res =>{
       let data   = res.data.groups;
-      let groups = {name: 'All Groups', id: '', isGroups: true};
-      data.unshift(groups);
-      let tabHeight = data.length * _this.listHeight;
+      this.groupsHeight = (data.length + 1) * _this.listHeight;
       _this.setState({
-        groupItems: data,
-        tabHeight: tabHeight
+        groupItems: data
       })
     })
   }
@@ -52,20 +44,22 @@ class  MainSideNav extends React.Component {
   renderSharedGroups() {
     let style = {height: 0};
     if (this.state.groupsExtended) {
-      style = {height: this.state.tabHeight};
+      style = {height: this.groupsHeight};
     }
     return (
-      <ul ref="groupShared" className={`grp-list ${this.state.groupsExtended ? 'side-panel-slide' : 'side-panel-slide-up'}`} style={style}>
+      <ul className={`grp-list ${this.state.groupsExtended ? 'side-panel-slide' : 'side-panel-slide-up'}`} style={style}>
+        <li> 
+          <a href={siteRoot + '#groups/'}>
+          <span className="sharp" aria-hidden="true">#</span>All Groups</a>
+        </li>
         {this.state.groupItems.map(item => {
-          let prefix = item.isGroups ? '#groups/' : '#group/';
           return (
-            <li key={item.id}> 
-              <a href={siteRoot + prefix + item.id + '/'}>
+            <li> 
+              <a href={siteRoot + '#group/' + item.id + '/'}>
               <span className="sharp" aria-hidden="true">#</span>{item.name}</a>
             </li>
           )
-        }
-        )}
+        })}
       </ul>
     )
   }
@@ -74,14 +68,13 @@ class  MainSideNav extends React.Component {
     let height = 0;
     if (this.state.sharedExtended) {
       if (!this.adminHeight) {
-        let childLength = this.refs.adminShared.childNodes.length;
-        this.adminHeight = childLength * this.listHeight;
+        this.adminHeight = 3 * this.listHeight;
       }
       height = this.adminHeight;
     }
     let style = {height: height};
     return (
-      <ul ref="adminShared" className={`${this.state.sharedExtended ? 'side-panel-slide' : 'side-panel-slide-up'}`} style={style} >
+      <ul className={`${this.state.sharedExtended ? 'side-panel-slide' : 'side-panel-slide-up'}`} style={style} >
         <li>
           <a href={siteRoot + '#share-admin-libs/'}><span aria-hidden="true" className="sharp">#</span>Libraries</a>
         </li>
@@ -105,7 +98,7 @@ class  MainSideNav extends React.Component {
             <li className="tab"><a href={serverRoot + siteRoot + '#shared-libs/'} className="ellipsis" title="Shared with me"><span className="sf2-icon-share" aria-hidden="true"></span>Shared with me</a></li>
             <li className="tab"><a href={serverRoot + siteRoot + '#org/'} className="ellipsis" title="Shared with all"><span className="sf2-icon-organization" aria-hidden="true"></span>Shared with all</a></li>
             <li className="tab" id="group-nav">
-              <a className="ellipsis user-select" title="Shared with groups" onClick={this.grpsExtend}><span className={`toggle-icon float-right ${this.state.groupsExtended ?'icon-caret-down':'icon-caret-left'}`} aria-hidden="true"></span><span className="sf2-icon-group" aria-hidden="true"></span>Shared with groups</a>
+              <a className="ellipsis user-select-no" title="Shared with groups" onClick={this.grpsExtend}><span className={`toggle-icon float-right ${this.state.groupsExtended ?'icon-caret-down':'icon-caret-left'}`} aria-hidden="true"></span><span className="sf2-icon-group" aria-hidden="true"></span>Shared with groups</a>
               {this.renderSharedGroups()}
             </li>
           </ul>
@@ -118,7 +111,7 @@ class  MainSideNav extends React.Component {
             <li className="tab"><a href={siteRoot + 'dashboard'}><span className="sf2-icon-clock" aria-hidden="true"></span>Acitivities</a></li>
             <li className="tab"><a href={siteRoot + '#devices/'} className="ellipsis" title="Linked Devices"><span className="sf2-icon-monitor" aria-hidden="true"></span>Linked Devices</a></li>
             <li className="tab" id="share-admin-nav">
-              <a className="ellipsis user-select" title="Share Admin" onClick={this.shExtend}><span className={`toggle-icon float-right ${this.state.sharedExtended ? 'icon-caret-down':'icon-caret-left'}`} aria-hidden="true"></span><span aria-hidden="true" className="sf2-icon-wrench"></span>Share Admin</a>
+              <a className="ellipsis user-select-no" title="Share Admin" onClick={this.shExtend}><span className={`toggle-icon float-right ${this.state.sharedExtended ? 'icon-caret-down':'icon-caret-left'}`} aria-hidden="true"></span><span aria-hidden="true" className="sf2-icon-wrench"></span>Share Admin</a>
               {this.renderSharedAdmin()}
             </li>
           </ul>
