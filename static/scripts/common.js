0f59f84804b03b85ca671090a4c789a0778d5d9d
zhengxie <xiez1989@gmail.com>
2018-08-30 11:40:45 +0800

    Use custom csrf cookie name to avoid name conflict

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -530,7 +530,7 @@ define([
             }
             if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
                 // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+                xhr.setRequestHeader("X-CSRFToken", getCookie(app.config.csrfCookieName));
             }
         },
 
