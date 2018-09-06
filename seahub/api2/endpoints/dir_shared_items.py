4b71739f79a0d696d8174e1bce3f9c28bfa92fd4
gpd <369318816@qq.com>
2018-09-06 14:05:37 +0800

    new_shared

--- seahub/api2/endpoints/dir_shared_items.py
+++ seahub/api2/endpoints/dir_shared_items.py
@@ -411,6 +411,7 @@ class DirSharedItemsEndpoint(APIView):
                         'error_msg': 'group_id %s invalid.' % gid
                         })
                     continue
+
                 gpd = models.GroupOptions.objects.filter(group_id=gid, option_key='shared_repo').first()
                 if not int(gpd.option_val):
                     if not is_group_admin_or_owner(gid, username):
