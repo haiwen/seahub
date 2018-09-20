c18f7827e561e5393fdd1bc20761ff7276b58935
shanshuirenjia <978987373@qq.com>
2018-09-20 14:39:45 +0800

    modify config file

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -34,35 +34,30 @@ module.exports = {
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: {
-    main: [
-      // We ship a few polyfills by default:
-      require.resolve('./polyfills'),
-      // Include an alternative client for WebpackDevServer. A client's job is to
-      // connect to WebpackDevServer by a socket and get notified about changes.
-      // When you save a file, the client will either apply hot updates (in case
-      // of CSS changes), or refresh the page (in case of JS changes). When you
-      // make a syntax error, this client will display a syntax error overlay.
-      // Note: instead of the default WebpackDevServer client, we use a custom one
-      // to bring better experience for Create React App users. You can replace
-      // the line below with these two lines if you prefer the stock client:
-      // require.resolve('webpack-dev-server/client') + '?/',
-      // require.resolve('webpack/hot/dev-server'),
-      require.resolve('react-dev-utils/webpackHotDevClient'),
-      // Finally, this is your app's code:
-      paths.appIndexJs,
-      // We include the app code last so that if there is a runtime error during
-      // initialization, it doesn't blow up the WebpackDevServer client, and
-      // changing JS code would still trigger a refresh.
-   ],
+  //   main: [
+  //     // We ship a few polyfills by default:
+  //     require.resolve('./polyfills'),
+  //     // Include an alternative client for WebpackDevServer. A client's job is to
+  //     // connect to WebpackDevServer by a socket and get notified about changes.
+  //     // When you save a file, the client will either apply hot updates (in case
+  //     // of CSS changes), or refresh the page (in case of JS changes). When you
+  //     // make a syntax error, this client will display a syntax error overlay.
+  //     // Note: instead of the default WebpackDevServer client, we use a custom one
+  //     // to bring better experience for Create React App users. You can replace
+  //     // the line below with these two lines if you prefer the stock client:
+  //     // require.resolve('webpack-dev-server/client') + '?/',
+  //     // require.resolve('webpack/hot/dev-server'),
+  //     require.resolve('react-dev-utils/webpackHotDevClient'),
+  //     // Finally, this is your app's code:
+  //     paths.appIndexJs,
+  //     // We include the app code last so that if there is a runtime error during
+  //     // initialization, it doesn't blow up the WebpackDevServer client, and
+  //     // changing JS code would still trigger a refresh.
+  //  ],
    wiki: [
      require.resolve('./polyfills'),
      require.resolve('react-dev-utils/webpackHotDevClient'),
      paths.appSrc + "/wiki.js",
-   ],
-   dashboard: [
-     require.resolve('./polyfills'),
-     require.resolve('react-dev-utils/webpackHotDevClient'),
-     paths.appSrc + "/dashboard.js",
    ],
    repoview: [
      require.resolve('./polyfills'),
@@ -74,11 +69,11 @@ module.exports = {
      require.resolve('react-dev-utils/webpackHotDevClient'),
      paths.appSrc + "/file-history.js",
    ],
-   drafts: [
+   app: [
     require.resolve('./polyfills'),
     require.resolve('react-dev-utils/webpackHotDevClient'),
-    paths.appSrc + "/drafts.js",
-  ]
+    paths.appSrc + "/app.js",
+   ]
   },
 
   output: {
