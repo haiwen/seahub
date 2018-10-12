c085d81438f174278cdc460ebd106a16baea6b6a
plt <daniel.pan@seafile.com>
2018-10-12 18:30:26 +0800

    Support open draw file

--- frontend/src/draw.js
+++ frontend/src/draw.js
@@ -6,15 +6,12 @@ var mxCodec = window.mxCodec;
 var mxUtils = window.mxUtils;
 
 function loadFile(editorUi) {
-  return seafileAPI.getFileDownloadLink('3e1b6ebf-c564-4cd2-87e9-69070c260fb4', '/mx-graph-v2.xml')
-  .then((res) => {
-    return seafileAPI.getFileContent(res.data).then((res) => {
-      var doc = window.mxUtils.parseXml(res.data);
-      console.log(doc.documentElement);
-      editorUi.editor.setGraphXml(doc.documentElement);
-      editorUi.editor.setModified(false);
-      editorUi.editor.undoManager.clear();
-    });
+  return seafileAPI.getFileContent(window.app.config.rawPath).then((res) => {
+    var doc = window.mxUtils.parseXml(res.data);
+    //console.log(doc.documentElement);
+    editorUi.editor.setGraphXml(doc.documentElement);
+    editorUi.editor.setModified(false);
+    editorUi.editor.undoManager.clear();
   });
 }
 
