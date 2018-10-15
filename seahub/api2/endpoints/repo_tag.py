f82ac948c232428018f7107925c332c8b095a9f2
wangjianhui <wang@163.com>
2018-10-15 11:46:14 +0800

    repo-tags

--- seahub/api2/endpoints/repo_tag.py
+++ seahub/api2/endpoints/repo_tag.py
@@ -9,8 +9,7 @@ from rest_framework import status
 
 from seahub.api2.authentication import TokenAuthentication
 from seahub.api2.throttling import UserRateThrottle
-from seahub.api2.permissions import IsRepoOwner
-from seahub.repo_tag.models import RepoTags, Tags
+from seahub.repo_tag.models import RepoTags
 from seahub.api2.utils import api_error
 from seahub.views import check_folder_permission
 
@@ -21,20 +20,13 @@ logger = logging.getLogger(__name__)
 
 class RepoTagsView(APIView):
     authentication_classes = (TokenAuthentication, SessionAuthentication)
-    permission_classes = (IsAuthenticated, IsRepoOwner)
+    permission_classes = (IsAuthenticated, )
     throttle_classes = (UserRateThrottle, )
 
     def get(self, request, repo_id):
 
-        # argument check
-        try:
-            repo = seafile_api.get_repo(repo_id)
-        except Exception as e:
-            logger.error(e)
-            error_msg = 'Internal Server Error.'
-            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
-
         # resource check
+        repo = seafile_api.get_repo(repo_id)
         if not repo:
             error_msg = 'Library %s not found.' % repo_id
             return api_error(status.HTTP_404_NOT_FOUND, error_msg)
@@ -44,110 +36,131 @@ class RepoTagsView(APIView):
             error_msg = 'Permission denied.'
             return api_error(status.HTTP_403_FORBIDDEN, error_msg)
 
-        tag_set = []
-        for repo_tag in RepoTags.objects.get_all_by_repo_id(repo_id):
-            tag_set.append(repo_tag.to_dict())
+        tag_list = []
+        try:
+            repo_tag_list = RepoTags.objects.get_all_by_repo_id(repo_id)
+        except Exception as e:
+            logger.error(e)
+            error_msg = 'Internal Server Error.'
+            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
 
-        return Response({"tag_set": tag_set}, status=status.HTTP_200_OK)
+        for repo_tag in repo_tag_list:
+            tag_list.append(repo_tag.to_dict())
+
+        return Response({"tag_list": tag_list}, status=status.HTTP_200_OK)
 
     def post(self, request, repo_id):
 
         # argument check
-        tag_name = request.data.get('tag_name')
+        tag_name = request.data.get('name')
         if not tag_name:
-            error_msg = 'tag_name invalid.'
+            error_msg = 'name invalid.'
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
-        tag_color = request.data.get('tag_color')
+        tag_color = request.data.get('color')
         if not tag_color:
-            error_msg = 'tag_color invalid.'
+            error_msg = 'color invalid.'
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
         # resource check
-        try:
-            repo = seafile_api.get_repo(repo_id)
-        except Exception as e:
-            logger.error(e)
-            error_msg = 'Internal Server Error.'
-            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
+        repo = seafile_api.get_repo(repo_id)
         if not repo:
             error_msg = 'Library %s not found.' % repo_id
             return api_error(status.HTTP_404_NOT_FOUND, error_msg)
 
         # permission check
-        if check_folder_permission(request, repo_id, '/') is None:
+        if check_folder_permission(request, repo_id, '/') != 'rw':
             error_msg = 'Permission denied.'
             return api_error(status.HTTP_403_FORBIDDEN, error_msg)
 
-        repo_tag, created = RepoTags.objects.create_repo_tag(repo_id, tag_name, tag_color)
+        try:
+            repo_tag = RepoTags.objects.get_or_create_repo_tag(repo_id, tag_name, tag_color)
+        except Exception as e:
+            logger.error(e)
+            error_msg = 'Internal Server Error.'
+            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
 
         return Response({"repo_tag": repo_tag.to_dict()}, status=status.HTTP_201_CREATED)
 
     def put(self, request, repo_id):
+
         # argument check
         tag_id = request.GET.get('id')
+        if not tag_id:
+            error_msg = 'id invalid.'
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+
+        tag_name = request.data.get('name')
+        if not tag_name:
+            error_msg = 'name invalid.'
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+
+        tag_color = request.data.get('color')
+        if not tag_color:
+            error_msg = 'color invalid.'
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+
+        # resource check
+        repo = seafile_api.get_repo(repo_id)
+        if not repo:
+            error_msg = 'Library %s not found.' % repo_id
+            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
+
+        try:
+            repo_tag = RepoTags.objects.get(pk=tag_id)
+        except Exception as e:
+            logger.error(e)
+            error_msg = 'Internal Server Error.'
+            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
+        if not repo_tag:
+            error_msg = 'repo_tag not found.'
+            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
+
+        # permission check
+        if check_folder_permission(request, repo_id, '/') != 'rw':
+            error_msg = 'Permission denied.'
+            return api_error(status.HTTP_403_FORBIDDEN, error_msg)
+
+        try:
+            repo_tag.name = tag_name
+            repo_tag.color = tag_color
+            repo_tag.save()
+        except Exception as e:
+            logger.error(e)
+            error_msg = 'Internal Server Error.'
+            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
+
+        return Response({"repo_tag": repo_tag.to_dict()}, status=status.HTTP_200_OK)
+
+    def delete(self, request, repo_id):
+
+        # argument check
+        tag_id = request.GET.get('id')
+        if not tag_id:
+            error_msg = 'id invalid.'
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+
+        # resource check
+        repo = seafile_api.get_repo(repo_id)
+        if not repo:
+            error_msg = 'Library %s not found.' % repo_id
+            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
+
         repo_tag = RepoTags.objects.get(pk=tag_id)
         if not repo_tag:
             error_msg = 'repo_tag not found.'
             return api_error(status.HTTP_404_NOT_FOUND, error_msg)
 
-        tag_name = request.data.get('tag_name')
-        if not tag_name:
-            error_msg = 'tag_name invalid.'
-            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+        # permission check
+        if check_folder_permission(request, repo_id, '/') != 'rw':
+            error_msg = 'Permission denied.'
+            return api_error(status.HTTP_403_FORBIDDEN, error_msg)
 
-        tag_color = request.data.get('tag_color')
-        if not tag_color:
-            error_msg = 'tag_color invalid.'
-            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
-
-        # resource check
         try:
-            repo = seafile_api.get_repo(repo_id)
+            RepoTags.objects.delete_repo_tag(tag_id)
         except Exception as e:
             logger.error(e)
             error_msg = 'Internal Server Error.'
             return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
-        if not repo:
-            error_msg = 'Library %s not found.' % repo_id
-            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
 
-        # permission check
-        if check_folder_permission(request, repo_id, '/') is None:
-            error_msg = 'Permission denied.'
-            return api_error(status.HTTP_403_FORBIDDEN, error_msg)
-
-        old_tag_name = repo_tag.to_dict()['tag_name']
-        tag = Tags.objects.get(name=old_tag_name)
-        tag.name, tag.color = tag_name, tag_color
-        tag.save()
-        repo_tag = RepoTags.objects.get(pk=tag_id)
-
-        return Response({"repo_tag": repo_tag.to_dict()}, status=status.HTTP_200_OK)
-
-    def delete(self, request, repo_id):
-        # argument check
-        tag_id = request.GET.get('id')
-        repo_tag = RepoTags.objects.get(pk=tag_id)
-        if not repo_tag:
-            error_msg = 'Repotag not found.'
-            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
-
-        # resource check
-        try:
-            repo = seafile_api.get_repo(repo_id)
-        except Exception as e:
-            logger.error(e)
-            error_msg = 'Internal Server Error.'
-            return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
-        if not repo:
-            error_msg = 'Library %s not found.' % repo_id
-            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
-
-        # permission check
-        if check_folder_permission(request, repo_id, '/') is None:
-            error_msg = 'Permission denied.'
-            return api_error(status.HTTP_403_FORBIDDEN, error_msg)
-
-        RepoTags.objects.delete_repo_tag(tag_id)
         return Response({"success": "true"}, status=status.HTTP_200_OK)
--- seahub/repo_tag/models.py
+++ seahub/repo_tag/models.py
@@ -5,54 +5,29 @@ from django.db import models
 from seaserv import seafile_api
 
 
-class TagsManager(models.Manager):
-
-    def get_or_create_tag(self, tag_name, tag_color):
-        try:
-            return super(TagsManager, self).get(name=tag_name, color=tag_color)
-        except self.model.DoesNotExist:
-            tag = self.model(name=tag_name, color=tag_color)
-            tag.save()
-            return tag
-
-
 class RepoTagsManager(models.Manager):
 
     def get_all_by_repo_id(self, repo_id):
-        return [tag for tag in super(RepoTagsManager, self).all().filter(repo_id=repo_id)]
+        return [repo_tag for repo_tag in super(RepoTagsManager, self).all().filter(repo_id=repo_id)]
 
-    def get_one_repo_tag(self, repo_id, tag_name, tag_color):
+    def get_or_create_repo_tag(self, repo_id, tag_name, tag_color):
         try:
-            return super(RepoTagsManager, self).get(repo_id=repo_id, tag__name=tag_name, tag__color=tag_color)
+            return super(RepoTagsManager, self).get(repo_id=repo_id, name=tag_name, color=tag_color)
         except self.model.DoesNotExist:
-            return None
-
-    def create_repo_tag(self, repo_id, tag_name, tag_color):
-        repo_tag = self.get_one_repo_tag(repo_id, tag_name, tag_color)
-        if repo_tag is None:
-            tag = Tags.objects.get_or_create_tag(tag_name, tag_color)
-            repo_tag = self.model(repo_id=repo_id, tag=tag)
+            repo_tag = self.model(repo_id=repo_id, name=tag_name, color=tag_color)
             repo_tag.save()
-            return repo_tag, True
-        else:
-            return repo_tag, False
+            return repo_tag
 
-    def delete_repo_tag(self, id):
-        tags = super(RepoTagsManager, self).filter(pk=id)
-        tags.delete()
-
-
-class Tags(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    color = models.CharField(max_length=255, unique=True)
-
-    objects = TagsManager()
+    def delete_repo_tag(self, tag_id):
+        repo_tag = super(RepoTagsManager, self).get(pk=tag_id)
+        repo_tag.delete()
 
 
 class RepoTags(models.Model):
 
     repo_id = models.CharField(max_length=36, db_index=True)
-    tag = models.ForeignKey("Tags", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
 
     objects = RepoTagsManager()
 
@@ -64,7 +39,6 @@ class RepoTags(models.Model):
         return {
             "id": self.pk,
             "repo_id": self.repo_id,
-            # 'tag_id': self.tag.primary_key,
-            "tag_name": self.tag.name,
-            "tag_color": self.tag.color,
+            "tag_name": self.name,
+            "tag_color": self.color,
         }
