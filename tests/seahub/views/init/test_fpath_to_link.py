cef641baf0f1bb1614fde7b0cbed6fe763564e0c
ilearnit <helloworld.c@outlook.com>
2018-10-19 09:17:12 +0000

    fix encode

--- tests/seahub/views/init/test_fpath_to_link.py
+++ tests/seahub/views/init/test_fpath_to_link.py
@@ -1,10 +1,14 @@
+# encoding: utf-8
 from seahub.test_utils import BaseTestCase
 from seahub.views import fpath_to_link
+from django.utils.http import urlquote
 
 
 class FpathToLinkTest(BaseTestCase):
     def test_fpath_to_link(self):
-        path = '/'
+        path = '/海文/'.decode('utf-8')
         resp = fpath_to_link(self.repo.id, path, is_dir=True)
-        assert '/#common/lib/%(repo_id)s/%(path)s' % {'repo_id': self.repo.id,
-                                    'path': path.encode('utf-8').strip('/')} in resp
+        url = '/#common/lib/%(repo_id)s/%(path)s' % {'repo_id': self.repo.id,
+                                    'path': path.strip('/')}
+
+        assert urlquote(url, safe='/#') in resp
