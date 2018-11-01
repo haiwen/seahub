58804cd1cf3f6ff00cf42759b90d375c1ba39169
shanshuirenjia <978987373@qq.com>
2018-11-01 18:38:43 +0800

    delete not need class name

--- frontend/src/components/dirent-list-view/dirent-list-item.js
+++ frontend/src/components/dirent-list-view/dirent-list-item.js
@@ -333,13 +333,13 @@ class DirentListItem extends React.Component {
             <div className="operations">
               <ul className="operation-group">
                 <li className="operation-group-item">
-                  <i className="sf2-icon-download download" title={gettext('Download')} onClick={this.onItemDownload}></i>
+                  <i className="sf2-icon-download" title={gettext('Download')} onClick={this.onItemDownload}></i>
                 </li>
                 <li className="operation-group-item">
-                  <i className="sf2-icon-share share" title={gettext('Share')} onClick={this.onItemShare}></i>
+                  <i className="sf2-icon-share" title={gettext('Share')} onClick={this.onItemShare}></i>
                 </li>
                 <li className="operation-group-item">
-                  <i className="sf2-icon-delete delete" title={gettext('Delete')} onClick={this.onItemDelete}></i>
+                  <i className="sf2-icon-delete" title={gettext('Delete')} onClick={this.onItemDelete}></i>
                 </li>
                 <li className="operation-group-item">
                   <i className="sf2-icon-caret-down sf-dropdown-toggle" title={gettext('More Operation')} onClick={this.onItemMenuToggle}></i>
