4c64d1edfb43d76a6816795c50f1448eccb04a71
llj <lingjun.li1@gmail.com>
2018-11-21 16:02:41 +0800

    [shared with me] rewrote it with react

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -254,6 +254,38 @@ export const Utils = {
     'admin': gettext("Admin"),
     'cloud-edit': gettext("Preview-Edit-on-Cloud"),
     'preview': gettext("Preview-on-Cloud")
+  },
+
+  formatSize: function(options) {
+    /*
+     * param: {bytes, precision}
+     */
+    var bytes = options.bytes;
+    var precision = options.precision || 0;
+
+    var kilobyte = 1024;
+    var megabyte = kilobyte * 1024;
+    var gigabyte = megabyte * 1024;
+    var terabyte = gigabyte * 1024;
+
+    if ((bytes >= 0) && (bytes < kilobyte)) {
+      return bytes + ' B';
+
+    } else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+      return (bytes / kilobyte).toFixed(precision) + ' KB';
+
+    } else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+      return (bytes / megabyte).toFixed(precision) + ' MB';
+
+    } else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+      return (bytes / gigabyte).toFixed(precision) + ' GB';
+
+    } else if (bytes >= terabyte) {
+      return (bytes / terabyte).toFixed(precision) + ' TB';
+
+    } else {
+      return bytes + ' B';
+    }
   }
 
 };
