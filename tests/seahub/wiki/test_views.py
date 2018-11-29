419c4a6385d90dd5917cb973fa0ae01b7029e301
zhengxie <xiez1989@gmail.com>
2018-11-29 10:58:04 +0800

    [wiki] Redirect old url to new url end with .md

---
+++ tests/seahub/wiki/test_views.py
@@ -0,0 +1,20 @@
+from django.core.urlresolvers import reverse
+
+from seahub.wiki.models import Wiki
+from seahub.test_utils import BaseTestCase
+
+
+class SlugTest(BaseTestCase):
+    def setUp(self, ):
+        self.wiki = Wiki.objects.add('new wiki', self.user.username)
+        self.login_as(self.user)
+
+    # def test_home_page(self, ):
+    #     resp = self.client.get(reverse('wiki:slug', args=['new-wiki']))
+    #     self.assertEqual(200, resp.status_code)
+    #     self.assertTemplateUsed(resp, 'wiki/wiki.html')
+
+    def test_old_home_page(self, ):
+        resp = self.client.get(reverse('wiki:slug', args=['new-wiki', 'home']))
+        self.assertEqual(302, resp.status_code)
+        self.assertRegexpMatches(resp['Location'], '/wikis/new-wiki/home.md')
