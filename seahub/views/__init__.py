60a187e4c7f864e951dd5aabe332662de93ee57c
ilearnit <helloworld.c@outlook.com>
2018-12-04 08:10:24 +0000

    add share days

--- seahub/views/__init__.py
+++ seahub/views/__init__.py
@@ -1215,7 +1215,9 @@ def react_fake_view(request):
 
     return render(request, "react_app.html", {
         'storages': get_library_storages(request),
-        'enable_repo_snapshot_label': settings.ENABLE_REPO_SNAPSHOT_LABEL
+        'enable_repo_snapshot_label': settings.ENABLE_REPO_SNAPSHOT_LABEL,
+        'share_link_expire_days_min': SHARE_LINK_EXPIRE_DAYS_MIN,
+        'share_link_expire_days_max': SHARE_LINK_EXPIRE_DAYS_MAX,
     })
 
 @login_required
