4d3812c96e08128e332aea18bc770a23d58ccc18
shanshuirenjia <978987373@qq.com>
2018-12-11 14:05:10 +0800

    modify wiki-list td line-height

--- frontend/src/components/wiki-list-view/wiki-list-view.js
+++ frontend/src/components/wiki-list-view/wiki-list-view.js
@@ -35,7 +35,7 @@ class WikiListView extends Component {
       return <p className="error text-center">{errorMsg}</p>;
     } else {
       return (
-        <table>
+        <table className="wiki-list-table">
           <thead>
             <tr>
               <th width="50%">{gettext('Name')}</th>
