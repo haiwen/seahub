4b297f5b64f89f001d131a103543de8e1f1e3493
shanshuirenjia <978987373@qq.com>
2018-12-26 17:44:55 +0800

    encapsulate share model

---
+++ frontend/src/models/shared-upload-info.js
@@ -0,0 +1,15 @@
+class SharedUploadInfo {
+   
+  constructor(object) {
+    this.repo_id = object.repo_id;
+    this.repo_name = object.repo_name;
+    this.path = object.path;
+    this.link = object.link;
+    this.obj_name = object.obj_name;
+    this.username = object.username;
+    this.ctime = object.ctime;
+    this.token = object.token;
+    this.view_cnt = object.view_cnt;
+  }
+
+}
