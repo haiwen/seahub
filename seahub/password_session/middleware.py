aa699dd40f23002962a95b85581400356f724d14
zhengxie <xiez1989@gmail.com>
2019-01-02 11:11:16 +0800

    [password session] Disable for non-database users

--- seahub/password_session/middleware.py
+++ seahub/password_session/middleware.py
@@ -8,5 +8,9 @@ class CheckPasswordHash(object):
     """Logout user if value of hash key in session is not equal to current password hash"""
     def process_view(self, request, *args, **kwargs):
         if getattr(request.user, 'is_authenticated') and request.user.is_authenticated():
+            if request.user.enc_password == '!':
+                # Disable for LDAP/Shibboleth/SAML/... users.
+                return None
+
             if request.session.get(PASSWORD_HASH_KEY) != get_password_hash(request.user):
                 logout(request)
