75281e23e39c2670d7201b476b6661beba41f732
shanshuirenjia <978987373@qq.com>
2019-01-10 13:40:01 +0800

    add icon for search item, add repo_name for link-content

--- frontend/src/components/search/search-result-item.js
+++ frontend/src/components/search/search-result-item.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { siteRoot } from '../../utils/constants';
+import { Utils } from '../../utils/utils';
 
 const propTypes = {
   item: PropTypes.object.isRequired,
@@ -15,10 +17,14 @@ class SearchResultItem extends React.Component {
 
   render() {
     let item = this.props.item;
+    let fileIconSize = Utils.isHiDPI() ? 48 : 24;
+    let fileIconUrl = item.is_dir ? siteRoot + 'media/img/folder-192.png' : Utils.getFileIconUrl(item.name, fileIconSize);
     return (
       <li className="search-result-item" onClick={this.onClickHandler}>
-        <span className="item-content item-name">{item.name}</span>
-        <span className="item-content item-link">{item.link_content}</span>
+        <span className="item-content item-name">
+          <img src={fileIconUrl} alt="" />{item.name}
+        </span>
+        <span className="item-content item-link">{item.repo_name}/{item.link_content}</span>
         <div className="item-content item-text" dangerouslySetInnerHTML={{__html: item.content}}></div>
       </li>
     );
