293c77f49d40cfbc2b235c414da5469d0609c41c
shanshuirenjia <978987373@qq.com>
2019-01-12 22:05:47 +0800

    improve repo-model define, order repo list to show

--- frontend/src/models/repo.js
+++ frontend/src/models/repo.js
@@ -9,13 +9,12 @@ class Repo {
     this.owner_name = object.owner_name;
     this.owner_email = object.owner_email;
     this.owner_contact_email = object.owner_contact_email;
-    this.is_admin = object.is_admin;
     this.encrypted = object.encrypted;
     this.last_modified = object.last_modified;
     this.modifier_contact_email = object.modifier_contact_email;
     this.modifier_email = object.modifier_email;
     this.modifier_name = object.modifier_name;
-    this.mtime = object.mtime;
+    this.type = object.type;
   }
 }
 
