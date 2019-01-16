07f80069acaf277db3411f2b2dfbf76b5d4a484f
杨顺强 <ysq9207@126.com>
2019-01-16 15:04:02 +0800

    bug repair (#2835)

--- frontend/src/wiki.js
+++ frontend/src/wiki.js
@@ -468,7 +468,7 @@ class Wiki extends Component {
   }
 
   isInternalMarkdownLink(url) {
-    var re = new RegExp(siteRoot + 'library/' + repoID + '.*/*.md$');
+    var re = new RegExp(siteRoot + 'lib/' + repoID + '.*\.md$');
     return re.test(url);
   }
 
@@ -478,25 +478,16 @@ class Wiki extends Component {
   }
 
   getPathFromInternalMarkdownLink(url) {
-    var re = new RegExp(siteRoot + 'library/' + repoID + '/' + slug + '(.*/*.md)');
+    var re = new RegExp(siteRoot + 'lib/' + repoID + '/file' + '(.*\.md)');
     var array = re.exec(url);
     var path = decodeURIComponent(array[1]);
     return path;
   }
 
   getPathFromInternalDirLink(url) {
-    var re = new RegExp(siteRoot + 'library/' + repoID + '/' + slug + '(/.*)');
+    var re = new RegExp(siteRoot + 'library/' + repoID + '/' + slug + '(.*)');
     var array = re.exec(url);
     var path = decodeURIComponent(array[1]);
-
-    var index = path.substring(1).indexOf('/');
-    var dirPath = path.substring(index + 1);
-    re = new RegExp('(^/.*)');
-    if (re.test(dirPath)) {
-      path = dirPath;
-    } else {
-      path = '/' + dirPath;
-    }
 
     return path;
   }
