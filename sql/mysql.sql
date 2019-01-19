a24e5f4ae3cdbf0dec18db60d1bec6c19ba81faa
zhengxie <xiez1989@gmail.com>
2019-01-19 18:24:20 +0800

    [sql] Update sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1187,3 +1187,121 @@ CREATE TABLE `wiki_wiki` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+
+CREATE TABLE IF NOT EXISTS `drafts_draft` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `origin_repo_id` varchar(36) NOT NULL,
+  `origin_file_version` varchar(100) NOT NULL,
+  `draft_file_path` varchar(1024) NOT NULL,
+  `origin_file_uuid_id` char(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `drafts_draft_origin_file_uuid_id_f150319e_fk_tags_file` (`origin_file_uuid_id`),
+  KEY `drafts_draft_created_at_e9f4523f` (`created_at`),
+  KEY `drafts_draft_updated_at_0a144b05` (`updated_at`),
+  KEY `drafts_draft_username_73e6738b` (`username`),
+  CONSTRAINT `drafts_draft_origin_file_uuid_id_f150319e_fk_tags_file` FOREIGN KEY (`origin_file_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `drafts_draftreview` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `creator` varchar(255) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `origin_repo_id` varchar(36) NOT NULL,
+  `draft_file_path` varchar(1024) NOT NULL,
+  `origin_file_version` varchar(100) NOT NULL,
+  `publish_file_version` varchar(100) DEFAULT NULL,
+  `draft_id_id` int(11) DEFAULT NULL,
+  `origin_file_uuid_id` char(32) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `draft_id_id` (`draft_id_id`),
+  KEY `drafts_draftreview_created_at_1e4356bb` (`created_at`),
+  KEY `drafts_draftreview_updated_at_ad2a1471` (`updated_at`),
+  KEY `drafts_draftreview_creator_ecbc7e5e` (`creator`),
+  KEY `drafts_draftreview_origin_file_uuid_id_113d5031_fk_tags_file` (`origin_file_uuid_id`),
+  KEY `drafts_draftreview_author_2ed48342` (`author`),
+  CONSTRAINT `drafts_draftreview_draft_id_id_9e6babe7_fk_drafts_draft_id` FOREIGN KEY (`draft_id_id`) REFERENCES `drafts_draft` (`id`),
+  CONSTRAINT `drafts_draftreview_origin_file_uuid_id_113d5031_fk_tags_file` FOREIGN KEY (`origin_file_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `drafts_reviewreviewer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `reviewer` varchar(255) NOT NULL,
+  `review_id_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `drafts_reviewreviewe_review_id_id_fc6bd0fd_fk_drafts_dr` (`review_id_id`),
+  KEY `drafts_reviewreviewer_reviewer_65880f6e` (`reviewer`),
+  CONSTRAINT `drafts_reviewreviewe_review_id_id_fc6bd0fd_fk_drafts_dr` FOREIGN KEY (`review_id_id`) REFERENCES `drafts_draftreview` (`id`)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `drafts_reviewcomment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `resolved` tinyint(1) NOT NULL,
+  `comment` longtext NOT NULL,
+  `detail` longtext NOT NULL,
+  `review_id_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `drafts_reviewcomment_review_id_id_1645670c_fk_drafts_dr` (`review_id_id`),
+  KEY `drafts_reviewcomment_created_at_370dbc87` (`created_at`),
+  KEY `drafts_reviewcomment_updated_at_46073bd4` (`updated_at`),
+  KEY `drafts_reviewcomment_author_5498ceea` (`author`),
+  KEY `drafts_reviewcomment_resolved_94bcb8e6` (`resolved`),
+  CONSTRAINT `drafts_reviewcomment_review_id_id_1645670c_fk_drafts_dr` FOREIGN KEY (`review_id_id`) REFERENCES `drafts_draftreview` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `repo_tags_repotags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repo_id` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `repo_tags_repotags_repo_id_1163a48f` (`repo_id`),
+  KEY `repo_tags_repotags_name_3f4c9027` (`name`),
+  KEY `repo_tags_repotags_color_1292b6c1` (`color`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE IF NOT EXISTS `file_tags_filetags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_uuid_id` char(32) NOT NULL,
+  `parent_folder_uuid_id` char(32) NOT NULL,
+  `repo_tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `file_tags_filetags_file_uuid_id_e30f0ec8_fk_tags_file` (`file_uuid_id`),
+  KEY `file_tags_filetags_parent_folder_uuid_i_df56f09b_fk_tags_file` (`parent_folder_uuid_id`),
+  KEY `file_tags_filetags_repo_tag_id_c39660cb_fk_repo_tags_repotags_id` (`repo_tag_id`),
+  CONSTRAINT `file_tags_filetags_file_uuid_id_e30f0ec8_fk_tags_file` FOREIGN KEY (`file_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`),
+  CONSTRAINT `file_tags_filetags_parent_folder_uuid_i_df56f09b_fk_tags_file` FOREIGN KEY (`parent_folder_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`),
+  CONSTRAINT `file_tags_filetags_repo_tag_id_c39660cb_fk_repo_tags_repotags_id` FOREIGN KEY (`repo_tag_id`) REFERENCES `repo_tags_repotags` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE IF NOT EXISTS `related_files_relatedfiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `o_uuid_id` char(32) NOT NULL,
+  `r_uuid_id` char(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `related_files_relate_o_uuid_id_aaa8e613_fk_tags_file` (`o_uuid_id`),
+  KEY `related_files_relate_r_uuid_id_031751df_fk_tags_file` (`r_uuid_id`),
+  CONSTRAINT `related_files_relate_o_uuid_id_aaa8e613_fk_tags_file` FOREIGN KEY (`o_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`),
+  CONSTRAINT `related_files_relate_r_uuid_id_031751df_fk_tags_file` FOREIGN KEY (`r_uuid_id`) REFERENCES `tags_fileuuidmap` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `organizations_orgsettings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_id` int(11) NOT NULL,
+  `role` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `organizations_orgsettings_org_id_630f6843_uniq` (`org_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
