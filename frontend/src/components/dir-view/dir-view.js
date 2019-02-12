6387275ca2a0ce762794537bfdd70d437fb12708
shanshuirenjia <978987373@qq.com>
2019-02-12 14:47:05 +0800

    improve caculate path algorithm

--- frontend/src/components/dir-view/dir-view.js
+++ frontend/src/components/dir-view/dir-view.js
@@ -108,10 +108,9 @@ class DirView extends React.Component {
         });
       }
 
-      let repoName = repoInfo.repo_name;
       let repoID = repoInfo.repo_id;
-      let path = location.slice(location.indexOf(repoID) + repoID.length); // get the string after repoID
-      path = path.slice(path.indexOf(repoName.toLowerCase()) + repoName.length); // get current path
+      let path = location.slice(location.indexOf(repoID) + repoID.length + 1); // get the string after repoID
+      path = path.slice(path.indexOf('/')); // get current path
       this.setState({path: path});
       if (!res.data.lib_need_decrypt) {
         this.updateDirentList(path);
