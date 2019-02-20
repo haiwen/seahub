b314001ab7e7a1e72ac851230fba0460e16ac3e9
shanshuirenjia <978987373@qq.com>
2019-02-20 16:36:43 +0800

    [optimized] adjust dir-content-nav  style

--- frontend/src/components/toolbar/view-mode-toolbar.js
+++ frontend/src/components/toolbar/view-mode-toolbar.js
@@ -24,7 +24,7 @@ class ViewModeToolbar extends React.Component {
       <div className="view-mode btn-group">
         <button className={`${baseClass} sf2-icon-list-view ${this.props.currentMode === 'list' ? 'current-mode' : ''}`} id='list' title={gettext('List')} onClick={this.switchViewMode}></button>
         {/* <button className={`${baseClass} sf2-icon-grid-view ${this.props.currentMode === 'grid' ? 'current-mode' : ''}`} id='grid' title={gettext('Grid')} onClick={this.switchViewMode}></button> */}
-        <button className={`${baseClass} sf2-icon-two-columns ${this.props.currentMode === 'column' ? 'current-mode' : ''}`} id='column' title={gettext('column')} onClick={this.switchViewMode}></button>
+        <button className={`${baseClass} sf2-icon-two-columns ${this.props.currentMode === 'column' ? 'current-mode' : ''}`} id='column' title={gettext('Column')} onClick={this.switchViewMode}></button>
       </div>
     );
   }
