268ff486dc12f8bc93ad7b931fbfabcd26600504
llj <lingjun.li1@gmail.com>
2019-02-22 15:57:46 +0800

    [image file view] modification

--- seahub/views/file.py
+++ seahub/views/file.py
@@ -685,7 +685,7 @@ def view_lib_file(request, repo_id, path):
         return render(request, 'view_file_image.html', return_dict)
 
     elif filetype == IMAGE:
-        template = 'file_view_react.html'
+        template = 'image_file_view_react.html'
         if file_size > FILE_PREVIEW_MAX_SIZE:
             error_msg = _(u'File size surpasses %s, can not be opened online.') % \
                 filesizeformat(FILE_PREVIEW_MAX_SIZE)
