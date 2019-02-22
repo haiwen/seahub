cb6dbb546cdbe67d1581f5381904886af1ef99a4
shanshuirenjia <978987373@qq.com>
2019-02-22 10:48:54 +0800

    update menu style

--- frontend/src/components/draft-list-view/draft-list-item.js
+++ frontend/src/components/draft-list-view/draft-list-item.js
@@ -102,12 +102,12 @@ class DraftListItem extends React.Component {
           }
         </td>
         <td className="update">{localTime}</td>
-        <td className="text-center cursor-pointer">
+        <td className="text-center">
           {(this.props.draft.review_status !== 'open' && this.state.isMenuIconShow) && (
             <Dropdown isOpen={this.state.isItemMenuShow} toggle={this.toggleOperationMenu}>
               <DropdownToggle 
                 tag="i" 
-                className="fas fa-ellipsis-v" 
+                className="fas fa-ellipsis-v attr-action-icon" 
                 title={gettext('More Operations')}
                 onClick={this.onDropdownToggleClick}
                 data-toggle="dropdown" 
