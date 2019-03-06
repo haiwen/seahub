92ea31ea628ae1c7c5adb88df016885314b2381f
shanshuirenjia <978987373@qq.com>
2019-03-06 15:21:20 +0800

    repair file chooser bug

--- frontend/src/components/file-chooser/file-chooser.js
+++ frontend/src/components/file-chooser/file-chooser.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import RepoListView from './repo-list-view';
 import { seafileAPI } from '../../utils/seafile-api';
 import { gettext } from '../../utils/constants';
 import { Utils } from '../../utils/utils';
 import RepoInfo from '../../models/repo-info';
+import RepoListView from './repo-list-view';
 
 import '../../css/file-chooser.css';
 
@@ -32,7 +32,7 @@ class FileChooser extends React.Component {
   }
 
   componentDidMount() {
-    if (this.props.repoID) {
+    if (this.props.repoID) {  // current_repo_and_other_repos, only_current_library
       let repoID = this.props.repoID;
       seafileAPI.getRepoInfo(repoID).then(res => {
         // need to optimized
@@ -42,6 +42,24 @@ class FileChooser extends React.Component {
           selectedRepo: repoInfo
         });
         this.props.onRepoItemClick(repoInfo);
+      });
+    } else { // only_all_repos
+      seafileAPI.listRepos().then(res => {
+        let repos = res.data.repos;
+        let repoList = [];
+        let repoIdList = [];
+        for(let i = 0; i < repos.length; i++) {
+          if (repos[i].permission !== 'rw') {
+            continue;
+          }
+          if (repoIdList.indexOf(repos[i].repo_id) > -1) {
+            continue;
+          }
+          repoList.push(repos[i]);
+          repoIdList.push(repos[i].repo_id);
+        }
+        repoList = Utils.sortRepos(repoList, 'name', 'asc');
+        this.setState({repoList: repoList});
       });
     }
   }
@@ -101,55 +119,107 @@ class FileChooser extends React.Component {
     });
   }
 
-  render() {
-    if (!this.state.selectedRepo) {
-      return '';
-    }
-    const mode = this.props.mode;
-    let libName = mode === 'current_repo_and_other_repos' ? gettext('Other Libraries') : gettext('Libraries');
+  renderSearchedView = () => {
+
+  }
+
+  renderRepoListView = () => {
     return (
       <div className="file-chooser-container">
-        {(mode === 'current_repo_and_other_repos' || mode === 'only_current_library') &&
-          <div className="list-view">
-            <div className="list-view-header">
-              <span className={`item-toggle fa ${this.state.isCurrentRepoShow ? 'fa-caret-down' : 'fa-caret-right'}`} onClick={this.onCurrentRepoToggle}></span>
-              <span className="library">{gettext('Current Library')}</span>
+        {this.props.mode === 'current_repo_and_other_repos' && (
+          <Fragment>
+            <div className="list-view">
+              <div className="list-view-header">
+                <span className={`item-toggle fa ${this.state.isCurrentRepoShow ? 'fa-caret-down' : 'fa-caret-right'}`} onClick={this.onCurrentRepoToggle}></span>
+                <span className="library">{gettext('Current Library')}</span>
+              </div>
+              {
+                this.state.isCurrentRepoShow && this.state.currentRepoInfo &&
+                <RepoListView 
+                  initToShowChildren={true}
+                  currentRepoInfo={this.state.currentRepoInfo}
+                  selectedRepo={this.state.selectedRepo}
+                  selectedPath={this.state.selectedPath}
+                  onRepoItemClick={this.onRepoItemClick} 
+                  onDirentItemClick={this.onDirentItemClick}
+                  isShowFile={this.props.isShowFile}
+                />
+              }
             </div>
-            {
-              this.state.isCurrentRepoShow && this.state.currentRepoInfo &&
-              <RepoListView 
-                initToShowChildren={true}
-                currentRepoInfo={this.state.currentRepoInfo}
-                selectedRepo={this.state.selectedRepo}
-                selectedPath={this.state.selectedPath}
-                onRepoItemClick={this.onRepoItemClick} 
-                onDirentItemClick={this.onDirentItemClick}
-                isShowFile={this.props.isShowFile}
-              />
-            }
-          </div>
-        }
-        {mode !== 'only_current_library' &&
-          <div className="list-view">
-            <div className="list-view-header">
-              <span className={`item-toggle fa ${this.state.isOtherRepoShow ? 'fa-caret-down' : 'fa-caret-right'}`} onClick={this.onOtherRepoToggle}></span>
-              <span className="library">{libName}</span>
+            <div className="list-view">
+              <div className="list-view-header">
+                <span className={`item-toggle fa ${this.state.isOtherRepoShow ? 'fa-caret-down' : 'fa-caret-right'}`} onClick={this.onOtherRepoToggle}></span>
+                <span className="library">{gettext('Other Libraries')}</span>
+              </div>
+              {
+                this.state.isOtherRepoShow && 
+                <RepoListView 
+                  initToShowChildren={false}
+                  repoList={this.state.repoList}
+                  selectedRepo={this.state.selectedRepo}
+                  selectedPath={this.state.selectedPath}
+                  onRepoItemClick={this.onRepoItemClick} 
+                  onDirentItemClick={this.onDirentItemClick}
+                  isShowFile={this.props.isShowFile}
+                /> 
+              }
             </div>
-            {
-              this.state.isOtherRepoShow && 
-              <RepoListView 
-                initToShowChildren={false}
-                repoList={this.state.repoList}
-                selectedRepo={this.state.selectedRepo}
-                selectedPath={this.state.selectedPath}
-                onRepoItemClick={this.onRepoItemClick} 
-                onDirentItemClick={this.onDirentItemClick}
-                isShowFile={this.props.isShowFile}
-              /> 
-            }
+          </Fragment>
+        )}
+        {this.props.mode === 'only_current_library' && (
+          <div className="file-chooser-container">
+            <div className="list-view">
+              <div className="list-view-header">
+                <span className={`item-toggle fa ${this.state.isCurrentRepoShow ? 'fa-caret-down' : 'fa-caret-right'}`} onClick={this.onCurrentRepoToggle}></span>
+                <span className="library">{gettext('Current Library')}</span>
+              </div>
+              {
+                this.state.isCurrentRepoShow && this.state.currentRepoInfo &&
+                <RepoListView 
+                  initToShowChildren={true}
+                  currentRepoInfo={this.state.currentRepoInfo}
+                  selectedRepo={this.state.selectedRepo}
+                  selectedPath={this.state.selectedPath}
+                  onRepoItemClick={this.onRepoItemClick} 
+                  onDirentItemClick={this.onDirentItemClick}
+                  isShowFile={this.props.isShowFile}
+                />
+              }
+            </div>
           </div>
-        }
+        )}
+        {this.props.mode === 'only_all_repos' && (
+          <div className="file-chooser-container">
+            <div className="list-view">
+              <div className="list-view-header">
+                <span className="item-toggle fa fa-caret-down"></span>
+                <span className="library">{gettext('Libraries')}</span>
+              </div>
+                <RepoListView 
+                  initToShowChildren={false}
+                  repoList={this.state.repoList}
+                  selectedRepo={this.state.selectedRepo}
+                  selectedPath={this.state.selectedPath}
+                  onRepoItemClick={this.onRepoItemClick} 
+                  onDirentItemClick={this.onDirentItemClick}
+                  isShowFile={this.props.isShowFile}
+                /> 
+            </div>
+          </div>
+        )}
       </div>
+    );
+  }
+
+  render() {
+    if (!this.state.selectedRepo && this.props.repoID) {
+      return '';
+    }
+
+    return (
+      <Fragment>
+        {this.renderRepoListView()}
+      </Fragment>
     );
   }
 }
