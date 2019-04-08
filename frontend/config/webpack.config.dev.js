0f6d6571a5afe7591113eaed4b1560893d2b0db0
llj <lingjun.li1@gmail.com>
2019-04-08 12:04:17 +0800

    [view shared dir] rewrote it with react (#3211)

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -84,6 +84,11 @@ module.exports = {
       require.resolve('react-dev-utils/webpackHotDevClient'),
       paths.appSrc + "/draw/draw.js",
     ],
+    sharedDirView: [
+      require.resolve('./polyfills'),
+      require.resolve('react-dev-utils/webpackHotDevClient'),
+      paths.appSrc + "/shared-dir-view.js",
+    ],
     sharedFileViewMarkdown: [
       require.resolve('./polyfills'),
       require.resolve('react-dev-utils/webpackHotDevClient'),
