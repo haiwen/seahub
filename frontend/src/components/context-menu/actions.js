c30fe88f6a8db78a1f50e1893e9b20df216d3d5c
shanshuirenjia <978987373@qq.com>
2019-04-10 20:52:21 +0800

    optimized code style

--- frontend/src/components/context-menu/actions.js
+++ frontend/src/components/context-menu/actions.js
@@ -8,20 +8,20 @@ export const MENU_HIDE = 'REACT_CONTEXTMENU_HIDE';
 
 export function dispatchGlobalEvent(eventName, opts, target = window) {
 	// Compatibale with IE
-	// @see http://stackoverflow.com/questions/26596123/internet-explorer-9-10-11-event-constructor-doesnt-work
-	let event;
+  // @see http://stackoverflow.com/questions/26596123/internet-explorer-9-10-11-event-constructor-doesnt-work
+  let event;
 
-	if (typeof window.CustomEvent === 'function') {
-		event = new window.CustomEvent(eventName, { detail: opts });
-	} else {
-		event = document.createEvent('CustomEvent');
-		event.initCustomEvent(eventName, false, true, opts);
-	}
+  if (typeof window.CustomEvent === 'function') {
+    event = new window.CustomEvent(eventName, { detail: opts });
+  } else {
+    event = document.createEvent('CustomEvent');
+    event.initCustomEvent(eventName, false, true, opts);
+  }
 
-	if (target) {
-		target.dispatchEvent(event);
-		assign(store, opts);
-	}
+  if (target) {
+    target.dispatchEvent(event);
+    assign(store, opts);
+  }
 }
 
 export function showMenu(opts = {}, target) {
--- frontend/src/components/context-menu/globalEventListener.js
+++ frontend/src/components/context-menu/globalEventListener.js
@@ -3,43 +3,43 @@ import { uniqueId, hasOwnProp, canUseDOM } from './helpers';
 
 class GlobalEventListener {
     
-	constructor() {
-		this.callbacks = {};
+  constructor() {
+    this.callbacks = {};
 
-		if (canUseDOM) {
-			window.addEventListener(MENU_SHOW, this.handleShowEvent);
-			window.addEventListener(MENU_HIDE, this.handleHideEvent);
-		}
-	}
+    if (canUseDOM) {
+    window.addEventListener(MENU_SHOW, this.handleShowEvent);
+    window.addEventListener(MENU_HIDE, this.handleHideEvent);
+  }
+}
 
-	handleShowEvent = (event) => {
-		for (const id in this.callbacks) {
-			if (hasOwnProp(this.callbacks, id)) this.callbacks[id].show(event);
-		}
-	}
+  handleShowEvent = (event) => {
+    for (const id in this.callbacks) {
+      if (hasOwnProp(this.callbacks, id)) this.callbacks[id].show(event);
+    }
+  }
 
-	handleHideEvent = (event) => {
-		for (const id in this.callbacks) {
-			if (hasOwnProp(this.callbacks, id)) this.callbacks[id].hide(event);
-		}
-	}
+  handleHideEvent = (event) => {
+    for (const id in this.callbacks) {
+      if (hasOwnProp(this.callbacks, id)) this.callbacks[id].hide(event);
+    }
+  }
 
-	register = (showCallback, hideCallback) => {
-		const id = uniqueId();
+  register = (showCallback, hideCallback) => {
+    const id = uniqueId();
 
-		this.callbacks[id] = {
-			show: showCallback,
-			hide: hideCallback
-		};
+    this.callbacks[id] = {
+      show: showCallback,
+      hide: hideCallback
+    };
 
-		return id;
-	}
+    return id;
+  }
 
-	unregister = (id) => {
-		if (id && this.callbacks[id]) {
-		delete this.callbacks[id];
-		}
-	}
+  unregister = (id) => {
+    if (id && this.callbacks[id]) {
+      delete this.callbacks[id];
+    }
+  }
 }
 
 export default new GlobalEventListener();
