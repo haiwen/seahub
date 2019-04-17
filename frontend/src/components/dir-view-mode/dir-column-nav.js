107a1a3979ea45eebb2b932b3b63412d8ec8ebf1
zxj96 <519213124@qq.com>
2019-04-17 09:56:36 +0800

    Optimization code

--- frontend/src/components/dir-view-mode/dir-column-nav.js
+++ frontend/src/components/dir-view-mode/dir-column-nav.js
@@ -127,7 +127,7 @@ class DirColumnNav extends React.Component {
   onFileTagChanged = () => {
     let currentPath;
     if (this.props.currentPath.charAt(this.props.currentPath.length-1) === '/') {
-      currentPath = this.props.currentPath === '/' ? `${this.props.currentPath}${this.state.nodeDirent.object.name}` : `${this.props.currentPath}/${this.state.nodeDirent.object.name}`;
+      currentPath = Utils.joinPath(this.props.currentPath, this.state.nodeDirent.object.name);
     } else {
       currentPath = this.props.currentPath;
     }
