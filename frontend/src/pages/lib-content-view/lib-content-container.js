05e76ebfb2741e2dcaa333a427c9508fe93aec23
shanshuirenjia <978987373@qq.com>
2019-04-17 14:18:12 +0800

    improve active item interactive

--- frontend/src/pages/lib-content-view/lib-content-container.js
+++ frontend/src/pages/lib-content-view/lib-content-container.js
@@ -8,6 +8,8 @@ import DirListView from '../../components/dir-view-mode/dir-list-view';
 import DirGridView from '../../components/dir-view-mode/dir-grid-view';
 import DirColumnView from '../../components/dir-view-mode/dir-column-view';
 
+import '../../css/lib-content-view.css';
+
 const propTypes = {
   pathPrefix: PropTypes.array.isRequired,
   currentMode: PropTypes.string.isRequired,
@@ -149,7 +151,7 @@ class LibContentContainer extends React.Component {
               onDeleteRepoTag={this.props.onDeleteRepoTag}
             />
           </div>
-          <div className={`cur-view-content ${this.props.currentMode === 'column' ? 'view-mode-container' : ''}`}>
+          <div className={`cur-view-content lib-content-container ${this.props.currentMode === 'column' ? 'view-mode-container' : ''}`}>
             {!this.props.pathExist && this.errMessage}
             {this.props.pathExist && (
               <Fragment>
