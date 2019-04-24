00599253b979454525da73119d7d646deef19db7
ilearnit <helloworld.c@outlook.com>
2019-04-24 08:01:55 +0000

    add WeChat login

--- seahub/views/sso.py
+++ seahub/views/sso.py
@@ -38,7 +38,8 @@ def sso(request):
     if getattr(settings, 'ENABLE_WORK_WEIXIN', False):
         return HttpResponseRedirect(reverse('work_weixin_oauth_login') + next_param)
 
-    return HttpResponseRedirect(next_page)
+    next_param = '?%s=' % REDIRECT_FIELD_NAME + urlquote(reverse('social_post_login'))
+    return HttpResponseRedirect(reverse('social:begin', args=['weixin']) + next_param)
 
 def shib_login(request):
     if REDIRECT_FIELD_NAME in request.GET:
@@ -47,3 +48,35 @@ def shib_login(request):
         return HttpResponseRedirect(reverse('sso') + next_param)
     else:
         return HttpResponseRedirect(reverse('sso'))
+
+# Set cookie after WeChat login for client SSO
+from seahub.api2.utils import get_token_v1, get_token_v2
+from seahub.auth.decorators import login_required
+
+@login_required
+def social_post_login(request):
+    username = request.user.username
+    keys = (
+        'platform',
+        'device_id',
+        'device_name',
+        'client_version',
+        'platform_version',
+    )
+
+    if all([key in request.GET for key in keys]):
+        platform = request.GET['platform']
+        device_id = request.GET['device_id']
+        device_name = request.GET['device_name']
+        client_version = request.GET['client_version']
+        platform_version = request.GET['platform_version']
+        token = get_token_v2(
+            request, request.user.username, platform, device_id,
+            device_name, client_version, platform_version)
+    else:
+        token = get_token_v1(request.user.username)
+
+    # redirect user to home page
+    response = HttpResponseRedirect(reverse('libraries'))
+    response.set_cookie('seahub_auth', username + '@' + token.key)
+    return response
