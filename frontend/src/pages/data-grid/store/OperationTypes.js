a115c4990e847e9374af30d63ba3e204bf5e4740
shanshuirenjia <978987373@qq.com>
2019-05-06 14:37:40 +0800

    change api

--- frontend/src/pages/data-grid/store/OperationTypes.js
+++ frontend/src/pages/data-grid/store/OperationTypes.js
@@ -1,8 +1,12 @@
-export default OperationTypes = {
+const OperationTypes = {
   DELETE_ROW: 'DELETE_NODE',
   INSERT_ROW: 'INSERT_ROW',
   DELETE_COLUMN: 'DELETE_COLUMN',
   INSERT_COLUMN: 'INSERT_COLUMN',
   MODIFY_CELL: 'MODIFY_CELL',
   MODIFY_COLUMN: 'MODIFY_COLUMN',
-}
+};
+
+export default OperationTypes;
+
+
