abdaeb8683fa4b4a6c700ca13fe1b596b953cb84
shanshuirenjia <978987373@qq.com>
2019-05-06 13:22:43 +0800

    Packaged data interface

---
+++ frontend/src/pages/data-grid/store/DTableStore.js
@@ -0,0 +1,71 @@
+import Operation from './Operation';
+import OperationTypes from './OperationTypes';
+// todo Immutable
+// Implement the current version with an array
+export default class DTableStore {
+
+  constructor(value) {
+    this.operations = [];
+    this.columns = value.columns || [];
+    this.rows = value.rows || [];
+  }
+
+  updateStoreValues({ columns, rows }) {
+    this.columns = columns;
+    this.rows = rows;
+  }
+
+  createOperation(op) {
+    return new Operation(op);
+  }
+
+  deleteRow(rowIdx) {
+    let type = OperationTypes.DELETE_ROW;
+    let operation = this.createOperation({type, rowIdx});
+    let next = operation.apply(type);
+    this.operations.push(operation);
+    return next;
+  }
+
+  insertRow(newRowIdx) {
+    let type = OperationTypes.INSERT_ROW;
+    let operation = this.createOperation({type, newRowIdx});
+    let next = operation.apply(type);
+    this.operations.push(operation);
+    return next;
+  }
+
+  deleteColumn(idx) {
+    let type = OperationTypes.INSERT_COLUMN;
+    let operation = this.createOperation({type, idx});
+    let next = operation.apply(this.columns);
+    this.operations.push(operation);
+    return next;
+  }
+
+  insertColumn(idx, columnName, columnType) {
+    let type = OperationTypes.INSERT_COLUMN;
+    let operation = this.createOperation({type, idx, columnName, columnType});
+    let next = operation.apply(this.columns);
+    this.operations.push(operation);
+    return next;
+  }
+
+  modifyColumn(idx, oldColumnName, newColumnName) {
+    let type = OperationTypes.MODIFY_COLUMN;
+    let operation = this.createOperation({type, idx, oldColumnName, newColumnName});
+    let next = operation.apply(this.columns);
+    this.operations.push(operation);
+    return next;
+  }
+
+  modifyCell(idx, rowIdx, oldCellValue, newCellValue) {
+    let type = OperationTypes.MODIFY_CELL;
+    let key = this.columns[idx].key;
+    let operation = this.createOperation({type, rowIdx, key, oldCellValue, newCellValue});
+    let next = operation.apply(this.rows);
+    this.operations.push(operation);
+    return next;
+  }
+
+}
