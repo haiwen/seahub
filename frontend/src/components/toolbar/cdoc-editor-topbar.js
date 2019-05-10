a19d92b22d8926cd689e826a6a1bb8f25d338561
cainiao222 <1325442034@qq.com>
2019-05-10 18:30:09 +0800

    fix warning

--- frontend/src/components/toolbar/cdoc-editor-topbar.js
+++ frontend/src/components/toolbar/cdoc-editor-topbar.js
@@ -12,7 +12,7 @@ class MoreMenu extends React.PureComponent {
     super(props);
     this.state = {
       tooltipOpen: false,
-      dropdownOpen:false
+      dropdownOpen: false
     };
   }
 
@@ -21,15 +21,15 @@ class MoreMenu extends React.PureComponent {
   }
 
   dropdownToggle = () => {
-    this.setState({ dropdownOpen:!this.state.dropdownOpen });
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
   }
 
   render() {
     return (
       <Dropdown isOpen={this.state.dropdownOpen} toggle={this.dropdownToggle} direction="down" className="mx-lg-1">
         <DropdownToggle id="moreButton">
-          <i className="fa fa-ellipsis-v"/>
-          <Tooltip toggle={this.tooltipToggle} delay={{show: 0, hide: 0}} target="moreButton" placement='bottom' isOpen={this.state.tooltipOpen}>{gettext('More')}
+          <i className="fa fa-ellipsis-v" />
+          <Tooltip toggle={this.tooltipToggle} delay={{ show: 0, hide: 0 }} target="moreButton" placement='bottom' isOpen={this.state.tooltipOpen}>{gettext('More')}
           </Tooltip>
         </DropdownToggle>
         <DropdownMenu className="drop-list" right={true}>
@@ -49,45 +49,45 @@ class CDOCTopbar extends React.Component {
 
   render() {
     let { contentChanged, saving } = this.props;
-      return (
-        <div className="sf-md-viewer-topbar">
-          <div className="sf-md-viewer-topbar-first d-flex justify-content-between">
-            <FileInfo
-              toggleStar={this.props.toggleStar}
-              editorUtilities={this.props.editorUtilities}
-              fileInfo={this.props.fileInfo}
-              showDraftSaved={this.props.showDraftSaved}
+    return (
+      <div className="sf-md-viewer-topbar">
+        <div className="sf-md-viewer-topbar-first d-flex justify-content-between">
+          <FileInfo
+            toggleStar={this.props.toggleStar}
+            editorUtilities={this.props.editorUtilities}
+            fileInfo={this.props.fileInfo}
+            showDraftSaved={this.props.showDraftSaved}
+          />
+          <div className="topbar-btn-container">
+            {this.props.collabUsers.length > 0 && <CollabUsersButton className={'collab-users-dropdown'}
+              users={this.props.collabUsers} id={'usersButton'} />}
+            <ButtonGroup>
+              {canGenerateShareLink &&
+                <IconButton id={'shareBtn'} text={gettext('Share')} icon={'fa fa-share-alt'}
+                  onMouseDown={this.props.toggleShareLinkDialog} />
+              }
+              <IconButton text={gettext('Back to parent directory')} id={'parentDirectory'}
+                icon={'fa fa-folder-open'} onMouseDown={this.props.backToParentDirectory} />
+              {
+                this.props.showFileHistory && <IconButton id={'historyButton'}
+                  text={gettext('File History')} onMouseDown={this.props.toggleHistory} icon={'fa fa-history'} />
+              }
+              {saving ?
+                <button type={'button'} className={'btn btn-icon btn-secondary btn-active'}>
+                  <i className={'fa fa-spin fa-spinner'} /></button>
+                :
+                <IconButton text={gettext('Save')} id={'saveButton'} icon={'fa fa-save'} disabled={!contentChanged}
+                  onMouseDown={this.props.onSave} isActive={contentChanged} />
+              }
+            </ButtonGroup>
+            <MoreMenu
+              openDialogs={this.props.openDialogs}
             />
-            <div className="topbar-btn-container">
-              {this.props.collabUsers.length > 0 && <CollabUsersButton className={'collab-users-dropdown'}
-                users={this.props.collabUsers} id={'usersButton'} />}
-              <ButtonGroup>
-                {canGenerateShareLink &&
-                  <IconButton id={'shareBtn'} text={gettext('Share')} icon={'fa fa-share-alt'}
-                  onMouseDown={this.props.toggleShareLinkDialog}/>
-                }
-                <IconButton text={gettext('Back to parent directory')} id={'parentDirectory'}
-                  icon={'fa fa-folder-open'} onMouseDown={this.props.backToParentDirectory}/>
-                {
-                  this.props.showFileHistory && <IconButton id={'historyButton'}
-                    text={gettext('File History')} onMouseDown={this.props.toggleHistory} icon={'fa fa-history'}/>
-                }
-                { saving ?
-                  <button type={'button'} className={'btn btn-icon btn-secondary btn-active'}>
-                    <i className={'fa fa-spin fa-spinner'}/></button>
-                  :
-                  <IconButton text={gettext('Save')} id={'saveButton'} icon={'fa fa-save'}  disabled={!contentChanged} 
-                    onMouseDown={this.props.onSave} isActive={contentChanged}/>
-                }
-              </ButtonGroup>
-              <MoreMenu
-                openDialogs={this.props.openDialogs}
-              />
-            </div>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 export default CDOCTopbar;
