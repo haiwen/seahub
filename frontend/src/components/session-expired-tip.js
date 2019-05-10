2a1268cda9d7d1967bb5f8ef09a554c8f7e6cda4
zxj96 <519213124@qq.com>
2019-05-10 17:45:33 +0800

    modify session-expired-tip style

--- frontend/src/components/session-expired-tip.js
+++ frontend/src/components/session-expired-tip.js
@@ -3,9 +3,9 @@ import { gettext, loginUrl } from '../utils/constants';
 
 function SessionExpiredTip() {
   return(
-    <div className="session-expired-tip">{gettext('You are log out.')}
+    <span className="session-expired-tip">{gettext('You are log out.')}
       <a href={`${loginUrl}?next=${encodeURIComponent(location.href)}`}>{' '}{gettext('Login again.')}</a>
-    </div>
+    </span>
   )
 } 
 
