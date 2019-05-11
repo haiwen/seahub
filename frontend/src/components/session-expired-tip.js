d3aa772cdf5a755d62e069ee4663ed6bd4381d27
zxj96 <519213124@qq.com>
2019-05-11 13:57:42 +0800

    modify session-expired-link color

--- frontend/src/components/session-expired-tip.js
+++ frontend/src/components/session-expired-tip.js
@@ -3,8 +3,8 @@ import { gettext, loginUrl } from '../utils/constants';
 
 function SessionExpiredTip() {
   return(
-    <span className="session-expired-tip">{gettext('You are log out.')}
-      <a href={`${loginUrl}?next=${encodeURIComponent(location.href)}`}>{' '}{gettext('Login again.')}</a>
+    <span className="session-expired-tip">{gettext('You are log out.')}{' '}
+      <a className="action-link session-expired-link" href={`${loginUrl}?next=${encodeURIComponent(location.href)}`}>{gettext('Login again.')}</a>
     </span>
   )
 } 
