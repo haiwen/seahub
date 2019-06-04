9fada2c2adb98439f44bd3c8c98ff5336c3fd19f
shanshuirenjia <978987373@qq.com>
2019-06-04 10:10:50 +0800

    Compatible with IE

--- frontend/config/polyfills.js
+++ frontend/config/polyfills.js
@@ -8,6 +8,8 @@ if (typeof Promise === 'undefined') {
   window.Promise = require('promise/lib/es6-extensions.js');
 }
 
+require('babel-polyfill');
+
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
 
