53a79ed7069bcc78c2e6b1ff1cbd9113780fbc1c
王健辉 <40563566+WangJianhui666@users.noreply.github.com>
2019-07-08 16:47:04 +0800

    public repo search (#3817)
    * public repo search
    
    * remove print code
    
    * add permission check

--- frontend/src/pages/wiki/main-panel.js
+++ frontend/src/pages/wiki/main-panel.js
@@ -7,6 +7,7 @@ import WikiMarkdownViewer from '../../components/wiki-markdown-viewer';
 import WikiDirListView from '../../components/wiki-dir-list-view/wiki-dir-list-view';
 import Loading from '../../components/loading';
 import { Utils } from '../../utils/utils';
+import Search from '../../components/search/search';
 
 const propTypes = {
   path: PropTypes.string.isRequired,
@@ -77,6 +78,21 @@ class MainPanel extends Component {
     return (
       <div className="main-panel wiki-main-panel o-hidden">
         <div className="main-panel-north panel-top border-left-show">
+          {!username &&
+            <Fragment>
+              <div className="cur-view-toolbar">
+                <span className="sf2-icon-menu hidden-md-up d-md-none side-nav-toggle" title="Side Nav Menu" onClick={this.onMenuClick}></span>
+              </div>
+              <div className="common-toolbar">
+                <Search
+                  isPublic={true}
+                  repoID={repoID}
+                  onSearchedClick={this.props.onSearchedClick}
+                  placeholder={gettext('Search files in this library')}
+                />
+              </div>
+            </Fragment>
+          }
           {username && (
             <Fragment>
               <div className="cur-view-toolbar">
--- seahub/urls.py
+++ seahub/urls.py
@@ -89,6 +89,7 @@ from seahub.api2.endpoints.related_files import RelatedFilesView, RelatedFileVie
 from seahub.api2.endpoints.webdav_secret import WebdavSecretView
 from seahub.api2.endpoints.starred_items import StarredItems
 from seahub.api2.endpoints.markdown_lint import MarkdownLintView
+from seahub.api2.endpoints.public_repos_search import PublicReposSearchView
 
 # Admin
 from seahub.api2.endpoints.admin.revision_tag import AdminTaggedItemsView
@@ -358,6 +359,9 @@ urlpatterns = [
 
     # user: markdown-lint
     url(r'^api/v2.1/markdown-lint/$', MarkdownLintView.as_view(), name='api-v2.1-markdown-lint'),
+
+    # public repos search
+    url(r'^api/v2.1/public-repos-search/$', PublicReposSearchView.as_view(), name='api-v2.1-public-repos-search'),
 
     # Deprecated
     url(r'^api/v2.1/repos/(?P<repo_id>[-0-9a-f]{36})/tags/$', FileTagsView.as_view(), name="api-v2.1-filetags-view"),
