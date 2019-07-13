20eba966b7d04cc3dc19bf836c714f3f487e87b6
王健辉 <wangjianhui0918@gmail.com>
2019-07-13 15:37:52 +0800

    add test case

--- tests/api/endpoints/test_dtable_share.py
+++ tests/api/endpoints/test_dtable_share.py
@@ -1,5 +1,6 @@
 import json
 
+from django.utils.translation import ugettext as _
 from django.core.urlresolvers import reverse
 from seahub.dtable.models import Workspaces, DTableShare, DTables
 from seaserv import seafile_api
@@ -12,6 +13,8 @@ try:
     from seahub.settings import LOCAL_PRO_DEV_ENV
 except ImportError:
     LOCAL_PRO_DEV_ENV = False
+
+GROUP_DOMAIN = '@seafile_group'
 
 
 class SharedDTablesViewTest(BaseTestCase):
@@ -80,15 +83,55 @@ class DTableShareViewTest(BaseTestCase):
 
         self.url = reverse('api-v2.1-dtable-share', args=[self.workspace.id, self.dtable.name])
 
+        # add user to group
+        self.login_as(self.user)
+        self.group_id = self.group.id
+        self.endpoint = reverse('api-v2.1-group-members', args=[self.group_id])
+        self.client.post(self.endpoint, {
+            'email': self.user.email
+        })
+        self.logout()
+
+        # create group workspace
+        group_repo_id = seafile_api.create_repo(
+            _("My Workspace"),
+            _("My Workspace"),
+            "dtable@seafile"
+        )
+        self.group_workspace = Workspaces.objects.create_workspace(
+            str(self.group_id) + GROUP_DOMAIN, group_repo_id
+        )
+        assert len(Workspaces.objects.all()) == 2
+
+        # create group dtable
+        seafile_api.post_empty_file(
+            group_repo_id, '/', 'group.dtable', self.user.username
+        )
+        self.group_dtable = DTables.objects.create_dtable(
+            self.user.username, self.group_workspace, 'group'
+        )
+        assert len(DTables.objects.all()) == 2
+
+        # share group dtable to admin
+        DTableShare.objects.add(
+            self.group_dtable, str(self.group_id) + GROUP_DOMAIN, self.admin.username, 'rw'
+        )
+        assert len(DTableShare.objects.all()) == 2
+
+        self.url2 = reverse(
+            'api-v2.1-dtable-share', args=[self.group_workspace.id, self.group_dtable.name]
+        )
+
     def tearDown(self):
         workspace = Workspaces.objects.get_workspace_by_owner(self.user.username)
         workspace_id = workspace.id
         Workspaces.objects.delete_workspace(workspace_id)
 
         self.remove_repo()
+        self.remove_group()
 
     def test_can_post(self):
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
@@ -102,8 +145,24 @@ class DTableShareViewTest(BaseTestCase):
         self.assertEqual(201, resp.status_code)
         assert len(DTableShare.objects.all()) == 1
 
-    def test_can_not_post_with_already_share(self):
+    def test_can_post_group_share(self):
+        assert len(DTableShare.objects.all()) == 2
+        DTableShare.objects.all().delete()
+        assert len(DTableShare.objects.all()) == 0
+
+        self.login_as(self.user)
+
+        data = {
+            'email': self.admin.username,
+            'permission': 'rw',
+        }
+
+        resp = self.client.post(self.url2, data)
+        self.assertEqual(201, resp.status_code)
         assert len(DTableShare.objects.all()) == 1
+
+    def test_can_not_post_with_already_share(self):
+        assert len(DTableShare.objects.all()) == 2
 
         self.login_as(self.user)
 
@@ -115,7 +174,7 @@ class DTableShareViewTest(BaseTestCase):
         self.assertEqual(409, resp.status_code)
 
     def test_can_not_post_with_not_owner(self):
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
@@ -129,7 +188,7 @@ class DTableShareViewTest(BaseTestCase):
         self.assertEqual(403, resp.status_code)
 
     def test_can_not_post_with_share_to_owner(self):
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
@@ -142,11 +201,25 @@ class DTableShareViewTest(BaseTestCase):
         resp = self.client.post(self.url, data)
         self.assertEqual(400, resp.status_code)
 
+    def test_can_not_post_to_group_member(self):
+        assert len(DTableShare.objects.all()) == 2
+        DTableShare.objects.all().delete()
+        assert len(DTableShare.objects.all()) == 0
+
+        self.login_as(self.user)
+
+        data = {
+            'email': self.user.username,
+            'permission': 'rw',
+        }
+        resp = self.client.post(self.url2, data)
+        self.assertEqual(400, resp.status_code)
+
     def test_can_not_post_with_share_to_org_user(self):
         if not LOCAL_PRO_DEV_ENV:
             return
 
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
@@ -193,8 +266,22 @@ class DTableShareViewTest(BaseTestCase):
         assert DTableShare.objects.get_by_dtable_and_to_user(
             self.dtable, self.admin.username).permission == 'r'
 
+    def test_can_put_group_share(self):
+        self.login_as(self.user)
+
+        data = {
+            'email': self.admin.username,
+            'permission': 'r',
+        }
+        resp = self.client.put(self.url2, json.dumps(data), 'application/json')
+        self.assertEqual(200, resp.status_code)
+
+        assert DTableShare.objects.get_by_dtable_and_to_user(
+            self.group_dtable, self.admin.username
+        ).permission == 'r'
+
     def test_can_not_put_with_not_shared(self):
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
@@ -228,6 +315,7 @@ class DTableShareViewTest(BaseTestCase):
         self.assertEqual(400, resp.status_code)
 
     def test_can_delete(self):
+        assert len(DTableShare.objects.all()) == 2
         self.login_as(self.user)
 
         data = {
@@ -235,7 +323,18 @@ class DTableShareViewTest(BaseTestCase):
         }
         resp = self.client.delete(self.url, json.dumps(data), 'application/json')
         self.assertEqual(200, resp.status_code)
-        assert len(DTableShare.objects.all()) == 0
+        assert len(DTableShare.objects.all()) == 1
+
+    def test_can_delete_group_share(self):
+        assert len(DTableShare.objects.all()) == 2
+        self.login_as(self.user)
+
+        data = {
+            'email': self.admin.username,
+        }
+        resp = self.client.delete(self.url2, json.dumps(data), 'application/json')
+        self.assertEqual(200, resp.status_code)
+        assert len(DTableShare.objects.all()) == 1
 
     def test_can_delete_with_share_user(self):
         self.login_as(self.admin)
@@ -245,10 +344,10 @@ class DTableShareViewTest(BaseTestCase):
         }
         resp = self.client.delete(self.url, json.dumps(data), 'application/json')
         self.assertEqual(200, resp.status_code)
-        assert len(DTableShare.objects.all()) == 0
+        assert len(DTableShare.objects.all()) == 1
 
     def test_can_not_delete_with_not_shared(self):
-        assert len(DTableShare.objects.all()) == 1
+        assert len(DTableShare.objects.all()) == 2
         DTableShare.objects.all().delete()
         assert len(DTableShare.objects.all()) == 0
 
