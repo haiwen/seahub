1e51d2e3bd1fd7cd920f904e539f1a64b60b11b8
sniper-py <dzmbbs@qq.com>
2019-07-23 18:18:27 +0800

    invitation revoke API test

--- tests/api/endpoints/test_invitation.py
+++ tests/api/endpoints/test_invitation.py
@@ -5,6 +5,7 @@ from seahub.base.accounts import UserPermissions
 from seahub.invitations.models import Invitation
 from seahub.test_utils import BaseTestCase
 from seahub.api2.permissions import CanInviteGuest
+from tests.common.utils import randstring
 
 
 class InvitationsTest(BaseTestCase):
@@ -68,4 +69,37 @@ class InvitationsTest(BaseTestCase):
         resp = self.client.delete(self.endpoint)
         self.assertEqual(204, resp.status_code)
 
+        assert len(Invitation.objects.all()) == 0
+
+
+class InvitationRevokeTest(BaseTestCase):
+    def setUp(self):
+        self.login_as(self.user)
+        self.username = self.user.username
+        self.tmp_username = 'user_%s@test.com' % randstring(4)
+
+        # add invitation
+        self.i = Invitation.objects.add(inviter=self.username, accepter=self.tmp_username)
+        self.endpoint = '/api/v2.1/invitations/' + self.i.token + '/revoke/'
+        assert len(Invitation.objects.all()) == 1
+
+        # accept invitation
+        self.i.accept()
+        self.tmp_user = self.create_user(self.tmp_username, is_staff=False)
+        assert self.tmp_user.is_active == 1
+
+    def tearDown(self):
+        self.remove_user(self.tmp_username)
+
+    @patch.object(CanInviteGuest, 'has_permission')
+    @patch.object(UserPermissions, 'can_invite_guest')
+    def test_can_delete(self, mock_can_invite_guest, mock_has_permission):
+
+        mock_can_invite_guest.return_val = True
+        mock_has_permission.return_val = True
+
+        resp = self.client.delete(self.endpoint)
+        self.assertEqual(204, resp.status_code)
+
+        assert self.tmp_user.is_active == 0
         assert len(Invitation.objects.all()) == 0
