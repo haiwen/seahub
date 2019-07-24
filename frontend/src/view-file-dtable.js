a591b8b3e85c4dee9cc2362836382c1cf434e194
杨顺强 <ysq9207@126.com>
2019-07-24 16:05:11 +0800

    update parmas (#3904)
    * update parmas
    
    * optimized code
    
    * optimized code
    
    * repair upload image bug
    
    * update dtable version
    
    * update config params

--- frontend/src/view-file-dtable.js
+++ frontend/src/view-file-dtable.js
@@ -4,11 +4,28 @@ import { seafileAPI } from './utils/seafile-api';
 import ViewFileDtable from '@seafile/dtable/es';
 import './css/view-file-dtable.css';
 
+const { server, workspaceID, username, userNickName, contactEmail, fileName, filePath, dtableUuid } = window.app.pageOptions;
+window.dtable = {};
+window.dtable = {
+  workspaceID: workspaceID,
+  username: username,
+  name: userNickName,
+  contactEmail: contactEmail,
+  server: server,
+  dtableServer: "http://dev.seafile.com/dtable-server/",
+  filePath: filePath,
+  fileName: fileName,
+  dtableUuid: dtableUuid,
+  accessToken: '12345678'
+};
+
+window.seafileAPI = seafileAPI;
+
 class ViewFileSDB extends React.Component {
 
   render() {
     return (
-      <ViewFileDtable seafileAPI={seafileAPI} pageOptions={window.app.pageOptions} />
+      <ViewFileDtable />
     );
   }
 }
