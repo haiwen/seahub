c361fceac17d5ab9cc1633503c9a1f0f23e4a8ad
王健辉 <wangjianhui0918@gmail.com>
2019-07-24 17:54:43 +0800

    update settings

--- seahub/settings.py
+++ seahub/settings.py
@@ -746,6 +746,9 @@ ENABLE_SUB_LIBRARY = True
 
 SEAFILE_COLLAB_SERVER = ''
 
+# dtable server url
+DTABLE_SERVER_URL = ''
+
 ############################
 # Settings for Seahub Priv #
 ############################
@@ -915,6 +918,3 @@ if ENABLE_REMOTE_USER_AUTHENTICATION:
 
 if ENABLE_OAUTH or ENABLE_WORK_WEIXIN:
     AUTHENTICATION_BACKENDS += ('seahub.oauth.backends.OauthRemoteUserBackend',)
-
-# dtable server url
-DTABLE_SERVER_URL = 'https://dev.seafile.com/dtable-server/'
