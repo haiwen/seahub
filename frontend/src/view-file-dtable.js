ca5cb99248af2eb882327b53ad65c14b2e7fc178
shanshuirenjia <978987373@qq.com>
2019-07-24 14:21:07 +0800

    optimized code

--- frontend/src/view-file-dtable.js
+++ frontend/src/view-file-dtable.js
@@ -4,12 +4,22 @@ import { seafileAPI } from './utils/seafile-api';
 import ViewFileDtable from '@seafile/dtable/es';
 import './css/view-file-dtable.css';
 
+const { workspaceID, fileName, filePath, dtableUuid } = window.app.pageOptions;
+window.dtable = {};
+window.dtable = {
+  workspaceID: workspaceID,
+  dtableServer: "http://dev.seafile.com/dtable-server/",
+  filePath: filePath,
+  fileName: fileName,
+  dtableUuid: dtableUuid,
+  accessToken: '12345678'
+};
+
 class ViewFileSDB extends React.Component {
 
   render() {
-    window.app.pageOptions['dtableServer'] = "http://dev.seafile.com/dtable-server/";
     return (
-      <ViewFileDtable seafileAPI={seafileAPI} pageOptions={window.app.pageOptions} />
+      <ViewFileDtable />
     );
   }
 }
