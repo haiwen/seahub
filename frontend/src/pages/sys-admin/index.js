de2408efdced573a1579102f8436df09948bc91f
llj <lingjun.li1@gmail.com>
2019-07-25 16:43:55 +0800

    Sys admin react reconstruct (#3918)
    * sysadmin reconstruct info page
    
    * optimize frontend code, add license post api response
    
    * update test case
    
    * optimize js code

--- frontend/src/pages/sys-admin/index.js
+++ frontend/src/pages/sys-admin/index.js
@@ -6,6 +6,7 @@ import SidePanel from './side-panel';
 import MainPanel from './main-panel';
 import FileScanRecords from './file-scan-records';
 import WorkWeixinDepartments from './work-weixin-departments';
+import Info from './info';
 
 import '../../assets/css/fa-solid.css';
 import '../../assets/css/fa-regular.css';
@@ -43,13 +44,16 @@ class SysAdmin extends React.Component {
         <SidePanel isSidePanelClosed={isSidePanelClosed} onCloseSidePanel={this.onCloseSidePanel} currentTab={currentTab}/>
         <MainPanel>
           <Router className="reach-router">
+            <Info
+              path={siteRoot + 'sys/info'}
+              currentTab={currentTab}
+              tabItemClick={this.tabItemClick}
+            />
             <FileScanRecords
               path={siteRoot + 'sys/file-scan-records'}
               currentTab={currentTab} 
               tabItemClick={this.tabItemClick}
             />
-          </Router>
-          <Router className="reach-router">
             <WorkWeixinDepartments
               path={siteRoot + 'sys/work-weixin/departments'}
               currentTab={currentTab}
