a459314bec2769d6c4873f9c029cb65556484dec
sniper-py <38058090+sniper-py@users.noreply.github.com>
2019-08-07 11:07:13 +0800

    dtable related users (#3951)
    * add dtable related users api
    
    * add dtable related users test
    
    * add dtable related users repo and path check

--- seahub/dtable/utils.py
+++ seahub/dtable/utils.py
@@ -2,6 +2,8 @@ from seahub.dtable.models import DTableShare
 from seahub.group.utils import is_group_member
 from seahub.constants import PERMISSION_READ_WRITE
 
+from seaserv import ccnet_api
+
 
 def check_dtable_share_permission(dtable, to_user):
     share_dtable_obj = DTableShare.objects.get_by_dtable_and_to_user(dtable, to_user)
@@ -26,3 +28,33 @@ def check_dtable_permission(username, owner):
             return None
         else:
             return PERMISSION_READ_WRITE
+
+
+def list_dtable_related_users(workspace, dtable):
+    """ Return all users who can view this dtable.
+
+    1. owner
+    2. shared users
+    3. groups users
+    """
+
+    user_list = list()
+    owner = workspace.owner
+
+    # 1. shared users
+    shared_queryset = DTableShare.objects.list_by_dtable(dtable)
+    user_list.extend([dtable_share.to_user for dtable_share in shared_queryset])
+
+    if '@seafile_group' not in owner:
+        # 2. owner
+        if owner not in user_list:
+            user_list.append(owner)
+    else:
+        # 3. groups users
+        group_id = int(owner.split('@')[0])
+        members = ccnet_api.get_group_members(group_id)
+        for member in members:
+            if member.user_name not in user_list:
+                user_list.append(member.user_name)
+
+    return user_list
