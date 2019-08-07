df9a9d12b2ef1e192bd239c565ffe1b3bc31dfe4
sniper-py <dzmbbs@qq.com>
2019-08-07 10:31:53 +0800

    add dtable related users repo and path check

--- seahub/api2/endpoints/dtable_related_users.py
+++ seahub/api2/endpoints/dtable_related_users.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 import seaserv
+from seaserv import seafile_api
 
 from seahub.api2.authentication import TokenAuthentication
 from seahub.api2.throttling import UserRateThrottle
@@ -15,6 +16,8 @@ from seahub.api2.utils import api_error, get_user_common_info
 from seahub.dtable.models import Workspaces, DTables
 from seahub.dtable.utils import check_dtable_permission, check_dtable_share_permission, \
     list_dtable_related_users
+from seahub.utils import normalize_file_path
+from seahub.api2.endpoints.dtable import FILE_TYPE
 
 logger = logging.getLogger(__name__)
 
@@ -28,6 +31,7 @@ class DTableRelatedUsersView(APIView):
         """list dtable related users
         """
         table_name = name
+        table_file_name = table_name + FILE_TYPE
 
         # resource check
         workspace = Workspaces.objects.get_workspace_by_id(workspace_id)
@@ -42,9 +46,21 @@ class DTableRelatedUsersView(APIView):
                 error_msg = 'Group %s not found.' % group_id
                 return api_error(status.HTTP_404_NOT_FOUND, error_msg)
 
+        repo_id = workspace.repo_id
+        repo = seafile_api.get_repo(repo_id)
+        if not repo:
+            error_msg = 'Library %s not found.' % repo_id
+            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
+
         dtable = DTables.objects.get_dtable(workspace, table_name)
         if not dtable:
             error_msg = 'dtable %s not found.' % table_name
+            return api_error(status.HTTP_404_NOT_FOUND, error_msg)
+
+        table_path = normalize_file_path(table_file_name)
+        table_file_id = seafile_api.get_file_id_by_path(repo_id, table_path)
+        if not table_file_id:
+            error_msg = 'file %s not found.' % table_file_name
             return api_error(status.HTTP_404_NOT_FOUND, error_msg)
 
         # permission check
