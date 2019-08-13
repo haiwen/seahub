cc29be527f6af9f0987193a970f88b862dc72ab5
lian <lian@seafile.com>
2019-08-13 13:33:25 +0800

    fix exception of Organization user quota full

--- seahub/api2/endpoints/copy_move_task.py
+++ seahub/api2/endpoints/copy_move_task.py
@@ -146,8 +146,16 @@ class CopyMoveTaskView(APIView):
                         src_repo.version, dir_id)
 
             # check if above quota for dst repo
-            if seafile_api.check_quota(dst_repo_id, current_size) < 0:
-                return api_error(HTTP_443_ABOVE_QUOTA, _("Out of quota."))
+            try:
+                if seafile_api.check_quota(dst_repo_id, current_size) < 0:
+                    return api_error(HTTP_443_ABOVE_QUOTA, _("Out of quota."))
+            except Exception as e:
+                if str(e) == 'Organization user quota full':
+                    return api_error(HTTP_443_ABOVE_QUOTA, _(u"Out of quota."))
+                else:
+                    logger.error(e)
+                    error_msg = 'Internal Server Error'
+                    return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
 
         new_dirent_name = check_filename_with_rename(dst_repo_id,
                 dst_parent_dir, src_dirent_name)
