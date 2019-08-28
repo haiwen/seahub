6d76e0e66889f7d8f181b71d538631e46caba5f9
sniper-py <dzmbbs@qq.com>
2019-08-28 12:01:24 +0800

    change app_label to base in SocialAuthUser

--- seahub/auth/models.py
+++ seahub/auth/models.py
@@ -156,7 +156,7 @@ class SocialAuthUser(models.Model):
 
     class Meta:
         """Meta data"""
-        app_label = "auth"
+        app_label = "base"
         unique_together = ('provider', 'uid')
         db_table = 'social_auth_usersocialauth'
 
