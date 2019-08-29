03865abd73a532edf6dd9aefd9ff3b51b734aadd
sniper-py <dzmbbs@qq.com>
2019-08-29 10:59:11 +0800

    update test conf in test

--- tests/seahubtests.sh
+++ tests/seahubtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-: ${PYTHON=python}
+: ${PYTHON=python3}
 
 : ${SEAHUB_TEST_USERNAME="test@seafiletest.com"}
 : ${SEAHUB_TEST_PASSWORD="testtest"}
@@ -24,11 +24,11 @@ if [[ ${TRAVIS} != "" ]]; then
 fi
 
 set -x
-SEAHUB_TESTSDIR=$(python -c "import os; print os.path.dirname(os.path.realpath('$0'))")
+SEAHUB_TESTSDIR=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 SEAHUB_SRCDIR=$(dirname "${SEAHUB_TESTSDIR}")
 
 export SEAHUB_LOG_DIR='/tmp/logs'
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages:${SEAHUB_SRCDIR}/thirdpart:${PYTHONPATH}"
+export PYTHONPATH="/usr/local/lib/python3.7/site-packages:/usr/local/lib/python3.7/dist-packages:/usr/lib/python3.7/site-packages:/usr/lib/python3.7/dist-packages:${SEAHUB_SRCDIR}/thirdpart:${PYTHONPATH}"
 cd "$SEAHUB_SRCDIR"
 set +x
 
@@ -36,12 +36,13 @@ function init() {
     ###############################
     # create database and two new users: an admin, and a normal user
     ###############################
+    $PYTHON ./manage.py makemigrations
     $PYTHON ./manage.py migrate --noinput
 
     # create normal user
-    $PYTHON -c "import ccnet; pool = ccnet.ClientPool('${CCNET_CONF_DIR}'); ccnet_threaded_rpc = ccnet.CcnetThreadedRpcClient(pool, req_pool=True); ccnet_threaded_rpc.add_emailuser('${SEAHUB_TEST_USERNAME}', '${SEAHUB_TEST_PASSWORD}', 0, 1);"
+    $PYTHON -c "import os; import ccnet; ccnet_pipe_path = os.path.join ('${CCNET_CONF_DIR}', 'ccnet-rpc.sock'); ccnet_threaded_rpc = ccnet.CcnetThreadedRpcClient(ccnet_pipe_path); ccnet_threaded_rpc.add_emailuser('${SEAHUB_TEST_USERNAME}', '${SEAHUB_TEST_PASSWORD}', 0, 1);"
     # create admin
-    $PYTHON -c "import ccnet; pool = ccnet.ClientPool('${CCNET_CONF_DIR}'); ccnet_threaded_rpc = ccnet.CcnetThreadedRpcClient(pool, req_pool=True); ccnet_threaded_rpc.add_emailuser('${SEAHUB_TEST_ADMIN_USERNAME}', '${SEAHUB_TEST_ADMIN_PASSWORD}', 1, 1);"
+    $PYTHON -c "import os; import ccnet; ccnet_pipe_path = os.path.join ('${CCNET_CONF_DIR}', 'ccnet-rpc.sock'); ccnet_threaded_rpc = ccnet.CcnetThreadedRpcClient(ccnet_pipe_path); ccnet_threaded_rpc.add_emailuser('${SEAHUB_TEST_ADMIN_USERNAME}', '${SEAHUB_TEST_ADMIN_PASSWORD}', 1, 1);"
 
 }
 
@@ -60,6 +61,12 @@ function run_tests() {
     set +e
     py.test $nose_opts tests
     rvalue=$?
+
+    # ignore 120 exited code in python3.6
+    if [[ $rvalue == 120 ]]; then
+    	  rvalue=0
+    fi
+
     if [[ ${TRAVIS} != "" ]]; then
         # On travis-ci, dump seahub logs when test finished
         for logfile in /tmp/logs/*.log; do
