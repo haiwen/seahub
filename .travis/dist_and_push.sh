9225d7dda20ce05775f0cd4edfb9d6e7481f4935
sniper-py <dzmbbs@qq.com>
2019-08-29 11:20:13 +0800

    update dist and push after test success

--- .travis/dist_and_push.sh
+++ .travis/dist_and_push.sh
@@ -7,23 +7,25 @@ if [[ ${TRAVIS} != "" ]]; then
 fi
 
 set -x
-SEAHUB_TESTSDIR=$(python -c "import os; print os.path.dirname(os.path.realpath('$0'))")
+SEAHUB_TESTSDIR=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 SEAHUB_SRCDIR=$(dirname "${SEAHUB_TESTSDIR}")
 
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages:${SEAHUB_SRCDIR}/thirdpart:${PYTHONPATH}"
+export PYTHONPATH="/usr/local/lib/python3.7/site-packages:/usr/local/lib/python3.7/dist-packages:/usr/lib/python3.7/site-packages:/usr/lib/python3.7/dist-packages:${SEAHUB_SRCDIR}/thirdpart:${PYTHONPATH}"
 cd "$SEAHUB_SRCDIR"
 set +x
 
 function commit_dist_files() {
-  git checkout -b dist-$TRAVIS_BRANCH
+  git checkout -b dist-$DRONE_COMMIT_BRANCH
   git add -u . && git add -A media/assets && git add -A static/scripts && git add -A frontend && git add -A locale
-  git commit -m "[dist] Travis build: #$TRAVIS_BUILD_NUMBER, based on commit $TRAVIS_COMMIT." -m "$TRAVIS_COMMIT_MESSAGE"
+  git config --global user.email "drone@seafile.com"
+  git config --global user.name "drone"
+  git commit -m "[dist] Drone CI build: #$DRONE_BUILD_NUMBER, based on commit $DRONE_COMMIT." -m "$DRONE_COMMIT_MESSAGE"
 }
 
 function upload_files() {
     echo 'push dist to seahub'
-    git remote add token-origin https://imwhatiam:${GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/haiwen/seahub.git
-    git push -f token-origin dist-$TRAVIS_BRANCH
+    git remote add token-origin https://$GITHUB_TOKEN@github.com/haiwen/seahub.git
+    git push -f token-origin dist-$DRONE_COMMIT_BRANCH
 }
 
 function make_dist() {
