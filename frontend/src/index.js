77fcca23c0a151881c1fa14172314492a8fcf903
shanshuirenjia <978987373@qq.com>
2019-09-01 19:46:44 +0800

    optimized code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MarkdownEditor from './markdown-editor';
 import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n-seafile-editor';
+import i18nSeafileEditor from './i18n-seafile-editor';
 import './assets/css/fa-solid.css';
 import './assets/css/fa-regular.css';
 import './assets/css/fontawesome.css';
 import './index.css';
 
 ReactDOM.render(
-  <I18nextProvider i18n={ i18n } >
+  <I18nextProvider i18n={ i18nSeafileEditor } >
     <MarkdownEditor />
   </I18nextProvider>,
   document.getElementById('root')
