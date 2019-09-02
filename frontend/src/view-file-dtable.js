64b7222bd2ed8e83111f355f60a45264843837d5
杨顺强 <ysq9207@126.com>
2019-09-02 15:11:37 +0800

    Optimized translate module (#4044)
    * combine dtable translate
    
    * update cdoc html
    
    * optimized code

--- frontend/src/view-file-dtable.js
+++ frontend/src/view-file-dtable.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { seafileAPI } from './utils/seafile-api';
 import ViewFileDtable from '@seafile/dtable/es';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n-dtable';
 import './css/view-file-dtable.css';
 
-const { server, workspaceID, username, userNickName, contactEmail, fileName, filePath, dtableUuid, dtableServer, dtableSocket, mediaUrl } = window.app.pageOptions;
+const { server, workspaceID, username, userNickName, contactEmail, fileName, filePath, dtableUuid, dtableServer, dtableSocket, mediaUrl, lang } = window.app.pageOptions;
 window.dtable = {};
 window.dtable = {
   workspaceID: workspaceID,
@@ -18,7 +20,8 @@ window.dtable = {
   fileName: fileName,
   dtableUuid: dtableUuid,
   mediaUrl: mediaUrl,
-  accessToken: ''
+  accessToken: '',
+  lang: lang
 };
 
 window.seafileAPI = seafileAPI;
@@ -33,6 +36,8 @@ class ViewFileSDB extends React.Component {
 }
 
 ReactDOM.render(
-  <ViewFileSDB />,
+  <I18nextProvider i18n={ i18n }>
+    <ViewFileSDB />
+  </I18nextProvider>,
   document.getElementById('wrapper')
 );
