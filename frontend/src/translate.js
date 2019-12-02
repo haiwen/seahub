d2ad7c6692fbd13853559809e1f31fd4fa75d5e5
王健辉 <40563566+jianhw@users.noreply.github.com>
2019-12-02 20:14:48 +0800

    update .po files (#4334)

---
+++ frontend/src/translate.js
@@ -0,0 +1,18 @@
+import { gettext } from './utils/constants';
+
+gettext('You have not shared any folders');
+gettext('You don\'t have any share links');
+gettext('You don\'t have any upload links');
+gettext('You don\'t have any favorite items yet');
+gettext('Modify');
+gettext('Import Users');
+gettext('No connected devices');
+gettext('No sync errors');
+gettext('No groups');
+gettext('No notifications');
+gettext('Notification Detail');
+gettext('No share links');
+gettext('No upload links');
+gettext('No invitations');
+gettext('No terms and conditions');
+gettext('Tip: libraries deleted {trashReposExpireDays} days ago will be cleaned automatically.');
