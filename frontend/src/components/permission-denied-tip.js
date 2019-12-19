e788a7d4f2284de35f79f8e523718e9fba76e5f4
shanshuirenjia <978987373@qq.com>
2019-12-19 12:12:16 +0800

    improve code

--- frontend/src/components/permission-denied-tip.js
+++ frontend/src/components/permission-denied-tip.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { gettext, loginUrl } from '../utils/constants';
 
 function PermissionDeniedTip() {
+  let reloginUrl = `${loginUrl}?next=${encodeURIComponent(location.href)}`;
+  let errorTip = gettext('Permission denied. Plaese try {placeholder-left}login again.{placeholder-right}');
+  errorTip = errorTip.replace('{placeholder-left}', '<a class="action-link p-0" href='+ reloginUrl + '>')
+  errorTip = errorTip.replace('{placeholder-right}', '</a>');
   return(
-    <span className="error">{gettext('Permission denied. Please try')}{' '}
-      <a className="action-link p-0" href={`${loginUrl}?next=${encodeURIComponent(location.href)}`}>{gettext('login again.')}</a>
-    </span>
+    <span className="error" dangerouslySetInnerHTML={{__html: errorTip}}></span>
   );
 } 
 
