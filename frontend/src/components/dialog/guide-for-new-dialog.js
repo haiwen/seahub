b1fd8cbd0046f0abe281665b3ade680478267f29
llj <lingjun.li1@gmail.com>
2019-12-20 17:32:35 +0800

    [my libs] 'guide for new' dialog: redesigned the UI

--- frontend/src/components/dialog/guide-for-new-dialog.js
+++ frontend/src/components/dialog/guide-for-new-dialog.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, ModalBody } from 'reactstrap';
-import { gettext, siteName, canAddRepo } from '../../utils/constants';
-
-import '../../css/guide-for-new.css';
+import { gettext, mediaUrl, siteName, canAddRepo } from '../../utils/constants';
 
 const propTypes = {
   toggleDialog: PropTypes.func.isRequired
@@ -19,16 +17,14 @@ class GuideForNewDialog extends React.Component {
     return (
       <Modal isOpen={true} toggle={this.toggle}>
         <ModalBody>
-          <button type="button" className="close" onClick={this.toggle}><span aria-hidden="true">×</span></button>
-          <div className="p-2 d-flex">
-            <span className="far fa-lightbulb new-guide-icon" aria-hidden="true"></span>
-            <div className="txt">
-              <h3 id="dialogTitle">{gettext('Welcome to {site_name_placeholder}').replace('{site_name_placeholder}', siteName)}</h3>
-              {canAddRepo ?
-                <p>{gettext('{site_name_placeholder} organizes files into libraries. Each library can be synced and shared separately. We have created a personal library for you. You can create more libraries later.').replace('{site_name_placeholder}', siteName)}</p> :
-                <p>{gettext('{site_name_placeholder} organizes files into libraries. Each library can be synced and shared separately. Howerver, since you are a guest user now, you can not create libraries.').replace('{site_name_placeholder}', siteName)}</p>
-              }
-            </div>
+          <button type="button" className="close text-gray" onClick={this.toggle}><span aria-hidden="true">×</span></button>
+          <div className="p-2 text-center">
+            <img src={`${mediaUrl}img/welcome.png`} width="180" alt="" />
+            <h3 id="dialogTitle">{gettext('Welcome to {site_name_placeholder}').replace('{site_name_placeholder}', siteName)}</h3>
+            {canAddRepo ?
+              <p>{gettext('{site_name_placeholder} organizes files into libraries. Each library can be synced and shared separately. We have created a personal library for you. You can create more libraries later.').replace('{site_name_placeholder}', siteName)}</p> :
+              <p>{gettext('{site_name_placeholder} organizes files into libraries. Each library can be synced and shared separately. Howerver, since you are a guest user now, you can not create libraries.').replace('{site_name_placeholder}', siteName)}</p>
+            }
           </div>
         </ModalBody>
       </Modal>
