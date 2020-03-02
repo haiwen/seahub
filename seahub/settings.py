8995362209824a30af943b9fb893bda957600452
王健辉 <40563566+jianhw@users.noreply.github.com>
2020-03-02 13:23:58 +0800

    temp test

--- seahub/settings.py
+++ seahub/settings.py
@@ -604,7 +604,7 @@ LOGGING = {
     }
 }
 
-#Login Attempt
+# Login Attempt
 LOGIN_ATTEMPT_LIMIT = 5
 LOGIN_ATTEMPT_TIMEOUT = 15 * 60 # in seconds (default: 15 minutes)
 FREEZE_USER_ON_LOGIN_FAILED = False # deactivate user account when login attempts exceed limit
