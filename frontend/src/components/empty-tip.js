e95e2308182ca62e2c913d42492c56f9bebdb614
llj <lingjun.li1@gmail.com>
2020-03-10 11:42:42 +0800

    [share links admin dialog] added 'empty tip'

--- frontend/src/components/empty-tip.js
+++ frontend/src/components/empty-tip.js
@@ -2,16 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { mediaUrl } from '../utils/constants';
 
+const propTypes = { 
+  forDialog: PropTypes.bool,
+  children: PropTypes.object.isRequired
+};
+
 class EmptyTip extends React.Component {
 
   render() {
     return (
-      <div className="empty-tip">
+      <div className={this.props.forDialog ? 'text-center mt-8' : 'empty-tip'}>
         <img src={`${mediaUrl}img/no-items-tip.png`} alt="" width="100" height="100" className="no-items-img-tip" />
         {this.props.children}
       </div>
     );
   }
 }
+
+EmptyTip.propTypes = propTypes;
 
 export default EmptyTip;
