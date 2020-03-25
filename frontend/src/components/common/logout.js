9d72b61c640eee921c04d5aabc5ddfe24a944167
shanshuirenjia <978987373@qq.com>
2020-03-25 16:53:16 +0800

    update sf-font

--- frontend/src/components/common/logout.js
+++ frontend/src/components/common/logout.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { siteRoot } from '../../utils/constants'
+import { siteRoot, gettext } from '../../utils/constants'
 
 export default function Logout() {
   const style = {
     width: '32px',
     height: '32px',
-    marginLeft: '8px'
-  }
+    marginLeft: '8px',
+    color: '#bbb',
+    textDecoration: 'none'
+  };
+  const classname = "d-flex align-items-center justify-content-center"
   return (
-    <a style={style} href={`${siteRoot}accounts/logout/`}>
-      <i></i>
+    <a className={classname} style={style} href={`${siteRoot}accounts/logout/`} title={gettext('Logout')}>
+      <i className="sf3-font sf3-font-logout" style={{fontSize: '24px'}}></i>
     </a>
   )
 }
