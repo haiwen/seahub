haiwen/seahub

commit 44459bb36b8c3c3f3d57c9976357ea1cc4103439

[decrypt page for share links] redesigned it. (#4360)
* added a common base html

seahub/templates/share_access_validation.html
@@ -1,22 +1,29 @@
-{% extends "base.html" %}
+{% extends "base_for_small_panel.html" %}
 {% load i18n %}
 
+{% block extra_style %}{{ block.super }}
+<style type="text/css">
+.small-panel {
+  padding: 50px 40px;
+}
+</style>
+{% endblock %}
+
 {% block main_panel %}
-<div class="wide-panel">
-    <p class="access-notice">{% trans "Please input the password if you want to browse the shared file/directory." %}</p>
-    {% if path %}
-    <form action="{% url view_name token %}?p={{ path }}" method="post" id="share-passwd-form">{% csrf_token %}
-    {% else %}
-    <form action="{% url view_name token %}" method="post" id="share-passwd-form">{% csrf_token %}
+<div class="mt-9 mb-4 mx-auto small-panel">
+  <p class="intro">{% trans "Please input the password if you want to browse the shared file/directory." %}</p>
+  <form action="{% url view_name token %}?p={{ path }}" method="post" id="share-passwd-form">
+    {% csrf_token %}
+    <input type="hidden" name="token" value="{{ token }}" />
+    <div class="form-group">
+      <label for="password">{% trans "Password" %}</label>
+      <input type="password" name="password" id="password" class="form-control" autofocus />
+    </div>
+    <button type="submit" class="btn btn-primary sf-btn-submit">{% trans "Submit" %}</button>
+    {% if err_msg %}
+    <p class="error">{{ err_msg }}</p>
     {% endif %}
-        <input type="text" name="token" value="{{ token }}" class="hide" />
-        <label>{% trans "Password: " %}</label><br />
-        <input type="password" name="password" class="input" autofocus /><br />
-        {% if err_msg %}
-        <p class="error">{{ err_msg }}</p>
-        {% endif %}
-        <input type="submit" value="{% trans "Submit" %}" />
-    </form>
+  </form>
 </div>
 {% endblock %}
 {% block extra_script %}

seahub/templates/base_for_small_panel.html
@@ -1,0 +1,41 @@
+{% extends "base.html" %}
+{% load i18n %}
+
+{% block extra_base_style %}{% endblock %}
+{% block extra_style %}
+<style type="text/css">
+/* to overwrite the code from seahub.css */
+input[type=password] {
+  box-sizing: border-box;
+  height: auto;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="{{ MEDIA_URL }}css/seafile-ui.css" />
+<style type="text/css">
+.small-panel {
+  max-width: 480px;
+  border-radius: 8px;
+  box-shadow: 0 2px 14px 2px rgba(0, 0, 0, 0.1);
+}
+.panel-title {
+  font-size: 1.5rem;
+  height: 90px;
+  line-height: 50px;
+  padding: 20px 40px;
+}
+.panel-content {
+  padding: 16px 40px 50px;
+}
+.intro {
+  font-size: 14px;
+  color: #999;
+}
+.sf-btn-submit {
+  background-image: none;
+}
+</style>
+{% endblock %}
+
+{% block header_right %}
+{# removed the content on the top right corner #}
+{% endblock %}

seahub/templates/decrypt_repo_form.html
@@ -1,44 +1,5 @@
-{% extends "base.html" %}
+{% extends "base_for_small_panel.html" %}
 {% load i18n %}
-
-{% block extra_base_style %}{% endblock %}
-{% block extra_style %}
-<style type="text/css">
-/* to overwrite the code from seahub.css */
-input[type=password] {
-  box-sizing: border-box;
-  height: auto;
-}
-</style>
-<link rel="stylesheet" type="text/css" href="{{ MEDIA_URL }}css/seafile-ui.css" />
-<style type="text/css">
-.small-panel {
-  max-width: 480px;
-  border-radius: 8px;
-  box-shadow: 0 2px 14px 2px rgba(0, 0, 0, 0.1);
-}
-.panel-title {
-  font-size: 1.5rem;
-  height: 90px;
-  line-height: 50px;
-  padding: 20px 40px;
-}
-.panel-content {
-  padding: 16px 40px 50px;
-}
-.intro {
-  font-size: 14px;
-  color: #999;
-}
-.sf-btn-submit {
-  background-image: none;
-}
-</style>
-{% endblock %}
-
-{% block header_right %}
-{# removed the content on the top right corner #}
-{% endblock %}
 
 {% block main_panel %}
 <div class="mt-9 mb-4 mx-auto small-panel">

commit 48dbf741c07c39938ca2116c9b5d7f43258572ab

Fix sharing with password.
Avoid generating URL with empty `p` parameter and also handle it correctly when one is generated.

Fix regression caused by 44459bb36b8c3c3f3d57c9976357ea1cc4103439

Fix #4440

seahub/templates/share_access_validation.html
@@ -4,7 +4,11 @@
 {% block main_panel %}
 <div class="mt-9 mb-4 mx-auto small-panel">
   <p class="intro">{% trans "Please input the password if you want to browse the shared file/directory." %}</p>
+  {% if path %}
   <form action="{% url view_name token %}?p={{ path }}" method="post" id="share-passwd-form">
+  {% else %}
+  <form action="{% url view_name token %}" method="post" id="share-passwd-form">
+  {% endif %}
     {% csrf_token %}
     <input type="hidden" name="token" value="{{ token }}" />
     <div class="form-group">

seahub/views/repo.py
@@ -262,7 +262,10 @@ def view_shared_dir(request, fileshare):
     # Get path from frontend, use '/' if missing, and construct request path
     # with fileshare.path to real path, used to fetch dirents by RPC.
     req_path = request.GET.get('p', '/')
-    if req_path[-1] != '/':
+    if req_path == '':
+        # Handle the case where `p` is present but empty.
+        req_path = '/'
+    elif req_path[-1] != '/':
         req_path += '/'
 
     if req_path == '/':