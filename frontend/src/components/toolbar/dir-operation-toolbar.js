66300cf07bd09c95a0f6ebbba492f5ebfeec7d5f
杨顺强 <ysq9207@126.com>
2020-06-24 18:35:38 +0800

    update readonly toolbar effect (#4605)

--- frontend/src/components/toolbar/dir-operation-toolbar.js
+++ frontend/src/components/toolbar/dir-operation-toolbar.js
@@ -162,10 +162,6 @@ class DirOperationToolbar extends React.Component {
 
   render() {
     let { path, repoName, userPerm } = this.props;
-    
-    if (userPerm !== 'rw' && userPerm !== 'admin') {
-      return '';
-    }
 
     let itemType = path === '/' ? 'library' : 'dir';
     let itemName = path == '/' ? repoName : Utils.getFolderName(path);
@@ -197,28 +193,30 @@ class DirOperationToolbar extends React.Component {
 
     return (
       <Fragment>
-        <div className="dir-operation">
-          <div className="operation">
-            {content}
+        {(userPerm === 'rw' || userPerm === 'admin') && (
+          <div className="dir-operation">
+            <div className="operation">
+              {content}
+            </div>
+            {this.state.isUploadMenuShow && (
+              <ul className="menu dropdown-menu" style={this.state.operationMenuStyle}>
+                <li className="dropdown-item" onClick={this.onUploadFile}>{gettext('Upload Files')}</li>
+                <li className="dropdown-item" onClick={this.onUploadFolder}>{gettext('Upload Folder')}</li>
+              </ul>
+            )}
+            {this.state.isCreateMenuShow && (
+              <ul className="menu dropdown-menu" style={this.state.operationMenuStyle}>
+                <li className="dropdown-item" onClick={this.onCreateFolderToggle}>{gettext('New Folder')}</li>
+                <li className="dropdown-item" onClick={this.onCreateFileToggle}>{gettext('New File')}</li>
+                <li className="dropdown-divider"></li>
+                <li className="dropdown-item" onClick={this.onCreateMarkdownToggle}>{gettext('New Markdown File')}</li>
+                <li className="dropdown-item" onClick={this.onCreateExcelToggle}>{gettext('New Excel File')}</li>
+                <li className="dropdown-item" onClick={this.onCreatePPTToggle}>{gettext('New PowerPoint File')}</li>
+                <li className="dropdown-item" onClick={this.onCreateWordToggle}>{gettext('New Word File')}</li>
+              </ul>
+            )}
           </div>
-          {this.state.isUploadMenuShow && (
-            <ul className="menu dropdown-menu" style={this.state.operationMenuStyle}>
-              <li className="dropdown-item" onClick={this.onUploadFile}>{gettext('Upload Files')}</li>
-              <li className="dropdown-item" onClick={this.onUploadFolder}>{gettext('Upload Folder')}</li>
-            </ul>
-          )}
-          {this.state.isCreateMenuShow && (
-            <ul className="menu dropdown-menu" style={this.state.operationMenuStyle}>
-              <li className="dropdown-item" onClick={this.onCreateFolderToggle}>{gettext('New Folder')}</li>
-              <li className="dropdown-item" onClick={this.onCreateFileToggle}>{gettext('New File')}</li>
-              <li className="dropdown-divider"></li>
-              <li className="dropdown-item" onClick={this.onCreateMarkdownToggle}>{gettext('New Markdown File')}</li>
-              <li className="dropdown-item" onClick={this.onCreateExcelToggle}>{gettext('New Excel File')}</li>
-              <li className="dropdown-item" onClick={this.onCreatePPTToggle}>{gettext('New PowerPoint File')}</li>
-              <li className="dropdown-item" onClick={this.onCreateWordToggle}>{gettext('New Word File')}</li>
-            </ul>
-          )}
-        </div>
+        )}
         {Utils.isDesktop() && <ViewModeToolbar currentMode={this.props.currentMode} switchViewMode={this.props.switchViewMode} />}
         {this.state.isCreateFileDialogShow && (
           <ModalPortal>
