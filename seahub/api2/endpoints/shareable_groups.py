a0f9a34e224d910c225e6a1568f651bb31061d3a
lian <lian@seafile.com>
2021-01-08 18:30:14 +0800

    update seahub web api

--- seahub/api2/endpoints/shareable_groups.py
+++ seahub/api2/endpoints/shareable_groups.py
@@ -13,26 +13,27 @@ from seahub.utils import is_org_context
 from seahub.utils.timeutils import timestamp_to_isoformat_timestr
 from seahub.api2.authentication import TokenAuthentication
 from seahub.api2.throttling import UserRateThrottle
-from seahub.avatar.settings import GROUP_AVATAR_DEFAULT_SIZE
+
+from seahub.settings import ENABLE_SHARE_TO_DEPARTMENT
 
 from constance import config
 
 try:
     current_path = os.path.dirname(os.path.abspath(__file__))
-    seafile_conf_dir = os.path.join(current_path, \
-            '../../../../../conf')
+    seafile_conf_dir = os.path.join(current_path, '../../../../../conf')
     sys.path.append(seafile_conf_dir)
     from seahub_custom_functions import custom_get_groups
     CUSTOM_GET_GROUPS = True
-except ImportError as e:
+except ImportError:
     CUSTOM_GET_GROUPS = False
+
 
 class ShareableGroups(APIView):
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAuthenticated,)
     throttle_classes = (UserRateThrottle, )
 
-    def _get_group_info(self, request, group, avatar_size):
+    def _get_group_info(self, request, group):
         isoformat_timestr = timestamp_to_isoformat_timestr(group.timestamp)
         group_info = {
             "id": group.id,
@@ -60,12 +61,13 @@ class ShareableGroups(APIView):
             else:
                 groups = ccnet_api.get_groups(username)
 
-        try:
-            avatar_size = int(request.GET.get('avatar_size',
-                GROUP_AVATAR_DEFAULT_SIZE))
-        except ValueError:
-            avatar_size = GROUP_AVATAR_DEFAULT_SIZE
+        filtered_groups = []
+        for group in groups:
+            if not ENABLE_SHARE_TO_DEPARTMENT and group.parent_group_id != 0:
+                continue
+            else:
+                filtered_groups.append(group)
 
-        result = [self._get_group_info(request, group, avatar_size) for group in groups]
+        result = [self._get_group_info(request, group) for group in filtered_groups]
 
         return Response(result)
