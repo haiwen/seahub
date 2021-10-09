5ac6baf575abf5bc6f3b06a7c2f546624a9064b1
skywalker <dzmbbs@qq.com>
2021-10-09 22:05:09 +0800

    sql-9.0 mysql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -75,6 +75,8 @@ CREATE TABLE `api2_tokenv2` (
   PRIMARY KEY (`key`),
   UNIQUE KEY `api2_tokenv2_user_platform_device_id_37005c24_uniq` (`user`,`platform`,`device_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `api2_tokenv2` CHANGE COLUMN `device_name` `device_name` varchar(40) CHARACTER SET 'utf8mb4' COLLATE utf8mb4_unicode_ci NOT NULL;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 /*!40000 ALTER TABLE `api2_tokenv2` DISABLE KEYS */;
@@ -1292,3 +1294,40 @@ CREATE TABLE `external_department` (
     UNIQUE KEY `group_id` (`group_id`),
     UNIQUE KEY `external_department_provider_outer_id_8dns6vkw_uniq` (`provider`,`outer_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `custom_share_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `repo_id` varchar(36) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `permission` longtext NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `custom_share_permission_repo_id_578fe49f` (`repo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `ocm_via_webdav_received_shares` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `owner_display_name` varchar(255) DEFAULT NULL,
+  `protocol_name` varchar(255) NOT NULL,
+  `shared_secret` varchar(255) NOT NULL,
+  `permissions` varchar(255) NOT NULL,
+  `provider_id` varchar(255) NOT NULL,
+  `resource_type` varchar(255) NOT NULL,
+  `share_type` varchar(255) NOT NULL,
+  `share_with` varchar(255) NOT NULL,
+  `shared_by` varchar(255) NOT NULL,
+  `shared_by_display_name` varchar(255) DEFAULT NULL,
+  `ctime` datetime(6) NOT NULL,
+  `is_dir` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ocm_via_webdav_share_received_owner_261eaa70` (`owner`),
+  KEY `ocm_via_webdav_share_received_shared_secret_fbb6be5a` (`shared_secret`),
+  KEY `ocm_via_webdav_share_received_provider_id_a55680e9` (`provider_id`),
+  KEY `ocm_via_webdav_share_received_resource_type_a3c71b57` (`resource_type`),
+  KEY `ocm_via_webdav_share_received_share_type_7615aaab` (`share_type`),
+  KEY `ocm_via_webdav_share_received_share_with_5a23eb17` (`share_with`),
+  KEY `ocm_via_webdav_share_received_shared_by_1786d580` (`shared_by`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
