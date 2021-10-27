56d81b0156460828b8fa766b0009c48f0ada7a0c
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2021-10-27 14:01:05 +0200

    fix: user id compliant with the html template

--- seahub/onlyoffice/utils.py
+++ seahub/onlyoffice/utils.py
@@ -154,7 +154,7 @@ def get_onlyoffice_dict(request, username, repo_id, file_path, file_id='',
                     "forcesave": ONLYOFFICE_FORCE_SAVE,
                 },
                 "user": {
-                    "id": hashlib.md5(force_bytes(username)).hexdigest(),
+                    "id": username,
                     "name": email2nickname(username)
                 }
             }
