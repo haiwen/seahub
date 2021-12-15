b8f45959c70958da4dcfe2805107dd684cb15aa3
lian <lian@seafile.com>
2021-12-15 11:40:11 +0800

    update admin org department libraries page
    check enableSysAdminViewRepo

--- frontend/src/pages/sys-admin/departments/repo-item.js
+++ frontend/src/pages/sys-admin/departments/repo-item.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Utils } from '../../../utils/utils.js';
 import { siteRoot, gettext } from '../../../utils/constants';
 
+const { enableSysAdminViewRepo } = window.sysadmin.pageOptions;
+
 const RepoItemPropTypes = {
   repo: PropTypes.object.isRequired,
   showDeleteRepoDialog: PropTypes.func.isRequired,
@@ -32,7 +34,11 @@ class RepoItem extends React.Component {
     return (
       <tr className={highlight ? 'tr-highlight' : ''} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
         <td><img src={iconUrl} width="24" alt={gettext('icon')}/></td>
-        <td><a href={siteRoot + 'sys/libraries/' + repo.repo_id + '/' + repo.name + '/'}>{repo.name}</a></td>
+        { enableSysAdminViewRepo ?
+          <td><a href={siteRoot + 'sys/libraries/' + repo.repo_id + '/' + repo.name + '/'}>{repo.name}</a></td>
+          :
+          <td>{repo.name}</td>
+        }
         <td>{Utils.bytesToSize(repo.size)}{' '}</td>
 	{ this.props.orgID == -1 ?
           <td className="cursor-pointer text-center" onClick={this.props.showDeleteRepoDialog.bind(this, repo)}>
