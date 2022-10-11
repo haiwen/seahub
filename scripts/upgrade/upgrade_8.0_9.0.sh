c58b4896a911526c5e9331018575a328d88edac5
Thomas Marko <85223119+arw-thomasm@users.noreply.github.com>
2022-10-11 05:05:43 +0200

    Fixed issue #5032 (#5277)

--- scripts/upgrade/upgrade_8.0_9.0.sh
+++ scripts/upgrade/upgrade_8.0_9.0.sh
@@ -190,7 +190,7 @@ function move_old_customdir_outside() {
 }
 
 function update_seahub_settings () {
-    service_url=`awk -F '=' '/\[General\]/{a=1}a==1&&$1~/SERVICE_URL/{print $2;exit}' ${default_conf_dir}/ccnet.conf`
+    service_url=`awk -F '=' '/\[General\]/{a=1}a==1&&$1~/^SERVICE_URL/{print $2;exit}' ${default_conf_dir}/ccnet.conf`
     service_url=$(echo $service_url)
     echo "SERVICE_URL = '${service_url}'">>${default_conf_dir}/seahub_settings.py
 }
