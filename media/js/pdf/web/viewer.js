4b0ba51b52929cf5d34cb834473f797c4ddc247e
llj <lingjun.li1@gmail.com>
2022-11-02 14:48:55 +0800

    [pdf file view] enable 'zoom in/out by pinch' for mobile

--- media/js/pdf/web/viewer.js
+++ media/js/pdf/web/viewer.js
@@ -533,6 +533,9 @@ const PDFViewerApplication = {
     });
 
     this._initializedCapability.resolve();
+
+    // for mobile
+    enablePinchZoom();
   },
 
   async _readPreferences() {
@@ -2816,6 +2819,52 @@ function webViewerTouchStart(evt) {
   if (evt.touches.length > 1) {
     evt.preventDefault();
   }
+}
+
+// for mobile
+function enablePinchZoom() {
+  let startX = 0, startY = 0;
+  let initialPinchDistance = 0;
+  let pinchScale = 1;
+  const viewer = document.getElementById("viewer");
+  const container = document.getElementById("viewerContainer");
+  const reset = () => { startX = startY = initialPinchDistance = 0; pinchScale = 1; };
+  // Prevent native iOS page zoom
+  //document.addEventListener("touchmove", (e) => { if (e.scale !== 1) { e.preventDefault(); } }, { passive: false });
+
+  document.addEventListener("touchstart", (e) => {
+    if (e.touches.length > 1) {
+      startX = (e.touches[0].pageX + e.touches[1].pageX) / 2;
+      startY = (e.touches[0].pageY + e.touches[1].pageY) / 2;
+      initialPinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+    } else {
+      initialPinchDistance = 0;
+    }
+  });
+
+  document.addEventListener("touchmove", (e) => {
+    if (initialPinchDistance <= 0 || e.touches.length < 2) { return; }
+    if (e.scale !== 1) { e.preventDefault(); }
+    const pinchDistance = Math.hypot((e.touches[1].pageX - e.touches[0].pageX), (e.touches[1].pageY - e.touches[0].pageY));
+    const originX = startX + container.scrollLeft;
+    const originY = startY + container.scrollTop;
+    pinchScale = pinchDistance / initialPinchDistance;
+    viewer.style.transform = `scale(${pinchScale})`;
+    viewer.style.transformOrigin = `${originX}px ${originY}px`;
+  }, { passive: false });
+
+  document.addEventListener("touchend", (e) => {
+    if (initialPinchDistance <= 0) { return; }
+    viewer.style.transform = `none`;
+    viewer.style.transformOrigin = `unset`;
+    PDFViewerApplication.pdfViewer.currentScale *= pinchScale;
+    const rect = container.getBoundingClientRect();
+    const dx = startX - rect.left;
+    const dy = startY - rect.top;
+    container.scrollLeft += dx * (pinchScale - 1);
+    container.scrollTop += dy * (pinchScale - 1);
+    reset();
+  });
 }
 
 function webViewerClick(evt) {
