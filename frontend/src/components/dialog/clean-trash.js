80b4b5fd6cbe52aa1d589d53d7409196d9aca3ad
杨顺强 <978987373@qq.com>
2022-12-22 16:02:11 +0800

    Update dependency 2 (#5342)
    * update react-select version
    
    * update react-responsive
    
    * delete unnecessary dependency
    
    * update react-mentions

--- frontend/src/components/dialog/clean-trash.js
+++ frontend/src/components/dialog/clean-trash.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import Select from 'react-select/lib/Creatable';
+import CreatableSelect from 'react-select/creatable';
 import { gettext } from '../../utils/constants';
 import { seafileAPI } from '../../utils/seafile-api';
 import { Utils } from '../../utils/utils';
@@ -63,7 +63,7 @@ class CleanTrash extends React.Component {
         <ModalBody>
           <React.Fragment>
             <p>{gettext('Clear files in trash and history：')}</p>
-            <Select
+            <CreatableSelect
               defaultValue={this.options[0]}
               options={this.options}
               autoFocus={false}
--- frontend/src/components/dialog/edit-repo-commit-labels.js
+++ frontend/src/components/dialog/edit-repo-commit-labels.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import CreatableSelect from 'react-select/lib/Creatable';
+import CreatableSelect from 'react-select/creatable';
 import { gettext } from '../../utils/constants';
 import { seafileAPI } from '../../utils/seafile-api';
 import { Utils } from '../../utils/utils';
