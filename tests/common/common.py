5dde8000f4f657b9d9e2770af8aeffcf4737f796
mainboarder <git@mainboarder.de>
2023-01-14 08:00:22 +0100

    rfc 2606 domain (#5320)
    * rfc 2606 domain
    
    * rfc 6761 domain

--- tests/common/common.py
+++ tests/common/common.py
@@ -2,9 +2,9 @@ import os
 import urllib.parse
 
 BASE_URL = os.getenv('SEAHUB_TEST_BASE_URL', 'http://127.0.0.1:8000')
-USERNAME = os.getenv('SEAHUB_TEST_USERNAME', 'test@seafiletest.com')
+USERNAME = os.getenv('SEAHUB_TEST_USERNAME', 'test@example.com')
 PASSWORD = os.getenv('SEAHUB_TEST_PASSWORD', 'testtest')
-ADMIN_USERNAME = os.getenv('SEAHUB_TEST_ADMIN_USERNAME', 'admin@seafiletest.com')
+ADMIN_USERNAME = os.getenv('SEAHUB_TEST_ADMIN_USERNAME', 'admin@example.com')
 ADMIN_PASSWORD = os.getenv('SEAHUB_TEST_ADMIN_PASSWORD', 'adminadmin')
 
 if os.getenv('SEAHUB_TEST_IS_PRO', '') == '':
