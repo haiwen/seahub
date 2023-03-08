2da3926132c0330665a2a9089d2fcedcabf10457
lian <imwhatiam123@gmail.com>
2023-03-08 17:19:57 +0800

    send search user request after one second

--- frontend/src/components/user-select.js
+++ frontend/src/components/user-select.js
@@ -21,6 +21,7 @@ class UserSelect extends React.Component {
   constructor(props) {
     super(props);
     this.options = [];
+    this.finalValue = '';
     this.state = {
       searchValue: '',
     };
@@ -36,37 +37,46 @@ class UserSelect extends React.Component {
   }
 
   loadOptions = (input, callback) => {
+
     const value = input.trim();
-    if (value.length > 0) {
-      seafileAPI.searchUsers(value).then((res) => {
-        this.options = [];
-        for (let i = 0 ; i < res.data.users.length; i++) {
-          const item = res.data.users[i];
-          let obj = {};
-          obj.value = item.name;
-          obj.email = item.email;
-          obj.label =
-              enableShowContactEmailWhenSearchUser ? (
-                <div className="d-flex">
-                  <img src={item.avatar_url} className="avatar" width="24" alt="" />
-                  <div className="ml-2">
-                    <span className="user-option-name">{item.name}</span><br />
-                    <span className="user-option-email">{item.contact_email}</span>
-                  </div>
-                </div>
-              ) : (
-                <React.Fragment>
-                  <img src={item.avatar_url} className="select-module select-module-icon avatar" alt=""/>
-                  <span className='select-module select-module-name'>{item.name}</span>
-                </React.Fragment>);
-          this.options.push(obj);
+
+    this.finalValue = value;
+
+    setTimeout(() => {
+      if (this.finalValue === value) {
+        if (value.length > 0) {
+          seafileAPI.searchUsers(value).then((res) => {
+            this.options = [];
+            for (let i = 0 ; i < res.data.users.length; i++) {
+              const item = res.data.users[i];
+              let obj = {};
+              obj.value = item.name;
+              obj.email = item.email;
+              obj.label =
+                  enableShowContactEmailWhenSearchUser ? (
+                    <div className="d-flex">
+                      <img src={item.avatar_url} className="avatar" width="24" alt="" />
+                      <div className="ml-2">
+                        <span className="user-option-name">{item.name}</span><br />
+                        <span className="user-option-email">{item.contact_email}</span>
+                      </div>
+                    </div>
+                  ) : (
+                    <React.Fragment>
+                      <img src={item.avatar_url} className="select-module select-module-icon avatar" alt=""/>
+                      <span className='select-module select-module-name'>{item.name}</span>
+                    </React.Fragment>);
+              this.options.push(obj);
+            }
+            callback(this.options);
+          }).catch(error => {
+            let errMessage = Utils.getErrorMsg(error);
+            toaster.danger(errMessage);
+          });
         }
-        callback(this.options);
-      }).catch(error => {
-        let errMessage = Utils.getErrorMsg(error);
-        toaster.danger(errMessage);
-      });
-    }
+      }
+    }, 1000);
+
   }
 
   clearSelect = () => {
