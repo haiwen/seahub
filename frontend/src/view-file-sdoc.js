3b97047ff20ffd6f70f6bd7fa3107cf77364dfa3
杨顺强 <978987373@qq.com>
2023-04-21 11:22:42 +0800

    update code

--- frontend/src/view-file-sdoc.js
+++ frontend/src/view-file-sdoc.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { SimpleEditor } from '@seafile/sdoc-editor';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './_i18n/i18n-sdoc-editor';
+import { Suspense } from 'react';
+import Loading from './components/loading';
 
 const { serviceURL } = window.app.config;
 const { repoID, docPath, docName, docUuid, seadocAccessToken, seadocServerUrl } = window.app.pageOptions;
@@ -16,13 +20,11 @@ window.seafile = {
   sdocServer: seadocServerUrl,
 };
 
-class ViewFileSdoc extends React.Component {
-
-
-
-  render() {
-    return (<SimpleEditor />);
-  }
-}
-
-ReactDom.render(<ViewFileSdoc />, document.getElementById('wrapper'));
+ReactDom.render(
+  <I18nextProvider i18n={ i18n } >
+    <Suspense fallback={<Loading />}>
+      <SimpleEditor />
+    </Suspense>
+  </I18nextProvider>,
+  document.getElementById('wrapper')
+);
