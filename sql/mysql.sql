8e3dfe4d7b14c796eedc0271c705ae63e84ad73c
skywalker <dzmbbs@qq.com>
2023-05-05 18:21:39 +0800

    operation_log

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1373,3 +1373,17 @@ CREATE TABLE `organizations_orgadminsettings` (
   UNIQUE KEY `organizations_orgadminsettings_org_id_key_a01cc7de_uniq` (`org_id`,`key`),
   KEY `organizations_orgadminsettings_org_id_4f70d186` (`org_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `operation_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_uuid` varchar(36) NOT NULL,
+  `op_id` bigint(20) NOT NULL,
+  `op_time` bigint(20) NOT NULL,
+  `operations` longtext NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `app` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `operation_log_op_time` (`op_time`),
+  KEY `operation_log_doc_uuid` (`doc_uuid`),
+  KEY `idx_operation_log_doc_uuid_op_id` (`doc_uuid`,`op_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
