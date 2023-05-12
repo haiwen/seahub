3746fedfb42ba4f95f39b3b4faf3614d8e91a4a4
杨顺强 <978987373@qq.com>
2023-05-12 14:12:28 +0800

    optimize code (#5467)
    * optimize code
    
    * optimize code
    
    * optimize code

--- frontend/src/view-file-sdoc.js
+++ frontend/src/view-file-sdoc.js
@@ -1,10 +1,10 @@
 import React, { Suspense } from 'react';
 import ReactDom from 'react-dom';
-import { SimpleEditor } from '@seafile/sdoc-editor';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './_i18n/i18n-sdoc-editor';
 import { Utils } from './utils/utils';
 import Loading from './components/loading';
+import SdocEditor from './pages/sdoc-editor';
 
 const { serviceURL, avatarURL, siteRoot } = window.app.config;
 const { username, name } = window.app.userInfo;
@@ -34,7 +34,7 @@ window.seafile = {
 ReactDom.render(
   <I18nextProvider i18n={ i18n } >
     <Suspense fallback={<Loading />}>
-      <SimpleEditor />
+      <SdocEditor />
     </Suspense>
   </I18nextProvider>,
   document.getElementById('wrapper')
