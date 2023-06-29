3f04f2ec7eb0b255d5857fbd18dd69cc2b91750a
llj <lingjun.li1@gmail.com>
2023-06-29 15:25:52 +0800

    [sdoc file view] added 'star/unstar'

--- frontend/src/pages/sdoc-editor/external-operations.js
+++ frontend/src/pages/sdoc-editor/external-operations.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { EventBus, EXTERNAL_EVENT } from '@seafile/sdoc-editor';
+import { seafileAPI } from '../../utils/seafile-api';
+import { Utils } from '../../utils/utils';
+import toaster from '../../components/toast';
 import InternalLinkDialog from '../../components/dialog/internal-link-dialog';
 
 const propTypes = {
   repoID: PropTypes.string.isRequired,
   docPath: PropTypes.string.isRequired,
+  isStarred: PropTypes.bool.isRequired,
+  toggleStar: PropTypes.func.isRequired
 };
 
 class ExternalOperations extends React.Component {
@@ -20,14 +25,35 @@ class ExternalOperations extends React.Component {
   componentDidMount() {
     const eventBus = EventBus.getInstance();
     this.unsubscribeInternalLinkEvent = eventBus.subscribe(EXTERNAL_EVENT.INTERNAL_LINK_CLICK, this.onInternalLinkToggle);
+    this.unsubscribeStar = eventBus.subscribe(EXTERNAL_EVENT.TOGGLE_STAR, this.toggleStar);
   }
 
   componentWillUnmount() {
     this.unsubscribeInternalLinkEvent();
+    this.unsubscribeStar();
   }
 
   onInternalLinkToggle = () => {
     this.setState({isShowInternalLinkDialog: !this.state.isShowInternalLinkDialog});
+  }
+
+  toggleStar = () => {
+    const { isStarred, repoID, docPath  } = this.props;
+    if (isStarred) {
+      seafileAPI.unstarItem(repoID, docPath).then((res) => {
+        this.props.toggleStar(false);
+      }).catch((error) => {
+        const errorMsg = Utils.getErrorMsg(error);
+        toaster.danger(errorMsg);
+      });
+    } else {
+      seafileAPI.starItem(repoID, docPath).then((res) => {
+        this.props.toggleStar(true);
+      }).catch((error) => {
+        const errorMsg = Utils.getErrorMsg(error);
+        toaster.danger(errorMsg);
+      });
+    }
   }
 
   render() {
