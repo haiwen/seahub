4e100a20df75642d787b2c759d587178d1882e33
skywalker <dzmbbs@qq.com>
2023-06-30 09:54:19 +0800

    update 11.0 sqlite3

--- sql/sqlite3.sql
+++ sql/sqlite3.sql
@@ -621,4 +621,9 @@ CREATE INDEX IF NOT EXISTS "base_usermonitoredrepos_email_55ead1b9" ON "base_use
 CREATE INDEX IF NOT EXISTS "base_usermonitoredrepos_repo_id_00e624c3" ON "base_usermonitoredrepos" ("repo_id");
 CREATE TABLE IF NOT EXISTS "organizations_orgadminsettings" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "org_id" integer NOT NULL, "key" varchar(255) NOT NULL, "value" TEXT NOT NULL, UNIQUE ("org_id", "key"));
 CREATE INDEX IF NOT EXISTS "organizations_orgadminsettings_org_id_4f70d186" ON "organizations_orgadminsettings" ("org_id");
+CREATE TABLE IF NOT EXISTS "history_name" ("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "doc_uuid" varchar(36) NOT NULL, "obj_id" varchar(40) NOT NULL, "name" varchar(255) NOT NULL, UNIQUE ("doc_uuid", "obj_id"));
+CREATE INDEX IF NOT EXISTS "history_name_doc_uuid" ON "history_name" ("doc_uuid");
+CREATE TABLE IF NOT EXISTS "sdoc_draft" ("id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "doc_uuid" varchar(36) NOT NULL UNIQUE, "repo_id" varchar(36) NOT NULL, "username" varchar(255) NOT NULL, "created_at" datetime NOT NULL);
+CREATE INDEX IF NOT EXISTS "sdoc_draft_repo_id" ON "sdoc_draft" ("repo_id");
+CREATE INDEX IF NOT EXISTS "sdoc_draft_username" ON "sdoc_draft" ("username");
 COMMIT;
