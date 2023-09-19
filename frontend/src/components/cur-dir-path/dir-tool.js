4de5c924cf7b9a015f3c2c33238c0b29084e0975
Michael An <2331806369@qq.com>
2023-09-19 11:43:52 +0800

    01 tags icon always show

--- frontend/src/components/cur-dir-path/dir-tool.js
+++ frontend/src/components/cur-dir-path/dir-tool.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { gettext, siteRoot } from '../../utils/constants';
 import { Utils } from '../../utils/utils';
 import PropTypes from 'prop-types';
@@ -64,69 +64,82 @@ class DirTool extends React.Component {
   };
 
   isMarkdownFile(filePath) {
-    let name = Utils.getFileName(filePath);
-    return name.indexOf('.md') > -1 ? true : false;
+    return Utils.getFileName(filePath).includes('.md');
   }
 
   render() {
     let { repoID, userPerm, currentPath } = this.props;
-    let isFile = this.isMarkdownFile(currentPath);
-    let name = Utils.getFileName(currentPath);
-    let trashUrl = siteRoot + 'repo/' + repoID + '/trash/';
-    let historyUrl = siteRoot + 'repo/history/' + repoID + '/';
-    if (userPerm === 'rw') {
-      if (!isFile) {
-        if (name) { // name not '' is not root path
-          trashUrl = siteRoot + 'repo/' + repoID + '/trash/?path=' + encodeURIComponent(currentPath);
-          return (
-            <ul className="path-toolbar">
-              <li className="toolbar-item"><a className="op-link sf2-icon-recycle" href={trashUrl} title={gettext('Trash')} aria-label={gettext('Trash')}></a></li>
-            </ul>
-          );
-        } else { // currentPath === '/' is root path
-          return (
-            <Fragment>
-              <ul className="path-toolbar">
-                <li className="toolbar-item"><a className="op-link sf2-icon-tag" href="#" role="button" onClick={this.onShowListRepoTag} title={gettext('Tags')} aria-label={gettext('Tags')}></a></li>
-                <li className="toolbar-item"><a className="op-link sf2-icon-recycle" href={trashUrl} title={gettext('Trash')} aria-label={gettext('Trash')}></a></li>
-                <li className="toolbar-item"><a className="op-link sf2-icon-history" href={historyUrl} title={gettext('History')} aria-label={gettext('History')}></a></li>
-              </ul>
-
-              {this.state.isRepoTagDialogShow && (
-                <ModalPortal>
-                  <Modal isOpen={true} autoFocus={false}>
-                    {this.state.isListRepoTagShow && (
-                      <ListTagDialog
-                        repoID={repoID}
-                        onListTagCancel={this.onCloseRepoTagDialog}
-                        onCreateRepoTag={this.onCreateRepoTagToggle}
-                      />
-                    )}
-                    {this.state.isCreateRepoTagShow && (
-                      <CreateTagDialog
-                        repoID={repoID}
-                        onClose={this.onCloseRepoTagDialog}
-                        toggleCancel={this.onCreateRepoTagToggle}
-                      />
-                    )}
-                    {this.state.isListTaggedFileShow && (
-                      <ListTaggedFilesDialog
-                        repoID={this.props.repoID}
-                        currentTag={this.state.currentTag}
-                        onClose={this.onCloseRepoTagDialog}
-                        toggleCancel={this.onListTaggedFileToggle}
-                        updateUsedRepoTags={this.props.updateUsedRepoTags}
-                      />
-                    )}
-                  </Modal>
-                </ModalPortal>
-              )}
-            </Fragment>
-          );
-        }
-      }
+    if (userPerm !== 'rw') {
+      return '';
     }
-    return '';
+    if (this.isMarkdownFile(currentPath)) {
+      return '';
+    }
+
+    let toolbarDom = null;
+    if (Utils.getFileName(currentPath)) { // name not '' is not root path
+      let trashUrl = siteRoot + 'repo/' + repoID + '/trash/?path=' + encodeURIComponent(currentPath);
+      toolbarDom = (
+        <ul className="path-toolbar">
+          <li className="toolbar-item">
+            <a className="op-link sf2-icon-tag" href="#" role="button" onClick={this.onShowListRepoTag} title={gettext('Tags')} aria-label={gettext('Tags')}></a>
+          </li>
+          <li className="toolbar-item">
+            <a className="op-link sf2-icon-recycle" href={trashUrl} title={gettext('Trash')} aria-label={gettext('Trash')}></a>
+          </li>
+        </ul>
+      );
+    } else { // currentPath === '/' is root path
+      let trashUrl = siteRoot + 'repo/' + repoID + '/trash/';
+      let historyUrl = siteRoot + 'repo/history/' + repoID + '/';
+      toolbarDom = (
+        <ul className="path-toolbar">
+          <li className="toolbar-item">
+            <a className="op-link sf2-icon-tag" href="#" role="button" onClick={this.onShowListRepoTag} title={gettext('Tags')} aria-label={gettext('Tags')}></a>
+          </li>
+          <li className="toolbar-item">
+            <a className="op-link sf2-icon-recycle" href={trashUrl} title={gettext('Trash')} aria-label={gettext('Trash')}></a>
+          </li>
+          <li className="toolbar-item">
+            <a className="op-link sf2-icon-history" href={historyUrl} title={gettext('History')} aria-label={gettext('History')}></a>
+          </li>
+        </ul>
+      );
+    }
+    return (
+      <>
+        {toolbarDom}
+        {this.state.isRepoTagDialogShow &&
+          <ModalPortal>
+            <Modal isOpen={true} autoFocus={false}>
+              {this.state.isListRepoTagShow && (
+                <ListTagDialog
+                  repoID={repoID}
+                  onListTagCancel={this.onCloseRepoTagDialog}
+                  onCreateRepoTag={this.onCreateRepoTagToggle}
+                />
+              )}
+              {this.state.isCreateRepoTagShow && (
+                <CreateTagDialog
+                  repoID={repoID}
+                  onClose={this.onCloseRepoTagDialog}
+                  toggleCancel={this.onCreateRepoTagToggle}
+                />
+              )}
+              {this.state.isListTaggedFileShow && (
+                <ListTaggedFilesDialog
+                  repoID={this.props.repoID}
+                  currentTag={this.state.currentTag}
+                  onClose={this.onCloseRepoTagDialog}
+                  toggleCancel={this.onListTaggedFileToggle}
+                  updateUsedRepoTags={this.props.updateUsedRepoTags}
+                />
+              )}
+            </Modal>
+          </ModalPortal>
+        }
+      </>
+    );
   }
 }
 
