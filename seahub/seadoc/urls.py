4be3e665e11284c1e635f230781c112ca7050e9c
欢乐马 <38058090+SkywalkerSpace@users.noreply.github.com>
2023-09-19 08:41:31 -0500

    SeadocFileUUIDView (#5646)
    * SeadocFileUUIDView
    
    * SeadocDirView IMAGE
    
    * fix file_uuid
    
    * fix path

--- seahub/seadoc/urls.py
+++ seahub/seadoc/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from .apis import SeadocAccessToken, SeadocUploadLink, SeadocDownloadLink, SeadocRevisionDownloadLinks, SeadocUploadFile, \
     SeadocUploadImage, SeadocDownloadImage, SeadocAsyncCopyImages, SeadocCopyHistoryFile, SeadocHistory, SeadocDrafts, SeadocMaskAsDraft, \
     SeadocCommentsView, SeadocCommentView, SeadocStartRevise, SeadocPublishRevision, SeadocRevisionsCount, SeadocRevisions, \
-    SeadocCommentRepliesView, SeadocCommentReplyView, SeadocFileView, SeadocDirView
+    SeadocCommentRepliesView, SeadocCommentReplyView, SeadocFileView, SeadocFileUUIDView, SeadocDirView
 
 
 # api/v2.1/seadoc/
@@ -28,5 +28,6 @@ urlpatterns = [
     re_path(r'^revisions-count/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocRevisionsCount.as_view(), name='seadoc_revisions_count'),
     re_path(r'^revisions/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocRevisions.as_view(), name='seadoc_revisions'),
     re_path(r'^file/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocFileView.as_view(), name='seadoc_file_view'),
+    re_path(r'^file-uuid/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocFileUUIDView.as_view(), name='seadoc_file_uuid_view'),
     re_path(r'^dir/(?P<file_uuid>[-0-9a-f]{36})/$', SeadocDirView.as_view(), name='seadoc_dir_view'),
 ]
