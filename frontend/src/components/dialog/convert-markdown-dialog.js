6272029a958e6e95aa587b7041e204adf8198904
杨顺强 <978987373@qq.com>
2023-09-25 17:19:52 +0800

    optimize code

--- frontend/src/components/dialog/convert-markdown-dialog.js
+++ frontend/src/components/dialog/convert-markdown-dialog.js
@@ -3,9 +3,11 @@ import { Modal, ModalBody } from 'reactstrap';
 import Loading from '../loading';
 import { gettext } from '../../utils/constants';
 
+import '../../css/convert-markdown.css';
+
 function ConvertMarkdownDialog() {
   return (
-    <Modal isOpen={true} className="container-markdown-container" style={{width: '300px'}}>
+    <Modal isOpen={true} centered={true} className="container-markdown-container">
       <ModalBody className='container-markdown-content'>
         <Loading />
         <div className='d-flex justify-content-center'>{gettext('Converting')}...</div>
