db11007b07a8f27096bb183c4b84e8a0463b7c7c
杨顺强 <978987373@qq.com>
2023-11-04 11:01:09 +0800

    add sdoc update script

---
+++ sdoc-helper.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd frontend
+
+npm install @seafile/sdoc-editor@latest
+
+cd ..
+
+cp -r frontend/node_modules/@seafile/sdoc-editor/public/media/sdoc-editor-font media/sdoc-editor/sdoc-editor-font
+cp -r frontend/node_modules/@seafile/sdoc-editor/public/media/sdoc-editor-font.css media/sdoc-editor/sdoc-editor-font.css
+
+tx pull -s -t -f seahub.sdoc-editor
+
+git add .
+
+git commit -m "update sdoc version and sdoc translate"
+
+# git push origin master
+
