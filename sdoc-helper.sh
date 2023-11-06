b5354fca576f88bc2f71a5551667ab03692c1884
杨顺强 <978987373@qq.com>
2023-11-06 11:25:48 +0800

    update sdoc translate

--- sdoc-helper.sh
+++ sdoc-helper.sh
@@ -9,7 +9,10 @@ cd ..
 cp -r frontend/node_modules/@seafile/sdoc-editor/public/media/sdoc-editor-font media/sdoc-editor/sdoc-editor-font
 cp -r frontend/node_modules/@seafile/sdoc-editor/public/media/sdoc-editor-font.css media/sdoc-editor/sdoc-editor-font.css
 
+
 tx pull -s -t -f seahub.sdoc-editor
+
+cp -r media/sdoc-editor/locales/zh_CN/sdoc-editor.json media/sdoc-editor/locales/zh-CN/sdoc-editor.json
 
 git add .
 
