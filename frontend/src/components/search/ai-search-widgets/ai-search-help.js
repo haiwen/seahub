70da4e0322b6eda34b541046fd84592bd45f9f63
Michael An <2331806369@qq.com>
2023-11-23 10:01:03 +0800

    change translation

--- frontend/src/components/search/ai-search-widgets/ai-search-help.js
+++ frontend/src/components/search/ai-search-widgets/ai-search-help.js
@@ -11,11 +11,11 @@ export default function AISearchHelp() {
       <div className='ai-search-help-container'>
         <div className="ai-search-help-detail" key={1}>
           <Icon symbol='helpful' />
-          <span className="pl-1">Yes</span>
+          <span className="pl-1">{gettext('Yes')}</span>
         </div>
         <div className="ai-search-help-detail" key={2}>
           <Icon symbol='helpless' />
-          <span className="pl-1">No</span>
+          <span className="pl-1">{gettext('No')}</span>
         </div>
       </div>
     </div>
