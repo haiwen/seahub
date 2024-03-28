7b0d2fbec0d08e381eb5f97639578b5b82d0f57c
杨顺强 <978987373@qq.com>
2024-03-28 12:29:03 +0800

    optimize code

---
+++ frontend/src/pages/plain-markdown-editor/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default function PlainMarkdownEditor() {
+  return (
+    <div>plainMarkdownEditor</div>
+  );
+}
+
+ReactDom.render(<PlainMarkdownEditor />, document.getElementById('root'));
