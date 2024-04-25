2810de60a69fcf981bc38b1a17d656ca9294a4a3
lian <imwhatiam123@gmail.com>
2024-04-25 18:04:44 +0800

    view odg file via collabora code (#6039)

--- seahub/utils/__init__.py
+++ seahub/utils/__init__.py
@@ -129,7 +129,7 @@ MAX_INT = 2147483647
 
 PREVIEW_FILEEXT = {
     IMAGE: ('gif', 'jpeg', 'jpg', 'png', 'ico', 'bmp', 'tif', 'tiff', 'psd', 'webp', 'jfif'),
-    DOCUMENT: ('doc', 'docx', 'docxf', 'oform', 'ppt', 'pptx', 'odt', 'fodt', 'odp', 'fodp'),
+    DOCUMENT: ('doc', 'docx', 'docxf', 'oform', 'ppt', 'pptx', 'odt', 'fodt', 'odp', 'fodp', 'odg'),
     SPREADSHEET: ('xls', 'xlsx', 'ods', 'fods'),
     SVG: ('svg',),
     PDF: ('pdf', 'ai'),
