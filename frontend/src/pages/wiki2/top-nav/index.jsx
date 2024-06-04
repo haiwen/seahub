1d41606df9ccad895a562c7f66818b0af1a0a562
Michael An <2331806369@qq.com>
2024-06-04 16:23:59 +0800

    03 change top nav style

--- frontend/src/pages/wiki2/top-nav/index.jsx
+++ frontend/src/pages/wiki2/top-nav/index.jsx
@@ -3,29 +3,54 @@ import PropTypes from 'prop-types';
 import NavItemIcon from '../view-structure/nav-item-icon';
 import './index.css';
 
+// Find the path array from the root to the leaf based on the currentPageId (leaf)
+function getPaths(navigation, currentPageId, pages) {
+  let idPageMap = {};
+  pages.forEach(page => idPageMap[page.id] = page);
+  navigation.forEach(item => {
+    if (!idPageMap[item.id]) {
+      idPageMap[item.id] = item;
+    }
+  });
+  let pathStr = null;
+  function runNode(node) {
+    const newPath = node._path ? (node._path + '-' + node.id) : node.id;
+    if (node.id === currentPageId) {
+      pathStr = newPath;
+      return;
+    }
+    if (node.children) {
+      node.children.forEach(child => {
+        child._path = newPath;
+        runNode(child);
+      });
+    }
+  }
+  let root = {};
+  root.id = '';
+  root._path = '';
+  root.children = navigation;
+  runNode(root);
+  if (!pathStr) return [];
+  return pathStr.split('-').map(id => idPageMap[id]);
+}
+
 function WikiTopNav({ config, currentPageId }) {
   const { navigation, pages } = config;
-  const folder = navigation.find(item => {
-    return item.type === 'folder' && item.children && item.children.find(item => item.id === currentPageId);
-  });
-  const page = pages.find(page => page.id === currentPageId);
+  const paths = getPaths(navigation, currentPageId, pages);
   return (
     <div className="wiki2-top-nav d-flex">
-      {folder &&
-        <>
-          <div className='wiki2-top-nav-item d-flex'>
-            <NavItemIcon symbol={'wiki-folder'} disable={true} />
-            {folder.name}
-          </div>
-          <div>/</div>
-        </>
-      }
-      {page &&
-        <div className='wiki2-top-nav-item d-flex'>
-          <NavItemIcon symbol={'file'} disable={true} />
-          {page.name}
-        </div>
-      }
+      {paths.map((item, index) => {
+        return (
+          <>
+            <div className='wiki2-top-nav-item d-flex'>
+              <NavItemIcon symbol={item.type === 'folder' ? 'wiki-folder' : 'file'} disable={true} />
+              {item.name}
+            </div>
+            {index !== paths.length - 1 && <div>/</div>}
+          </>
+        );
+      })}
     </div>
   );
 }
