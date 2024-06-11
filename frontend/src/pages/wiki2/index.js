5e947976f9ae043c7016e1432d05254f160590e1
liuhongbo <916196375@qq.com>
2024-06-11 18:28:27 +0800

    feat: wiki editor title seperation

--- frontend/src/pages/wiki2/index.js
+++ frontend/src/pages/wiki2/index.js
@@ -5,7 +5,7 @@ import { Modal } from 'reactstrap';
 import { Utils } from '../../utils/utils';
 import wikiAPI from '../../utils/wiki-api';
 import SDocServerApi from '../../utils/sdoc-server-api';
-import { wikiId, siteRoot, lang, isWiki2, seadocServerUrl } from '../../utils/constants';
+import { wikiId, siteRoot, lang, isWiki2, seadocServerUrl, gettext } from '../../utils/constants';
 import WikiConfig from './models/wiki-config';
 import toaster from '../../components/toast';
 import SidePanel from './side-panel';
@@ -171,7 +171,7 @@ class Wiki extends Component {
       callback && callback();
     });
   };
-
+  
   onUpdatePage = (pageId, newPage) => {
     if (newPage.name === '') {
       toaster.danger(gettext('Page name cannot be empty'));
@@ -188,7 +188,6 @@ class Wiki extends Component {
     const newConfig = { ...config, pages: newPages };
     this.saveWikiConfig(newConfig);
   };
-  
   render() {
 
     return (
@@ -201,6 +200,7 @@ class Wiki extends Component {
           saveWikiConfig={this.saveWikiConfig}
           setCurrentPage={this.setCurrentPage}
           currentPageId={this.state.currentPageId}
+          onUpdatePage={this.onUpdatePage}
         />
         <MainPanel
           path={this.state.path}
@@ -213,6 +213,7 @@ class Wiki extends Component {
           permission={this.state.permission}
           seadoc_access_token={this.state.seadoc_access_token}
           assets_url={this.state.assets_url}
+          onUpdatePage={this.onUpdatePage}
         />
         <MediaQuery query="(max-width: 767.8px)">
           <Modal isOpen={!this.state.closeSideBar} toggle={this.onCloseSide} contentClassName="d-none"></Modal>
