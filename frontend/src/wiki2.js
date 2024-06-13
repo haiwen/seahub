d6ed64410afe13b1b974316ce8a53a7a7002280c
liuhongbo <916196375@qq.com>
2024-06-13 17:51:42 +0800

    feat: i18

--- frontend/src/wiki2.js
+++ frontend/src/wiki2.js
@@ -5,6 +5,11 @@ import Wiki from './pages/wiki2';
 import i18n from './_i18n/i18n-sdoc-editor';
 import Loading from './components/loading';
 
+// i18 need `zh-CN`
+if(window.app.config.lang === 'zh-cn') {
+  window.app.config.lang = 'zh-CN';
+}
+
 ReactDom.render(
   <I18nextProvider i18n={i18n}>
     <Suspense fallback={<Loading />}>
