ae2f0f0b22dcb150cc8e34e3ac1d2642befd06b4
Junxiang Huang <wacmkxiaoyi@gmail.com>
2024-06-13 17:17:00 +0800

    optmize index

--- seahub/repo_metadata/models.py
+++ seahub/repo_metadata/models.py
@@ -5,10 +5,13 @@ logger = logging.getLogger(__name__)
 
 class RepoMetadata(models.Model):
 
-    repo_id = models.CharField(max_length=36, primary_key=True, db_index=True)
+    repo_id = models.CharField(max_length=36, unique=True)
     created_time = models.DateTimeField(auto_now_add=True)
     modified_time = models.DateTimeField(auto_now=True)
     enabled = models.BooleanField()
 
     class Meta:
         db_table = 'repo_metadata'
+        unique_together = [
+            ['repo_id', 'enabled']
+        ]
