13afceee66696bdd7c754d39e14b9e0e295fa8a0
r350178982 <32759763+r350178982@users.noreply.github.com>
2024-07-26 16:06:40 +0800

    Update change-group-dialog.js

--- frontend/src/components/dialog/change-group-dialog.js
+++ frontend/src/components/dialog/change-group-dialog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal, ModalHeader, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalFooter, ModalBody } from 'reactstrap';
 import { Utils } from '../../utils/utils';
 import { gettext } from '../../utils/constants';
 
@@ -26,12 +26,15 @@ class ChangeGroupDialog extends React.Component {
 
   render() {
     const groupName = '<span class="op-target">' + Utils.HTMLescape(this.props.groupName) + '</span>';
-    const msg = gettext('Change Group {placeholder} to Department').replace('{placeholder}', groupName);
+    const msg = gettext('Are you sure to change Group {placeholder} to Department ?').replace('{placeholder}', groupName);
     return (
       <Modal isOpen={true} toggle={this.props.toggleDialog}>
         <ModalHeader toggle={this.props.toggleDialog}>
-          <span dangerouslySetInnerHTML={{ __html: msg }}></span>
+          {gettext('Change group to departmen')}
         </ModalHeader>
+        <ModalBody>
+          <p dangerouslySetInnerHTML={{ __html: msg }}></p>
+        </ModalBody>
         <ModalFooter>
           <Button color="secondary" onClick={this.props.toggleDialog}>{gettext('Cancel')}</Button>
           <Button color="primary" onClick={this.submit}>{gettext('Submit')}</Button>
