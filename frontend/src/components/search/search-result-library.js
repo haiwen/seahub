1c97702e9fbc666ca337e29e1d9b1c9990c6f97b
Michael An <2331806369@qq.com>
2024-08-02 08:42:52 +0800

    searched repos support keyboard events (#6454)

--- frontend/src/components/search/search-result-library.js
+++ frontend/src/components/search/search-result-library.js
@@ -6,7 +6,11 @@ import { Utils } from '../../utils/utils';
 function SearchResultLibrary(props) {
   const { item, isHighlight, onClick } = props;
   return (
-    <li className={classnames('search-result-item', { 'search-result-item-highlight': isHighlight })} onClick={() => onClick(item)}>
+    <li
+      className={classnames('search-result-item', { 'search-result-item-highlight': isHighlight })}
+      onClick={() => onClick(item)}
+      ref={ref => props.setRef(ref)}
+    >
       <img className='lib-item-img' src={Utils.getDefaultLibIconUrl(true)} alt="" />
       <div className="item-content d-flex justify-content-between align-items-center ellipsis">{item.name}</div>
     </li>
@@ -17,6 +21,7 @@ SearchResultLibrary.propTypes = {
   item: PropTypes.object.isRequired,
   onClick: PropTypes.func.isRequired,
   isHighlight: PropTypes.bool,
+  setRef: PropTypes.func,
 };
 
 export default SearchResultLibrary;
