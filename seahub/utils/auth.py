d648ce61619cf2c8fae15fc402f05475429bfcc1
r350178982 <32759763+r350178982@users.noreply.github.com>
2024-08-06 09:34:05 +0800

    update

--- seahub/utils/auth.py
+++ seahub/utils/auth.py
@@ -1,6 +1,7 @@
 import os
 from seahub.settings import LOGIN_BG_IMAGE_PATH, MEDIA_ROOT
 from seahub.utils import gen_token
+from datetime import datetime
 
 VIRTUAL_ID_EMAIL_DOMAIN = '@auth.local'
 
@@ -9,11 +10,29 @@ AUTHORIZATION_PREFIX = [
     'bearer'
 ]
 
+MONTH_SEASON_MAP = {
+    1:  'winter',
+    2:  'winter',
+    3:  'spring',
+    4:  'spring',
+    5:  'spring',
+    6:  'summer',
+    7:  'summer',
+    8:  'summer',
+    9:  'autumn',
+    10: 'autumn',
+    11: 'autumn',
+    12: 'winter'
+}
 
 def get_login_bg_image_path():
     """ Return custom background image path if it exists, otherwise return default background image path.
     """
-    login_bg_image_path = LOGIN_BG_IMAGE_PATH
+    current_month = datetime.today().month
+    login_bg_image_path = "img/login-background/%s-bg.jpg" % MONTH_SEASON_MAP.get(current_month)
+    if not os.path.exists(os.path.join(MEDIA_ROOT, login_bg_image_path)):
+        login_bg_image_path = LOGIN_BG_IMAGE_PATH
+    
     # get path that background image of login page
     custom_login_bg_image_path = get_custom_login_bg_image_path()
     if os.path.exists(os.path.join(MEDIA_ROOT, custom_login_bg_image_path)):
