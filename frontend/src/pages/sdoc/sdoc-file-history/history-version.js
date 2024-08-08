1671ef349dd7fd39a051668612680ed53667a544
yinjianfei <1358009667@qq.com>
2024-08-08 15:33:15 +0800

    add Permissions

--- frontend/src/pages/sdoc/sdoc-file-history/history-version.js
+++ frontend/src/pages/sdoc/sdoc-file-history/history-version.js
@@ -166,7 +166,7 @@ class HistoryVersion extends React.Component {
                   aria-label={gettext('More operations')}
                 />
                 <DropdownMenu>
-                  <DropdownItem onClick={this.onRestore}>{gettext('Restore')}</DropdownItem>
+                  {(path[0] + path[1] + path[2] !== 0) && <DropdownItem onClick={this.onRestore}>{gettext('Restore')}</DropdownItem>}
                   <DropdownItem tag='a' href={url} onClick={this.onItemDownLoad}>{gettext('Download')}</DropdownItem>
                   {(path[0] !== 0 && path[1] !== 0 && path[2] !== 0) && <DropdownItem onClick={this.onItemCopy}>{gettext('Copy')}</DropdownItem>}
                   <DropdownItem onClick={this.toggleRename}>{gettext('Rename')}</DropdownItem>
