a3c5f9d0c28e1f5644f862751ec6ed7e94318163
skywalker <dzmbbs@qq.com>
2024-08-16 17:55:15 +0800

    add sdoc_operation_log

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1543,3 +1543,16 @@ CREATE TABLE `repo_metadata_view` (
   PRIMARY KEY (`id`),
   KEY `idx_repo_meatadata_view_repo_id` (`repo_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `sdoc_operation_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_uuid` varchar(36) NOT NULL,
+  `op_id` bigint(20) NOT NULL,
+  `op_time` bigint(20) NOT NULL,
+  `operations` longtext NOT NULL,
+  `author` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sdoc_operation_log_op_time` (`op_time`),
+  KEY `sdoc_operation_log_doc_uuid` (`doc_uuid`),
+  KEY `sdoc_idx_operation_log_doc_uuid_op_id` (`doc_uuid`,`op_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
