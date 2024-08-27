f2ff48f84543c3f38b5449e0d79c126181e0573e
杨国璇 <ygx@Hello-word.local>
2024-08-27 13:42:01 +0800

    feat: metadata turn off confirm

--- frontend/src/metadata/metadata-status-manage-dialog/index.js
+++ frontend/src/metadata/metadata-status-manage-dialog/index.js
@@ -7,12 +7,14 @@ import Switch from '../../components/common/switch';
 import metadataAPI from '../api';
 import { Utils } from '../../utils/utils';
 import toaster from '../../components/toast';
+import TurnOffConfirmDialog from './turn-off-confirm';
 
 import './index.css';
 
 const MetadataStatusManagementDialog = ({ value: oldValue, repoID, toggle, submit }) => {
   const [value, setValue] = useState(oldValue);
   const [submitting, setSubmitting] = useState(false);
+  const [showTurnOffConfirmDialog, setShowTurnOffConfirmDialog] = useState(false);
 
   const onToggle = useCallback(() => {
     if (submitting) return;
@@ -20,10 +22,13 @@ const MetadataStatusManagementDialog = ({ value: oldValue, repoID, toggle, submi
   }, [submitting, toggle]);
 
   const onSubmit = useCallback(() => {
+    if (!value) {
+      setShowTurnOffConfirmDialog(true);
+      return;
+    }
     setSubmitting(true);
-    const apiName = value ? 'createMetadata' : 'deleteMetadata';
-    metadataAPI[apiName](repoID).then(res => {
-      submit(value);
+    metadataAPI.createMetadata(repoID).then(res => {
+      submit(true);
       toggle();
     }).catch(error => {
       const errorMsg = Utils.getErrorMsg(error);
@@ -32,33 +37,57 @@ const MetadataStatusManagementDialog = ({ value: oldValue, repoID, toggle, submi
     });
   }, [repoID, value, submit, toggle]);
 
+  const turnOffConfirmToggle = useCallback(() => {
+    setShowTurnOffConfirmDialog(!showTurnOffConfirmDialog);
+  }, [showTurnOffConfirmDialog]);
+
+  const turnOffConfirmSubmit = useCallback(() => {
+    setShowTurnOffConfirmDialog(false);
+    setSubmitting(true);
+    metadataAPI.deleteMetadata(repoID).then(res => {
+      submit(false);
+      toggle();
+    }).catch(error => {
+      const errorMsg = Utils.getErrorMsg(error);
+      toaster.danger(errorMsg);
+      setSubmitting(false);
+    });
+  }, [repoID, submit, toggle]);
+
   const onValueChange = useCallback(() => {
     const nextValue = !value;
     setValue(nextValue);
   }, [value]);
 
   return (
-    <Modal className="metadata-status-management-dialog" isOpen={true} toggle={onToggle}>
-      <ModalHeader toggle={onToggle}>{gettext('Extended properties management')}</ModalHeader>
-      <ModalBody>
-        <Switch
-          checked={value}
-          disabled={submitting}
-          size="large"
-          textPosition="right"
-          className={classnames('change-metadata-status-management w-100', { 'disabled': submitting })}
-          onChange={onValueChange}
-          placeholder={gettext('Enable extended properties')}
-        />
-        <div className="tip">
-          {gettext('After enable extended properties for files, you can add different properties to files, like collaborators, file expiring time, file description. You can also create different views for files based extended properties.')}
-        </div>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="secondary" onClick={onToggle}>{gettext('Cancel')}</Button>
-        <Button color="primary" disabled={oldValue === value || submitting} onClick={onSubmit}>{gettext('Submit')}</Button>
-      </ModalFooter>
-    </Modal>
+    <>
+      {!showTurnOffConfirmDialog && (
+        <Modal className="metadata-status-management-dialog" isOpen={true} toggle={onToggle}>
+          <ModalHeader toggle={onToggle}>{gettext('Extended properties management')}</ModalHeader>
+          <ModalBody>
+            <Switch
+              checked={value}
+              disabled={submitting}
+              size="large"
+              textPosition="right"
+              className={classnames('change-metadata-status-management w-100', { 'disabled': submitting })}
+              onChange={onValueChange}
+              placeholder={gettext('Enable extended properties')}
+            />
+            <div className="tip">
+              {gettext('After enable extended properties for files, you can add different properties to files, like collaborators, file expiring time, file description. You can also create different views for files based extended properties.')}
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={onToggle}>{gettext('Cancel')}</Button>
+            <Button color="primary" disabled={oldValue === value || submitting} onClick={onSubmit}>{gettext('Submit')}</Button>
+          </ModalFooter>
+        </Modal>
+      )}
+      {showTurnOffConfirmDialog && (
+        <TurnOffConfirmDialog toggle={turnOffConfirmToggle} submit={turnOffConfirmSubmit} />
+      )}
+    </>
   );
 };
 
