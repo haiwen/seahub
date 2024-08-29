1bb214f6c7e47f5681baf62189b765e2a061cfa8
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2024-08-29 16:09:39 +0800

    Update internal_api.py (#6661)
    * Update internal_api.py
    
    * update
    
    * Update utils.py
    
    * Update utils.py
    
    * update
    
    * Update utils.py
    
    * Update settings.py

--- seahub/settings.py
+++ seahub/settings.py
@@ -122,6 +122,8 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'n*v0=jz-1rz@(4gx^tf%6^e7c&um@2)g-l=3_)t@19a69n1nv6'
 
+JWT_PRIVATE_KEY = ''
+
 ENABLE_REMOTE_USER_AUTHENTICATION = False
 
 # Order is important
@@ -969,6 +971,8 @@ else:
 
     load_local_settings(seahub_settings)
     del seahub_settings
+
+JWT_PRIVATE_KEY = os.environ.get('JWT_PRIVATE_KEY', '') or JWT_PRIVATE_KEY
 
 # Remove install_topdir from path
 sys.path.pop(0)
