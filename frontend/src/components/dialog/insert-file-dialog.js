df7f222f481f2850762308607e38133beec91d7f
杨国璇 <ygx@Hello-word.local>
2024-08-29 15:00:16 +0800

    feat: optimize code

--- frontend/src/components/dialog/insert-file-dialog.js
+++ frontend/src/components/dialog/insert-file-dialog.js
@@ -51,7 +51,7 @@ class InsertFileDialog extends React.Component {
   render() {
     const toggle = this.props.toggleCancel;
     return (
-      <Modal isOpen={true} toggle={toggle} zIndex={1071}>
+      <Modal isOpen={true} toggle={toggle}>
         <ModalHeader toggle={toggle}>{gettext('Select File')}</ModalHeader>
         <ModalBody>
           <FileChooser
