0f8ffba337a5630e7469b7e5e6d363d4eac5b12a
杨国璇 <ygx@Hello-word.local>
2024-09-03 11:01:25 +0800

    feat: metadata view icon

--- frontend/src/metadata/metadata-view/_basic/constants/view.js
+++ frontend/src/metadata/metadata-view/_basic/constants/view.js
@@ -2,3 +2,9 @@ export const VIEW_TYPE = {
   TABLE: 'table',
   GALLERY: 'gallery'
 };
+
+export const VIEW_TYPE_ICON = {
+  [VIEW_TYPE.TABLE]: 'table',
+  [VIEW_TYPE.GALLERY]: 'image',
+  'image': 'image'
+};
