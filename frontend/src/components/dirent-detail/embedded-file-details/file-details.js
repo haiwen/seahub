5672cf51b694bfc69e690ad4da3b6ae02fe5612a
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2024-09-03 10:46:53 +0800

    feat: sdoc metadata (#6687)
    * feat: sdoc metadata
    
    * feat: update code
    
    ---------
    
    Co-authored-by: 杨国璇 <ygx@Hello-word.local>

---
+++ frontend/src/components/dirent-detail/embedded-file-details/file-details.js
@@ -0,0 +1,51 @@
+import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { Formatter } from '@seafile/sf-metadata-ui-component';
+import DetailItem from '../detail-item';
+import { CellType } from '../../../metadata/metadata-view/_basic';
+import { gettext } from '../../../utils/constants';
+import { Utils } from '../../../utils/utils';
+import { MetadataDetails, useEnableMetadata } from '../../../metadata';
+
+const FileDetails = ({ repoID, repoInfo, path, direntDetail }) => {
+  const { enableMetadata } = useEnableMetadata();
+
+  const sizeField = useMemo(() => ({ type: 'size', name: gettext('Size') }), []);
+  const lastModifierField = useMemo(() => ({ type: CellType.LAST_MODIFIER, name: gettext('Last modifier') }), []);
+  const lastModifiedTimeField = useMemo(() => ({ type: CellType.MTIME, name: gettext('Last modified time') }), []);
+
+  return (
+    <>
+      <DetailItem field={sizeField} className="sf-metadata-property-detail-formatter">
+        <Formatter field={sizeField} value={Utils.bytesToSize(direntDetail.size)} />
+      </DetailItem>
+      <DetailItem field={lastModifierField} className="sf-metadata-property-detail-formatter">
+        <Formatter
+          field={lastModifierField}
+          value={direntDetail.last_modifier_email}
+          collaborators={[{
+            name: direntDetail.last_modifier_name,
+            contact_email: direntDetail.last_modifier_contact_email,
+            email: direntDetail.last_modifier_email,
+            avatar_url: direntDetail.last_modifier_avatar,
+          }]}
+        />
+      </DetailItem >
+      <DetailItem field={lastModifiedTimeField} className="sf-metadata-property-detail-formatter">
+        <Formatter field={lastModifiedTimeField} value={direntDetail.last_modified}/>
+      </DetailItem>
+      {window.app.pageOptions.enableMetadataManagement && enableMetadata && (
+        <MetadataDetails repoID={repoID} filePath={path} repoInfo={repoInfo} direntType="file" />
+      )}
+    </>
+  );
+};
+
+FileDetails.propTypes = {
+  repoID: PropTypes.string,
+  repoInfo: PropTypes.object,
+  path: PropTypes.string,
+  direntDetail: PropTypes.object,
+};
+
+export default FileDetails;
