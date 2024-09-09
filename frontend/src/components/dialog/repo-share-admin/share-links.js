46ac951f09f9601933ee5788a1357fd6928ad495
llj <lingjun.li1@gmail.com>
2024-09-09 16:08:38 +0800

    [repo 'share admin'] 'Share Links' panel: display user avatar, add 'delete selected links', add 'scroll to the bottom to load the next page of data'

--- frontend/src/components/dialog/repo-share-admin/share-links.js
+++ frontend/src/components/dialog/repo-share-admin/share-links.js
@@ -1,9 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import classnames from 'classnames';
 import { Link } from '@gatsbyjs/reach-router';
 import { Utils } from '../../../utils/utils';
 import { seafileAPI } from '../../../utils/seafile-api';
+import { repoShareAdminAPI } from '../../../utils/repo-share-admin-api';
 import { gettext, siteRoot } from '../../../utils/constants';
 import Loading from '../../../components/loading';
 import toaster from '../../../components/toast';
@@ -12,7 +14,8 @@ import CommonOperationConfirmationDialog from '../../../components/dialog/common
 
 const itemPropTypes = {
   item: PropTypes.object.isRequired,
-  deleteItem: PropTypes.func.isRequired
+  deleteItem: PropTypes.func.isRequired,
+  toggleSelectLink: PropTypes.func.isRequired
 };
 
 class Item extends Component {
@@ -58,7 +61,12 @@ class Item extends Component {
     }
 
     return (
-      <tr onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave} onFocus={this.onMouseEnter}>
+      <tr
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onFocus={this.onMouseEnter}
+        className={classnames({ 'tr-highlight': item.isSelected })}
+      >
         <td className="text-center">
           <input
             type="checkbox"
@@ -67,7 +75,10 @@ class Item extends Component {
             onChange={this.toggleSelectLink}
           />
         </td>
-        <td className="name">{item.creator_name}</td>
+        <td>
+          <img src={item.creator_avatar} alt={item.creator_name} width="24" className="rounded-circle mr-2" />
+          <a href={`${siteRoot}profile/${encodeURIComponent(item.creator_email)}/`} target="_blank" className="align-middle" rel="noreferrer">{item.creator_name}</a>
+        </td>
         <td>
           {item.is_dir ?
             <Link to={objUrl}>{item.obj_name}</Link>
@@ -88,7 +99,7 @@ class Item extends Component {
           <i
             tabIndex="0"
             role="button"
-            className={`sf3-font-delete1 sf3-font action-icon ${this.state.isOperationShow ? '' : 'invisible'}`}
+            className={`sf3-font-delete1 sf3-font op-icon ${this.state.isOperationShow ? '' : 'invisible'}`}
             onClick={this.onDeleteLink}
             onKeyDown={Utils.onKeyDown}
             title={gettext('Delete')}
@@ -107,12 +118,17 @@ const propTypes = {
   repo: PropTypes.object.isRequired,
 };
 
+const PER_PAGE = 25;
+
 class RepoShareAdminShareLinks extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
+      hasMore: false,
+      isLoadingMore: false,
+      page: 1,
       errorMsg: '',
       items: [],
       isDeleteShareLinksDialogOpen: false
@@ -120,9 +136,12 @@ class RepoShareAdminShareLinks extends Component {
   }
 
   componentDidMount() {
-    seafileAPI.listRepoShareLinks(this.props.repo.repo_id).then((res) => {
+    const { repo } = this.props;
+    const { page } = this.state;
+    repoShareAdminAPI.listRepoShareLinks(repo.repo_id, page).then((res) => {
       this.setState({
         loading: false,
+        hasMore: res.data.length == PER_PAGE,
         items: res.data,
       });
     }).catch((error) => {
@@ -181,8 +200,90 @@ class RepoShareAdminShareLinks extends Component {
     });
   };
 
+  deleteShareLinks = () => {
+    const { items } = this.state;
+    const tokens = items.filter(item => item.isSelected).map(link => link.token);
+    seafileAPI.deleteShareLinks(tokens).then(res => {
+      const { success, failed } = res.data;
+      if (success.length) {
+        let newShareLinkList = items.filter(shareLink => {
+          return !success.some(deletedShareLink => {
+            return deletedShareLink.token == shareLink.token;
+          });
+        });
+        this.setState({
+          items: newShareLinkList
+        });
+        const length = success.length;
+        const msg = length == 1 ?
+          gettext('Successfully deleted 1 share link') :
+          gettext('Successfully deleted {number_placeholder} share links')
+            .replace('{number_placeholder}', length);
+        toaster.success(msg);
+      }
+      failed.forEach(item => {
+        const msg = `${item.token}: ${item.error_msg}`;
+        toaster.danger(msg);
+      });
+    }).catch((error) => {
+      let errMessage = Utils.getErrorMsg(error);
+      toaster.danger(errMessage);
+    });
+  };
+
+  getTheadContent = (withCheckbox, isAllLinksSelected) => {
+    return (
+      <tr>
+        <th width="5%" className="text-center">
+          {withCheckbox && <input type="checkbox" checked={isAllLinksSelected} className="vam" onChange={this.toggleSelectAllLinks} />}
+        </th>
+        <th width="20%">{gettext('Creator')}</th>
+        <th width="24%">{gettext('Name')}</th>
+        <th width="19%">{gettext('Link')}</th>
+        <th width="17%">{gettext('Expiration')}</th>
+        <th width="8%">{gettext('Visits')}</th>
+        <th width="7%"></th>
+      </tr>
+    );
+  };
+
+  handleScroll = (event) => {
+    if (!this.state.isLoadingMore && this.state.hasMore) {
+      const clientHeight = event.target.clientHeight;
+      const scrollHeight = event.target.scrollHeight;
+      const scrollTop = event.target.scrollTop;
+      const isBottom = (clientHeight + scrollTop + 1 >= scrollHeight);
+      if (isBottom) { // scroll to the bottom
+        this.setState({ isLoadingMore: true }, () => {
+          this.getMore();
+        });
+      }
+    }
+  };
+
+  getMore = () => {
+    const { page, items } = this.state;
+    const { repo } = this.props;
+
+    repoShareAdminAPI.listRepoShareLinks(repo.repo_id, page + 1).then((res) => {
+      this.setState({
+        isLoadingMore: false,
+        hasMore: res.data.length == PER_PAGE,
+        page: page + 1,
+        items: items.concat(res.data)
+      });
+    }).catch(error => {
+      this.setState({
+        isLoadingMore: false
+      });
+      let errMessage = Utils.getErrorMsg(error);
+      toaster.danger(errMessage);
+    });
+  };
+
+
   render() {
-    const { loading, errorMsg, items, isDeleteShareLinksDialogOpen } = this.state;
+    const { loading, isLoadingMore, errorMsg, items, isDeleteShareLinksDialogOpen } = this.state;
     const selectedLinks = items.filter(item => item.isSelected);
     const isAllLinksSelected = items.length == selectedLinks.length;
     return (
@@ -203,40 +304,37 @@ class RepoShareAdminShareLinks extends Component {
         {!loading && !errorMsg && !items.length &&
         <EmptyTip text={gettext('No share links')}/>
         }
-        {!loading && !errorMsg && items.length > 0 &&
-        <table className="table-hover">
-          <thead>
-            <tr>
-              <th width="5%" className="text-center">
-                <input type="checkbox" checked={isAllLinksSelected} className="vam" onChange={this.toggleSelectAllLinks} />
-              </th>
-              <th width="20%">{gettext('Creator')}</th>
-              <th width="24%">{gettext('Name')}</th>
-              <th width="19%">{gettext('Link')}</th>
-              <th width="17%">{gettext('Expiration')}</th>
-              <th width="8%">{gettext('Visits')}</th>
-              <th width="7%"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => {
-              return (
-                <Item
-                  key={index}
-                  item={item}
-                  deleteItem={this.deleteItem}
-                  toggleSelectLink={this.toggleSelectLink}
-                />
-              );
-            })}
-          </tbody>
-        </table>
-        }
+        {!loading && !errorMsg && items.length > 0 && (
+          <>
+            <table>
+              <thead>{this.getTheadContent(true, isAllLinksSelected)}</thead>
+              <tbody></tbody>
+            </table>
+            <div className='table-real-container' onScroll={this.handleScroll}>
+              <table className="table-hover table-thead-hidden">
+                <thead>{this.getTheadContent(false)}</thead>
+                <tbody>
+                  {items.map((item, index) => {
+                    return (
+                      <Item
+                        key={index}
+                        item={item}
+                        deleteItem={this.deleteItem}
+                        toggleSelectLink={this.toggleSelectLink}
+                      />
+                    );
+                  })}
+                </tbody>
+              </table>
+              {isLoadingMore && <Loading />}
+            </div>
+          </>
+        )}
         {isDeleteShareLinksDialogOpen && (
           <CommonOperationConfirmationDialog
             title={gettext('Delete share links')}
             message={gettext('Are you sure you want to delete the selected share link(s) ?')}
-            executeOperation={this.props.deleteShareLinks}
+            executeOperation={this.deleteShareLinks}
             confirmBtnText={gettext('Delete')}
             toggleDialog={this.toggleDeleteShareLinksDialog}
           />
---
+++ frontend/src/utils/repo-share-admin-api.js
@@ -0,0 +1,59 @@
+import cookie from 'react-cookies';
+import { siteRoot } from './constants';
+import axios from 'axios';
+
+class RepoShareAdminAPI {
+  init({ server, username, password, token }) {
+    this.server = server;
+    this.username = username;
+    this.password = password;
+    this.token = token; // none
+    if (this.token && this.server) {
+      this.req = axios.create({
+        baseURL: this.server,
+        headers: { 'Authorization': 'Token ' + this.token },
+      });
+    }
+    return this;
+  }
+
+  initForSeahubUsage({ siteRoot, xcsrfHeaders }) {
+    if (siteRoot && siteRoot.charAt(siteRoot.length - 1) === '/') {
+      let server = siteRoot.substring(0, siteRoot.length - 1);
+      this.server = server;
+    } else {
+      this.server = siteRoot;
+    }
+    this.req = axios.create({
+      headers: {
+        'X-CSRFToken': xcsrfHeaders,
+      }
+    });
+    return this;
+  }
+
+  _sendPostRequest(url, form) {
+    if (form.getHeaders) {
+      return this.req.post(url, form, {
+        headers: form.getHeaders()
+      });
+    } else {
+      return this.req.post(url, form);
+    }
+  }
+
+  listRepoShareLinks(repoID, page, perPage) {
+    const url = this.server + '/api/v2.1/repos/' + repoID + '/share-links/';
+    const params = {
+      page: page || 1,
+      per_page: perPage || 25
+    };
+    return this.req.get(url, { params: params });
+  }
+
+}
+
+let repoShareAdminAPI = new RepoShareAdminAPI();
+let xcsrfHeaders = cookie.load('sfcsrftoken');
+repoShareAdminAPI.initForSeahubUsage({ siteRoot, xcsrfHeaders });
+export { repoShareAdminAPI };
