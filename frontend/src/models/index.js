227a9d5a906d9e618f573fd1d7a24235e9957f96
Michael An <2331806369@qq.com>
2024-09-14 13:51:28 +0800

    change group view empty tip style (#6777)

--- frontend/src/models/index.js
+++ frontend/src/models/index.js
@@ -2,5 +2,7 @@ import Dirent from './dirent';
 import FileTag from './file-tag';
 import RepoTag from './repo-tag';
 import RepoInfo from './repo-info';
+import Group from './group';
+import Repo from './repo';
 
-export { Dirent, FileTag, RepoTag, RepoInfo };
+export { Dirent, FileTag, RepoTag, RepoInfo, Group, Repo };
