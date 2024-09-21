427d1705a31ed2d228a9414b5b2cf5da644f7b46
Aries <urchinzhou@gmail.com>
2024-09-21 16:29:41 +0800

    fix auto scrolling, improve slider button ux (#6807)
    * fix auto scrolling, improve slider button ux
    
    * update button background color
    
    * update gallery-groupby-setter color

--- frontend/src/metadata/constants/index.js
+++ frontend/src/metadata/constants/index.js
@@ -121,6 +121,10 @@ export {
 
 export const DATE_TAG_HEIGHT = 44;
 
+export const GALLERY_ZOOM_GEAR_MIN = -2;
+
+export const GALLERY_ZOOM_GEAR_MAX = 2;
+
 export const GALLERY_DATE_MODE = {
   YEAR: 'year',
   MONTH: 'month',
