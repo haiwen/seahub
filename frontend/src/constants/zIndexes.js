5e55cbd1fe15a108145e838b19d9da1438472084
Michael An <2331806369@qq.com>
2024-09-23 16:20:20 +0800

    change search z-index (#6832)

--- frontend/src/constants/zIndexes.js
+++ frontend/src/constants/zIndexes.js
@@ -4,10 +4,11 @@ export const FOLDED_SIDE_NAV_FILES = 2;
 
 export const FOLDED_SIDE_NAV = 3;
 
+// search mask should higher than file tags (20) but lower than modal mask (1050)
+export const SEARCH_MASK = 1045;
+
+export const SEARCH_CONTAINER = 1046;
+
+export const SEARCH_CONTAINER_SETTINGS = 1047;
+
 export const EXTRA_ATTRIBUTES_DIALOG_MODAL = 1048;
-
-export const SEARCH_MASK = 1050;
-
-export const SEARCH_CONTAINER = 1051;
-
-export const SEARCH_CONTAINER_SETTINGS = 1052;
