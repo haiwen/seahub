08533673c26d84f95d8a8e9e8343a253daa25b05
杨国璇 <ygx@Hello-word.local>
2024-09-27 18:26:33 +0800

    feat: optimize code

--- frontend/src/metadata/model/metadata/view.js
+++ frontend/src/metadata/model/metadata/view.js
@@ -15,10 +15,10 @@ class View {
     this.filters = object.filters || [];
     this.filter_conjunction = object.filter_conjunction || 'Or';
 
-    this.basic_filters = object.basic_filters && object.basic_filters.length > 0 ? object.basic_filters : VIEW_TYPE_DEFAULT_BASIC_FILTER[this.type];
-
-    if (this.basic_filters.length !== VIEW_TYPE_DEFAULT_BASIC_FILTER[this.type].length) {
-      this.basic_filters = [...this.basic_filters, ...VIEW_TYPE_DEFAULT_BASIC_FILTER[this.type].slice(this.basic_filters.length)];
+    const defaultBasicFilters = VIEW_TYPE_DEFAULT_BASIC_FILTER[this.type];
+    this.basic_filters = object.basic_filters && object.basic_filters.length > 0 ? object.basic_filters : defaultBasicFilters;
+    if (this.basic_filters.length !== defaultBasicFilters.length) {
+      this.basic_filters = [...this.basic_filters, ...defaultBasicFilters.slice(this.basic_filters.length)];
     }
 
     // sort
