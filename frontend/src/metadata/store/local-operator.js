fda509c945d3bc6ac9bd9c2dcd3efa2ef4e8988a
renjie-run <rj.aiyayao@gmail.com>
2024-09-29 13:54:24 +0800

    feat(metadata-table): update operations

---
+++ frontend/src/metadata/store/local-operator.js
@@ -0,0 +1,27 @@
+import { OPERATION_TYPE } from './operations';
+
+class LocalOperator {
+
+  applyOperation(operation) {
+    const { op_type } = operation;
+
+    switch (op_type) {
+      case OPERATION_TYPE.MODIFY_COLUMN_WIDTH: {
+        const { column_key, new_width } = operation;
+        try {
+          const oldValue = window.sfMetadataContext.localStorage.getItem('columns_width') || {};
+          window.sfMetadataContext.localStorage.setItem('columns_width', { ...oldValue, [column_key]: new_width });
+        } catch (err) {
+          break;
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+}
+
+export default LocalOperator;
