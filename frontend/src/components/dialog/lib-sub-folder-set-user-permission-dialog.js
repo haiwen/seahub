d89adb2cfcb7ef2f3599a5d3054ee03c96c79f3d
llj <lingjun.li1@gmail.com>
2024-10-15 12:02:31 +0800

    ['Folder Permission' dialog] added a tip to the panels (#6897)

--- frontend/src/components/dialog/lib-sub-folder-set-user-permission-dialog.js
+++ frontend/src/components/dialog/lib-sub-folder-set-user-permission-dialog.js
@@ -270,6 +270,7 @@ class LibSubFolderSetUserPermissionDialog extends React.Component {
     );
     return (
       <Fragment>
+        <p className="text-gray small">{gettext('Folder permission is only effective after the library is shared to users or groups. It is used to fine tune sub-folder permissions.')}</p>
         <table className="w-xs-250">
           {thead}
           <tbody>
