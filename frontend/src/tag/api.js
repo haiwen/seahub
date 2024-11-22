943d05a545f871ef9a32d999fde2cdd6050381f2
杨国璇 <ygx@Hello-word.local>
2024-11-22 18:23:49 +0800

    feat: optimize code

--- frontend/src/tag/api.js
+++ frontend/src/tag/api.js
@@ -1,8 +1,48 @@
+import axios from 'axios';
 import cookie from 'react-cookies';
 import { siteRoot } from '../utils/constants';
-import { MetadataManagerAPI } from '../metadata';
 
-class TagsManagerAPI extends MetadataManagerAPI {
+class TagsManagerAPI {
+
+  init({ server, username, password, token }) {
+    this.server = server;
+    this.username = username;
+    this.password = password;
+    this.token = token; // none
+    if (this.token && this.server) {
+      this.req = axios.create({
+        baseURL: this.server,
+        headers: { 'Authorization': 'Token ' + this.token },
+      });
+    }
+    return this;
+  }
+
+  initForSeahubUsage({ siteRoot, xcsrfHeaders }) {
+    if (siteRoot && siteRoot.charAt(siteRoot.length - 1) === '/') {
+      var server = siteRoot.substring(0, siteRoot.length - 1);
+      this.server = server;
+    } else {
+      this.server = siteRoot;
+    }
+
+    this.req = axios.create({
+      headers: {
+        'X-CSRFToken': xcsrfHeaders,
+      }
+    });
+    return this;
+  }
+
+  _sendPostRequest(url, form) {
+    if (form.getHeaders) {
+      return this.req.post(url, form, {
+        headers: form.getHeaders()
+      });
+    } else {
+      return this.req.post(url, form);
+    }
+  }
 
   getTagsStatus = (repoID) => {
     const url = this.server + '/api/v2.1/repos/' + repoID + '/metadata/tags-status/';
