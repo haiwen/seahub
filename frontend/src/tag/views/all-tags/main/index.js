af0ad3e81cadadfd38b0aa085f13cba1bef4ffe3
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2024-11-28 20:47:38 +0800

    feat: metadata all tags scroll (#7115)
    Co-authored-by: 杨国璇 <ygx@Hello-word.local>

--- frontend/src/tag/views/all-tags/main/index.js
+++ frontend/src/tag/views/all-tags/main/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { gettext } from '../../../../utils/constants';
 import Tag from './tag';
 import EmptyTip from '../../../../components/empty-tip';
 import { getTagId } from '../../../utils/cell/core';
+import { debounce } from '../../../../metadata/utils/common';
+import { isCellValueChanged } from '../../../../metadata/utils/cell';
 
 import './index.css';
 
-const Main = ({ context, tags, onChangeDisplayTag }) => {
+const Main = React.memo(({ context, tags, onChangeDisplayTag }) => {
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    if (!tableRef.current) return;
+    const currentLocalStorage = context.localStorage;
+    const lastScrollTop = currentLocalStorage.getItem('scroll_top') || 0;
+    if (lastScrollTop) {
+      tableRef.current.scrollTop = Number(lastScrollTop);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handelScroll = debounce(() => {
+    const currentLocalStorage = context.localStorage;
+    const currentScrollTop = tableRef.current.scrollTop || 0;
+    currentLocalStorage.setItem('scroll_top', currentScrollTop);
+  }, 200);
+
   if (tags.length === 0) {
     return (
       <div className="w-100 h-100 d-flex align-items-center justify-content-center">
@@ -17,7 +37,7 @@ const Main = ({ context, tags, onChangeDisplayTag }) => {
   }
 
   return (
-    <div className="sf-metadata-tags-table">
+    <div className="sf-metadata-tags-table" ref={tableRef} onScroll={handelScroll}>
       <div className="sf-metadata-tags-table-header sf-metadata-tags-table-row">
         <div className="sf-metadata-tags-table-cell">{gettext('Tag')}</div>
         <div className="sf-metadata-tags-table-cell">{gettext('File count')}</div>
@@ -29,7 +49,9 @@ const Main = ({ context, tags, onChangeDisplayTag }) => {
       })}
     </div>
   );
-};
+}, (props, nextProps) => {
+  return !isCellValueChanged(props.tags, nextProps.tags);
+});
 
 Main.propTypes = {
   context: PropTypes.object,
