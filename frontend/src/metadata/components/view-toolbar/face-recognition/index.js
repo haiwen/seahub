aff82961d1138a17400ef6e3825c17ae39c5518f
杨国璇 <ygx@Hello-word.local>
2024-12-06 17:34:51 +0800

    feat: people sorts

--- frontend/src/metadata/components/view-toolbar/face-recognition/index.js
+++ frontend/src/metadata/components/view-toolbar/face-recognition/index.js
@@ -1,18 +1,24 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { GalleryGroupBySetter, GallerySliderSetter } from '../../data-process-setter';
+import { GalleryGroupBySetter, GallerySliderSetter, SortSetter } from '../../data-process-setter';
 import { gettext } from '../../../../utils/constants';
-import { EVENT_BUS_TYPE } from '../../../constants';
+import { EVENT_BUS_TYPE, FACE_RECOGNITION_VIEW_ID, VIEW_TYPE } from '../../../constants';
 
-const FaceRecognitionViewToolbar = ({ isCustomPermission, view, showDetail }) => {
+const FaceRecognitionViewToolbar = ({ readOnly, isCustomPermission, view, showDetail }) => {
   const [isShow, setShow] = useState(false);
+  const [columns, setColumns] = useState([]);
 
-  const onToggle = useCallback((isShow) => {
+  const onToggle = useCallback((isShow, columns = []) => {
+    setColumns(columns);
     setShow(isShow);
   }, []);
 
+  const modifySorts = useCallback((sorts) => {
+    console.log(sorts);
+  }, []);
+
   useEffect(() => {
-    const unsubscribeToggle = window.sfMetadataContext.eventBus.subscribe(EVENT_BUS_TYPE.TOGGLE_VIEW_TOOLBAR, onToggle);
+    const unsubscribeToggle = window.sfMetadataContext.eventBus.subscribe(EVENT_BUS_TYPE.TOGGLE_VIEW_TOOLBAR, onToggle, columns);
     return () => {
       unsubscribeToggle && unsubscribeToggle();
     };
@@ -24,8 +30,18 @@ const FaceRecognitionViewToolbar = ({ isCustomPermission, view, showDetail }) =>
   return (
     <>
       <div className="sf-metadata-tool-left-operations">
-        <GalleryGroupBySetter view={view} />
-        <GallerySliderSetter view={view} />
+        <GalleryGroupBySetter view={{ _id: FACE_RECOGNITION_VIEW_ID }} />
+        <GallerySliderSetter view={{ _id: FACE_RECOGNITION_VIEW_ID }} />
+        <SortSetter
+          isNeedSubmit={true}
+          wrapperClass="sf-metadata-view-tool-operation-btn sf-metadata-view-tool-sort"
+          target="sf-metadata-sort-popover"
+          readOnly={readOnly}
+          sorts={view.sorts}
+          type={VIEW_TYPE.FACE_RECOGNITION}
+          columns={columns}
+          modifySorts={modifySorts}
+        />
         {!isCustomPermission && (
           <div className="cur-view-path-btn ml-2" onClick={showDetail}>
             <span className="sf3-font sf3-font-info" aria-label={gettext('Properties')} title={gettext('Properties')}></span>
