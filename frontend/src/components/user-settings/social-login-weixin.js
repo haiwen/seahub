f6fb7ce71506664e739c9c83867b711ea19be67c
lian <imwhatiam123@gmail.com>
2024-12-16 10:10:40 +0800

    weixin connect (#7184)
    * weixin connect
    
    * update

---
+++ frontend/src/components/user-settings/social-login-weixin.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { gettext, siteRoot } from '../../utils/constants';
+import ModalPortal from '../modal-portal';
+import ConfirmDisconnectWeixin from '../dialog/confirm-disconnect-weixin';
+
+const {
+  csrfToken,
+  langCode,
+  socialConnectedWeixin,
+  socialNextPage
+} = window.app.pageOptions;
+
+class SocialLoginWeixin extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isConfirmDialogOpen: false
+    };
+  }
+
+  confirmDisconnect = () => {
+    this.setState({
+      isConfirmDialogOpen: true
+    });
+  };
+
+  toggleDialog = () => {
+    this.setState({
+      isConfirmDialogOpen: !this.state.isConfirmDialogOpen
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="setting-item" id="social-auth">
+          <h3 className="setting-item-heading">{gettext('Social Login')}</h3>
+          <p className="mb-2">{langCode == 'zh-cn' ? '微信' : 'Weixin'}</p>
+          {socialConnectedWeixin ?
+            <button className="btn btn-outline-primary" onClick={this.confirmDisconnect}>{gettext('Disconnect')}</button> :
+            <a href={`${siteRoot}weixin/oauth-connect/?next=${encodeURIComponent(socialNextPage)}`} className="btn btn-outline-primary">{gettext('Connect')}</a>
+          }
+        </div>
+        {this.state.isConfirmDialogOpen && (
+          <ModalPortal>
+            <ConfirmDisconnectWeixin
+              formActionURL={`${siteRoot}weixin/oauth-disconnect/?next=${encodeURIComponent(socialNextPage)}`}
+              csrfToken={csrfToken}
+              toggle={this.toggleDialog}
+            />
+          </ModalPortal>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default SocialLoginWeixin;
--- seahub/weixin/views.py
+++ seahub/weixin/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.core.files.base import ContentFile
 from django.utils.translation import gettext as _
+from django.shortcuts import render
 
 from seahub.api2.utils import get_api_token
 
@@ -18,13 +19,15 @@ from seahub.avatar.models import Avatar
 from seahub.profile.models import Profile
 from seahub.utils import render_error, get_site_scheme_and_netloc
 from seahub.auth.models import SocialAuthUser
+from seahub.utils.auth import VIRTUAL_ID_EMAIL_DOMAIN
+from seahub.auth.decorators import login_required
 from seahub.settings import SITE_ROOT
 
 from seahub.weixin.settings import ENABLE_WEIXIN, \
         WEIXIN_OAUTH_APP_ID, WEIXIN_OAUTH_APP_SECRET, \
         WEIXIN_OAUTH_SCOPE, WEIXIN_OAUTH_RESPONSE_TYPE, WEIXIN_OAUTH_QR_CONNECT_URL, \
         WEIXIN_OAUTH_GRANT_TYPE, WEIXIN_OAUTH_ACCESS_TOKEN_URL, \
-        WEIXIN_OAUTH_USER_INFO_URL
+        WEIXIN_OAUTH_USER_INFO_URL, WEIXIN_OAUTH_CREATE_UNKNOWN_USER
 
 logger = logging.getLogger(__name__)
 
@@ -34,15 +37,17 @@ logger = logging.getLogger(__name__)
 def weixin_oauth_login(request):
 
     if not ENABLE_WEIXIN:
-        return render_error(request, _('Error, please contact administrator.'))
+        return render_error(request, _('Feature is not enabled.'))
 
     state = str(uuid.uuid4())
     request.session['weixin_oauth_login_state'] = state
-    request.session['weixin_oauth_login_redirect'] = request.GET.get(auth.REDIRECT_FIELD_NAME, '/')
+    redirect_to = request.GET.get(auth.REDIRECT_FIELD_NAME, SITE_ROOT)
+    request.session['weixin_oauth_login_redirect'] = redirect_to
 
+    redirect_uri = get_site_scheme_and_netloc() + reverse('weixin_oauth_callback')
     data = {
         'appid': WEIXIN_OAUTH_APP_ID,
-        'redirect_uri': get_site_scheme_and_netloc() + reverse('weixin_oauth_callback'),
+        'redirect_uri': redirect_uri,
         'response_type': WEIXIN_OAUTH_RESPONSE_TYPE,
         'scope': WEIXIN_OAUTH_SCOPE,
         'state': state,
@@ -54,7 +59,7 @@ def weixin_oauth_login(request):
 def weixin_oauth_callback(request):
 
     if not ENABLE_WEIXIN:
-        return render_error(request, _('Error, please contact administrator.'))
+        return render_error(request, _('Feature is not enabled.'))
 
     state = request.GET.get('state', '')
     if not state or state != request.session.get('weixin_oauth_login_state', ''):
@@ -88,6 +93,9 @@ def weixin_oauth_callback(request):
     else:
         email = None
         is_new_user = True
+
+    if is_new_user and not WEIXIN_OAUTH_CREATE_UNKNOWN_USER:
+        return render(request, 'weixin/weixin_user_not_found_error.html')
 
     try:
         user = auth.authenticate(remote_user=email)
@@ -146,4 +154,89 @@ def weixin_oauth_callback(request):
     response = HttpResponseRedirect(request.session.get('weixin_oauth_login_redirect',
                                                         SITE_ROOT))
     response.set_cookie('seahub_auth', email + '@' + api_token.key)
+    return response
+
+
+@login_required
+def weixin_oauth_connect(request):
+
+    if not ENABLE_WEIXIN:
+        return render_error(request, _('Feature is not enabled.'))
+
+    state = str(uuid.uuid4())
+    request.session['weixin_oauth_connect_state'] = state
+    redirect_to = request.GET.get(auth.REDIRECT_FIELD_NAME, SITE_ROOT)
+    request.session['weixin_oauth_connect_redirect'] = redirect_to
+
+    redirect_uri = get_site_scheme_and_netloc() + reverse('weixin_oauth_connect_callback')
+    data = {
+        'appid': WEIXIN_OAUTH_APP_ID,
+        'redirect_uri': redirect_uri,
+        'response_type': WEIXIN_OAUTH_RESPONSE_TYPE,
+        'scope': WEIXIN_OAUTH_SCOPE,
+        'state': state,
+    }
+    url = WEIXIN_OAUTH_QR_CONNECT_URL + '?' + urllib.parse.urlencode(data)
+    return HttpResponseRedirect(url)
+
+
+@login_required
+def weixin_oauth_connect_callback(request):
+
+    if not ENABLE_WEIXIN:
+        return render_error(request, _('Feature is not enabled.'))
+
+    state = request.GET.get('state', '')
+    if not state or state != request.session.get('weixin_oauth_connect_state', ''):
+        logger.error('invalid state')
+        return render_error(request, _('Error, please contact administrator.'))
+
+    # get access_token and user openid
+    parameters = {
+        'appid': WEIXIN_OAUTH_APP_ID,
+        'secret': WEIXIN_OAUTH_APP_SECRET,
+        'code': request.GET.get('code'),
+        'grant_type': WEIXIN_OAUTH_GRANT_TYPE,
+    }
+
+    access_token_url = WEIXIN_OAUTH_ACCESS_TOKEN_URL + '?' + urllib.parse.urlencode(parameters)
+    access_token_json = requests.get(access_token_url).json()
+
+    openid = access_token_json.get('openid', '')
+    access_token = access_token_json.get('access_token', '')
+    if not access_token or not openid:
+        logger.error('invalid access_token or openid')
+        logger.error(access_token_url)
+        logger.error(access_token_json)
+        return render_error(request, _('Error, please contact administrator.'))
+
+    auth_user = SocialAuthUser.objects.get_by_provider_and_uid('weixin', openid)
+    if auth_user:
+        logger.warning('weixin account already exists %s' % openid)
+        return render_error(request, '出错了，此微信账号已被绑定')
+
+    username = request.user.username
+    SocialAuthUser.objects.add(username, 'weixin', openid)
+
+    weixin_oauth_connect_redirect = request.session['weixin_oauth_connect_redirect']
+    response = HttpResponseRedirect(weixin_oauth_connect_redirect)
+    return response
+
+
+@login_required
+def weixin_oauth_disconnect(request):
+
+    if not ENABLE_WEIXIN:
+        return render_error(request, _('Feature is not enabled.'))
+
+    username = request.user.username
+    if username[-(len(VIRTUAL_ID_EMAIL_DOMAIN)):] == VIRTUAL_ID_EMAIL_DOMAIN:
+        profile = Profile.objects.get_profile_by_user(username)
+        if not profile:
+            return render_error(request, '出错了，当前账号不能解绑微信')
+
+    SocialAuthUser.objects.delete_by_username_and_provider(username, 'weixin')
+
+    # redirect user to page
+    response = HttpResponseRedirect(request.GET.get(auth.REDIRECT_FIELD_NAME, SITE_ROOT))
     return response
