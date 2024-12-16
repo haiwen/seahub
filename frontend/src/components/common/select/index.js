12839948fc3fde20d8a0572c1a58ccd5b65fe327
Michael An <2331806369@qq.com>
2024-12-16 17:16:14 +0800

    05 remove old NoGroupMessage

--- frontend/src/components/common/select/index.js
+++ frontend/src/components/common/select/index.js
@@ -1,5 +1,4 @@
 import SeahubSelect from './seahub-select';
-import { NoGroupMessage } from './no-group-message';
 import { MenuSelectStyle, UserSelectStyle, NoOptionsStyle } from './seahub-select-style';
 
-export { SeahubSelect, NoGroupMessage, MenuSelectStyle, UserSelectStyle, NoOptionsStyle };
+export { SeahubSelect, MenuSelectStyle, UserSelectStyle, NoOptionsStyle };
