3aee3e21e814269a13ba7992329bc5fba2d64d46
杨国璇 <ygx@Hello-word.local>
2024-12-23 18:30:20 +0800

    feat: deleted repos ui

---
+++ frontend/src/components/dialog/my-deleted-repos-dialog/index.js
@@ -0,0 +1,65 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { gettext, trashReposExpireDays } from '../../../utils/constants';
+import { seafileAPI } from '../../../utils/seafile-api';
+import { Utils } from '../../../utils/utils';
+import toaster from '../../toast';
+import Loading from '../../loading';
+import EmptyTip from '../../empty-tip';
+import Repos from './repos';
+
+import './index.css';
+
+const MyDeletedReposDialog = ({ toggleDialog }) => {
+
+  const [isLoading, setLoading] = useState(true);
+  const [deletedRepoList, setDeletedRepoList] = useState([]);
+
+  useEffect(() => {
+    seafileAPI.listDeletedRepo().then(res => {
+      setDeletedRepoList(res.data);
+      setLoading(false);
+    }).catch(error => {
+      const errMessage = Utils.getErrorMsg(error);
+      toaster.danger(errMessage);
+    });
+  }, []);
+
+  const filterRestoredRepo = useCallback((restoredRepoID) => {
+    const newDeletedRepoList = deletedRepoList.filter(item => {
+      return item.repo_id !== restoredRepoID;
+    });
+    setDeletedRepoList(newDeletedRepoList);
+  }, [deletedRepoList]);
+
+  return (
+    <Modal isOpen={true} toggle={toggleDialog} className="my-deleted-repos-dialog">
+      <ModalHeader toggle={toggleDialog}>{gettext('Deleted Libraries')}</ModalHeader>
+      <ModalBody className="my-deleted-repos-container">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            {deletedRepoList.length === 0 ? (
+              <EmptyTip
+                className="my-deleted-repos-empty-tip"
+                title={gettext('No deleted libraries')}
+                text={gettext('You have not deleted any libraries in the last {placeholder} days. A deleted library will be cleaned automatically after this period.').replace('{placeholder}', trashReposExpireDays)}
+              />
+            ) : (
+              <Repos repos={deletedRepoList} filterRestoredRepo={filterRestoredRepo} />
+            )}
+          </>
+        )}
+      </ModalBody>
+    </Modal>
+  );
+
+};
+
+MyDeletedReposDialog.propTypes = {
+  toggleDialog: PropTypes.func.isRequired
+};
+
+export default MyDeletedReposDialog;
---
+++ frontend/src/components/dialog/my-deleted-repos-dialog/repo-item.js
@@ -0,0 +1,71 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { seafileAPI } from '../../../utils/seafile-api';
+import { gettext, lang } from '../../../utils/constants';
+import toaster from '../../toast';
+import { Utils } from '../../../utils/utils';
+
+dayjs.locale(lang);
+dayjs.extend(relativeTime);
+
+const RepoItem = ({ repo, filterRestoredRepo }) => {
+  const repoID = useMemo(() => repo.repo_id, [repo]);
+  const repoName = useMemo(() => repo.repo_name, [repo]);
+  const localTime = useMemo(() => {
+    const timeDate = dayjs.utc(repo.del_time).toDate();
+    return dayjs(timeDate).fromNow();
+  }, [repo]);
+  const iconUrl = useMemo(() => Utils.getLibIconUrl(repo), [repo]);
+
+  const [highlight, setHighlight] = useState(false);
+
+  const onMouseEnter = useCallback(() => {
+    setHighlight(true);
+  }, []);
+
+  const onMouseLeave = useCallback(() => {
+    setHighlight(false);
+  }, []);
+
+  const restoreDeletedRepo = useCallback((event) => {
+    event.preventDefault();
+    seafileAPI.restoreDeletedRepo(repoID).then(res => {
+      const message = gettext('Successfully restored the library {library_name}.').replace('{library_name}', repoName);
+      toaster.success(message);
+      filterRestoredRepo(repoID);
+    }).catch(error => {
+      const errMessage = Utils.getErrorMsg(error);
+      toaster.danger(errMessage);
+    });
+  }, [repoID, repoName, filterRestoredRepo]);
+
+  return (
+    <tr
+      className={highlight ? 'tr-highlight' : ''}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      tabIndex="0"
+      onFocus={onMouseEnter}
+    >
+      <td className="text-center pl-2 pr-2"><img src={iconUrl} alt='' width="24" /></td>
+      <td className="name">{repoName}</td>
+      <td className="update">{localTime}</td>
+      <td>
+        <a href="#" onClick={restoreDeletedRepo} title={gettext('Restore')}
+          role="button" aria-label={gettext('Restore')}
+          className={`sf2-icon-reply action-icon ${highlight ? '' : 'vh'}`}>
+        </a>
+      </td>
+    </tr>
+  );
+
+};
+
+RepoItem.propTypes = {
+  repo: PropTypes.object.isRequired,
+  filterRestoredRepo: PropTypes.func.isRequired,
+};
+
+export default RepoItem;
