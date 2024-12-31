36d57abfb5a397061d594a92eb4844c0cb040e73
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2024-12-31 11:47:07 +0800

    add metrics api

--- seahub/utils/__init__.py
+++ seahub/utils/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 # encoding: utf-8
+import base64
 from functools import partial
 import os
 import re
@@ -855,6 +856,9 @@ if EVENTS_CONFIG_FILE:
     def get_file_history_suffix():
         return seafevents_api.get_file_history_suffix(parsed_events_conf)
 
+    def get_seafevents_metrics():
+        return seafevents_api.get_metrics()
+
     def get_trash_records(repo_id, show_time, start, limit):
         with _get_seafevents_session() as session:
             res, total_count = seafevents_api.get_delete_records(session, repo_id, show_time, start, limit)
@@ -1556,3 +1560,24 @@ def transfer_repo(repo_id, new_owner, is_share, org_id=None):
             else:
                 seafile_api.set_repo_owner(repo_id, new_owner)
 
+
+def check_metric_auth(auth_header):
+    cfg = configparser.ConfigParser()
+    if 'SEAFILE_CENTRAL_CONF_DIR' in os.environ:
+        confdir = os.environ['SEAFILE_CENTRAL_CONF_DIR']
+    else:
+        confdir = os.environ['SEAFILE_CONF_DIR']
+    conf = os.path.join(confdir, 'seafile.conf')
+    cfg.read(conf)
+    if cfg.has_option('metrics', 'enabled'):
+        auth_username = cfg.get('metrics', 'auth_username')
+        auth_password = cfg.get('metrics', 'auth_password')
+        try:
+            auth_decoded = base64.b64decode(auth_header.split(' ')[1]).decode('utf-8')
+            username, password = auth_decoded.split(':')
+            if username == auth_username and password == auth_password:
+                return True
+        except Exception as e:
+            logger.error(e)
+            return False
+    return False
