27a881a0202612eaaf5bea525911eaab27a9411b
杨顺强 <978987373@qq.com>
2025-01-04 10:00:04 +0800

    optimize code (#7302)

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -90,7 +90,7 @@ export const ocmRemoteServers = window.app.pageOptions.ocmRemoteServers;
 export const enableOCMViaWebdav = window.app.pageOptions.enableOCMViaWebdav;
 export const enableSSOToThirdpartWebsite = window.app.pageOptions.enableSSOToThirdpartWebsite;
 export const enableSeadoc = window.app.pageOptions.enableSeadoc;
-export const enableTldraw = window.app.pageOptions.enableTldraw;
+export const enableWhiteboard = window.app.pageOptions.enableWhiteboard;
 export const enableMultipleOfficeSuite = window.app.pageOptions.enableMultipleOfficeSuite;
 
 export const curNoteMsg = window.app.pageOptions.curNoteMsg;
