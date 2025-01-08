cd9780dac14bf05b211f91437b858a31cc569572
Jerry Ren <rj.aiyayao@gmail.com>
2025-01-08 14:40:57 +0800

    fix(tag): init self links if not exist (#7334)

--- frontend/src/tag/utils/column.js
+++ frontend/src/tag/utils/column.js
@@ -1,3 +1,4 @@
+import { CellType } from '../../metadata/constants';
 import { PRIVATE_COLUMN_KEYS, PRIVATE_COLUMN_KEY } from '../constants';
 
 const DEFAULT_COLUMN_WIDTH = 200;
@@ -11,8 +12,27 @@ export const getColumnOriginName = (column) => {
 
 export const normalizeColumns = (columns) => {
   if (!Array.isArray(columns) || columns.length === 0) return [];
+  let normalizedColumns = [...columns];
+
+  // init with parent/sub links columns if not exist
+  const hasSelfLinkColumns = normalizedColumns.some((column) => column.key === PRIVATE_COLUMN_KEY.PARENT_LINKS);
+  if (!hasSelfLinkColumns) {
+    normalizedColumns.push(
+      {
+        key: PRIVATE_COLUMN_KEY.PARENT_LINKS,
+        name: PRIVATE_COLUMN_KEY.PARENT_LINKS,
+        type: CellType.LINK,
+      },
+      {
+        key: PRIVATE_COLUMN_KEY.SUB_LINKS,
+        name: PRIVATE_COLUMN_KEY.SUB_LINKS,
+        type: CellType.LINK,
+      },
+    );
+  }
+
   const keyColumnWidth = window.sfTagsDataContext.localStorage.getItem('columns_width') || {};
-  return columns.map((column) => {
+  return normalizedColumns.map((column) => {
     const { key } = column;
     let width = keyColumnWidth[column.key];
     if (!width) {
