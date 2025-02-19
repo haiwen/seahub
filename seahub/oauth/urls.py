e09ce188aaa973dcfcb61cf2e1da0d6a703dbbfe
欢乐马 <38058090+SkywalkerSpace@users.noreply.github.com>
2025-02-19 12:01:19 +0800

    custom oauth (#6269)

--- seahub/oauth/urls.py
+++ seahub/oauth/urls.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 
 from django.urls import path
-from seahub.oauth.views import oauth_login, oauth_callback
+from seahub.oauth.views import oauth_login, oauth_callback, \
+    custom_oauth_login_view, custom_oauth_callback_view
+
+import seahub.settings as settings
+ENABLE_CUSTOM_OAUTH = getattr(settings, 'ENABLE_CUSTOM_OAUTH', False)
+
 
 urlpatterns = [
     path('login/', oauth_login, name='oauth_login'),
     path('callback/', oauth_callback, name='oauth_callback'),
 ]
+
+if ENABLE_CUSTOM_OAUTH:
+    urlpatterns = [
+        path('login/', custom_oauth_login_view, name='oauth_login'),
+        path('callback/', custom_oauth_callback_view, name='oauth_callback'),
+   ]
