60c09ddd3ea316ed8f6b50e3d7ac4b0d8a090c9d
llj <lingjun.li1@gmail.com>
2025-02-19 15:14:09 +0800

    [wiki] added a resizing bar (enable users to resize the width of side panel and main panel)

--- frontend/src/pages/wiki2/main-panel.js
+++ frontend/src/pages/wiki2/main-panel.js
@@ -23,6 +23,7 @@ const propTypes = {
   isUpdateBySide: PropTypes.bool,
   onUpdatePage: PropTypes.func,
   onAddWikiPage: PropTypes.func,
+  style: PropTypes.object.isRequired,
   mobileOpenSidePanel: PropTypes.func.isRequired
 };
 
@@ -64,12 +65,12 @@ class MainPanel extends Component {
   };
 
   render() {
-    const { permission, pathExist, isDataLoading, config, onUpdatePage, isUpdateBySide } = this.props;
+    const { permission, pathExist, isDataLoading, config, onUpdatePage, isUpdateBySide, style } = this.props;
     const { currentPageConfig = {} } = this.state;
     const isViewingFile = pathExist && !isDataLoading;
     const isReadOnly = !(permission === 'rw');
     return (
-      <div className="wiki2-main-panel">
+      <div className="wiki2-main-panel" style={style}>
         <div className='wiki2-main-panel-north'>
           <div className="d-flex align-items-center flex-fill o-hidden">
             <div className='wiki2-main-panel-north-content'>
