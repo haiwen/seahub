3a6b11690a9b61b57e4117647b7404d37a5c1d38
zhouwenxuan <aries@Mac.local>
2025-02-19 15:50:06 +0800

    fix close button error

--- frontend/src/metadata/components/dialog/peoples-dialog/index.js
+++ frontend/src/metadata/components/dialog/peoples-dialog/index.js
@@ -1,12 +1,13 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button, FormGroup, Label } from 'reactstrap';
+import { Modal, ModalBody, ModalFooter, Button, FormGroup, Label } from 'reactstrap';
 import { CustomizeSelect } from '@seafile/sf-metadata-ui-component';
 import { gettext } from '../../../../utils/constants';
+import SeahubModalHeader from '@/components/common/seahub-modal-header';
+import { useMetadataView } from '../../../hooks/metadata-view';
 import People from './people';
 
 import './index.css';
-import { useMetadataView } from '../../../hooks/metadata-view';
 
 
 const PeoplesDialog = ({ selectedImages, onToggle, onSubmit }) => {
@@ -41,7 +42,7 @@ const PeoplesDialog = ({ selectedImages, onToggle, onSubmit }) => {
 
   return (
     <Modal isOpen={true} toggle={() => onToggle()} className="sf-metadata-peoples-dialog">
-      <ModalHeader toggle={() => onToggle()}>{gettext('People')}</ModalHeader>
+      <SeahubModalHeader toggle={() => onToggle()}>{gettext('People')}</SeahubModalHeader>
       <ModalBody>
         <FormGroup className="">
           <Label>{gettext('People')}</Label>
