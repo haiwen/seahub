00c4171006508e730d125ad50e3d6a043d84b4fc
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-03-03 14:51:02 +0800

    update
    update

--- frontend/src/metadata/components/metadata-details/location/index.js
+++ frontend/src/metadata/components/metadata-details/location/index.js
@@ -101,14 +101,15 @@ class Location extends React.Component {
     this.setState({ isLoading: false }, () => {
       if (!window.BMapGL.Map) return;
       if (!isValidPosition(position?.lng, position?.lat)) return;
-      const gcPosition = wgs84_to_gcj02(position.lng, position.lat);
-      const bdPosition = gcj02_to_bd09(gcPosition.lng, gcPosition.lat);
-      const { lng, lat } = bdPosition;
+      let gcPosition = wgs84_to_gcj02(position.lng, position.lat);
+      let { lng = position.lng, lat = position.lat } = gcPosition || {};
+      const bdPosition = gcj02_to_bd09(lng, lat);
+      const { lng: bdLng, lat: bdLat } = bdPosition;
       this.map = new window.BMapGL.Map(this.ref, { enableMapClick: false });
-      const point = new window.BMapGL.Point(lng, lat);
+      const point = new window.BMapGL.Point(bdLng, bdLat);
       this.map.centerAndZoom(point, 16);
       this.map.enableScrollWheelZoom(true);
-      this.addMarkerByPosition(lng, lat);
+      this.addMarkerByPosition(bdLng, bdLat);
       let location_translated = this.props.record._location_translated;
       if (location_translated) {
         this.setState({ address: location_translated.address });
