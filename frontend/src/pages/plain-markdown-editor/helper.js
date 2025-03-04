2684e54aed0f5c23474bf7da02f45d22e7ae9723
Michael An <2331806369@qq.com>
2025-03-04 12:00:40 +0800

    02 普通文本编辑器

--- frontend/src/pages/plain-markdown-editor/helper.js
+++ frontend/src/pages/plain-markdown-editor/helper.js
@@ -1,18 +1,21 @@
 import { seafileAPI } from '../../utils/seafile-api';
 import { Utils } from '../../utils/utils';
 
+// API 获取文档信息
 const getFileInfo = async (repoID, filePath) => {
   const fileInfoRes = await seafileAPI.getFileInfo(repoID, filePath);
   const { mtime, size, starred, permission, last_modifier_name: lastModifier, id } = fileInfoRes.data;
   return { mtime, size, starred, permission, lastModifier, id };
 };
 
+// API 获取下载链接
 const getFileDownloadUrl = async (repoID, filePath) => {
   const fileDownloadUrlRes = await seafileAPI.getFileDownloadLink(repoID, filePath);
   const downloadUrl = fileDownloadUrlRes.data;
   return downloadUrl;
 };
 
+// API 获取权限
 const setPermission = async (permission, repoID) => {
   let hasPermission = permission === 'rw' || permission === 'cloud-edit';
   // get custom permission
@@ -26,12 +29,14 @@ const setPermission = async (permission, repoID) => {
   return hasPermission;
 };
 
+// API 获取文档内容
 const setFileContent = async (downloadUrl) => {
   const fileContentRes = await seafileAPI.getFileContent(downloadUrl);
   const markdownContent = fileContentRes.data;
   return markdownContent;
 };
 
+// 获取全部信息入口
 export const getPlainOptions = async ({ fileName, filePath, repoID }) => {
   const fileIcon = Utils.getFileIconUrl(fileName);
   document.getElementById('favicon').href = fileIcon;
@@ -39,6 +44,16 @@ export const getPlainOptions = async ({ fileName, filePath, repoID }) => {
   const downloadUrl = await getFileDownloadUrl(repoID, filePath);
   const markdownContent = await setFileContent(downloadUrl);
   const hasPermission = await setPermission(fileInfo.permission, repoID);
+
+  // 早期是多个 await 串行执行，可能消耗较多时间；可以改成多个 API 同时执行，减少网络请求的时间
+  // const [fileInfo, downloadUrl] = await Promise.all([
+  //   getFileInfo(repoID, filePath),
+  //   getFileDownloadUrl(repoID, filePath),
+  // ]);
+  // const [markdownContent, hasPermission] = await Promise.all([
+  //   setFileContent(downloadUrl),
+  //   setPermission(fileInfo.permission, repoID),
+  // ]);
 
   return { markdownContent, hasPermission, fileInfo };
 };
