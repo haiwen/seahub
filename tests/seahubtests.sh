6e094524160c3c5333d3e3ed1992e328edc7c74b
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-03-06 18:06:50 +0800

    Update seahubtests.sh

--- tests/seahubtests.sh
+++ tests/seahubtests.sh
@@ -26,9 +26,9 @@ fi
 set -x
 SEAHUB_TESTSDIR=$(python -c "import os; print(os.path.dirname(os.path.realpath('$0')))")
 SEAHUB_SRCDIR=$(dirname "${SEAHUB_TESTSDIR}")
-SEAFEVENTS_SRCDIR='/tmp/seafevents'
+SEAFEVENTS_SRCDIR='/tmp'
 
-export SEAHUB_LOG_DIR='/tmp'
+export SEAHUB_LOG_DIR='/tmp/logs'
 export PYTHONPATH="/usr/local/lib/python3.8/site-packages:/usr/local/lib/python3.8/dist-packages:/usr/lib/python3.8/site-packages:/usr/lib/python3.8/dist-packages:${SEAHUB_SRCDIR}/thirdpart:${SEAFEVENTS_SRCDIR}/:${PYTHONPATH}"
 cd "$SEAHUB_SRCDIR"
 set +x
