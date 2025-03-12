b3e22542973414017385a1f3df5143c2a5310a06
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-03-12 17:25:58 +0800

    Update mysql.sql

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1624,4 +1624,17 @@ CREATE TABLE `org_last_active_time` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `org_id` (`org_id`),
   KEY `ix_org_last_active_time_org_id` (`org_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `GroupInvite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `org_id` int(11) NOT NULL,
+  `operator` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `action_type` enum('INVITE','LEAVE') NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_group_invite_org_id` (`org_id`),
+  KEY `idx_group_invite_timestamp` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
