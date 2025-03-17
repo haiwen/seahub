c551e97c2f8f8082ee28f0e232d921bf694b9aa3
skywalker <dzmbbs@qq.com>
2025-03-17 17:53:04 +0800

    update 13.0 sql

--- scripts/upgrade/sql/13.0.0/mysql/ccnet.sql
+++ scripts/upgrade/sql/13.0.0/mysql/ccnet.sql
@@ -1,3 +1,4 @@
 ALTER TABLE EmailUser ADD COLUMN `is_department_owner` BOOL NOT NULL DEFAULT 0;
 CREATE INDEX EmailUser_is_active on EmailUser(is_active);
 CREATE INDEX EmailUser_is_department_owenr on EmailUser(is_department_owner);
+UPDATE `EmailUser` SET is_department_owner=1 WHERE email LIKE "%@seafile_group";
