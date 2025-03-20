368b8bbbe6a7e6ccef91924181091ed6c684a888
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-03-20 14:03:24 +0800

    freeze wiki page

--- frontend/src/utils/wiki-api.js
+++ frontend/src/utils/wiki-api.js
@@ -224,6 +224,14 @@ class WikiAPI {
     return this.req.get(url);
   }
 
+  updateWiki2PageLocked(wikiId, pageId, locked) {
+    const url = this.server + '/api/v2.1/wiki2/' + wikiId + '/page/' + pageId + '/';
+    let params = {
+      locked: locked
+    };
+    return this.req.put(url, params);
+  }
+
   renameWiki2(wikiId, wikiName) {
     const url = this.server + '/api/v2.1/wiki2/' + wikiId + '/';
     let params = {
