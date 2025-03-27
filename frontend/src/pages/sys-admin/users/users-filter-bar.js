0666b7a303cd12aa1aef1e16287defcf81552bc6
llj <lingjun.li1@gmail.com>
2025-03-27 14:15:45 +0800

    [system admin / users] redesigned the 'user filter' toolbar for the 'Database' tab (#7667)

--- frontend/src/pages/sys-admin/users/users-filter-bar.js
+++ frontend/src/pages/sys-admin/users/users-filter-bar.js
@@ -1,117 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap';
 import { gettext } from '../../../utils/constants';
+import Selector from '../../../components/single-selector';
 import './users-filter-bar.css';
 
 const { availableRoles } = window.sysadmin.pageOptions;
 
 class UsersFilterBar extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isStatusOpen: false,
-      isRoleOpen: false,
-    };
-  }
-
-  translateStatus = (status) => {
-    switch (status) {
-      case '0':
-        return gettext('Inactive');
-      case '1':
-        return gettext('Active');
-      default:
-        return gettext('All');
-    }
-  };
-
   translateRole = (role) => {
     switch (role) {
+      case '':
+        return gettext('All');
       case 'default':
         return gettext('Default');
       case 'guest':
         return gettext('Guest');
       default:
-        return gettext('All');
+        return role;
     }
   };
 
-  toggleStatusDropdown = () => {
-    this.setState({ isStatusOpen: !this.state.isStatusOpen });
+  selectStatusOption = (option) => {
+    this.props.onStatusChange(option.value);
   };
 
-  toggleRoleDropdown = () => {
-    this.setState({ isRoleOpen: !this.state.isRoleOpen });
-  };
-
-  renderCheck = () => {
-    return <span className="sf2-icon-tick text-gray font-weight-bold"></span>;
+  selectRoleOption = (option) => {
+    this.props.onRoleChange(option.value);
   };
 
   render() {
-    const { onStatusChange, onRoleChange } = this.props;
+    const { isActive, role } = this.props;
+
+    this.statusOptions = [
+      { value: '', text: gettext('All') },
+      { value: '1', text: gettext('Active') },
+      { value: '0', text: gettext('Inactive') }
+    ].map(item => {
+      item.isSelected = isActive == item.value;
+      return item;
+    });
+    const currentSelectedStatusOption = this.statusOptions.filter(item => item.isSelected)[0];
+
+    this.roleOptions = [''].concat(availableRoles).map(item => {
+      return {
+        value: item,
+        text: this.translateRole(item),
+        isSelected: item == role
+      };
+    });
+    const currentSelectedRoleOption = this.roleOptions.filter(item => item.isSelected)[0] || { // `|| {...}`: to be compatible with old data(roles not in the present  `availableRoles`
+      value: role,
+      text: this.translateRole(role),
+      isSelected: true
+    };
+
     return (
-      <div className="users-filter-bar mt-4 mb-2 d-flex">
-        <Dropdown isOpen={this.state.isStatusOpen} toggle={this.toggleStatusDropdown}>
-          <DropdownToggle
-            tag="div"
-            data-toggle="dropdown"
-            aria-expanded={this.state.isStatusOpen}
-            className="users-filter-bar-dropdown-toggle"
-          >
-            <span>{gettext('Status')}{': '}{this.translateStatus(this.props.isActive)}</span>
-            <span className='sf3-font-down sf3-font'></span>
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem key={0} onClick={() => { onStatusChange(''); }}>
-              <span>{gettext('All')}</span>{this.props.isActive === '' && this.renderCheck()}
-            </DropdownItem>
-            <DropdownItem key={1} onClick={() => { onStatusChange('1'); }}>
-              <span>{gettext('Active')}</span>{this.props.isActive === '1' && this.renderCheck()}
-            </DropdownItem>
-            <DropdownItem key={2} onClick={() => { onStatusChange('0'); }}>
-              <span>{gettext('Inactive')}</span>{this.props.isActive === '0' && this.renderCheck()}
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-        <Dropdown isOpen={this.state.isRoleOpen} toggle={this.toggleRoleDropdown} className="ml-4">
-          <DropdownToggle
-            tag="div"
-            data-toggle="dropdown"
-            aria-expanded={this.state.isRoleOpen}
-            className="users-filter-bar-dropdown-toggle"
-          >
-            <span>{gettext('Role')}{': '}{this.translateRole(this.props.role)}</span>
-            <span className='sf3-font-down sf3-font'></span>
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem key={0} onClick={() => { onRoleChange(''); }}>
-              <span>{gettext('All')}</span>
-              {this.props.role === '' && this.renderCheck()}
-            </DropdownItem>
-            {availableRoles.map((item, index) => {
-              return (
-                <DropdownItem key={index} onClick={() => { onRoleChange(item); }}>
-                  <span>{this.translateRole(item)}</span>
-                  {this.props.role === item && this.renderCheck()}
-                </DropdownItem>
-              );
-            })}
-          </DropdownMenu>
-        </Dropdown>
+      <div className="users-filter-bar mt-4 mb-2 d-flex align-items-center">
+        <span className="filter-item mr-2">{`${gettext('Status')}:`}</span>
+        <Selector
+          isDropdownToggleShown={true}
+          currentSelectedOption={currentSelectedStatusOption}
+          options={this.statusOptions}
+          selectOption={this.selectStatusOption}
+        />
+
+        <span className="filter-item mr-2 ml-4">{`${gettext('Role')}:`}</span>
+        <Selector
+          isDropdownToggleShown={true}
+          currentSelectedOption={currentSelectedRoleOption}
+          options={this.roleOptions}
+          selectOption={this.selectRoleOption}
+        />
       </div>
     );
   }
 }
 
 UsersFilterBar.propTypes = {
-  loading: PropTypes.bool,
-  curPerPage: PropTypes.number,
-  sortBy: PropTypes.string,
-  currentPage: PropTypes.number,
-  sortOrder: PropTypes.string,
   onStatusChange: PropTypes.func,
   onRoleChange: PropTypes.func,
   role: PropTypes.string,
