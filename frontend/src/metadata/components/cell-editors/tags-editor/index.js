a42d5c142f580420ac202d813bf8f55237976be3
Michael An <1822852997@qq.com>
2025-04-07 10:26:37 +0800

    remove search tag by tag RGB color

--- frontend/src/metadata/components/cell-editors/tags-editor/index.js
+++ frontend/src/metadata/components/cell-editors/tags-editor/index.js
@@ -8,7 +8,7 @@ import { Utils } from '../../../../utils/utils';
 import { KeyCodes } from '../../../../constants';
 import { gettext } from '../../../../utils/constants';
 import { useTags } from '../../../../tag/hooks';
-import { getTagColor, getTagId, getTagName, getTagsByName, getTagByName } from '../../../../tag/utils/cell';
+import { getTagId, getTagName, getTagsByName, getTagByName } from '../../../../tag/utils/cell';
 import { getRecordIdFromRecord } from '../../../utils/cell';
 import { getRowById } from '../../../../components/sf-table/utils/table';
 import { SELECT_OPTION_COLORS } from '../../../constants';
@@ -278,8 +278,7 @@ const TagsEditor = forwardRef(({
       if (!row) return;
       const value = searchValue.toLowerCase();
       const tagName = getTagName(row).toLowerCase();
-      const tagColor = getTagColor(row).toLowerCase();
-      if (!tagName.includes(value) && !tagColor.includes(value)) return;
+      if (!tagName.includes(value)) return;
       const nodesWithAncestors = getNodesWithAncestors(node, tree).filter(node => checkIsTreeNodeShown(getTreeNodeKey(node), searchedKeyNodeFoldedMap));
       searchedNodes = [...searchedNodes, ...nodesWithAncestors];
     });
