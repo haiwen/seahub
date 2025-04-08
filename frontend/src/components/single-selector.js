baddc4bad726cc7086d0983ce33bc1aea302c961
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2025-04-08 11:20:42 +0800

    Update single-selector.js (#7707)

--- frontend/src/components/single-selector.js
+++ frontend/src/components/single-selector.js
@@ -73,7 +73,7 @@ class Selector extends Component {
         <div onClick={this.onToggleClick}>
           {customSelectorToggle ? customSelectorToggle : (
             <span className="cur-option">
-              {currentSelectedOption.text}
+              {currentSelectedOption ? currentSelectedOption.text : ''}
               {isDropdownToggleShown && <i className="sf3-font sf3-font-down ml-1 toggle-icon"></i>}
             </span>
           )}
