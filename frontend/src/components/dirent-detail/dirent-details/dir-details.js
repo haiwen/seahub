accf00e7d7a5275a76e05cbb6d08f0bbc9def05f
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-04-19 18:32:39 +0800

    exclude special dir

--- frontend/src/components/dirent-detail/dirent-details/dir-details.js
+++ frontend/src/components/dirent-detail/dirent-details/dir-details.js
@@ -7,23 +7,33 @@ import { gettext } from '../../../utils/constants';
 import { MetadataDetails } from '../../../metadata';
 import { useMetadataStatus } from '../../../hooks';
 import { Utils } from '../../../utils/utils';
+import { SYSTEM_FOLDERS } from '../../../constants';
 
 const DirDetails = ({ direntDetail }) => {
   const { enableMetadata, enableMetadataManagement } = useMetadataStatus();
   const lastModifiedTimeField = useMemo(() => {
     return { type: CellType.MTIME, name: gettext('Last modified time') };
   }, []);
+
   const sizeField = useMemo(() => ({ type: 'size', name: gettext('Size') }), []);
   const filesField = useMemo(() => ({ type: CellType.NUMBER, name: gettext('Files') }), []);
+  let file_count = direntDetail.file_count || 0;
+  let size = Utils.bytesToSize(direntDetail.size);
+  const path = direntDetail.path.replace(/\/$/, '');
+
   return (
     <>
       {enableMetadataManagement && enableMetadata && (
         <>
-          <DetailItem field={filesField} value={direntDetail.file_count || 0} className="sf-metadata-property-detail-formatter">
-            <Formatter field={filesField} value={direntDetail.file_count || 0} />
+          <DetailItem field={filesField} value={file_count} className="sf-metadata-property-detail-formatter">
+            {SYSTEM_FOLDERS.includes(path) ?
+              <Formatter field={CellType.TEXT} value={'--'} /> :
+              <Formatter field={filesField} value={file_count} />}
           </DetailItem>
-          <DetailItem field={sizeField} value={Utils.bytesToSize(direntDetail.size)} className="sf-metadata-property-detail-formatter">
-            <Formatter field={sizeField} value={Utils.bytesToSize(direntDetail.size)} />
+          <DetailItem field={sizeField} value={size} className="sf-metadata-property-detail-formatter">
+            {SYSTEM_FOLDERS.includes(path) ?
+              <Formatter field={CellType.TEXT} value={'--'} /> :
+              <Formatter field={sizeField} value={size} />}
           </DetailItem>
           <MetadataDetails />
         </>
