38c6ea36ae0e14dca1fbe9479a27c77e689da79c
awu0403 <76416779+awu0403@users.noreply.github.com>
2025-04-20 08:53:12 +0800

    Optimize dir size (#7732)
    * add dir size
    
    * update
    
    * use sql query file md
    
    * optimize sql
    
    * update sql
    
    * exclude special dir
    
    * update
    
    ---------
    
    Co-authored-by: 孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>

--- frontend/src/components/dirent-detail/dirent-details/dir-details.js
+++ frontend/src/components/dirent-detail/dirent-details/dir-details.js
@@ -6,6 +6,8 @@ import { CellType } from '../../../metadata/constants';
 import { gettext } from '../../../utils/constants';
 import { MetadataDetails } from '../../../metadata';
 import { useMetadataStatus } from '../../../hooks';
+import { Utils } from '../../../utils/utils';
+import { SYSTEM_FOLDERS } from '../../../constants';
 
 const DirDetails = ({ direntDetail }) => {
   const { enableMetadata, enableMetadataManagement } = useMetadataStatus();
@@ -13,14 +15,35 @@ const DirDetails = ({ direntDetail }) => {
     return { type: CellType.MTIME, name: gettext('Last modified time') };
   }, []);
 
+  const sizeField = useMemo(() => ({ type: 'size', name: gettext('Size') }), []);
+  const filesField = useMemo(() => ({ type: CellType.NUMBER, name: gettext('Files') }), []);
+  let file_count = direntDetail.file_count || 0;
+  let size = Utils.bytesToSize(direntDetail.size);
+  let special_folder = false;
+  if (direntDetail.path !== undefined) {
+    special_folder = SYSTEM_FOLDERS.some(folder => direntDetail.path.startsWith(folder));
+  }
+
   return (
     <>
+      {enableMetadataManagement && enableMetadata && (
+        <>
+          <DetailItem field={filesField} value={file_count} className="sf-metadata-property-detail-formatter">
+            {special_folder ?
+              <Formatter field={CellType.TEXT} value={'--'} /> :
+              <Formatter field={filesField} value={file_count} />}
+          </DetailItem>
+          <DetailItem field={sizeField} value={size} className="sf-metadata-property-detail-formatter">
+            {special_folder ?
+              <Formatter field={CellType.TEXT} value={'--'} /> :
+              <Formatter field={sizeField} value={size} />}
+          </DetailItem>
+          <MetadataDetails />
+        </>
+      )}
       <DetailItem field={lastModifiedTimeField} className="sf-metadata-property-detail-formatter">
         <Formatter field={lastModifiedTimeField} value={direntDetail.mtime} />
       </DetailItem>
-      {enableMetadataManagement && enableMetadata && (
-        <MetadataDetails />
-      )}
     </>
   );
 };
