700e58bf3c9d6935ec7db13ded442994eb111116
zhouwenxuan <aries@Mac.local>
2025-04-29 12:14:10 +0800

    fix windows scrollbar width

--- frontend/src/metadata/views/table/table-main/records/index.js
+++ frontend/src/metadata/views/table/table-main/records/index.js
@@ -738,7 +738,7 @@ class Records extends Component {
         {this.isWindows && this.isWebkit && (
           <HorizontalScrollbar
             ref={this.setHorizontalScrollbarRef}
-            innerWidth={totalWidth + CANVAS_RIGHT_INTERVAL}
+            innerWidth={totalWidth + CANVAS_RIGHT_INTERVAL + SEQUENCE_COLUMN_WIDTH}
             onScrollbarScroll={this.onHorizontalScrollbarScroll}
             onScrollbarMouseUp={this.onHorizontalScrollbarMouseUp}
           />
