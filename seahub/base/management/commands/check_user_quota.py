168fd3a078f6a09894f7e10ab8f24d1049959626
r350178982 <32759763+r350178982@users.noreply.github.com>
2025-05-16 17:49:55 +0800

    update

--- seahub/base/management/commands/check_user_quota.py
+++ seahub/base/management/commands/check_user_quota.py
@@ -7,7 +7,7 @@ from seahub.base.accounts import User
 from seahub.profile.models import Profile
 from seahub.utils import IS_EMAIL_CONFIGURED, send_html_email, \
     get_site_name
-from seahub.base.models import UserQuotaUsage
+from seahub.base.models import UserQuotaUsage, OrgQuotaUsage
 from seahub.utils.ccnet_db import CcnetDB
 
 logger = logging.getLogger(__name__)
@@ -37,9 +37,7 @@ class Command(BaseCommand):
     def get_user_language(self, username):
         return Profile.objects.get_user_language(username)
     
-    def send_email(self, user_info):
-        if not IS_EMAIL_CONFIGURED:
-            return
+    def send_user_quota_alert_email(self, user_info):
 
         # save current language
         cur_language = translation.get_language()
@@ -63,8 +61,36 @@ class Command(BaseCommand):
         # restore current language
         translation.activate(cur_language)
         
+        
+    def send_org_quota_alert_email(self, org_info, ccnet_db):
+        # save current language
+        cur_language = translation.get_language()
+        org_id = org_info.org_id
+        quota_total = org_info.quota
+        quota_usage = org_info.usage
+        org_admins = ccnet_db.get_org_staffs(org_id)
+        for email in org_admins:
+    
+            # get and active user language
+            user_language = self.get_user_language(email)
+            translation.activate(user_language)
+        
+            data = {'email': email, 'quota_total': quota_total, 'quota_usage': quota_usage}
+            contact_email = Profile.objects.get_contact_email_by_user(email)
+            logger.info('Send email to %s(%s)' % (contact_email, email))
+        
+            send_html_email(_(u'The team quota is almost full on %s') % get_site_name(),
+                            'team_quota_full.html', data, None, [contact_email])
+        
+        # restore current language
+        translation.activate(cur_language)
+        
 
     def handle(self, *args, **options):
+        
+        if not IS_EMAIL_CONFIGURED:
+            return
+        
         auto_run = options.get('auto', None)
         email = options.get('email', None)
         
@@ -75,33 +101,38 @@ class Command(BaseCommand):
             recent_days = -1
         
         alert_users = UserQuotaUsage.objects.get_quota_alert_users(recent_days=recent_days)
-        if not alert_users:
+        alert_orgs = OrgQuotaUsage.objects.get_quota_alert_orgs(recent_days=recent_days)
+        if not (alert_users or alert_orgs):
             return
         
-        alert_users_email_list = [u.username for u in alert_users]
-
-        available_alert_user_email_list = ccnet_db.get_active_users_by_user_list(alert_users_email_list)
-        if not available_alert_user_email_list:
-            return
-        available_alert_user_email_map = {u.username: u for u in alert_users if u.username in available_alert_user_email_list}
-        user_obj_email_list = available_alert_user_email_map.keys()
-        
-        
-        if email:
-            try:
-                if email not in available_alert_user_email_list:
-                    return
-                User.objects.get(email=email)
-                user_info = available_alert_user_email_map.get(email)
-                self.send_email(user_info)
-                print("Done")
-            except User.DoesNotExist:
-                print('User %s not found' % email)
-                
+        if alert_users:
+            alert_users_email_list = [u.username for u in alert_users]
+    
+            available_alert_user_email_list = ccnet_db.get_active_users_by_user_list(alert_users_email_list)
+            available_alert_user_email_map = {u.username: u for u in alert_users if u.username in available_alert_user_email_list}
+            user_obj_email_list = available_alert_user_email_map.keys()
             
-        else:
-            for email in user_obj_email_list:
-                user_info = available_alert_user_email_map.get(email)
-                self.send_email(user_info)
+            if email:
+                try:
+                    if email not in available_alert_user_email_list:
+                        return
+                    User.objects.get(email=email)
+                    user_info = available_alert_user_email_map.get(email)
+                    self.send_user_quota_alert_email(user_info)
+                    print("Done")
+                except User.DoesNotExist:
+                    print('User %s not found' % email)
+                    
+                
+            else:
+                for email in user_obj_email_list:
+                    user_info = available_alert_user_email_map.get(email)
+                    self.send_user_quota_alert_email(user_info)
+                    
+        if alert_orgs:
+            for org_info in alert_orgs:
+                self.send_org_quota_alert_email(org_info, ccnet_db)
+                
+        print('Done')
 
-            print("Done")
+            
--- seahub/base/models.py
+++ seahub/base/models.py
@@ -519,6 +519,69 @@ class GroupMemberAudit(models.Model):
 
     class Meta:
         db_table = 'group_member_audit'
+
+
+class UserQuotaUsageManager(models.Manager):
+    def get_quota_alert_users(self, threshold=0.9, recent_days=1):
+        if recent_days == -1:
+            alert_users = self.filter(
+                quota__gt=0,
+                usage__gt=F('quota') * threshold
+            )
+        else:
+            recent_date = timezone.now() - datetime.timedelta(days=recent_days)
+            alert_users = self.filter(
+                timestamp__gte=recent_date,
+                quota__gt=0,
+                usage__gt=F('quota') * threshold
+            )
+        
+        if not alert_users.exists():
+            return None
+        
+        return alert_users
+
+class UserQuotaUsage(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    org_id = models.IntegerField(db_index=True)
+    quota = models.BigIntegerField()
+    usage = models.BigIntegerField()
+    timestamp = models.DateTimeField(db_index=True)
+    objects = UserQuotaUsageManager()
+    
+    class Meta:
+        db_table = 'user_quota_usage'
+
+
+class OrgQuotaUsageManager(models.Manager):
+    def get_quota_alert_orgs(self, threshold=0.9, recent_days=1):
+        if recent_days == -1:
+            alert_orgs = self.filter(
+                quota__gt=0,
+                usage__gt=F('quota') * threshold
+            )
+        else:
+            recent_date = timezone.now() - datetime.timedelta(days=recent_days)
+            alert_orgs = self.filter(
+                timestamp__gte=recent_date,
+                quota__gt=0,
+                usage__gt=F('quota') * threshold
+            )
+        
+        if not alert_orgs.exists():
+            return None
+        
+        return alert_orgs
+
+class OrgQuotaUsage(models.Model):
+    org_id = models.IntegerField(db_index=True, unique=True)
+    quota = models.BigIntegerField()
+    usage = models.BigIntegerField()
+    timestamp = models.DateTimeField(db_index=True)
+    objects = OrgQuotaUsageManager()
+    
+    class Meta:
+        db_table = 'org_quota_usage'
         
 
 
@@ -542,41 +605,3 @@ def add_group_invite_log(sender, org_id, group_id, users, operator, operation, *
             operation=operation
         ))
     GroupMemberAudit.objects.bulk_create(group_member_audit_list)
-
-
-
-class UserQuotaUsageManager(models.Manager):
-    
-    def get_quota_alert_users(self, threshold=0.9, recent_days=1):
-        if recent_days == -1:
-            alert_users = self.filter(
-                quota__gt=0,
-                usage__gt=F('quota') * threshold
-            )
-        else:
-            recent_date = timezone.now() - datetime.timedelta(days=recent_days)
-            alert_users = self.filter(
-                timestamp__gte=recent_date,
-                quota__gt=0,
-                usage__gt=F('quota') * threshold
-            )
-            
-        if not alert_users.exists():
-            return None
-        
-        return alert_users
-    
-    
-
-class UserQuotaUsage(models.Model):
-    username = models.CharField(max_length=255, unique=True, null=False, blank=False)
-    org_id = models.IntegerField(db_index=True)
-    quota = models.BigIntegerField()
-    usage = models.BigIntegerField()
-    timestamp = models.DateTimeField(db_index=True)
-    objects = UserQuotaUsageManager()
-
-    class Meta:
-        db_table = 'user_quota_usage'
-
-
