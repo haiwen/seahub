54f40079043dfd777c7e1364e043fbc38dd4060f
lian <imwhatiam123@gmail.com>
2025-05-21 16:47:49 +0800

    support view/edit .pages file via onlyoffice

--- seahub/onlyoffice/settings.py
+++ seahub/onlyoffice/settings.py
@@ -14,10 +14,10 @@ ONLYOFFICE_FILE_EXTENSION = getattr(settings,
                                     ('doc', 'docx', 'ppt', 'pptx',
                                      'xls', 'xlsx', 'odt', 'fodt',
                                      'odp', 'fodp', 'ods', 'fods',
-                                     'ppsx', 'pps', 'csv'))
+                                     'ppsx', 'pps', 'csv', 'pages'))
 ONLYOFFICE_EDIT_FILE_EXTENSION = getattr(settings,
                                          'ONLYOFFICE_EDIT_FILE_EXTENSION',
-                                         ('docx', 'pptx', 'xlsx', 'csv'))
+                                         ('docx', 'pptx', 'xlsx', 'csv', 'pages'))
 
 VERIFY_ONLYOFFICE_CERTIFICATE = getattr(settings, 'VERIFY_ONLYOFFICE_CERTIFICATE', True)
 ONLYOFFICE_JWT_HEADER = getattr(settings, 'ONLYOFFICE_JWT_HEADER', 'Authorization')
