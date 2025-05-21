94889af091af0b23389fd449e9e606fcd5617044
lian <imwhatiam123@gmail.com>
2025-05-21 17:08:35 +0800

    support view/edit .pages file via onlyoffice (#7841)

--- seahub/utils/__init__.py
+++ seahub/utils/__init__.py
@@ -120,7 +120,7 @@ MAX_INT = 2147483647
 
 PREVIEW_FILEEXT = {
     IMAGE: ('gif', 'jpeg', 'jpg', 'png', 'heic', 'ico', 'bmp', 'tif', 'tiff', 'psd', 'webp', 'jfif'),
-    DOCUMENT: ('doc', 'docx', 'docxf', 'oform', 'ppt', 'pptx', 'odt', 'fodt', 'odp', 'fodp', 'odg'),
+    DOCUMENT: ('doc', 'docx', 'docxf', 'oform', 'ppt', 'pptx', 'odt', 'fodt', 'odp', 'fodp', 'odg', 'pages'),
     SPREADSHEET: ('xls', 'xlsx', 'ods', 'fods'),
     SVG: ('svg',),
     PDF: ('pdf', 'ai'),
