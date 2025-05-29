62c8895ce4d746d618273541af549ac56930b8d7
zhouwenxuan <aries@Mac.local>
2025-05-29 14:30:45 +0800

    update embedded details

--- frontend/src/components/dirent-detail/embedded-file-details/index.js
+++ frontend/src/components/dirent-detail/embedded-file-details/index.js
@@ -70,10 +70,7 @@ const EmbeddedFileDetails = ({ repoID, repoInfo, dirent, path, onClose, width = 
       direntType="file"
     >
       <div
-        className={classnames('cur-view-detail', className, {
-          'cur-view-detail-small': width < 400,
-          'cur-view-detail-large': width > 400
-        })}
+        className={classnames('cur-view-detail', className)}
         style={{ width }}
       >
         <Header title={dirent?.name || ''} icon={Utils.getDirentIcon(dirent, true)} onClose={onClose} component={headerComponent}>
