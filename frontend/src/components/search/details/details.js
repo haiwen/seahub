8cc63be9fd6da7c271edba0927fbf533d41556ee
Michael An <2331806369@qq.com>
2025-06-10 09:55:55 +0800

    change searched item loading (#7913)

--- frontend/src/components/search/details/details.js
+++ frontend/src/components/search/details/details.js
@@ -1,10 +1,8 @@
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Body, Header } from '../../dirent-detail/detail';
-import { siteRoot, thumbnailSizeForGrid } from '../../../utils/constants';
 import { Utils } from '../../../utils/utils';
 import FileDetails from '../../dirent-detail/dirent-details/file-details';
 import DirDetails from '../../dirent-detail/dirent-details/dir-details';
-import { useEffect, useState } from 'react';
 import { useMetadataStatus } from '../../../hooks';
 import tagsAPI from '../../../tag/api';
 import { PER_LOAD_NUMBER } from '../../../metadata/constants';
@@ -13,7 +11,7 @@ import { TAGS_DEFAULT_SORT } from '../../../tag/constants/sort';
 import TagsData from '../../../tag/model/tagsData';
 import toaster from '../../toast';
 
-const Details = ({ repoID, repoInfo, path, dirent, direntDetail }) => {
+const Details = ({ repoID, path, dirent, direntDetail }) => {
   const [tagsData, setTagsData] = useState(null);
   const { enableMetadata, enableTags } = useMetadataStatus();
 
@@ -33,55 +31,33 @@ const Details = ({ repoID, repoInfo, path, dirent, direntDetail }) => {
     }
   }, [repoID, enableMetadata, enableTags]);
 
-  let src = '';
-  if (repoInfo.encrypted) {
-    src = `${siteRoot}repo/${repoID}/raw` + Utils.encodePath(`${path === '/' ? '' : path}/${dirent.name}`);
-  } else {
-    src = `${siteRoot}thumbnail/${repoID}/${thumbnailSizeForGrid}` + Utils.encodePath(`${path === '/' ? '' : path}/${dirent.name}`) + '?mtime=' + direntDetail.mtime;
-  }
   return (
-    <div className="searched-item-details">
-      <div
-        className="cur-view-detail"
-        style={{ width: 300 }}
-      >
-        <Header title={dirent?.name || ''} icon={Utils.getDirentIcon(dirent, true)}></Header>
-        <Body>
-          {Utils.imageCheck(dirent.name) && (
-            <div className="detail-image">
-              <img src={src} alt="" />
-            </div>
-          )}
-          <div className="detail-content">
-            {dirent.type !== 'file' ? (
-              <DirDetails
-                direntDetail={direntDetail}
-                readOnly={true}
-                tagsData={tagsData}
-              />
-            ) : (
-              <FileDetails
-                repoID={repoID}
-                dirent={dirent}
-                path={path}
-                direntDetail={direntDetail}
-                repoTags={[]}
-                fileTagList={dirent ? dirent.file_tags : []}
-                readOnly={true}
-                tagsData={tagsData}
-                onFileTagChanged={() => {}}
-              />
-            )}
-          </div>
-        </Body>
-      </div>
+    <div className="detail-content">
+      {dirent.type !== 'file' ?
+        <DirDetails
+          direntDetail={direntDetail}
+          readOnly={true}
+          tagsData={tagsData}
+        />
+        :
+        <FileDetails
+          repoID={repoID}
+          dirent={dirent}
+          path={path}
+          direntDetail={direntDetail}
+          repoTags={[]}
+          fileTagList={dirent ? dirent.file_tags : []}
+          readOnly={true}
+          tagsData={tagsData}
+          onFileTagChanged={() => {}}
+        />
+      }
     </div>
   );
 };
 
 Details.propTypes = {
   repoID: PropTypes.string.isRequired,
-  repoInfo: PropTypes.object.isRequired,
   path: PropTypes.string.isRequired,
   dirent: PropTypes.object.isRequired,
   direntDetail: PropTypes.object.isRequired,
