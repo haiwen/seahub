763389cb0975996e41b5acd11715abd60633660a
zhouwenxuan <aries@Mac.local>
2025-06-23 09:54:38 +0800

    optimize

--- seahub/repo_metadata/metadata_server_api.py
+++ seahub/repo_metadata/metadata_server_api.py
@@ -51,7 +51,7 @@ def collect_all_tag_ids(tag_ids, metadata_server_api):
     all_ids = set(tag_ids)
     to_check = set(tag_ids)
     while to_check:
-        sql = f"SELECT `{TAGS_TABLE.columns.id.name}`, `{TAGS_TABLE.columns.name.name}`, `{TAGS_TABLE.columns.sub_links.name}` FROM `{TAGS_TABLE.name}` WHERE `{TAGS_TABLE.columns.id.name}` IN ({', '.join([f'\"{tid}\"' for tid in to_check])})"
+        sql = f"SELECT `{TAGS_TABLE.columns.id.name}`, `{TAGS_TABLE.columns.name.name}`, `{TAGS_TABLE.columns.sub_links.name}` FROM `{TAGS_TABLE.name}` WHERE `{TAGS_TABLE.columns.id.name}` IN ({', '.join([f'"{tid}"' for tid in to_check])})"
         tags = metadata_server_api.query_rows(sql).get('results', [])
         next_ids = set()
         for tag in tags:
