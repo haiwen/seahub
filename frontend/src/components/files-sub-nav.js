131936e4eedf9e7eb62af199b9748f3d3aedf96e
llj <lingjun.li1@gmail.com>
2025-06-26 18:23:27 +0800

    [user side panel] 'Files' sub nav items: added '.ellipsis' and display full text when hover on them

--- frontend/src/components/files-sub-nav.js
+++ frontend/src/components/files-sub-nav.js
@@ -34,11 +34,11 @@ class FilesSubNav extends React.Component {
             <li key={item.id} className={`nav-item ${this.getActiveClass(item.name)}`}>
               <Link
                 to={siteRoot + 'group/' + item.id + '/'}
-                className={`nav-link ellipsis ${this.getActiveClass(item.name)}`}
+                className={`nav-link ${this.getActiveClass(item.name)}`}
                 onClick={(e) => this.tabItemClick(e, item.name, item.id)}
               >
                 <span className={`${item.parent_group_id == 0 ? 'sf3-font-group' : 'sf3-font-department'} sf3-font nav-icon`} aria-hidden="true"></span>
-                <span className="nav-text">{item.name}</span>
+                <span className="nav-text ellipsis" title={item.name}>{item.name}</span>
               </Link>
             </li>
           );
@@ -52,23 +52,35 @@ class FilesSubNav extends React.Component {
       <>
         {canAddRepo && (
           <li className={`nav-item ${this.getActiveClass('my-libs') || this.getActiveClass('deleted')}`}>
-            <Link to={ siteRoot + 'my-libs/' } className={`nav-link ellipsis ${this.getActiveClass('my-libs') || this.getActiveClass('deleted') }`} title={gettext('My Libraries')} onClick={(e) => this.tabItemClick(e, 'my-libs')}>
+            <Link
+              to={siteRoot + 'my-libs/'}
+              className={`nav-link ${this.getActiveClass('my-libs') || this.getActiveClass('deleted') }`}
+              onClick={(e) => this.tabItemClick(e, 'my-libs')}
+            >
               <span className="sf3-font-mine sf3-font nav-icon" aria-hidden="true"></span>
-              <span className="nav-text">{gettext('My Libraries')}</span>
+              <span className="nav-text ellipsis" title={gettext('My Libraries')}>{gettext('My Libraries')}</span>
             </Link>
           </li>
         )}
         <li className={`nav-item ${this.getActiveClass('shared-libs')}`}>
-          <Link to={siteRoot + 'shared-libs/'} className={`nav-link ellipsis ${this.getActiveClass('shared-libs')}`} title={gettext('Shared with me')} onClick={(e) => this.tabItemClick(e, 'shared-libs')}>
+          <Link
+            to={siteRoot + 'shared-libs/'}
+            className={`nav-link ${this.getActiveClass('shared-libs')}`}
+            onClick={(e) => this.tabItemClick(e, 'shared-libs')}
+          >
             <span className="sf3-font-share-with-me sf3-font nav-icon" aria-hidden="true"></span>
-            <span className="nav-text">{gettext('Shared with me')}</span>
+            <span className="nav-text ellipsis" title={gettext('Shared with me')}>{gettext('Shared with me')}</span>
           </Link>
         </li>
         {canViewOrg &&
-        <li className={`nav-item ${this.getActiveClass('org')}`} onClick={(e) => this.tabItemClick(e, 'org')}>
-          <Link to={ siteRoot + 'org/' } className={`nav-link ellipsis ${this.getActiveClass('org')}`} title={gettext('Shared with all')}>
+        <li className={`nav-item ${this.getActiveClass('org')}`}>
+          <Link
+            to={siteRoot + 'org/'}
+            className={`nav-link ${this.getActiveClass('org')}`}
+            onClick={(e) => this.tabItemClick(e, 'org')}
+          >
             <span className="sf3-font-share-with-all sf3-font nav-icon" aria-hidden="true"></span>
-            <span className="nav-text">{gettext('Shared with all')}</span>
+            <span className="nav-text ellipsis" title={gettext('Shared with all')}>{gettext('Shared with all')}</span>
           </Link>
         </li>
         }
