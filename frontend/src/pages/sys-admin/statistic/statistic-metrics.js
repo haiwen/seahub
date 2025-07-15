9d8aad861e9d3cfc6eb63a5410ad158b80738044
zhouwenxuan <aries@Mac.local>
2025-07-15 14:12:48 +0800

    update border color

--- frontend/src/pages/sys-admin/statistic/statistic-metrics.js
+++ frontend/src/pages/sys-admin/statistic/statistic-metrics.js
@@ -265,13 +265,13 @@ const style = `
     .metrics-container .table td {
       vertical-align: middle;
       background: var(--bs-body-bg);
-      border-bottom: 1px solid var(--bs-td-border-color);
+      border-bottom: 1px solid var(--bs-border-color);
       padding-left: 8px;
     }
 
     .metrics-container .table th {
       background: var(--bs-body-bg);
-      border-bottom: 1px solid var(--bs-td-border-color);
+      border-bottom: 1px solid var(--bs-border-color);
       color: #666;
       font-size: 14px;
     }
