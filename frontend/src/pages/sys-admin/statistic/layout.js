d5833e3b5c43b4470bf2781a24a548881c296c5e
Aries <urchinzhou@gmail.com>
2025-07-21 11:24:15 +0800

    Fix/popover UI and statistics page (#8062)
    * account popover ui
    
    * fix statistics page ui
    
    ---------
    
    Co-authored-by: zhouwenxuan <aries@Mac.local>

--- frontend/src/pages/sys-admin/statistic/layout.js
+++ frontend/src/pages/sys-admin/statistic/layout.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { useLocation } from '@gatsbyjs/reach-router';
+import { Router, useLocation } from '@gatsbyjs/reach-router';
 import StatisticNav from './statistic-nav';
 import MainPanelTopbar from '../main-panel-topbar';
+import StatisticFile from './statistic-file';
+import StatisticStorage from './statistic-storage';
+import StatisticUsers from './statistic-users';
+import StatisticTraffic from './statistic-traffic';
+import StatisticMetrics from './statistic-metrics';
+import StatisticReports from './statistic-reports';
 
 const StatisticLayout = ({ children, ...commonProps }) => {
   const location = useLocation();
@@ -11,7 +17,14 @@ const StatisticLayout = ({ children, ...commonProps }) => {
     <>
       <MainPanelTopbar {...commonProps} />
       <StatisticNav currentItem={currentItem} />
-      <div className="w-100 h-100 d-flex overflow-auto">{children}</div>
+      <Router className="d-flex overflow-hidden">
+        <StatisticFile path="file" />
+        <StatisticStorage path="storage" />
+        <StatisticUsers path="user" />
+        <StatisticTraffic path="traffic" />
+        <StatisticReports path="reports" />
+        <StatisticMetrics path="metrics" />
+      </Router>
     </>
   );
 };
