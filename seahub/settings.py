2cefe488be8d87179bd58dfc113f2d935506942a
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-08-06 18:32:41 +0800

    test

--- seahub/settings.py
+++ seahub/settings.py
@@ -109,7 +109,8 @@ STORAGES = {
         "BACKEND":  'django.core.files.storage.FileSystemStorage'
     },
     "staticfiles": {
-       "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        # "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+       "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
