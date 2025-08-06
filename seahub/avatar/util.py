6550c1a07e8dcc6f82af039c218d894276ecaaf7
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-08-06 18:12:41 +0800

    update

--- seahub/avatar/util.py
+++ seahub/avatar/util.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.core.cache import cache
 from django.core.files.storage import default_storage
+from django.utils.module_loading import import_string
 from urllib.parse import quote
 
 from seahub.base.accounts import User
@@ -112,7 +113,9 @@ def get_primary_avatar(user, size=AVATAR_DEFAULT_SIZE):
             avatar.create_thumbnail(size)
     return avatar
 
-from django.utils.module_loading import import_string
+def get_storage_class(import_path=None):
+    return import_string(import_path or settings.STORAGES['default'])
+
 def get_avatar_file_storage():
     """Get avatar file storage, defaults to file system storage.
     """
@@ -126,4 +129,4 @@ def get_avatar_file_storage():
             'data_column': 'data',
             'size_column': 'size',
             }
-        return import_string(AVATAR_FILE_STORAGE or settings.STORAGES['default'])(options=dbs_options)
+        return get_storage_class(AVATAR_FILE_STORAGE)(options=dbs_options)
