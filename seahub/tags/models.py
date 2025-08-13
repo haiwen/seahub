733bc5ef6b32ce052df03e3ae1ed0b6693adeafd
r350178982 <32759763+r350178982@users.noreply.github.com>
2025-08-13 16:46:46 +0800

    update

--- seahub/tags/models.py
+++ seahub/tags/models.py
@@ -207,8 +207,8 @@ class Char32UUIDField(models.UUIDField):
 
     def get_db_prep_value(self, value, connection, prepared=False):
         value = super().get_db_prep_value(value, connection, prepared)
-        if value is not None:
-            value = value
+        if isinstance(value, uuid.UUID):
+            value = value.hex
         return value
 
 class FileUUIDMap(models.Model):
