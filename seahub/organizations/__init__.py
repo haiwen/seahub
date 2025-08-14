fe95e3d80558bdd81bca4362545b6e996c112149
lian <imwhatiam123@gmail.com>
2025-08-14 10:50:03 +0800

    org operation signal

--- seahub/organizations/__init__.py
+++ seahub/organizations/__init__.py
@@ -1,22 +1,16 @@
 import os
 import sys
 import logging
-from seahub.organizations.signals import org_created, org_reactivated
+from seahub.organizations.signals import org_operation_signal
 logger = logging.getLogger(__name__)
 
 try:
     conf_dir = os.environ['SEAFILE_CENTRAL_CONF_DIR']
     sys.path.append(conf_dir)
     try:
-        from seahub_custom_functions import org_created_callback
-        org_created.connect(org_created_callback)
+        from seahub_custom_functions import org_operation_callback
+        org_operation_signal.connect(org_operation_callback)
     except ImportError as e:
-        logger.debug(e)
-
-    try:
-        from seahub_custom_functions import org_reactivated_callback
-        org_reactivated.connect(org_reactivated_callback)
-    except ImportError as e:
-        logger.debug(e)
+        logger.error(e)
 except KeyError as e:
-    logger.debug(e)
+    logger.error(e)
