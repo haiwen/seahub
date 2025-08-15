84b846d8b6c120fccbeadd583b9710edb952990b
Guodong Su <stephen.su66@gmail.com>
2025-08-15 10:21:34 +0800

    fix(dara-width): wiki left panel max width

--- frontend/src/components/resize-bar/constants.js
+++ frontend/src/components/resize-bar/constants.js
@@ -1,4 +1,4 @@
 export const DRAG_HANDLER_HEIGHT = 26;
 export const INIT_SIDE_PANEL_RATE = 0.22;
-export const MAX_SIDE_PANEL_RATE = 0.40;
+export const MAX_SIDE_PANEL_RATE = 0.32;
 export const MIN_SIDE_PANEL_RATE = 0.15;
