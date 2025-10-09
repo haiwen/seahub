93ffb286255a0f5c8a0e858145cee8484516a1a1
Michael An <1822852997@qq.com>
2025-10-09 15:28:16 +0800

    txt viewer support set auto wrap line

--- frontend/src/shared-file-view-text.js
+++ frontend/src/shared-file-view-text.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 import SharedFileView from './components/shared-file-view/shared-file-view';
 import SharedFileViewTip from './components/shared-file-view/shared-file-view-tip';
@@ -7,24 +7,32 @@ import './css/text-file-view.css';
 
 const { err, fileExt, fileContent } = window.shared.pageOptions;
 
-class FileContent extends React.Component {
-  render() {
-    if (err) {
-      return <SharedFileViewTip />;
-    }
-
-    return (
-      <div className="shared-file-view-body text-file-view">
-        <SeafileCodeMirror fileExt={fileExt} value={fileContent} />
-      </div>
-    );
+const FileContent = ({ lineWrapping }) => {
+  if (err) {
+    return <SharedFileViewTip />;
   }
-}
+  return (
+    <div className="shared-file-view-body text-file-view">
+      <SeafileCodeMirror fileExt={fileExt} value={fileContent} lineWrapping={lineWrapping} />
+    </div>
+  );
+};
 
-class SharedFileViewText extends React.Component {
-  render() {
-    return <SharedFileView content={<FileContent />} />;
-  }
-}
+const SharedFileViewText = () => {
+  let [lineWrapping, setLineWrapping] = useState(localStorage.getItem('sf_txt_file_line_wrapping') === 'true' || false);
+
+  const updateLineWrapping = (newLineWrapping) => {
+    setLineWrapping(newLineWrapping);
+  };
+
+  return (
+    <SharedFileView
+      content={<FileContent lineWrapping={lineWrapping}/>}
+      updateLineWrapping={updateLineWrapping}
+      lineWrapping={lineWrapping}
+      canWrapLine={true}
+    />
+  );
+};
 
 ReactDom.render(<SharedFileViewText />, document.getElementById('wrapper'));
