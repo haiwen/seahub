5b52cebabe15a4119658254b5d65f7532d261c93
lian <imwhatiam123@gmail.com>
2025-10-23 10:40:11 +0800

    not log error when ImportError occurred

--- seahub/organizations/__init__.py
+++ seahub/organizations/__init__.py
@@ -1,16 +1,11 @@
 import os
 import sys
-import logging
 from seahub.organizations.signals import org_operation_signal
-logger = logging.getLogger(__name__)
 
 try:
     conf_dir = os.environ['SEAFILE_CENTRAL_CONF_DIR']
     sys.path.append(conf_dir)
-    try:
-        from seahub_custom_functions import org_operation_callback
-        org_operation_signal.connect(org_operation_callback)
-    except ImportError as e:
-        logger.error(e)
-except KeyError as e:
-    logger.error(e)
+    from seahub_custom_functions import org_operation_callback
+    org_operation_signal.connect(org_operation_callback)
+except Exception:
+    pass
