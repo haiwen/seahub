08dca840d320cfc969d788a9fb2849c89ebcdf6d
小强 <shuntian@Mac.lan>
2025-10-28 14:12:48 +0800

    optimize code

--- frontend/src/pages/wiki-repo-view/index.js
+++ frontend/src/pages/wiki-repo-view/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 
+const { repoID, viewID, } = window.app.pageOptions;
+
 export default function WikiRepoView() {
+
+  const [metadata, setMetaData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className='wiki-repo-view'>
       <div className='wiki-repo-view-header'>
