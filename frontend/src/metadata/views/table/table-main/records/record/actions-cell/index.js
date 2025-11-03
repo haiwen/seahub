b328b1f9d4a6e0c8e6a4618311611d4026e711f1
llj <lingjun.li1@gmail.com>
2025-11-03 17:22:09 +0800

    [a11y] metadata - table: add keyboard a11y for 'select row' & 'expand row'

--- frontend/src/metadata/views/table/table-main/records/record/actions-cell/index.js
+++ frontend/src/metadata/views/table/table-main/records/record/actions-cell/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Tooltip } from 'reactstrap';
 import { gettext } from '../../../../../../../utils/constants';
-import { isMobile } from '../../../../../../../utils/utils';
+import { isMobile, Utils } from '../../../../../../../utils/utils';
 import { SEQUENCE_COLUMN_WIDTH } from '../../../../../../constants';
 import IconBtn from '../../../../../../../components/icon-btn';
 
@@ -73,25 +73,29 @@ class ActionsCell extends Component {
       >
         {!isSelected && <div className="sf-metadata-result-column-content row-index text-truncate">{index + 1}</div>}
         <div className='sf-metadata-result-column-content actions-checkbox'>
-          <div className='select-cell-checkbox-container' onClick={this.props.onSelectRecord}>
+          <div className='select-cell-checkbox-container'>
             <input
               id={`select-cell-checkbox-${recordId}`}
               className='select-cell-checkbox'
               type='checkbox'
               name='row-selection'
-              checked={isSelected || false}
-              readOnly
-            />
-            <label
-              htmlFor={`select-cell-checkbox-${recordId}`}
-              name={gettext('Select')}
               title={gettext('Select')}
               aria-label={gettext('Select')}
-            >
-            </label>
+              checked={isSelected || false}
+              onClick={this.props.onSelectRecord}
+              onKeyDown={Utils.onKeyDown}
+            />
           </div>
         </div>
-        <IconBtn symbol="expand" className="row-expand" iconClassName="row-expand-icon" onClick={this.handleShowExpandedProps} />
+        <IconBtn
+          symbol="expand"
+          className="row-expand"
+          iconClassName="row-expand-icon"
+          onClick={this.handleShowExpandedProps}
+          tabIndex="0"
+          role="button"
+          onKeyDown={Utils.onKeyDown}
+        />
       </div>
     );
   }
