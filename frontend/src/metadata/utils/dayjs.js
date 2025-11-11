bda4bf047caeba41da65e63166f804b5277cb542
zhouwenxuan <aries@Mac.local>
2025-11-11 14:37:54 +0800

    feat(ui): update calendar

--- frontend/src/metadata/utils/dayjs.js
+++ frontend/src/metadata/utils/dayjs.js
@@ -1,6 +1,10 @@
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/en-gb';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(localizedFormat);
 
 export default dayjs;
