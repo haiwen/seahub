haiwen/seahub

commit f4f4a5566adfbfd4bbac8cd4f59c3903a00ccac0

fix(file-chooser): prevent ghost node (#8348)
Co-authored-by: zhouwenxuan <aries@Mac.local>

frontend/src/components/file-chooser/repo-list-item.js
@@ -77,7 +77,7 @@ class RepoListItem extends React.Component {
       parentPath = pathElements.join('/');
     }
     // create new folder in selected repo or folder
-    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo !== selectedRepo && this.isComponentMounted) {
+    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo.repo_id !== selectedRepo.repo_id && this.isComponentMounted) {
       seafileAPI.listDir(repo.repo_id, parentPath).then(res => {
         const { dirent_list = [], dir_id = '', user_perm = 'r' } = res?.data || {};
         const dirent = dirent_list.find(item => item.type === 'dir' && item.name === newFolderName);

commit 383b99b1148d3cf138266ff88a997d6297ab9a6d

Fix download file select repo bug (#8397)
* 01 classNames

* 02 fix bug caused by https://github.com/haiwen/seahub/pull/8348/

* 03 fix click save button invalid

frontend/src/components/dirent-list-view/dirent-list-item.js
@@ -796,8 +796,8 @@ class DirentListItem extends React.Component {
             <td className={classnames('pl10 pr-2 cursor-pointer', { 'tr-drag-effect': this.state.isDragTipShow })} onClick={this.onItemSelected}>
               <input
                 type="checkbox"
-                className="vam cursor-pointer"
-                style={{ position: 'relative', top: -1 }}
+                className="vam cursor-pointer position-relative"
+                style={{ top: -1 }}
                 checked={isSelected}
                 aria-label={isSelected ? gettext('Unselect this item') : gettext('Select this item')}
                 title={isSelected ? gettext('Unselect this item') : gettext('Select this item')}

frontend/src/components/file-chooser/repo-list-item.js
@@ -77,7 +77,10 @@ class RepoListItem extends React.Component {
       parentPath = pathElements.join('/');
     }
     // create new folder in selected repo or folder
-    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo.repo_id !== selectedRepo.repo_id && this.isComponentMounted) {
+    if (!this.isComponentMounted || !repo || !selectedRepo) return;
+    const isSameRepo = repo.repo_id === selectedRepo.repo_id;
+    const isNewlySelectedRepo = !prevProps.selectedRepo || prevProps.selectedRepo.repo_id !== selectedRepo.repo_id;
+    if (isSameRepo && isNewlySelectedRepo) {
       seafileAPI.listDir(repo.repo_id, parentPath).then(res => {
         const { dirent_list = [], dir_id = '', user_perm = 'r' } = res?.data || {};
         const dirent = dirent_list.find(item => item.type === 'dir' && item.name === newFolderName);

frontend/src/shared-dir-view.js
@@ -870,7 +870,7 @@ class SharedDirView extends React.Component {
                         {(canDownload && loginUser && (loginUser !== sharedBy)) &&
                         <OpIcon
                           className="sf3-font-save sf3-font cur-view-path-btn ml-4"
-                          onClick={this.saveSelectedItems}
+                          op={this.saveSelectedItems}
                           title={gettext('Save')}
                         />
                         }

frontend/src/tag/views/tag-files/list/item.js
@@ -123,8 +123,8 @@ const TagFile = ({ repoID, file, tagsData, isRenaming, onRenameCancel, onRenameC
       <td className="pl10 pr-2" role="button" onClick={handleSelected} aria-label={isSelected ? gettext('Unselect this item') : gettext('Select this item')}>
         <input
           type="checkbox"
-          className="vam cursor-pointer"
-          style={{ position: 'relative', top: -1 }}
+          className="vam cursor-pointer position-relative"
+          style={{ top: -1 }}
           checked={isSelected}
           aria-label={isSelected ? gettext('Unselect this item') : gettext('Select this item')}
           onChange={handleSelected}