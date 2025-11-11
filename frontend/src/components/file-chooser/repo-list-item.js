haiwen/seahub

commit f4f4a5566adfbfd4bbac8cd4f59c3903a00ccac0

fix(file-chooser): prevent ghost node (#8348)
Co-authored-by: zhouwenxuan <aries@Mac.local>

frontend/src/components/file-chooser/repo-list-item.js
@@ -77,7 +77,7 @@ class RepoListItem extends React.Component {
       parentPath = pathElements.join('/');
     }
     // create new folder in selected repo or folder
-    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo !== selectedRepo && this.isComponentMounted) {
+    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo.repo_id !== selectedRepo.repo_id && this.isComponentMounted) {
       seafileAPI.listDir(repo.repo_id, parentPath).then(res => {
         const { dirent_list = [], dir_id = '', user_perm = 'r' } = res?.data || {};
         const dirent = dirent_list.find(item => item.type === 'dir' && item.name === newFolderName);

commit e7e4144408413a3c8fe78fee59a4c12c7b3ee805

02 fix bug caused by https://github.com/haiwen/seahub/pull/8348/

frontend/src/components/file-chooser/repo-list-item.js
@@ -77,7 +77,10 @@ class RepoListItem extends React.Component {
       parentPath = pathElements.join('/');
     }
     // create new folder in selected repo or folder
-    if (repo && selectedRepo && repo.repo_id === selectedRepo.repo_id && prevProps.selectedRepo.repo_id !== selectedRepo.repo_id && this.isComponentMounted) {
+    if (!this.isComponentMounted || !repo || !selectedRepo) return;
+    const isSameRepo = repo.repo_id === selectedRepo.repo_id;
+    const isNewlySelectedRepo = !prevProps.selectedRepo || prevProps.selectedRepo.repo_id !== selectedRepo.repo_id;
+    if (isSameRepo && isNewlySelectedRepo) {
       seafileAPI.listDir(repo.repo_id, parentPath).then(res => {
         const { dirent_list = [], dir_id = '', user_perm = 'r' } = res?.data || {};
         const dirent = dirent_list.find(item => item.type === 'dir' && item.name === newFolderName);