140c4f3ca178fde9742b92e1e3f6603d471d6560
seafile-dev <57566977+seafile-dev@users.noreply.github.com>
2025-11-14 15:56:09 +0800

    edit pdf when user click edit btn (#8413)
    Co-authored-by: lian <imwhatiam123@gmail.com>

--- frontend/src/components/file-view/file-toolbar.js
+++ frontend/src/components/file-view/file-toolbar.js
@@ -33,6 +33,7 @@ const {
   // fileEnc, // for 'edit', not undefined only for some kinds of files (e.g. text file)
   canDownloadFile,
   fileDownloadURL,
+  canEditPDF,
 } = window.app.pageOptions;
 
 class FileToolbar extends React.Component {
@@ -161,6 +162,14 @@ class FileToolbar extends React.Component {
               id="seafile-pdf-print"
               icon="print"
               text={gettext('Print')}
+            />
+          )}
+          {(fileType == 'PDF' && canEditPDF) && (
+            <IconButton
+              id="seafile-pdf-edit"
+              icon="edit"
+              text={gettext('Edit')}
+              href={'?edit_pdf=true'}
             />
           )}
           {showLockUnlockBtn && (
