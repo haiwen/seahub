90cb11d7139e4aff465ee31b376d938acea9d681
llj <lingjun.li1@gmail.com>
2025-11-18 10:50:09 +0800

    [a11y] 'back' icon: added keyboard a11y for it in multiple dialogs

--- frontend/src/components/back-icon.js
+++ frontend/src/components/back-icon.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { gettext } from '../utils/constants';
+import OpIcon from './op-icon';
 
 function BackIcon({ onClick }) {
   return (
-    <i
-      role="button"
+    <OpIcon
       className="sf3-font sf3-font-arrow rotate-180 op-icon op-icon-bg-light mr-1"
       title={gettext('Back')}
-      aria-label={gettext('Back')}
-      onClick={onClick}
-    >
-    </i>
+      op={onClick}
+    />
   );
 }
 
