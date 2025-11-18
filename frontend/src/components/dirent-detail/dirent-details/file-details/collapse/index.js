4b566ab44456f437a2c3ef334400d5cd1ea75cb1
llj <lingjun.li1@gmail.com>
2025-11-18 11:33:50 +0800

    [a11y] 'file details' dialog: added keyboard a11y for the 'toggle' icon of 'general info' & 'capture info'

--- frontend/src/components/dirent-detail/dirent-details/file-details/collapse/index.js
+++ frontend/src/components/dirent-detail/dirent-details/file-details/collapse/index.js
@@ -2,6 +2,8 @@ import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { CAPTURE_INFO_SHOW_KEY } from '../../../../../constants';
+import { gettext } from '../../../../../utils/constants';
+import OpElement from '../../../../op-element';
 
 import './index.css';
 
@@ -17,9 +19,13 @@ const Collapse = ({ className, title, children, isShow = true }) => {
     <div className={classnames('file-details-collapse', className)}>
       <div className="file-details-collapse-header">
         <div className="file-details-collapse-header-title">{title}</div>
-        <div className="file-details-collapse-header-operation" onClick={toggleShowChildren}>
-          <i aria-hidden="true" className={`sf3-font sf3-font-down ${showChildren ? '' : 'rotate-90'}`}></i>
-        </div>
+        <OpElement
+          className="file-details-collapse-header-operation"
+          op={toggleShowChildren}
+          title={showChildren ? gettext('Fold') : gettext('Unfold')}
+        >
+          <i className={`sf3-font sf3-font-down ${showChildren ? '' : 'rotate-90'}`}></i>
+        </OpElement>
       </div>
       {showChildren && (
         <div className="file-details-collapse-body">
