12b7b5795637a7c259cd6515b22de3a16408059c
Michael An <1822852997@qq.com>
2025-12-05 18:00:33 +0800

    remove useless two_factor function

--- seahub/two_factor/urls.py
+++ seahub/two_factor/urls.py
@@ -1,17 +1,13 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 from django.urls import path
 from seahub.two_factor.views import (BackupTokensView, SetupCompleteView,
-                                     ProfileView, SetupView, QRGeneratorView,
-                                     TwoFactorVerifyView, DisableView)
+                                     SetupView, QRGeneratorView,
+                                     TwoFactorVerifyView)
 
 urlpatterns = [
-    # path(regex=r'^$',
-    #     view=ProfileView.as_view(),
-    #     name='profile', ),
     path('setup/', view=SetupView.as_view(), name='setup', ),
     path('qrcode/', view=QRGeneratorView.as_view(), name='qr', ),
     path('setup/complete/', view=SetupCompleteView.as_view(), name='setup_complete', ),
     path('backup/tokens/', view=BackupTokensView.as_view(), name='backup_tokens', ),
-    path('disable/', view=DisableView.as_view(), name='disable', ),
     path('verify/', view=TwoFactorVerifyView.as_view(), name='verify', ),
 ]
--- seahub/two_factor/views/__init__.py
+++ seahub/two_factor/views/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2012-2016 Seafile Ltd.
 from .core import SetupView, BackupTokensView, SetupCompleteView, QRGeneratorView
 from .mixins import OTPRequiredMixin
-from .profile import ProfileView, DisableView
 from .login import TwoFactorVerifyView
