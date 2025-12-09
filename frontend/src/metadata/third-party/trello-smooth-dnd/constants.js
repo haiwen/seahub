daddd64edbb571a4f9ea2d70f772ea2fbd7e5ea3
Michael An <1822852997@qq.com>
2025-12-09 10:22:34 +0800

    change creator is department avatar

--- frontend/src/metadata/third-party/trello-smooth-dnd/constants.js
+++ frontend/src/metadata/third-party/trello-smooth-dnd/constants.js
@@ -2,7 +2,7 @@ export const containerInstance = 'smooth-dnd-container-instance';
 
 export const defaultGroupName = '@@smooth-dnd-default-group@@';
 export const wrapperClass = 'smooth-dnd-draggable-wrapper';
-export const defaultGrabHandleClass = 'smooth-dnd-default-grap-handle';
+export const defaultGrabHandleClass = 'smooth-dnd-default-grab-handle';
 export const animationClass = 'animated';
 export const translationValue = '__smooth_dnd_draggable_translation_value';
 export const visibilityValue = '__smooth_dnd_draggable_visibility_value';
@@ -14,9 +14,9 @@ export const extraSizeForInsertion = 'smooth-dnd-extra-size-for-insertion';
 export const stretcherElementClass = 'smooth-dnd-stretcher-element';
 export const stretcherElementInstance = 'smooth-dnd-stretcher-instance';
 
-export const isDraggableDetached = 'smoth-dnd-is-draggable-detached';
+export const isDraggableDetached = 'smooth-dnd-is-draggable-detached';
 
-export const disbaleTouchActions = 'smooth-dnd-disable-touch-action';
+export const disableTouchActions = 'smooth-dnd-disable-touch-action';
 export const noUserSelectClass = 'smooth-dnd-no-user-select';
 
 export const preventAutoScrollClass = 'smooth-dnd-prevent-auto-scroll-class';
--- frontend/src/metadata/third-party/trello-smooth-dnd/styles.js
+++ frontend/src/metadata/third-party/trello-smooth-dnd/styles.js
@@ -1,4 +1,4 @@
-import { containerClass, disbaleTouchActions, dropPlaceholderFlexContainerClass, dropPlaceholderInnerClass, dropPlaceholderWrapperClass, ghostClass, noUserSelectClass, stretcherElementClass, wrapperClass, dropPlaceholderDefaultClass } from './constants';
+import { containerClass, disableTouchActions, dropPlaceholderFlexContainerClass, dropPlaceholderInnerClass, dropPlaceholderWrapperClass, ghostClass, noUserSelectClass, stretcherElementClass, wrapperClass, dropPlaceholderDefaultClass } from './constants';
 
 const verticalWrapperClass = {
   'overflow': 'hidden',
@@ -44,7 +44,7 @@ const css = {
   [`.${ghostClass} *`]: {
     'pointer-events': 'none'
   },
-  [`.${disbaleTouchActions} *`]: {
+  [`.${disableTouchActions} *`]: {
     'touch-action': 'none',
     '-ms-touch-action': 'none'
   },
