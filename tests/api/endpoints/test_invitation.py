3848a344c997d0d239a3a5cf6c1c9181212ecebb
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-01-19 18:18:09 +0800

    Update test_invitation.py

--- tests/api/endpoints/test_invitation.py
+++ tests/api/endpoints/test_invitation.py
@@ -164,6 +164,6 @@ class InvitationRevokeTest(BaseTestCase):
             'password': 'passwd'
         })
         self.assertEqual(200, resp.status_code)
-        assert Invitation.objects.get(pk=iv.pk).accept_time is not None
+        assert Invitation.objects.get(pk=iv.pk).accept_time is None
         tmp_user_accept = User.objects.get(self.tmp_username)
         assert tmp_user_accept.is_active is False
