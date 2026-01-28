2666d7719b0703b0a5cdca826acaf38fedd5b0d8
Michael An <1822852997@qq.com>
2026-01-28 11:47:45 +0800

    change statistics icon

--- frontend/src/metadata/views/statistics/charts/summary-cards.js
+++ frontend/src/metadata/views/statistics/charts/summary-cards.js
@@ -1,19 +1,31 @@
-import { gettext } from '../../../../utils/constants';
-import Icon from '../../../../components/icon';
+import { gettext, mediaUrl } from '../../../../utils/constants';
+
 import './summary-cards.css';
 
 const SummaryCards = ({ totalFiles, totalCollaborators }) => {
   return (
     <div className="card-container">
       <div className="summary-card">
-        <Icon className="summary-icon" symbol="file-count" />
+        <img
+          className="summary-icon"
+          width="16"
+          src={`${mediaUrl}img/file-count.png`}          
+          alt={gettext('Files')}
+          title={gettext('Files')}
+        />
         <div className="summary-content">
           <div className="summary-number">{totalFiles.toLocaleString()}</div>
           <div className="summary-label">{gettext('Files')}</div>
         </div>
       </div>
       <div className="summary-card">
-        <Icon className="summary-icon" symbol="collaborator-count" />
+        <img
+          className="summary-icon"
+          width="16"
+          src={`${mediaUrl}img/collaborator-count.png`}
+          alt={gettext('Collaborators')}
+          title={gettext('Collaborators')}
+        />
         <div className="summary-content">
           <div className="summary-number">{totalCollaborators.toLocaleString()}</div>
           <div className="summary-label">{gettext('Collaborators')}</div>
