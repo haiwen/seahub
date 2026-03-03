902e3734d5d0f66177d171eaa168a9c2e334fa8a
杨顺强 <978987373@qq.com>
2026-03-03 14:25:47 +0800

    optimize code (#8721)
    Co-authored-by: 小强 <shuntian@Mac.lan>

--- frontend/src/components/dir-view-mode/dir-table-view/formatter/modified.js
+++ frontend/src/components/dir-view-mode/dir-table-view/formatter/modified.js
@@ -1,13 +1,19 @@
-import CTimeFormatter from '@/metadata/components/cell-formatter/ctime';
+import React from 'react';
+import classnames from 'classnames';
+import dayjs from 'dayjs';
+import { formatWithTimezone } from '../../../../utils/time';
 
-const LastModifiedFormatter = ({ value }) => {
+const LastModifiedFormatter = ({ value, className, children: emptyFormatter, format }) => {
+  if (!value) return emptyFormatter || null;
   return (
-    <CTimeFormatter
-      value={value}
-      format="relativeTime"
-      className="dir-table-modified-formatter"
-    />
+    <div
+      className={classnames('sf-metadata-ui cell-formatter-container ctime-formatter', className)}
+      title={formatWithTimezone(value)}
+    >
+      {dayjs.unix(value).format('YYYY-MM-DD HH:mm:ss')}
+    </div>
   );
 };
 
 export default LastModifiedFormatter;
+
