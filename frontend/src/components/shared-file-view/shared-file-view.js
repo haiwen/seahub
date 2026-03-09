cf8ad00a4b176483c515b5f2013e88331a5ea6ed
Guodong Su <stephen.su66@gmail.com>
2026-03-09 11:34:18 +0800

    fix: optimize readonly file scroll

--- frontend/src/components/shared-file-view/shared-file-view.js
+++ frontend/src/components/shared-file-view/shared-file-view.js
@@ -188,7 +188,7 @@ class SharedFileView extends React.Component {
           )}
           {loginUser && <Account />}
         </div>
-        <div className="flex-fill d-flex flex-column">
+        <div className="flex-fill d-flex flex-column overflow-auto">
           {!isDesktop && (
             <div className={'shared-file-view-head'}>
               {this.renderFileViewHeader()}
