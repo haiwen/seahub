f7e1af827b3affb7ced4c0758c913d87a2e395e2
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-04-02 09:40:19 +0800

    update

--- seahub/avatar/urls.py
+++ seahub/avatar/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('add/', add, name='avatar_add'),
 #    url('^change/$', 'change', name='avatar_change'),
 #    url('^delete/$', 'delete', name='avatar_delete'),
-    re_path('^render_primary/(?P<user>[^/]+)/(?P<size>[\d]+)/$', render_primary, name='avatar_render_primary'),
+    re_path(r'^render_primary/(?P<user>[^/]+)/(?P<size>[\d]+)/$', render_primary, name='avatar_render_primary'),
 ]
