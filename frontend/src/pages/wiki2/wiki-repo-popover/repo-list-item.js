6e2ef768c9df3d49b389632daab0e7185819d222
zhouwenxuan <aries@Mac.local>
2026-05-19 11:49:44 +0800

    fix: make repo name display as ellipsis when overflow

--- frontend/src/pages/wiki2/wiki-repo-popover/repo-list-item.js
+++ frontend/src/pages/wiki2/wiki-repo-popover/repo-list-item.js
@@ -10,10 +10,10 @@ export default function RepoListItem({ item, onItemClick }) {
   }, [item, onItemClick]);
 
   return (
-    <div className='repo-list-item' onClick={onClick}>
-      <div className='d-flex'>
-        <img src={iconUrl} width={'24px'} alt="" className='mr-2'></img>
-        <div className='option-label'>{item.label}</div>
+    <div className="repo-list-item" onClick={onClick}>
+      <div className="w-100 d-flex">
+        <img src={iconUrl} width={'24px'} alt="" className="mr-2"></img>
+        <div className="option-label">{item.label}</div>
       </div>
     </div>
   );
