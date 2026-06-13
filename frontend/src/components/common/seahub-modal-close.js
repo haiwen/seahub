5f576798ebe9a62e382bbbd3d2dead723c4efac5
zhouwenxuan <aries@Mac.local>
2026-06-13 15:50:41 +0800

    fix tooltip placement in notification

--- frontend/src/components/common/seahub-modal-close.js
+++ frontend/src/components/common/seahub-modal-close.js
@@ -6,8 +6,8 @@ import '../../css/seahub-modal-header.css';
 
 const SeahubModalCloseIcon = (props) => {
   return (
-    <button id="modal-close-btn" type="button" className={`close seahub-modal-btn ${props.className ? props.className : ''}`} data-dismiss="modal" aria-label={gettext('Close')} onClick={props.toggle}>
-      <span className="seahub-modal-btn-inner">
+    <button type="button" className={`close seahub-modal-btn ${props.className ? props.className : ''}`} data-dismiss="modal" aria-label={gettext('Close')} onClick={props.toggle}>
+      <span id="modal-close-btn" className="seahub-modal-btn-inner">
         <Icon symbol="close" />
         <Tooltip target="modal-close-btn">{gettext('Close')}</Tooltip>
       </span>
