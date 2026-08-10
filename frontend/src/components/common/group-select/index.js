00ee565a5258bd9b58ee63dbb9910b15996d89af
zhouwenxuan <aries@Mac.local>
2026-08-10 15:23:22 +0800

    fix: group select ux, update seafile-ui.css

--- frontend/src/components/common/group-select/index.js
+++ frontend/src/components/common/group-select/index.js
@@ -45,46 +45,46 @@ class GroupSelect extends Component {
     const { isShowSelectOptions } = this.state;
     return (
       <>
-          <div
-            id="group-select"
-            ref={(node) => this.selector = node}
-            className={classnames('group-select sf-select',
-              { 'focus': isShowSelectOptions },
-              className
-            )}
-            tabIndex={0}
-            role="combobox"
-            aria-expanded={isShowSelectOptions}
-            aria-haspopup="listbox"
-            aria-label={placeholder}
-            aria-controls="group-select-listbox"
-            onKeyDown={Utils.onKeyDown}
-            onClick={this.toggle}
-          >
-            <div className="selected-option">
-              {selectedOptions.length > 0 ?
-                <span className="selected-option-show">
-                  {selectedOptions.map(item =>
-                    <span key={item.id} className="selected-option-item">
-                      <span className='selected-option-item-name'>{item.name}</span>
-                      <span className="d-flex align-items-center" onClick={(e) => { e.stopPropagation(); this.props.onDeleteOption(item); }}><Icon symbol="close" /></span>
-                    </span>
-                  )}
-                </span>
-                :
-                <span className="select-placeholder">{placeholder}</span>
-              }
-              <SelectDropdownIndicator />
-            </div>
+        <div
+          id="group-select"
+          ref={(node) => this.selector = node}
+          className={classnames('group-select sf-select',
+            { 'focus': isShowSelectOptions },
+            className
+          )}
+          tabIndex={0}
+          role="combobox"
+          aria-expanded={isShowSelectOptions}
+          aria-haspopup="listbox"
+          aria-label={placeholder}
+          aria-controls="group-select-listbox"
+          onKeyDown={Utils.onKeyDown}
+          onClick={this.toggle}
+        >
+          <div className="selected-option">
+            {selectedOptions.length > 0 ?
+              <span className="selected-option-show">
+                {selectedOptions.map(item =>
+                  <span key={item.id} className="selected-option-item">
+                    <span className='selected-option-item-name'>{item.name}</span>
+                    <span className="d-flex align-items-center" onClick={(e) => { e.stopPropagation(); this.props.onDeleteOption(item); }}><Icon symbol="close" /></span>
+                  </span>
+                )}
+              </span>
+              :
+              <span className="select-placeholder">{placeholder}</span>
+            }
+            <SelectDropdownIndicator />
           </div>
-          {!isInModal && (
-            <Popover
-              isOpen={isShowSelectOptions}
-              target="group-select"
-              placement="bottom-start"
-              hideArrow={true}
-              fade={false}
-            >
+        </div>
+        {!isInModal && (
+          <Popover
+            isOpen={isShowSelectOptions}
+            target="group-select"
+            placement="bottom-start"
+            hideArrow={true}
+            fade={false}
+          >
             <SelectOptionGroup
               selectedOptions={selectedOptions}
               options={options}
@@ -102,28 +102,28 @@ class GroupSelect extends Component {
           </Popover>
         )}
         {isShowSelectOptions && isInModal && (
-            <ModalPortal>
-              <SelectOptionGroup
-                className={className}
-                selectedOptions={selectedOptions}
-                position={this.selector && this.selector.getBoundingClientRect()}
-                isInModal={isInModal}
-                top={this.selector ? this.selector.getBoundingClientRect().height : 38}
-                options={options}
-                onSelectOption={this.props.onSelectOption}
-                searchPlaceholder={searchPlaceholder}
-                noOptionsPlaceholder={noOptionsPlaceholder}
-                onClickOutside={(e) => {
-                  const optionGroup = document.querySelector('.option-group');
-                  if (optionGroup && optionGroup.contains(e.target)) return;
-                  this.closeSelect();
-                }}
-                closeSelect={this.closeSelect}
-                getFilterOptions={this.getFilterOptions}
-              />
-            </ModalPortal>
-          )}
-        </>
+          <ModalPortal>
+            <SelectOptionGroup
+              className={className}
+              selectedOptions={selectedOptions}
+              position={this.selector && this.selector.getBoundingClientRect()}
+              isInModal={isInModal}
+              top={this.selector ? this.selector.getBoundingClientRect().height : 38}
+              options={options}
+              onSelectOption={this.props.onSelectOption}
+              searchPlaceholder={searchPlaceholder}
+              noOptionsPlaceholder={noOptionsPlaceholder}
+              onClickOutside={(e) => {
+                const optionGroup = document.querySelector('.option-group');
+                if (optionGroup && optionGroup.contains(e.target)) return;
+                this.closeSelect();
+              }}
+              closeSelect={this.closeSelect}
+              getFilterOptions={this.getFilterOptions}
+            />
+          </ModalPortal>
+        )}
+      </>
     );
   }
 }
