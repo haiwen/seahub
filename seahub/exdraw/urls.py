9932a19eb08f358fee26ba80135666ef924c0121
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-08-24 12:09:48 +0800

    update

--- seahub/exdraw/urls.py
+++ seahub/exdraw/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path
 from .apis import ExdrawAccessToken, ExdrawDownloadLink, ExdrawUploadFile, ExdrawEditorCallBack, ExdrawUploadImage, \
-    ExdrawDownloadImage
+    ExdrawDownloadImage, ExdrawContentView
 
 urlpatterns = [
     re_path(r'^access-token/(?P<repo_id>[-0-9a-f]{36})/$', ExdrawAccessToken.as_view(), name='exdraw_access_token'),
     re_path(r'^upload-file/(?P<file_uuid>[-0-9a-f]{36})/$', ExdrawUploadFile.as_view(), name='exdraw_upload_file'),
     re_path(r'^download-link/(?P<file_uuid>[-0-9a-f]{36})/$', ExdrawDownloadLink.as_view(), name='exdraw_download_link'),
+    re_path(r'^content/(?P<file_uuid>[-0-9a-f]{36})/$', ExdrawContentView.as_view(), name='exdraw_content'),
     re_path(r'^editor-status-callback/(?P<file_uuid>[-0-9a-f]{36})/$', ExdrawEditorCallBack.as_view(), name='exdraw_editor_callback'),
     re_path(r'^upload-image/(?P<file_uuid>[-0-9a-f]{36})/$', ExdrawUploadImage.as_view(), name='exdraw_upload_image'),
     re_path(r'^download-image/(?P<file_uuid>[-0-9a-f]{36})/(?P<filename>.*)$', ExdrawDownloadImage.as_view(), name='exdraw_download_image'),
