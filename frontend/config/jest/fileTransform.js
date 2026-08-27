eaa6dcf81429e1acca7d39a9600ebc4d0d401b2c
zhouwenxuan <aries@Mac.local>
2026-08-27 14:20:13 +0800

    test: cover search load-more regression

--- frontend/config/jest/fileTransform.js
+++ frontend/config/jest/fileTransform.js
@@ -17,7 +17,7 @@ module.exports = {
         pascalCase: true,
       });
       const componentName = `Svg${pascalCaseFilename}`;
-      return `const React = require('react');
+      return { code: `const React = require('react');
       module.exports = {
         __esModule: true,
         default: ${assetFilename},
@@ -32,9 +32,9 @@ module.exports = {
             })
           };
         }),
-      };`;
+      };` };
     }
 
-    return `module.exports = ${assetFilename};`;
+    return { code: `module.exports = ${assetFilename};` };
   },
 };
