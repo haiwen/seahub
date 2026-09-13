45b5e55de28abaf2401457975076d6eba073cc88
杨顺强 <978987373@qq.com>
2026-09-13 14:27:23 +0800

    refactor: colocate page-specific frontend components (#9508)

--- frontend/src/pages/file-history/history-list-view/history-list-view.js
+++ frontend/src/pages/file-history/history-list-view/history-list-view.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Loading from '../loading';
+import Loading from '../../../components/loading';
 import HistoryListItem from './history-list-item';
 
 const propTypes = {
