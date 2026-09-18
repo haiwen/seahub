31f277694823b6d443a98c827d3d276f1bf25372
SkywalkerSpace <dzmbbs@qq.com>
2026-09-18 10:08:38 +0800

    DATABASES utf8mb4

--- seahub/settings.py
+++ seahub/settings.py
@@ -39,6 +39,7 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
 _preset_db_cfg = copy.deepcopy(DATABASES)
